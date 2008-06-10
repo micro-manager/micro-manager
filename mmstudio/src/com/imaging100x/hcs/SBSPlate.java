638c8c3969becc0fe1f08ddc7ae70959e027575a
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-06-10 03:04:47 +0000

    plate class updated
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@1317 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/com/imaging100x/hcs/SBSPlate.java
+++ mmstudio/src/com/imaging100x/hcs/SBSPlate.java
@@ -23,20 +23,41 @@
 
 package com.imaging100x.hcs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Hashtable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.micromanager.metadata.ImageKey;
+import org.micromanager.metadata.MMAcqDataException;
 
 public class SBSPlate {
    private int numColumns_;
    private int numRows_;
-   private double wellPitchX_;
-   private double wellPitchY_;
-   private double sizeXUm_;
-   private double sizeYUm_;
+   private double wellSpacingX_;
+   private double wellSpacingY_;
+   private double sizeXUm;
+   private double sizeYUm;
    private String id_;
    private String description_;
    private double firstWellX_;
    private double firstWellY_;
    private Hashtable<String, Well> wellMap_;
+   
+   private static String ROWS = "rows";
+   private static String COLS = "cols";
+   private static String WELL_SPACING_X = "well_spacing_X";
+   private static String WELL_SPACING_Y = "well_spacing_Y";
+   private static String PLATE_SIZE_X = "plate_size_X";
+   private static String PLATE_SIZE_Y = "plate_size_Y";
+   private static String ID = "id";
+   private static String DESCRIPTION = "description";
+   private static String FIRST_WELL_X = "first_well_x";
+   private static String FIRST_WELL_Y = "first_well_y";
    
    public static String SBS_96_WELL= "96WELL";
    public static String SBS_384_WELL= "384WELL";
@@ -52,40 +73,91 @@ public class SBSPlate {
          id_ = SBS_96_WELL;
          numColumns_ = 12;
          numRows_ = 8;
-         sizeXUm_ = 127760.0;
-         sizeYUm_ = 85480.0;
-         wellPitchX_ = 9000.0;
-         wellPitchY_ = 9000.0;
+         sizeXUm = 127760.0;
+         sizeYUm = 85480.0;
+         wellSpacingX_ = 9000.0;
+         wellSpacingY_ = 9000.0;
          firstWellX_ = 14380.0;
          firstWellY_ = 11240.0;
       } else {
          id_ = SBS_384_WELL;
          numColumns_ = 24;
          numRows_ = 16;
-         sizeXUm_ = 127760.0;
-         sizeYUm_ = 85480.0;
-         wellPitchX_ = 4500.0;
-         wellPitchY_ = 4500.0;
+         sizeXUm = 127760.0;
+         sizeYUm = 85480.0;
+         wellSpacingX_ = 4500.0;
+         wellSpacingY_ = 4500.0;
          firstWellX_ = 12130.0;
          firstWellY_ = 8990.0;        
       }
    }
    
-   public void load(String path) {
+   public void load(String path) throws HCSException {
+      StringBuffer contents = new StringBuffer();
+      try {
+         // read metadata from file            
+         BufferedReader input = null;
+         input = new BufferedReader(new FileReader(path));
+         String line = null;
+         while (( line = input.readLine()) != null){
+            contents.append(line);
+            contents.append(System.getProperty("line.separator"));
+         }
+
+         restore(contents.toString());
+      } catch (IOException e) {
+         throw new HCSException(e);
+      }
       
    }
    
-   public void save(String path) {
-      
+   public void save(String path) throws HCSException {     
+      try {
+         FileWriter fw = new FileWriter(path);
+         fw.write(serialize());
+         fw.close();
+      } catch (IOException e) {
+         throw new HCSException("Unable to create plate definition file: " + e.getMessage());
+      }      
    }
    
-   public String serialize() {
+   public String serialize() throws HCSException {
       JSONObject plate = new JSONObject();
-      return null;
+      try {
+         plate.put(ROWS, numRows_);
+         plate.put(COLS, numColumns_);
+         plate.put(WELL_SPACING_X, wellSpacingX_);
+         plate.put(WELL_SPACING_Y, wellSpacingY_);
+         plate.put(PLATE_SIZE_X, sizeXUm);
+         plate.put(PLATE_SIZE_Y, sizeYUm);
+         plate.put(ID, id_);
+         plate.put(DESCRIPTION, description_);
+         plate.put(FIRST_WELL_X, firstWellX_);
+         plate.put(FIRST_WELL_Y, firstWellY_);
+         return plate.toString(3);
+      } catch (JSONException e) {
+         throw new HCSException(e);
+      }
    }
    
-   public void restore(String ser) {
+   public void restore(String ser) throws HCSException {
       
+      JSONObject plate;
+      try {
+         plate = new JSONObject(ser);
+         numRows_ = plate.getInt(ROWS);
+         numColumns_ = plate.getInt(COLS);
+         wellSpacingX_ = plate.getInt(WELL_SPACING_X);
+         wellSpacingY_ = plate.getInt(WELL_SPACING_Y);
+         sizeXUm = plate.getInt(PLATE_SIZE_X);
+         sizeYUm = plate.getInt(PLATE_SIZE_Y);
+         id_ = plate.getString(ID);
+         description_ = plate.getString(DESCRIPTION);
+         firstWellX_ = plate.getDouble(FIRST_WELL_X);
+         firstWellY_ = plate.getDouble(FIRST_WELL_Y);
+      } catch (JSONException e) {
+         throw new HCSException(e);
+      }
    }
    
    public String getID() {
