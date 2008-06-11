e7230e38957f22a6ba11340e0715e32b37e33518
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-06-11 04:29:13 +0000

    test program updated for SBS plate
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@1320 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/com/imaging100x/hcs/TestSBSPlate.java
+++ mmstudio/src/com/imaging100x/hcs/TestSBSPlate.java
@@ -1,3 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//FILE:           TestSBSPlate.java
+//PROJECT:        Micro-Manager-S
+//SUBSYSTEM:      high content screening
+//-----------------------------------------------------------------------------
+//
+//AUTHOR:         Nenad Amodaj, nenad@amodaj.com, June 3, 2008
+//
+//COPYRIGHT:      100X Imaging Inc, www.100ximaging.com, 2008
+//                
+//LICENSE:        This file is distributed under the GPL license.
+//                License text is included with the source distribution.
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
+//CVS:            $Id: MetadataDlg.java 1275 2008-06-03 21:31:24Z nenad $
+
 package com.imaging100x.hcs;
 
 import org.micromanager.navigation.MultiStagePosition;
