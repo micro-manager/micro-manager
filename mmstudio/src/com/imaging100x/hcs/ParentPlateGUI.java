035307cacda1c5890df21276c610c49f46c7c0c3
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-09-30 23:52:45 +0000

    imaging100x module removed from the micro-manager standard dist
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@1595 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/com/imaging100x/hcs/ParentPlateGUI.java
+++
@@ -1,8 +0,0 @@
-package com.imaging100x.hcs;
-
-
-public interface ParentPlateGUI {
-   public void updatePointerXYPosition(double x, double y, String wellLabel, String siteLabel);
-   public void updateStageXYPosition(double x, double y, String wellLabel, String siteLabel);
-   public String getXYStageName();
-}
