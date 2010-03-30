6364e39e1db9d1b8b96fdbffa82522aeb5c75916
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-03-30 03:15:21 +0000

    suppress warnings caused by boost
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4273 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- MMCore/CoreUtils.h
+++ MMCore/CoreUtils.h
@@ -30,8 +30,9 @@
 //#include "ace/Log_Msg.h"
 
 //#include "../../3rdparty/boost/boost/date_time/posix_time/posix_time.hpp"
+#pragma warning(push, 0)
 #include "boost/date_time/posix_time/posix_time.hpp"
-
+#pragma warning(pop)
 
 #define CORE_LOG_PREFIX "LOG(%P, %t:): "
 
