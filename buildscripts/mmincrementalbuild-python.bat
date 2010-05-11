e7b9bf93c73703b0477f037059f8922895bb4b70
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-05-11 23:49:40 +0000

    split 32 and 64 bit builds
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4495 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- buildscripts/mmincrementalbuild-python.bat
+++ buildscripts/mmincrementalbuild-python.bat
@@ -1,4 +1,4 @@
 devenv /BUILD Release .\MMCorePy_wrap\MMCorePy_wrap.sln
-copy .\bin\MMCorePy.py .\Install\micro-manager
-copy .\bin\_MMCorePy.pyd .\Install\micro-manager
-copy .\MMCorePy_wrap\MMCoreWrapDemo.py .\Install\micro-manager
+copy .\bin32\MMCorePy.py .\Install32\micro-manager
+copy .\bin32\_MMCorePy.pyd .\Install32\micro-manager
+copy .\MMCorePy_wrap\MMCoreWrapDemo.py .\Install32\micro-manager
