fda6894e83cbddd2c1b68d80c2c8ccf275658081
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-05-27 04:15:31 +0000

    Added MaestroServo to 1.4
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4620 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ DeviceAdapters/MaestroServo/MaestroServo.cpp
@@ -0,0 +1,371 @@
+//////////////////////////////////////////////////////////////////////////////
+// FILE:          MaestroServo.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Adapter for MaestroServo controller (http://www.pololu.com/docs/0J40/all) 
+// COPYRIGHT:     University of California, San Francisco, 2010
+// LICENSE:       This file is distributed under the LGPLD license.
+//                License text is included with the source distribution.
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+// AUTHOR:        Nico Stuurman, 10/03/2009
+
+#ifdef WIN32
+#include <windows.h>
+#define snprintf _snprintf 
+#endif
+
+#include "MaestroServo.h"
+#include <string>
+#include <math.h>
+#include "../../MMDevice/ModuleInterface.h"
+#include "../../MMDevice/DeviceUtils.h"
+#include <sstream>
+#include <iostream>
+
+const char* g_MaestroServoName = "MaestroServo";
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Exported MMDevice API
+///////////////////////////////////////////////////////////////////////////////
+MODULE_API void InitializeModuleData()
+{
+   AddAvailableDeviceName(g_MaestroServoName, "Pololu MaestroServo controller" );
+}
+
+MODULE_API MM::Device* CreateDevice(const char* deviceName)
+{
+   if (deviceName == 0)
+      return 0;
+
+   if (strcmp(deviceName, g_MaestroServoName) == 0)
+   {
+      MaestroServo* pMaestroServo = new MaestroServo();
+      return pMaestroServo;
+   }
+
+   return 0;
+}
+
+MODULE_API void DeleteDevice(MM::Device* pDevice)
+{
+   delete pDevice;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// MaestroServo device
+///////////////////////////////////////////////////////////////////////////////
+
+MaestroServo::MaestroServo() :
+   initialized_(false),
+   moving_(false),
+   servoNr_(0),
+   minPos_(900),
+   maxPos_(2000),
+   port_("Undefined"),
+   changedTime_(0.0)
+{
+   InitializeDefaultErrorMessages();
+
+   // create pre-initialization properties
+   // ------------------------------------
+
+   // Name
+   CreateProperty(MM::g_Keyword_Name, g_MaestroServoName, MM::String, true);
+
+   // Description
+   CreateProperty(MM::g_Keyword_Description, "MaestroServo controller", MM::String, true);
+
+   // Port
+   CPropertyAction* pAct = new CPropertyAction (this, &MaestroServo::OnPort);
+   CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
+
+   // Select Servo
+   pAct = new CPropertyAction(this, &MaestroServo::OnServoNr);
+   CreateProperty("ServoNr", "0", MM::Integer, false, pAct, true);
+   SetPropertyLimits("ServoNr", 0, 23);
+
+   // Minimum Position
+   pAct = new CPropertyAction(this, &MaestroServo::OnMinPosition);
+   CreateProperty("Minimum Position", "900", MM::Integer, false, pAct, true);
+
+   // Maximum Position
+   pAct = new CPropertyAction(this, &MaestroServo::OnMaxPosition);
+   CreateProperty("Maximum Position", "2000", MM::Integer, false, pAct, true);
+
+
+   EnableDelay(true);
+}
+
+MaestroServo::~MaestroServo()
+{
+   Shutdown();
+}
+
+void MaestroServo::GetName(char* Name) const
+{
+   CDeviceUtils::CopyLimitedString(Name, g_MaestroServoName);
+}
+
+int MaestroServo::Initialize()
+{
+   changedTime_ = GetCurrentMMTime();
+
+   CPropertyAction* pAct = new CPropertyAction(this, &MaestroServo::OnPosition);
+   CreateProperty("Position", "1000", MM::Float, false, pAct);
+   SetPropertyLimits("Position", minPos_, maxPos_);
+
+   // Speed
+   pAct = new CPropertyAction(this, &MaestroServo::OnSpeed);
+   CreateProperty("Speed", "0", MM::Integer, false, pAct);
+   SetPropertyLimits("Speed", 0, 0xef);
+
+   // Acceleration
+   pAct = new CPropertyAction(this, &MaestroServo::OnAcceleration);
+   CreateProperty("Acceleration", "0", MM::Integer, false, pAct);
+   SetPropertyLimits("Acceleration", 0, 255);
+
+   initialized_ = true;
+   return DEVICE_OK;
+}
+
+int MaestroServo::Shutdown()
+{
+   if (initialized_)
+   {
+      initialized_ = false;
+   }
+   return DEVICE_OK;
+}
+
+/*
+ */
+bool MaestroServo::Busy()
+{
+   // Pololu does not really signal when it is done, rather when it is done sending pulses,
+   // so wait for it to be done and then add a user-specified delay
+   if (moving_) {
+      unsigned char buf[1];
+      buf[0] = 0x93;
+      int ret = WriteToComPort(port_.c_str(), buf, 1);
+
+      MM::MMTime start = GetCurrentMMTime();
+      MM::MMTime t(1,0);
+      bool timeout = false;
+      unsigned long nRead = 0;
+      while (!timeout && moving_) {
+         ret = ReadFromComPort (port_.c_str(), buf, 1, nRead);
+         if (nRead == 1) {
+            if (buf[0] == 1)
+               return true;
+            else {
+               moving_ = false;
+               changedTime_ = GetCurrentMMTime();
+            }
+         }
+         CDeviceUtils::SleepMs(15);
+         timeout = (GetCurrentMMTime() - start) > t;
+      }
+   }
+
+   MM::MMTime delay(GetDelayMs() * 1000);
+   if (GetCurrentMMTime() - changedTime_ > delay)
+      return false;
+
+   return true;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+/*
+ * Sets the Serial Port to be used.
+ * Should be called before initialization
+ */
+int MaestroServo::OnPort(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(port_.c_str());
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      if (initialized_)
+      {
+         // revert
+         pProp->Set(port_.c_str());
+         return ERR_PORT_CHANGE_FORBIDDEN;
+      }
+
+      pProp->Get(port_);
+   }
+   return DEVICE_OK;
+}
+
+
+/**
+ * Sets the "Active" Servo"
+ */
+int MaestroServo::OnServoNr(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(servoNr_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(servoNr_);
+   }
+
+   return DEVICE_OK;
+}
+
+
+/**
+ * Minimum value for Position
+ */
+int MaestroServo::OnMinPosition(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(minPos_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      long pos;
+      pProp->Get(pos);
+      if (pos < 0)
+         pos = 0;
+      if (pos >= maxPos_)
+         pos = maxPos_ -1;
+      minPos_ = pos;
+   }
+
+   return DEVICE_OK;
+}
+
+
+/**
+ * Maximum value for Position
+ */
+int MaestroServo::OnMaxPosition(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(maxPos_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      long pos;
+      pProp->Get(pos);
+      if (pos <= minPos_)
+         pos = minPos_ + 1;
+      maxPos_ = pos;
+   }
+
+   return DEVICE_OK;
+}
+
+/**
+ * Sets the amplitude
+ */
+int MaestroServo::OnPosition(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      // TODO: Read position from Servo
+      pProp->Set(position_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(position_);
+
+      const int bufSize = 4;
+      unsigned char buf[bufSize];
+      int pos = (int) position_ * 4;
+      buf[0] = 0x84;
+      buf[1] = (unsigned char) servoNr_;
+      buf[2] = ( pos & 0x7F);
+      buf[3] = ( pos >> 7) & 0x7F;
+
+      int ret = WriteToComPort(port_.c_str(), buf, bufSize);
+      if (ret != DEVICE_OK)
+         return ret;
+      moving_ = true;
+   }
+
+   return DEVICE_OK;
+}
+
+
+/**
+ * Sets Speed.  A value of 0 corresponds to max speed.  Units are (0.25us)/(10ms)
+ */
+int MaestroServo::OnSpeed(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      // TODO: Read position from Servo
+      pProp->Set(speed_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(speed_);
+
+      const int bufSize = 4;
+      unsigned char buf[bufSize];
+      int pos = (int) speed_;
+      buf[0] = 0x87;
+      buf[1] = (unsigned char) servoNr_;
+      buf[2] = ( pos & 0x7F);
+      buf[3] = ( pos >> 7) & 0x7F;
+
+      int ret = WriteToComPort(port_.c_str(), buf, bufSize);
+      if (ret != DEVICE_OK)
+         return ret;
+   }
+
+   return DEVICE_OK;
+}
+
+
+/**
+ * Sets Acceleration.  A value of 0 corresponds to max speed.  Units are (0.25us)/(10ms)
+ */
+int MaestroServo::OnAcceleration(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      // TODO: Read position from Servo
+      pProp->Set(acceleration_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(acceleration_);
+
+      const int bufSize = 4;
+      unsigned char buf[bufSize];
+      unsigned int pos = (unsigned int) acceleration_;
+      buf[0] = 0x89;
+      buf[1] = (unsigned char) servoNr_;
+      buf[2] = ( pos & 0x7F);
+      buf[3] = ( pos >> 7) & 0x7F;
+
+      int ret = WriteToComPort(port_.c_str(), buf, bufSize);
+      if (ret != DEVICE_OK)
+         return ret;
+   }
+
+   return DEVICE_OK;
+}
---
+++ DeviceAdapters/MaestroServo/MaestroServo.h
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          MaestroServo.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Adapter for MaestroServo controller with USB interface
+// COPYRIGHT:     University of California, San Francisco, 2010
+// LICENSE:       This file is distributed under the LGPS license.
+//                License text is included with the source distribution.
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+// AUTHOR:        Nico Stuurman, 10/03/2009
+//
+
+#ifndef _MAESTROSERVO_H_
+#define _MAESTROSERVO_H_
+
+#include "../../MMDevice/MMDevice.h"
+#include "../../MMDevice/DeviceBase.h"
+#include <string>
+#include <map>
+
+//////////////////////////////////////////////////////////////////////////////
+// Error codes
+//
+#define ERR_UNKNOWN_POSITION         10002
+#define ERR_INVALID_SPEED            10003
+#define ERR_PORT_CHANGE_FORBIDDEN    10004
+#define ERR_SET_POSITION_FAILED      10005
+#define ERR_INVALID_STEP_SIZE        10006
+#define ERR_LOW_LEVEL_MODE_FAILED    10007
+#define ERR_INVALID_MODE             10008
+
+class MaestroServo : public CGenericBase<MaestroServo>
+{
+public:
+   MaestroServo();
+   ~MaestroServo();
+  
+   // Device API
+   // ----------
+   int Initialize();
+   int Shutdown();
+  
+   void GetName(char* pszName) const;
+   bool Busy();
+
+   // action interface
+   // ---------------- 
+   int OnPosition(MM::PropertyBase* pProp, MM::ActionType eAct);  
+   int OnMinPosition(MM::PropertyBase* pProp, MM::ActionType eAct);  
+   int OnMaxPosition(MM::PropertyBase* pProp, MM::ActionType eAct);  
+   int OnPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnServoNr(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnSpeed(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnAcceleration(MM::PropertyBase* pProp, MM::ActionType eAct);
+
+private:
+   bool initialized_;
+   bool moving_;
+   long minPos_;
+   long maxPos_;
+   long servoNr_;
+   long speed_;
+   long acceleration_;
+   // MMCore name of serial port
+   std::string port_;
+   // Command exchange with MMCore
+   std::string command_;
+   // Time that last command was sent to device
+   MM::MMTime changedTime_;
+   double position_;
+};
+
+
+#endif //_MaestroServo_H_
