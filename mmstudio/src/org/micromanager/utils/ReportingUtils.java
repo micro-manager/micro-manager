aef5b6fddb92507cffe2d810bc7c05a60b91002c
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-06-08 01:01:00 +0000

    fix tabs in ReportingUtils
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4692 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/utils/ReportingUtils.java
+++ mmstudio/src/org/micromanager/utils/ReportingUtils.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.micromanager.utils;
 
 import java.awt.event.ActionEvent;
@@ -22,103 +21,103 @@ import mmcorej.CMMCore;
  */
 public class ReportingUtils {
 
-    private static CMMCore core_ = null;
-    private static JFrame owningFrame_;
+   private static CMMCore core_ = null;
+   private static JFrame owningFrame_;
 
+   public static void SetContainingFrame(JFrame f) {
+      owningFrame_ = f;
+   }
 
-    public static void SetContainingFrame( JFrame f){
-        owningFrame_ = f;
-    }
+   public static void setCore(CMMCore core) {
+      core_ = core;
+   }
 
-    public static void setCore(CMMCore core) {
-        core_ = core;
-    }
+   public static void logMessage(String msg) {
+      if (core_ == null) {
+         System.out.println(msg);
+      } else {
+         core_.logMessage(msg);
+      }
+   }
 
-    public static void logMessage(String msg) {
-        if (core_ == null)
-            System.out.println(msg);
-        else
-            core_.logMessage(msg);
-    }
+   public static void showMessage(String msg) {
+      JOptionPane.showMessageDialog(null, msg);
+   }
 
-    public static void showMessage(String msg) {
-        JOptionPane.showMessageDialog(null, msg);
-    }
+   public static void logError(Throwable e, String msg) {
+      if (e != null) {
+         String stackTrace = getStackTraceAsString(e);
+         logMessage(msg + "\n" + e.toString() + " in "
+                 + Thread.currentThread().toString() + "\n" + stackTrace + "\n");
+      } else {
+         logMessage("Error: " + msg);
+      }
+   }
 
-    public static void logError(Throwable e, String msg) {
-        if (e != null) {
-            String stackTrace = getStackTraceAsString(e);
-            logMessage(msg + "\n" + e.toString() + " in " +
-                Thread.currentThread().toString() + "\n" + stackTrace + "\n");
-        } else {
-            logMessage("Error: "+msg);
-        }
-    }
+   public static void logError(Throwable e) {
+      logError(e, "");
+   }
 
-    public static void logError(Throwable e) {
-        logError(e, "");
-    }
+   public static void logError(String msg) {
+      logError(null, msg);
+   }
 
-    public static void logError(String msg) {
-        logError(null, msg);
-    }
+   public static void showError(Throwable e, String msg) {
+      logError(e, msg);
+      if (e != null && e.getMessage() != null) {
+         JOptionPane.showMessageDialog(null, "Error: " + msg + "\n" + e.getMessage());
+      } else if (msg.length() > 0) {
+         JOptionPane.showMessageDialog(null, "Error: " + msg);
+      } else if (msg.length() > 0) {
+         JOptionPane.showMessageDialog(null, "Error: " + msg);
+      } else if (e != null) {
+         JOptionPane.showMessageDialog(null, "Error: " + e.getStackTrace()[0]);
+      } else {
+         JOptionPane.showMessageDialog(null, "Unknown error (please check CoreLog.txt file for more information)");
+      }
+   }
 
-    public static void showError(Throwable e, String msg) {
-        logError(e, msg);
-        if (e != null && e.getMessage() != null) {
-            JOptionPane.showMessageDialog(null, "Error: "+msg + "\n"  + e.getMessage());
-        } else if (msg.length() > 0) {
-            JOptionPane.showMessageDialog(null, "Error: "+msg);
-        } else if (msg.length()>0) {
-            JOptionPane.showMessageDialog(null, "Error: "+msg);
-        } else if (e!=null){
-            JOptionPane.showMessageDialog(null, "Error: "+e.getStackTrace()[0]);
-        } else {
-            JOptionPane.showMessageDialog(null, "Unknown error (please check CoreLog.txt file for more information)");
-        }
-    }
+   public static void showError(Throwable e) {
+      showError(e, "");
+   }
 
-    public static void showError(Throwable e) {
-        showError(e, "");
-    }
+   public static void showError(String msg) {
+      showError(null, msg);
+   }
 
-    public static void showError(String msg) {
-        showError(null, msg);
-    }
+   private static String getStackTraceAsString(Throwable aThrowable) {
+      String result = "";
+      for (StackTraceElement line : aThrowable.getStackTrace()) {
+         result += "  at " + line.toString() + "\n";
+      }
+      return result;
+   }
 
-    private static String getStackTraceAsString(Throwable aThrowable) {
-       String result = "";
-        for (StackTraceElement line:aThrowable.getStackTrace())
-        {
-           result += "  at " + line.toString() + "\n";
-        }
-        return result;
-    }
+   public static void showError(ActionEvent e) {
+      throw new UnsupportedOperationException("Not yet implemented");
+   }
 
-    public static void showError(ActionEvent e) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+   public static void displayNonBlockingMessage(String message) {
+      if (null != owningFrame_) {
+         Calendar c = Calendar.getInstance();
+         final JOptionPane optionPane = new JOptionPane(c.getTime().toString() + " " + message, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION);
+         /* the false parameter is for not modal */
+         final JDialog dialog = new JDialog(owningFrame_, "μManager Warning: ", false);
+         optionPane.addPropertyChangeListener(
+                 new PropertyChangeListener() {
 
-    public static void displayNonBlockingMessage (String message) {
-        if (null != owningFrame_){
-            Calendar c = Calendar.getInstance();
-            final JOptionPane optionPane = new JOptionPane(c.getTime().toString() + " " + message, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION);
-            /* the false parameter is for not modal */
-            final JDialog dialog = new JDialog(owningFrame_, "μManager Warning: ", false);
-            optionPane.addPropertyChangeListener(
-            new PropertyChangeListener() {
-                public void propertyChange(PropertyChangeEvent e) {
-                    String prop = e.getPropertyName();
-                    if (dialog.isVisible() && (e.getSource() == optionPane)  && (prop.equals(JOptionPane.VALUE_PROPERTY))) {
-                        dialog.setVisible(false);
+                    public void propertyChange(PropertyChangeEvent e) {
+                       String prop = e.getPropertyName();
+                       if (dialog.isVisible() && (e.getSource() == optionPane) && (prop.equals(JOptionPane.VALUE_PROPERTY))) {
+                          dialog.setVisible(false);
+                       }
                     }
-                }
-            });
-            dialog.setContentPane(optionPane);
-            /* adapting the frame size to its content */
-            dialog.pack();
-            dialog.setLocationRelativeTo(owningFrame_);
-            dialog.setVisible(true);
-        }
-    }
+                 });
+         dialog.setContentPane(optionPane);
+         /* adapting the frame size to its content */
+         dialog.pack();
+         dialog.setLocationRelativeTo(owningFrame_);
+         dialog.setVisible(true);
+      }
+   }
 }
