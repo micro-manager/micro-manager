f66e2d3088f6d775ed676b044a048e16ffb7db0d
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-06-14 18:48:44 +0000

    just launch imageJ from command line for now, since ImageJ.exe launch doesn't find the correct Java machine on 64 bit windows
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4739 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ bin_x64/LaunchMicroManager.bat
@@ -0,0 +1,4 @@
+rem LaunchMicroManager.bat
+rem simple Launcher for ImageJ
+rem on 64 bit Windows this will start the 64 bit Java machine
+start "MicroManager" /B "C:\Program Files\Java\jre6\bin\javaw" -Xmx4g -cp ij.jar ij.ImageJ
