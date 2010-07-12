a70b21f250b0658af2873e68d9e7ac133bb3ed34
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-07-12 16:37:59 +0000

    Code cleanup.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4839 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/utils/JavaUtils.java
+++ mmstudio/src/org/micromanager/utils/JavaUtils.java
@@ -262,22 +262,20 @@ public class JavaUtils {
       return dir;
    }
 
-   public static boolean isWindows(){
-		String os = System.getProperty("os.name").toLowerCase();
-		//windows
-	    return (os.indexOf( "win" ) >= 0);
-	}
+   public static boolean isWindows() {
+      String os = System.getProperty("os.name").toLowerCase();
+      return (os.indexOf("win") >= 0);
+   }
 
-	public static boolean isMac(){
-		String os = System.getProperty("os.name").toLowerCase();
-		//Mac
-	    return (os.indexOf( "mac" ) >= 0);
-	}
+   public static boolean isMac() {
+      String os = System.getProperty("os.name").toLowerCase();
+      return (os.indexOf("mac") >= 0);
+   }
 
-	public static boolean isUnix(){
-		String os = System.getProperty("os.name").toLowerCase();
-		//linux or unix
-	    return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0);
-	}
+   public static boolean isUnix() {
+      String os = System.getProperty("os.name").toLowerCase();
+      //linux or unix
+      return (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0);
+   }
 }
 
