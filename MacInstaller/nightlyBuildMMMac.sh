645410d4ca427e9ad926f23b62ad02f75275bb06
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-07-19 18:59:56 +0000

    Backport of 1.3 Mac build scripts
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4910 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- MacInstaller/nightlyBuildMMMac.sh
+++ MacInstaller/nightlyBuildMMMac.sh
@@ -1,29 +1,28 @@
 # This script builds binaries for three architectures (ppc, i386, and x86_64)
 # from three repositories (which should be checked out already).
 # It assumes the following directory structure:
-# $REPOSITORYROOT - micro-manager1.4
-#                 - micro-manager1.4-ppc
-#                 - micro-manager1.4-i386
-#                 - micro-manager1.4-x86_64
+# $REPOSITORYROOT - micro-manager1.3
+#                 - micro-manager1.3-ppc
+#                 - micro-manager1.3-i386
+#                 - micro-manager1.3-x86_64
 # contents of $BUILDDIR will be removed!!!
 
 REPOSITORYROOT=/Users/MM/svn
 BUILDDIR=/Users/MM/MMBuild
-UPLOADPLACE=valelab.ucsf.edu:/home/MM/public_html/nightlyBuilds/1.4/Mac/
+UPLOADPLACE=valelab.ucsf.edu:/home/MM/public_html/nightlyBuilds/1.3/Mac/
 
 # No edits should be needed below this line
 
-TARGET=$BUILDDIR/Micro-Manager1.4
+TARGET=$BUILDDIR/Micro-Manager1.3
 
-PPC=$BUILDDIR/Micro-Manager1.4-ppc
-I386=$BUILDDIR/Micro-Manager1.4-i386
-X86_64=$BUILDDIR/Micro-Manager1.4-x86_64
+PPC=$BUILDDIR/Micro-Manager1.3-ppc
+I386=$BUILDDIR/Micro-Manager1.3-i386
+X86_64=$BUILDDIR/Micro-Manager1.3-x86_64
 
-REPOSITORY=$REPOSITORYROOT/micromanager1.4
-RPPC=$REPOSITORYROOT/micromanager1.4-ppc
-RI386=$REPOSITORYROOT/micromanager1.4-i386
-RX86_64=$REPOSITORYROOT/micromanager1.4-x86_64
-CLASSEXT=$REPOSITORY/../3rdpartypublic/classext
+REPOSITORY=$REPOSITORYROOT/micromanager1.3
+RPPC=$REPOSITORYROOT/micromanager1.3-ppc
+RI386=$REPOSITORYROOT/micromanager1.3-i386
+RX86_64=$REPOSITORYROOT/micromanager1.3-x86_64
 
 test -d $BUILDDIR && rm -rf $TARGET*
 mkdir $BUILDDIR
@@ -47,29 +46,29 @@ svn update
 cd $RPPC
 
 cp -r MacInstaller/Micro-Manager $TARGET
-cp $CLASSEXT/ij.jar $TARGET
-cp $CLASSEXT/bsh-2.0b4.jar $TARGET/plugins/
-cp $CLASSEXT/swingx-0.9.5.jar $TARGET/plugins/
-cp $CLASSEXT/swing-layout-1.0.4.jar $TARGET/plugins/
-cp $CLASSEXT/commons-math-2.0.jar $TARGET/plugins/
+cp classext/ij.jar $TARGET
+cp classext/bsh-2.0b4.jar $TARGET/plugins/
+cp classext/swingx-0.9.5.jar $TARGET/plugins/
+cp classext/swing-layout-1.0.4.jar $TARGET/plugins/
+cp classext/commons-math-2.0.jar $TARGET/plugins/
 cp -r MacInstaller/Micro-Manager $PPC
-cp $CLASSEXT/ij.jar $PPC
-cp $CLASSEXT/bsh-2.0b4.jar $PPC/plugins/
-cp $CLASSEXT/swingx-0.9.5.jar $PPC/plugins/
-cp $CLASSEXT/swing-layout-1.0.4.jar $PPC/plugins/
-cp $CLASSEXT/commons-math-2.0.jar $PPC/plugins/
+cp classext/ij.jar $PPC
+cp classext/bsh-2.0b4.jar $PPC/plugins/
+cp classext/swingx-0.9.5.jar $PPC/plugins/
+cp classext/swing-layout-1.0.4.jar $PPC/plugins/
+cp classext/commons-math-2.0.jar $PPC/plugins/
 cp -r MacInstaller/Micro-Manager $I386
-cp $CLASSEXT/ij.jar $I386
-cp $CLASSEXT/bsh-2.0b4.jar $I386/plugins/
-cp $CLASSEXT/swingx-0.9.5.jar $I386/plugins/
-cp $CLASSEXT/swing-layout-1.0.4.jar $I386/plugins/
-cp $CLASSEXT/commons-math-2.0.jar $I386/plugins/
+cp classext/ij.jar $I386
+cp classext/bsh-2.0b4.jar $I386/plugins/
+cp classext/swingx-0.9.5.jar $I386/plugins/
+cp classext/swing-layout-1.0.4.jar $I386/plugins/
+cp classext/commons-math-2.0.jar $I386/plugins/
 cp -r MacInstaller/Micro-Manager $X86_64
-cp $CLASSEXT/ij.jar $X86_64
-cp $CLASSEXT/bsh-2.0b4.jar $X86_64/plugins/
-cp $CLASSEXT/swingx-0.9.5.jar $X86_64/plugins/
-cp $CLASSEXT/swing-layout-1.0.4.jar $X86_64/plugins/
-cp $CLASSEXT/commons-math-2.0.jar $X86_64/plugins/
+cp classext/ij.jar $X86_64
+cp classext/bsh-2.0b4.jar $X86_64/plugins/
+cp classext/swingx-0.9.5.jar $X86_64/plugins/
+cp classext/swing-layout-1.0.4.jar $X86_64/plugins/
+cp classext/commons-math-2.0.jar $X86_64/plugins/
 
 
 # build PPC
@@ -79,34 +78,36 @@ VERSION=`cat version.txt`
 #daily build
 VERSION=$VERSION-`date "+%Y%m%d"`
 echo $VERSION
-sed -i -e "s/\"1.4.*\"/\"$VERSION\"/"  mmstudio/src/org/micromanager/MMStudioMainFrame.java || exit
+sed -i -e "s/\"1.3.*\"/\"$VERSION\"/"  mmstudio/src/org/micromanager/MMStudioMainFrame.java || exit
 
 
-./mmUnixBuild.sh || exit
+autoreconf || exit
 MACOSX_DEPLOYMENT_TARGET=10.4
-./configure --with-imagej=$PPC --enable-python --enable-arch=ppc --with-boost=/usr/local/ppc CXX="g++-4.0" CXXFLAGS="-g -O2 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch ppc" --disable-dependency-tracking || exit
+./configure --with-imagej=$PPC --enable-python --enable-arch=ppc CXX="g++ -V 4.0.1" CXXFLAGS="-g -O2 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch ppc" --disable-dependency-tracking || exit
+make clean || exit
 make || exit
 make install || exit
 
 # build i386
 cd $RI386
-./mmUnixBuild.sh || exit
+autoreconf || exit
 MACOSX_DEPLOYMENT_TARGET=10.4
-./configure --with-imagej=$I386 --enable-arch=i386 --with-boost=/usr/local/i386 CXX="g++-4.0" CXXFLAGS="-g -O2 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386" --disable-dependency-tracking || exit
+./configure --with-imagej=$I386 --enable-arch=i386 CXX="g++ -V 4.0.1" CXXFLAGS="-g -O2 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386" --disable-dependency-tracking || exit
+make clean || exit
 make || exit
 make install || exit
 
 # build x86_64
 cd $RX86_64
-./mmUnixBuild.sh || exit
+autoreconf || exit
 export MACOSX_DEPLOYMENT_TARGET=10.5
-./configure --with-imagej=$X86_64 --enable-arch=x86_64 --with-boost=/usr/local/x86_64 CXX="g++-4.2" CXXFLAGS="-g -O2 -mmacosx-version-min=10.5 -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch x86_64" --disable-dependency-tracking || exit
+./configure --with-imagej=$X86_64 --enable-arch=x86_64 CXX="g++ -V 4.2.1" CXXFLAGS="-g -O2 -mmacosx-version-min=10.5 -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch x86_64" --disable-dependency-tracking || exit
+make clean || exit
 make || exit
 make install || exit
 
 # Use lipo to make Universal Binaries
 lipo -create $PPC/libMMCoreJ_wrap.jnilib $I386/libMMCoreJ_wrap.jnilib $X86_64/libMMCoreJ_wrap.jnilib -o $TARGET/libMMCoreJ_wrap.jnilib
-lipo -create $PPC/libNativeGUI.jnilib $I386/libNativeGUI.jnilib $X86_64/libNativeGUI.jnilib -o $TARGET/libNativeGUI.jnilib
 #strip -X -S $TARGET/libMMCoreJ_wrap.jnilib
 cd $PPC
 FILES=libmmgr*
