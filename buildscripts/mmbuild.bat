9ffc0d78a5908d778a45b878bde163d5475da107
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-07-30 19:32:16 +0000

    add boost to %include%
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@4958 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- buildscripts/mmbuild.bat
+++ buildscripts/mmbuild.bat
@@ -1,6 +1,9 @@
 ECHO building native libraries...
 cd "\Program Files\Microsoft Visual Studio 9.0\VC\"
+
+set include=
 call vcvarsall.bat
+set include=d:\projects\3rdpartypublic\boost;%include%
  
 cd "\projects\micromanager\"
 devenv /REBUILD Release .\MMCore\MMCore.vcproj
