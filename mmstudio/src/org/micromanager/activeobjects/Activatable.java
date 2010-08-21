bac407ffef93a3118bf15d2b4f8ccaecd23b8fbb
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-08-21 00:07:42 +0000

    Active objects
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5149 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/activeobjects/Activatable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.micromanager.activeobjects;
+
+/**
+ *
+ * @author arthur
+ */
+public interface Activatable {
+   public void pause();
+   public void resume();
+   public void stop();
+}
