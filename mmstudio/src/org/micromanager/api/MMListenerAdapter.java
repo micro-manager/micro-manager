d256af511136fbb9fe986af60c52c0bab0023425
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-08-24 21:57:25 +0000

    Added MMListenerAdapter
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5178 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/api/MMListenerAdapter.java
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////
+//FILE:          MMListenerAdapter.java
+//PROJECT:       Micro-Manager
+//SUBSYSTEM:     mmstudio
+//-----------------------------------------------------------------------------
+//
+// AUTHOR:       Nico Stuurman, nico@cmp.ucsf.edu, August 24, 2010
+//
+// COPYRIGHT:    University of California, San Francisco, 2010
+//
+// LICENSE:      This file is distributed under the BSD license.
+//               License text is included with the source distribution.
+//
+//               This file is distributed in the hope that it will be useful,
+//               but WITHOUT ANY WARRANTY; without even the implied warranty
+//               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
+
+package org.micromanager.api;
+
+
+public class MMListenerAdapter implements MMListenerInterface {
+   public void propertiesChangedAlert(){
+   };
+   public void propertyChangedAlert(String device, String property, String value){
+   };
+   public void configGroupChangedAlert(String groupName, String newConfig){
+   };
+   public void pixelSizeChangedAlert(double newPixelSizeUm){
+   };
+   public void stagePositionChangedAlert(String deviceName, double pos){
+   };
+   public void xyStagePositionChanged(String deviceName, double xPos, double yPos){
+   };
+}
