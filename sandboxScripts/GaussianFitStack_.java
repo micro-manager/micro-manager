e55fe7770d50b58b6c3fa4dd6e6092257b5b59b4
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-09-09 00:31:45 +0000

    First attempt to get Gaussian fitting from all spots in a stack working
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5297 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ sandboxScripts/GaussianFitStack_.java
@@ -0,0 +1,206 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import ij.plugin.*;
+import ij.plugin.frame.*;
+import ij.plugin.filter.MaximumFinder;
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+import ij.IJ;
+import ij.measure.ResultsTable;
+
+import org.apache.commons.math.analysis.*;
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.optimization.direct.NelderMead;
+import org.apache.commons.math.optimization.direct.MultiDirectional;
+import org.apache.commons.math.optimization.RealPointValuePair;
+import org.apache.commons.math.optimization.GoalType;
+import org.apache.commons.math.optimization.SimpleScalarValueChecker;
+
+
+import java.awt.*;
+import java.lang.Math;
+
+
+public class GaussianFitStack_ implements PlugIn {
+	double[] params0_ = {16000.0, 5.0, 5.0, 1.0, 850.0};
+	double[] steps_ = new double[5];
+	String [] paramNames_ = {"A", "x_c", "y_c", "sigma", "b"};
+
+   GaussianResidual gs_;
+   NelderMead nm_;
+   SimpleScalarValueChecker convergedChecker_;;
+
+	private void print(String myText) {
+		ij.IJ.log(myText);
+	}
+
+   public class GaussianResidual implements MultivariateRealFunction {
+       short[] data_;
+       int nx_;
+       int ny_;
+       int count_ = 0;
+
+
+       public void setImage(short[] data, int width, int height) {
+          data_ = data;
+          nx_ = width;
+          ny_ = height;
+       }
+
+       public double value(double[] params) {
+          double residual = 0.0;
+          for (int i = 0; i < nx_; i++) {
+             for (int j = 0; j < ny_; j++) {
+                residual += sqr(gaussian(params, i, j) - data_[(i*nx_) + j]);
+             }
+          }
+   /*
+         for (int i=0; i< params.length; i++)
+                  print(" " + paramNames_[i] + ": " + params[i]);
+
+         print("Residual: " + residual);
+   */
+         return residual;
+      }
+
+      public double sqr(double val) {
+         return val*val;
+      }
+
+      public double gaussian(double[] params, int x, int y) {
+
+                  /* Gaussian function of the form:
+                   * A *  exp(-((x-xc)^2+(y-yc)^2)/(2 sigy^2))+b
+                   * A = params[0]  (total intensity)
+                   * xc = params[1]
+                   * yc = params[2]
+                   * sig = params[3]
+                   * b = params[4]  (background)
+                   */
+
+         if (params.length < 5) {
+                          // Problem, what do we do???
+                          //MMScriptException e;
+                          //e.message = "Params for Gaussian function has too few values";
+                          //throw (e);
+         }
+
+         double exponent = (sqr(x - params[1])  + sqr(y - params[2])) / (2 * sqr(params[3]));
+         double res = params[0] * Math.exp(-exponent) + params[4];
+         return res;
+      }
+   }
+
+
+
+	public double[] doGaussianFit (ImagePlus siPlus) {
+
+		ImageProcessor siProc = siPlus.getProcessor();
+      short[] imagePixels = (short[])siProc.getPixels();
+		gs_.setImage((short[])siProc.getPixels(), siProc.getWidth(), siProc.getHeight());
+
+      // Hard code estimate for sigma:
+      params0_[3] = 1.115;
+
+      // estimate background by averaging pixels at the edge
+      double bg = 0.0;
+      int n = 0;
+      int lastRowOffset = (siProc.getHeight() - 1) * siProc.getWidth();
+      for (int i =0; i < siProc.getWidth(); i++) {
+         bg += imagePixels[i];
+         bg += imagePixels[i + lastRowOffset];
+         n += 2;
+      }
+      for (int i = 1; i < siProc.getHeight() - 1; i++) {
+         bg += imagePixels[i * siProc.getWidth()];
+         bg += imagePixels[(i + 1) *siProc.getWidth() - 1];
+         n += 2;
+      }
+      params0_[4] = bg / n;
+
+      // estimate signal by subtracting background from total intensity 
+      double ti = 0.0;
+      double mt = 0.0;
+      for (int i = 0; i < siProc.getHeight() * siProc.getWidth(); i++) {
+         mt += imagePixels[i];
+      }
+      ti = mt - ( (bg / n) * siProc.getHeight() * siProc.getWidth());
+      params0_[0] = ti / (2 * Math.PI * params0_[3] * params0_[3]);
+      // print("Total signal: " + ti + "Estimate: " + params0_[0]);
+
+      // estimate center of mass
+      double mx = 0.0;
+      double my = 0.0;
+      for (int i = 0; i < siProc.getHeight() * siProc.getWidth(); i++) {
+         mx += imagePixels[i] * (i % siProc.getWidth() );
+         my += imagePixels[i] * (Math.floor (i / siProc.getWidth()));
+      }
+      params0_[1] = mx/mt;
+      params0_[2] = my/mt;
+
+      // set step size during estimate
+		for (int i=0;i<params0_.length;++i)
+			steps_[i] = params0_[i]*0.3;
+
+		nm_.setStartConfiguration(steps_);
+		nm_.setConvergenceChecker(convergedChecker_);
+
+		nm_.setMaxIterations(200);
+		double[] paramsOut = {0.0};
+		try {
+			RealPointValuePair result = nm_.optimize(gs_, GoalType.MINIMIZE, params0_);
+			paramsOut = result.getPoint();
+		} catch (Exception e) {}
+
+      return paramsOut;
+	}
+
+	public void run(String arg) {
+
+		gs_ = new GaussianResidual();
+		nm_ = new NelderMead();
+		convergedChecker_ = new SimpleScalarValueChecker(1e-5,-1);
+
+      // for now, take the active ImageJ image
+		ImagePlus siPlus = IJ.getImage();
+
+		long startTime = System.nanoTime();
+
+      // first find local maxima
+      MaximumFinder maxFinder = new MaximumFinder();
+      IJ.log("Stack has: " + siPlus.getStackSize() + " images");
+      for (int i= 1; i <= siPlus.getStackSize(); i++ ) {
+         siPlus.setSlice(i);
+         IJ.log ("Processing image: " + i);
+         IJ.run("Find Maxima...", "noise=500 output=List");
+         // maxFinder.findMaxima(siPlus.getStack().getProcessor(i), 500.0, ImageProcessor.NO_THRESHOLD, MaximumFinder.LIST, false, false); 
+
+         ResultsTable rt = ResultsTable.getResultsTable();
+
+         for (int j=0; j < rt.getCounter(); j++) {
+            int x = (int) rt.getValueAsDouble(0, j);
+            int y = (int) rt.getValueAsDouble(1, j);
+            IJ.log(" " + j + " " + x + " " + y);
+         }
+      }
+
+    /* 
+
+      double[] paramsOut = doGaussianFit(siPlus);
+
+		long endTime = System.nanoTime(); 
+		double took = (endTime - startTime) / 1E6;
+
+		print("\n\nResult:");
+		for (int i=0; i<paramsOut.length; i++)
+       		print(" " + paramNames_[i] + ": " + paramsOut[i]);
+
+		double anormalized = paramsOut[0] * (2 * Math.PI * paramsOut[3] * paramsOut[3]);
+		print("Amplitude normalized: " + anormalized);
+
+
+		print("Calculation took: " + took + " milli seconds"); 
+      */
+   }
+}
