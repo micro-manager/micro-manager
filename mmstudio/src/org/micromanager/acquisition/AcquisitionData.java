15bb9edbd94e4f08e560a2b757c2696d48217f01
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-01 02:02:53 +0000

    Add missing file.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5819 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/acquisition/AcquisitionData.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.micromanager.acquisition;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author arthur
+ */
+public class AcquisitionData extends JSONObject {
+
+}
