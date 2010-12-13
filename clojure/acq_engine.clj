a8ef264f64d347de020550a077ebcb0d26955e05
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-13 19:14:27 +0000

    deal with snaps
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5880 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- clojure/acq_engine.clj
+++ clojure/acq_engine.clj
@@ -27,6 +27,9 @@
     (reduce #(apply rekey %1 %2)
       m (partition 2 (conj ks knew kold)))))
 
+(defn select-and-rekey [m & ks]
+    (apply rekey (select-keys m (apply concat (partition 1 2 ks))) ks))
+
 ;; mm utils
 (defn get-default-devices []
   {:camera          (. mmc getCameraDevice)
@@ -48,16 +51,14 @@
 (defn ChannelSpec-to-map [^ChannelSpec chan]
   (-> chan
     (data-object-to-map)
-    (rekey
+    (select-and-rekey
       :config_                 :name
       :exposure_               :exposure
       :zOffset_                :z-offset
       :doZStack_               :use-z-stack
       :skipFactorFrame_        :skip-frames
       :useChannel_             :use-channel
-      :color_                  :color
     )
-    (dissoc :name_ :contrast16_ :contrast8_ :min_ :max_)
     (assoc :properties (get-config (. mmc getChannelGroup) (.config_ chan)))))
 
 (defn MultiStagePosition-to-map [^MultiStagePosition msp]
@@ -71,75 +72,7 @@
               2 [(.x stage-pos) (.y stage-pos)])])))})
 
 ;; acq-engine
-
-(defn set-stage-position
-  ([stage-dev z] (. mmc setPosition stage-dev z))
-  ([stage-dev x y] (. mmc setXYPosition stage-dev x y)))
-  
-(def device-agents
-  (let [devs (seq (. mmc getLoadedDevices))]
-    (zipmap devs (repeatedly (count devs) #(agent nil)))))
-    
-(defn create-presnap-actions [event]
-  (into {} (concat
-    (for [[axis pos] (get-in event [:position :axes])]
-      [axis #(apply set-stage-position axis pos)])
-    (for [prop (get-in event [:channel :properties])]
-      [(prop 0) #(apply* .setProperty mmc prop)]))))
-
-(defn send-device-action [dev action]
-  (send-off (device-agents dev) (fn [_] (action))))
-
-(defn run-actions [action-map]
-  (doseq [[dev action] action-map]
-    (send-device-action dev action))
-  (doseq [dev (keys action-map)]
-    (send-device-action dev #(. mmc waitForDevice dev)))
-  (apply await-for 10000 (vals device-agents)))
-
-(defn snap-image [event auto-shutter]
-  (if (and auto-shutter (. mmc getShutterOpen))
-    (. mmc setShutterOpen true)
-    (. mmc waitForDevice (. mmc getShutterDevice)))
-  (. mmc snapImage)
-  (if (and auto-shutter (event :close-shutter))
-     (. mmc setShutterOpen false))
-  (. mmc getImage))
-
-(defn start-burst [n-images]
-  (. mmc startSequenceAcquisition n-images 0 false)) 
-
-(defn collect-burst-image []
-  (while (zero? (. mmc remainingImageCount))
-    (Thread/sleep 5))
-  (. mmc popNextImage))   
-  
-(defn update-channel [event]
-  (when-let [channel (event :channel)]
-    (let [channel-group (. mmc getChannelGroup)
-          config (.config channel)]
-      (. mmc setConfig channel-group config))))
-      
-(defn update-slice [event]
-  (when-let [slice (event :slice)]
-    (when-let [focusDevice (. mmc getFocusDevice)]
-      (. mmc setPosition focusDevice slice))))
-
-(defn get-stage-positions [^MultiStagePosition msp]
-  (map #(.get msp %) (range (.size msp))))
-
-(defn update-position [event]
-  (when-let [position (event :position)]
-    (for [sp (get-stage-positions position)]
-      (let [stage (.stageName sp)]
-        (condp = (.numAxes sp)
-          1 (. mmc setPosition stage (.x sp))
-          2 (. mmc setXYPosition stage (.x sp) (.y sp)))))))
-          
-(defn run-autofocus [event]
-  (when (event :use-autofocus)
-    (.. gui getAutofocusManager getDevice fullFocus)))
-          
+ 
 (defn clock-ms []
   (quot (System/nanoTime) 1000000))
 
@@ -149,38 +82,63 @@
                      (clock-ms))]
     (when (pos? sleep-time)
       (Thread/sleep sleep-time)))
-  (clock-ms))
-  
-(defn wait-for-devices [event]
-  (. mmc waitForDevice (. mmc getFocusDevice))
-  ;; more
-  )
-  
-(defn run-event [event last-wake-time]
-  (update-channel event)
-  (update-position event)
-  (let [new-wake-time (acq-sleep event last-wake-time)]
-    (run-autofocus event)
-    (update-slice event)
-    (wait-for-devices event)
-    (snap-image event)
-    new-wake-time))
+  (clock-ms)) 
+ 
+(def device-agents
+  (let [devs (seq (. mmc getLoadedDevices))]
+    (zipmap devs (repeatedly (count devs) #(agent nil)))))
 
-(defn run-events [events]
-  (doall (map run-event events)))
+(defn set-stage-position
+  ([stage-dev z] (. mmc setPosition stage-dev z))
+  ([stage-dev x y] (. mmc setXYPosition stage-dev x y)))
+
+(defn set-property
+  ([dev prop] (. mmc setProperty (prop 0) (prop 1) (prop 2))))
+  
+(defn send-device-action [dev action]
+  (send-off (device-agents dev) (fn [_] (action))))
+    
+(defn create-presnap-actions [event]
+  (into {} (concat
+    (for [[axis pos] (get-in event [:position :axes])]
+      [axis #(apply set-stage-position axis pos)])
+    (for [prop (get-in event [:channel :properties])]
+      [(prop 0) #(.setProperty mmc (prop 0) (prop 1) (prop 2))]))))
+
+(defn run-actions [action-map]
+  (doseq [[dev action] action-map]
+    (send-device-action dev action))
+  (doseq [dev (keys action-map)]
+    (send-device-action dev #(. mmc waitForDevice dev)))
+  (doall (map (partial await-for 10000) (vals device-agents))))
+
+(defn snap-image [open-before close-after]
+  (if open-before
+    (. mmc setShutterOpen true)
+    (. mmc waitForDevice (. mmc getShutterDevice)))
+  (. mmc snapImage)
+  (if close-after
+    (. mmc setShutterOpen false))
+    (. mmc waitForDevice (. mmc getShutterDevice)))
+
+(defn collect-image [event]
+  (. mmc getImage))
+  
+(defn run-event [event]
+  (run-actions (create-presnap-actions event))
+  (await-for 10000 (device-agents (. mmc getCameraDevice)))
+  (snap-image true true)
+  (send-device-action (. mmc getCameraDevice) #(collect-image event)))
   
 (defn run-acquisition [settings] 
   (def acq-settings settings)
   (let [acq-seq (generate-acq-sequence settings)]
      (def acq-sequence acq-seq)
-     (doall (map println acq-seq))))
+     (map run-event acq-seq)))
   
 (defn convert-settings [^SequenceSettings settings]
   (-> settings
     (data-object-to-map)
-    (assoc :frames (range (.numFrames settings))
-           :channels (filter :use-channel (map ChannelSpec-to-map (.channels settings)))
-           :positions (map MultiStagePosition-to-map (.positions settings)))
     (rekey
       :slicesFirst             :slices-first
       :timeFirst               :time-first
@@ -190,12 +148,16 @@
       :skipAutofocusCount      :autofocus-skip
       :relativeZSlice          :relative-slices
       :intervalMs              :interval-ms
-    )))
+      :slices                  :slices
+    )
+    (assoc :frames (range (.numFrames settings))
+           :channels (filter :use-channel (map ChannelSpec-to-map (.channels settings)))
+           :positions (map MultiStagePosition-to-map (.positions settings)))))
 
 (defn run-acquisition-from-settings [^SequenceSettings settings]
   (println settings)
   (def orig-settings settings)
-  (run-acquisition (convert-settings settings)))
+  (println (run-acquisition (convert-settings settings))))
 
 (defn create-acq-eng []
   (doto
@@ -209,6 +171,5 @@
 (defn test-dialog [eng]
   (.show (AcqControlDlg. eng (Preferences/userNodeForPackage (.getClass gui)) gui)))
 
-   
 
    
