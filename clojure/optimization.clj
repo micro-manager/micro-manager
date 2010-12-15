6615f640def639ce0eac53f530cc50631710caf3
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-15 01:58:25 +0000

    optimization routines for clojure
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5918 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ clojure/optimization.clj
@@ -0,0 +1,28 @@
+(ns optimization
+  (:import [org.apache.commons.math.optimization GoalType]
+           [org.apache.commons.math.optimization.univariate BrentOptimizer]
+           [org.apache.commons.math.optimization.direct NelderMead]
+           [org.apache.commons.math.analysis UnivariateRealFunction MultivariateRealFunction]))
+
+(defn brent-min
+  "Find the minimum of univariate function f
+  using Brent's method."
+  [f xmin xmax]
+  (.optimize
+    (BrentOptimizer.)
+    (reify UnivariateRealFunction
+      (value [_ x] (f x)))
+    GoalType/MINIMIZE xmin xmax))
+
+
+(defn nelder-mead-min
+  "Find the minimum of multivariate function f
+  using the Nelder-Mead method."
+  [f stepsizes]
+  (->
+    (.optimize
+      (NelderMead.)
+      (reify MultivariateRealFunction
+        (value [_ x] (apply f x)))
+      GoalType/MINIMIZE (double-array stepsizes))
+    .getPoint vec))
