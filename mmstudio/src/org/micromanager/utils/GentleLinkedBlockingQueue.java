1d293b8df28e49d3e74e8157d647494bc14bb5a1
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-16 01:48:48 +0000

    A LinkedBlockingQueue that blocks if there isn't much memory left in the JVM.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5932 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/utils/GentleLinkedBlockingQueue.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.micromanager.utils;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ *
+ * @author arthur
+ */
+public class GentleLinkedBlockingQueue<E> extends LinkedBlockingQueue<E> {
+
+   @Override
+   public void put(E e) throws InterruptedException {
+      final int n = 1000 / 5; // Timeout after 1 second.
+      final long limitBytes = 5000000;
+      for (int i = 0;
+         (i<n) && (Runtime.getRuntime().freeMemory() < limitBytes);
+         ++i) {
+         JavaUtils.sleep(5);
+      }
+      super.put(e);
+   }
+}
