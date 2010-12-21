77643a01074b2dbbbb6ea03d8cdf408264d83220
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-21 00:07:45 +0000

    make classes directory, compile, and compress to jar file
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@5978 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/clojure/compile.sh
+++ mmstudio/clojure/compile.sh
@@ -1,3 +1,5 @@
 #! sh
+mkdir classes
 java -cp /Users/arthur/Programs/ImageJ/plugins/*:/Users/arthur/Programs/ImageJ/plugins/Micro-Manager/*:/Users/arthur/Programs/ImageJ/ij.jar:classes:./src -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.acq-engine
+jar cvf mm_clojure.jar -C classes/ .
 
