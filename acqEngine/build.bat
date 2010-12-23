742170f5945bbdb5f8a6455a9bda96de8df18be9
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-23 06:54:37 +0000

    make build.bat work correctly for acqEngine
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6031 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -1,9 +1,9 @@
-del /Q classes
+rmdir classes /s /q
 mkdir classes
-copy -R src/org classes/
+xcopy /E /y src classes\
 java -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
-jar cvf MMAcqEngine.jar -C classes/ .    
-copy MMAcqEngine.jar "/Program Files/ImageJ/plugins/Micro-Manager"
-copy MMAcqEngine.jar "../bin_Win32/"
-copy MMAcqEngine.jar "../bin_Win64/"
-copy MMAcqEngine.jar "../Install_Win32/micro-manager/plugins/Micro-Manager"
+jar cvf MMAcqEngine.jar -C classes\ .    
+copy /Y MMAcqEngine.jar \Program Files\ImageJ\plugins\Micro-Manager
+copy /Y MMAcqEngine.jar ..\bin_Win32\
+copy /Y MMAcqEngine.jar ..\bin_x64\
+copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
