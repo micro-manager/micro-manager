f445fd9bcf9b6f891b78a697c41527a57deb179f
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-23 07:14:48 +0000

    remove obsolte compile.sh; build.bat should copy to x64 install dir as well as win_32
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6033 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -7,3 +7,4 @@ copy /Y MMAcqEngine.jar \Program Files\ImageJ\plugins\Micro-Manager
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
 copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
+copy /Y MMAcqEngine.jar ..\Install_x64\micro-manager\plugins\Micro-Manager"
--- acqEngine/compile.sh
+++
@@ -1,6 +0,0 @@
-#! sh
-mkdir classes
-cp -R src/org classes/
-java -cp /Users/arthur/Programs/ImageJ/plugins/*:/Users/arthur/Programs/ImageJ/plugins/Micro-Manager/*:/Users/arthur/Programs/ImageJ/ij.jar:classes:./src -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.acq-engine
-jar cvf mm_clojure.jar -C classes/ .
-
