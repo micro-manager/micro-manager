04a59a9119fde44bb1f0cddca37f357c61d1945f
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2010-12-30 19:35:16 +0000

    smarter path for java and jar executables
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6077 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -1,8 +1,8 @@
 rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
-java -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
-jar cvf MMAcqEngine.jar -C classes\ .    
+"%JAVA_HOME%\bin\java" -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
+"%JAVA_HOME%\bin\jar" cvf MMAcqEngine.jar -C classes\ .    
 copy /Y MMAcqEngine.jar \Program Files\ImageJ\plugins\Micro-Manager
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
