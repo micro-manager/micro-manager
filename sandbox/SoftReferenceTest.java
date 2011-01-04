bcb31168b18142355cd159219bf25abc37c0e097
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-01-04 04:25:37 +0000

    get rid of package
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6118 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- sandbox/SoftReferenceTest.java
+++ sandbox/SoftReferenceTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package softreferencetest;
-
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
