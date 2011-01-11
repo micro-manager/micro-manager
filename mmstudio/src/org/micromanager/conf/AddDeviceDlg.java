321b94df83d0fb3fb7b5ff61bbc32fbcf1d6d0ff
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-01-11 23:35:26 +0000

    use JTree for the device listing - worked OK on windows - needs some more testing...
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6229 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/conf/AddDeviceDlg.java
+++ mmstudio/src/org/micromanager/conf/AddDeviceDlg.java
@@ -20,179 +20,189 @@
 //               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
 //
 // CVS:          $Id$
-
 package org.micromanager.conf;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.KeyStroke;
-import javax.swing.ListSelectionModel;
-import javax.swing.table.AbstractTableModel;
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
 import org.micromanager.utils.ReportingUtils;
 
 /**
  * Dialog to add a new device to the configuration.
  */
-public class AddDeviceDlg extends JDialog implements MouseListener{
-   private static final long serialVersionUID = 1L;
+public class AddDeviceDlg extends JDialog implements MouseListener, TreeSelectionListener {
 
-   class Dev_TableModel extends AbstractTableModel {
-      private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-      Device devs_[];
-      
-      public final String[] COLUMN_NAMES = new String[] {
-            "Library",
-            "Adapter",
-            "Description"
-      };
-      
-      public Dev_TableModel(MicroscopeModel model) {
-         devs_ = model.getAvailableDeviceList();
-      }
-      
-      public int getRowCount() {
-         return devs_.length;
-      }
-      public int getColumnCount() {
-         return COLUMN_NAMES.length;
-      }
-      public String getColumnName(int columnIndex) {
-         return COLUMN_NAMES[columnIndex];
-      }
-      public Object getValueAt(int rowIndex, int columnIndex) {
-         if (columnIndex == 1) {
-            return devs_[rowIndex].getAdapterName();
-         } else if (columnIndex == 0) {
-            return devs_[rowIndex].getLibrary();
-         } else
-            return devs_[rowIndex].getDescription();
-      }
-      
-      public boolean isCellEditable(int nRow, int nCol) {
-         return false;
-      }
-      
-      public Device getDevice(int row) {
-         if (row >= 0 && row <devs_.length)
-            return devs_[row];
-         
-         return null;
-      }
-   }
+    class TreeNodeShowsDeviceAndDescription extends DefaultMutableTreeNode {
 
-   private JTable devTable_;
-   private MicroscopeModel model_;
-   private DevicesPage devicesPage_;
+        public TreeNodeShowsDeviceAndDescription(String value) {
+            super(value);
+        }
 
-   /**
-    * Create the dialog
-    */
-   public AddDeviceDlg(MicroscopeModel model, DevicesPage devicesPage) {
-      super();
-      setModal(true);
-      setResizable(false);
-      getContentPane().setLayout(null);
-      setTitle("Add Device");
-      setBounds(400, 100, 596, 529);
-      devicesPage_ = devicesPage;
+        public String toString() {
+            String ret = "";
+            Object uo = getUserObject();
+            if (null != uo) {
+                if (uo.getClass().isArray()) {
+                    Object[] userData = (Object[]) uo;
+                    if (2 < userData.length) {
+                        ret = userData[1].toString() + " | " + userData[2].toString();
+                    }
+                } else {
+                    ret = uo.toString();
+                }
+            }
+            return ret;
+        }
+    }
 
-      final JScrollPane scrollPane = new JScrollPane();
-      scrollPane.setBounds(10, 10, 471, 475);
-      getContentPane().add(scrollPane);
+    private MicroscopeModel model_;
+    private DevicesPage devicesPage_;
+    private JTree theTree_;
 
-      final JButton addButton = new JButton();
-      addButton.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent arg0) {
-            if (addDevice())
-               rebuildTable();
-         }
-      });
-      addButton.setText("Add");
-      addButton.setBounds(490, 10, 93, 23);
-      getContentPane().add(addButton);
-      getRootPane().setDefaultButton(addButton);
 
-      final JButton doneButton = new JButton();
-      doneButton.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent arg0) {
-            dispose();
-         }
-      });
-      doneButton.setText("Done");
-      doneButton.setBounds(490, 39, 93, 23);
-      getContentPane().add(doneButton);
-      
-      model_ = model;
-      Dev_TableModel tm = new Dev_TableModel(model);
-      
-      devTable_= new JTable();
-      devTable_.setModel(tm);
-      devTable_.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-      InputMap im = devTable_.getInputMap(JTable.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-      im.put( KeyStroke.getKeyStroke( KeyEvent.VK_ENTER, 0 ), "none" );
-      scrollPane.setViewportView(devTable_);
-      devTable_.addMouseListener(this);
-   }
+    /**
+     * Create the dialog
+     */
+    public AddDeviceDlg(MicroscopeModel model, DevicesPage devicesPage) {
+        super();
+        setModal(true);
+        setResizable(false);
+        getContentPane().setLayout(null);
+        setTitle("Add Device - Open a library to see available devices & descriptions");
+        setBounds(400, 100, 596, 529);
+        devicesPage_ = devicesPage;
 
-   private void rebuildTable() {
-      devicesPage_.rebuildTable();
-   }
+        Device allDevs_[] = model.getAvailableDeviceList();
+        String thisLibrary = "";
+        TreeNodeShowsDeviceAndDescription root = new TreeNodeShowsDeviceAndDescription("");
+        TreeNodeShowsDeviceAndDescription node = null;
+        for (int idd = 0; idd < allDevs_.length; ++idd) {
+            // assume that the first library doesn't have an empty name! (of course!)
+            if (0 != thisLibrary.compareTo(allDevs_[idd].getLibrary())) {
+                // create a new node of devices for this library
+                node = new TreeNodeShowsDeviceAndDescription(allDevs_[idd].getLibrary());
+                root.add(node);
+                thisLibrary = allDevs_[idd].getLibrary(); // remember which library we are processing
+            }
+            TreeNodeShowsDeviceAndDescription aLeaf = new TreeNodeShowsDeviceAndDescription(allDevs_[idd].getAdapterName() + " | " + allDevs_[idd].getDescription());
+            Object[] userObject = {allDevs_[idd].getLibrary(), allDevs_[idd].getAdapterName(), allDevs_[idd].getDescription()};
+            aLeaf.setUserObject(userObject);
+            node.add(aLeaf);
+        }
+        // try building a tree
+        theTree_ = new JTree(root);
+        theTree_.addTreeSelectionListener(this);
+        // for debugging the tree selection:
 
-   public void mouseClicked(MouseEvent e) {
-      if (e.getClickCount() >= 2)
-         if (addDevice())
+        //currentSelectDbg_ = new JTextField("Current Selection: NONE");
+        //getContentPane().add(currentSelectDbg_, BorderLayout.NORTH);
+
+        final JScrollPane scrollPane = new JScrollPane(theTree_);
+        scrollPane.setBounds(10, 10, 471, 475);
+        getContentPane().add(scrollPane);
+
+        final JButton addButton = new JButton();
+        addButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                if (addDevice()) {
+                    rebuildTable();
+                }
+            }
+        });
+        addButton.setText("Add");
+        addButton.setBounds(490, 10, 93, 23);
+        getContentPane().add(addButton);
+        getRootPane().setDefaultButton(addButton);
+
+        final JButton doneButton = new JButton();
+        doneButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                dispose();
+            }
+        });
+        doneButton.setText("Done");
+        doneButton.setBounds(490, 39, 93, 23);
+        getContentPane().add(doneButton);
+        model_ = model;
+        //dtm_ = new Dev_TableModel(model);
+
+        /*
+        devTable_ = new JTable();
+        devTable_.setModel(tm);
+        devTable_.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        InputMap im = devTable_.getInputMap(JTable.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "none");
+        scrollPane.setViewportView(devTable_);
+        devTable_.addMouseListener(this);
+         */
+    }
+
+    private void rebuildTable() {
+        devicesPage_.rebuildTable();
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    protected boolean addDevice() {
+        int srows[] = theTree_.getSelectionRows();
+        if (0 < srows.length) {
+            if (0 < srows[0]) {
+                TreeNodeShowsDeviceAndDescription node = (TreeNodeShowsDeviceAndDescription) theTree_.getLastSelectedPathComponent();
+
+                Object[] userData = (Object[]) node.getUserObject();
+                String name = userData[1].toString();
+                boolean validName = false;
+                while (!validName) {
+                    name = JOptionPane.showInputDialog("Please type in the new device name", name);
+                    if (name == null) {
+                        return false;
+                    }
+                    Device newDev = new Device(name, userData[0].toString(), userData[1].toString(), userData[2].toString());
+                    try {
+                        model_.addDevice(newDev);
+                        validName = true;
+                    } catch (MMConfigFileException e) {
+                        ReportingUtils.showError(e);
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void valueChanged(TreeSelectionEvent event) {
+        //currentSelectDbg_.setText("Current Selection: "
+         //       + theTree_.getLastSelectedPathComponent().toString());
+
+        if(addDevice())
             rebuildTable();
-   }
-
-   public void mousePressed(MouseEvent e) {
-   }
-
-   public void mouseReleased(MouseEvent e) {
-   }
-
-   public void mouseEntered(MouseEvent e) {
-   }
-
-   public void mouseExited(MouseEvent e) {
-   }
-
-   protected boolean addDevice() {
-      int idx = devTable_.getSelectedRow();
-      if (idx < 0) {
-         JOptionPane.showMessageDialog(this, "No device is currently selected."); 
-         return false;
-      }
-      
-      Dev_TableModel tm = (Dev_TableModel)devTable_.getModel();
-      Device dev = tm.getDevice(idx);
-      if (dev == null)
-         return false;
-      
-      String name = new String(dev.getAdapterName());
-      boolean validName = false;
-      while (!validName) {
-         name = JOptionPane.showInputDialog("Please type in the new device name", name);
-         if (name == null)
-            return false;
-         Device newDev = new Device(name, dev.getLibrary(), dev.getAdapterName(), dev.getDescription());
-         try {
-            model_.addDevice(newDev);
-            validName = true;
-         } catch (MMConfigFileException e) {
-            ReportingUtils.showError(e);
-         }
-      }
-      return true;
-   }
+    }
 }
