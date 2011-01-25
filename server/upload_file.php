cc5ef8981325287ba1b54674fd1d661dc3e578b2
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-01-25 17:36:34 +0000

    initial version of configuration uploader
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6343 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ server/upload_file.php
@@ -0,0 +1,25 @@
+<?php
+print_r($_FILES);
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+    if (file_exists("/Users/karlhoover/upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "/Users/karlhoover/upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+?>
