0729500f02543da9165753be67d4b82dfd4e3187
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-01-26 23:41:23 +0000

    initial version of corelog uploader.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6362 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ server/upload_corelog.php
@@ -0,0 +1,43 @@
+<?php
+//print_r($_FILES);
+
+
+date_default_timezone_set('America/Los_Angeles');
+$month =  date('Ym');
+$uploadPlace_ = '/home/MM/configUploads/' . $month;
+if (!file_exists($uploadPlace_)) 
+{
+   mkdir($uploadPlace_, 0750);
+}
+
+if ($_FILES['file']['error'] > 0)
+{
+  echo 'Return Code: ' . $_FILES['file']['error'] . '<br />';
+}
+else
+{
+   echo 'Upload: ' . $_FILES['file']['name'] . '<br />';
+   echo 'Type: ' . $_FILES['file']['type'] . '<br />';
+   echo 'Size: ' . ($_FILES['file']['size'] / 1024) . ' Kb<br />';
+   echo 'Temp file: ' . $_FILES['file']['tmp_name'] . '<br />';
+   $opath = $uploadPlace_ . '/'. $_FILES['file']['name'];
+
+   if (file_exists($opath . '.uu' ))
+   {
+      echo $_FILES['file']['name'] . ' already exists. ';
+   }
+   else
+   {
+      move_uploaded_file($_FILES['file']['tmp_name'],
+      $opath . '.uu');
+      echo 'Stored in: ' . $opath . '.uu  <br />';
+      $dcommand = 'uudecode -o ' . $opath . '.gz ' . $opath . '.uu';
+      echo 'decode command: ' . $dcommand . '<br />';
+      exec( $dcommand);
+      $gcommand = 'gzip -d ' . $opath . '.gz';
+      echo 'gzip command:  ' . $gcommand . '<br />';
+      exec( $gcommand);
+      
+   }
+}
+?>
