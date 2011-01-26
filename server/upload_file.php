b017bdcf56ebcfb84b15514ca092248fe2eb5180
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-01-26 22:28:08 +0000

    uploads the uuencoded files.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6358 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- server/upload_file.php
+++ server/upload_file.php
@@ -1,25 +1,35 @@
 <?php
-print_r($_FILES);
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-    echo "Type: " . $_FILES["file"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+//print_r($_FILES);
 
-    if (file_exists("/Users/karlhoover/upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "/Users/karlhoover/upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
-    }
+
+date_default_timezone_set('America/Los_Angeles');
+$month =  date('Ym');
+$uploadPlace_ = '/home/MM/configUploads/' . $month;
+if (!file_exists($uploadPlace_)) 
+{
+   mkdir($uploadPlace_, 0750);
+}
+
+if ($_FILES['file']['error'] > 0)
+{
+  echo 'Return Code: ' . $_FILES['file']['error'] . '<br />';
+}
+else
+{
+   echo 'Upload: ' . $_FILES['file']['name'] . '<br />';
+   echo 'Type: ' . $_FILES['file']['type'] . '<br />';
+   echo 'Size: ' . ($_FILES['file']['size'] / 1024) . ' Kb<br />';
+   echo 'Temp file: ' . $_FILES['file']['tmp_name'] . '<br />';
+
+   if (file_exists($uploadPlace_ . '/'. $_FILES['file']['name']))
+   {
+      echo $_FILES['file']['name'] . ' already exists. ';
+   }
+   else
+   {
+      move_uploaded_file($_FILES['file']['tmp_name'],
+      $uploadPlace_ . '/' . $_FILES['file']['name']);
+      echo 'Stored in: ' . $uploadPlace . '/' . $_FILES['file']['name'];
+   }
+}
 ?>
