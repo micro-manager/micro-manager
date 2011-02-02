d0ef0e91f4d32a7cda10b75e739effef9cbad41b
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-02-02 22:08:33 +0000

    fix memory leak (failure to call UnloadAllDevices) in Reset
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6411 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- MMCore/MMCore.cpp
+++ MMCore/MMCore.cpp
@@ -758,9 +758,15 @@ void CMMCore::unloadAllDevices() throw (CMMError)
 void CMMCore::reset() throw (CMMError)
 {
    // before unloading everything try to apply shutdown configuration
+
+   try
+   {
    if (isConfigDefined(MM::g_CFGGroup_System, MM::g_CFGGroup_System_Shutdown))
       this->setConfig(MM::g_CFGGroup_System, MM::g_CFGGroup_System_Shutdown);
-   
+   }
+   catch(...)
+   {
+   }
    
    // of course one reason to reset is that some device is not configured correctly,
    // so we need to handle any exception thrown from here
