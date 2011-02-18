2f3b19c307c3cf8b98cc7cf0c4c4fd3be1c4d453
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-02-18 05:02:45 +0000

    Commented out code that caused closing images to fail
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6561 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/acquisition/TaggedImageStorageDiskDefault.java
+++ mmstudio/src/org/micromanager/acquisition/TaggedImageStorageDiskDefault.java
@@ -505,11 +505,13 @@ public class TaggedImageStorageDiskDefault implements TaggedImageStorage {
       } catch (Exception e) {
          ReportingUtils.logError(e);
       }
+      /*
       this.summaryMetadata_ = null;
       this.metadataStreams_ = null;
       this.metadataTable_ = null;
       this.filenameTable_ = null;
       this.displaySettings_ = null;
+       */
    }
 
    public String getDiskLocation() {
--- mmstudio/src/org/micromanager/acquisition/TaggedImageStorageRam.java
+++ mmstudio/src/org/micromanager/acquisition/TaggedImageStorageRam.java
@@ -60,9 +60,9 @@ public class TaggedImageStorageRam implements TaggedImageStorage {
    }
 
    public void close() {
-      imageMap_ = null;
-      summaryMetadata_ = null;
-      displaySettings_ = null;
+      //imageMap_ = null;
+      //summaryMetadata_ = null;
+      //displaySettings_ = null;
       // do nothing for now.
    }
 
