21bea4552ae1765996647c04cbeebe60d23f64b8
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-02-21 09:00:48 +0000

    image flipper example
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6577 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/examples/imageflipper/TaggedImageFlipper.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.micromanager.examples.imageflipper;
+
+import org.micromanager.api.AcquisitionEngine;
+import org.micromanager.api.MMPlugin;
+import org.micromanager.api.ScriptInterface;
+
+/**
+ *
+ * @author arthur
+ */
+public class TaggedImageFlipper implements MMPlugin {
+   public static String menuName = "Image Flipper";
+   private ScriptInterface gui_;
+
+   public void dispose() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public void setApp(ScriptInterface app) {
+      gui_ = app;
+   }
+
+   public void show() {
+      ImageFlipperControls ctls = new ImageFlipperControls();
+      AcquisitionEngine eng = gui_.getAcquisitionEngine();
+      eng.addImageProcessor(ctls.getProcessor());
+      ctls.show();
+   }
+
+   public void configurationChanged() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getDescription() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getInfo() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getVersion() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getCopyright() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+}
