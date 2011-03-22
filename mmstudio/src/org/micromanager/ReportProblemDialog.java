142fda7cd82847700ebaa03214fdac8c0d207f75
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-03-22 18:43:18 +0000

    correct e-mail string
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@6752 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/ReportProblemDialog.java
+++ mmstudio/src/org/micromanager/ReportProblemDialog.java
@@ -246,7 +246,7 @@ public class ReportProblemDialog extends javax.swing.JDialog {
     private void BuildAndSendReport() {
         reportPreamble_ = "#User Name:" + name_.getText();
         reportPreamble_ += ("#Organization: " + organization_.getText());
-        reportPreamble_ += ("#User e-mail: " + emailText_.getSelectedText());
+        reportPreamble_ += ("#User e-mail: " + emailText_.getText());
         reportPreamble_ += ("#User Description: " + descriptionPane_.getText());
         descriptionPane_.setEnabled(false);
         descriptionPane_.setEditable(false);
