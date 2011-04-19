b0e9e7fced482e00accfa75dac794d8d77e852d2
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-04-19 19:34:58 +0000

    license statements
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7037 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DataBrowser/src/org/micromanager/browser/core.clj
+++ plugins/DataBrowser/src/org/micromanager/browser/core.clj
@@ -1,3 +1,17 @@
+; FILE:         browser/core.clj
+; PROJECT:      Micro-Manager Data Browser Plugin
+; ----------------------------------------------------------------------------
+; AUTHOR:       Arthur Edelstein, arthuredelstein@gmail.com, April 19, 2011
+; COPYRIGHT:    University of California, San Francisco, 2011
+; LICENSE:      This file is distributed under the BSD license.
+;               License text is included with the source distribution.
+;               This file is distributed in the hope that it will be useful,
+;               but WITHOUT ANY WARRANTY; without even the implied warranty
+;               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+;               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+;               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+
 (ns org.micromanager.browser.core
   (:import [javax.swing BorderFactory JButton JComboBox JFrame JLabel
                         JList JPanel JScrollPane JSplitPane
--- plugins/DataBrowser/src/org/micromanager/browser/plugin.clj
+++ plugins/DataBrowser/src/org/micromanager/browser/plugin.clj
@@ -1,3 +1,18 @@
+; FILE:         browser/plugin.clj
+; PROJECT:      Micro-Manager Data Browser Plugin
+; ----------------------------------------------------------------------------
+; AUTHOR:       Arthur Edelstein, arthuredelstein@gmail.com, April 19, 2011
+; COPYRIGHT:    University of California, San Francisco, 2011
+; LICENSE:      This file is distributed under the BSD license.
+;               License text is included with the source distribution.
+;               This file is distributed in the hope that it will be useful,
+;               but WITHOUT ANY WARRANTY; without even the implied warranty
+;               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+;               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+;               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+
+
 (ns org.micromanager.browser.plugin
   (:use [org.micromanager.browser.core :only (start-browser)])
   (:gen-class
