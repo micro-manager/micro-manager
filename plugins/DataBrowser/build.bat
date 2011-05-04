052a2ad5e804d2d54d4e04fb1534e8684fae801c
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-05-04 00:35:29 +0000

    copy DataBrowser.jar to the correct directories
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7138 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DataBrowser/build.bat
+++ plugins/DataBrowser/build.bat
@@ -3,8 +3,5 @@ md classes
 xcopy /E /Y src classes\
 "%JAVA_HOME%\bin\java" -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/clojure-contrib.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
 "%JAVA_HOME%\bin\jar" cvf DataBrowser.jar -C classes\ .  
-copy /Y DataBrowser.jar \Program Files\ImageJ\plugins\Micro-Manager
-copy /Y DataBrowser.jar ..\..\bin_Win32\
-copy /Y DataBrowser.jar ..\..\bin_x64\
-copy /Y DataBrowser.jar ..\..\Install_Win32\micro-manager\plugins\Micro-Manager"
-copy /Y DataBrowser.jar ..\..\Install_x64\micro-manager\plugins\Micro-Manager"
+copy /Y DataBrowser.jar \Program Files\ImageJ\mmplugins\
+copy /Y DataBrowser.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
