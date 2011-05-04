964564dc1773db2c759f955b7c1e8ff35e366dda
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-05-04 21:36:20 +0000

    add missing quotes to file path
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7149 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DataBrowser/build.bat
+++ plugins/DataBrowser/build.bat
@@ -3,5 +3,5 @@ md classes
 xcopy /E /Y src classes\
 "%JDK6%\bin\java" -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/clojure-contrib.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
 "%JDK6%\bin\jar" cvf DataBrowser.jar -C classes\ .  
-copy /Y DataBrowser.jar \Program Files\ImageJ\mmplugins\
+copy /Y DataBrowser.jar "\Program Files\ImageJ\mmplugins\"
 copy /Y DataBrowser.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
