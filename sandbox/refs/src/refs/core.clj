8e0b082181c0a3b66826379709ec5ec4b26182fd
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-05-11 23:09:26 +0000

    code for adding pmids and pmcids to micromanager refs on the wiki
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7209 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ sandbox/refs/src/refs/core.clj
@@ -0,0 +1,66 @@
+(ns refs.core
+  (use [clojure.xml :as xml]))
+
+(def url-stub "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?")
+
+(defn get-xml [doi]
+  (clojure.xml/parse (str url-stub "term=" doi)))
+
+(defn get-pmid [xml]
+  (first
+    (for [x (xml-seq xml) :when (= (:tag x) :Id)]
+      (first (:content x)))))
+    
+(defn doi-to-pmid [doi]
+  (get-pmid (get-xml doi)))
+
+(def test-doi "10.1074/jbc.M109.031203")
+
+(def doi-re #"http://dx.doi.org/(.*?)\s")
+
+(defn has-pmid [para]
+  (.contains para "PMID"))
+
+(defn has-pmcid [para]
+  (.contains para "{{PMCID|"))
+
+(defn read-doi [para]
+  (second (re-find doi-re para)))
+  
+(defn append-pmid [para]
+  (or
+    (when-not (has-pmid para)
+      (when-let [doi (read-doi para)]
+        (when-let [pmid (doi-to-pmid doi)]
+          (str para
+               " PMID "
+               pmid
+               "."))))
+    para))
+
+(def pmcid-url-stub
+  (str "http://www.ncbi.nlm.nih.gov/sites/pmctopmid?"
+       "PMCRALayout.PMCIDS.PMCIDS_Portlet.Db=pubmed&p%24a=PMCRALayout.PMCIDS.PMCIDS_Portlet.Convert"
+       "&p%24l=PMCRALayout&p%24st=pmctopmid&PMCRALayout.PMCIDS.PMCIDS_Portlet.Ids="))
+
+(def pmc-re #">PMC(\d*?)</td>")
+
+(defn pmid-to-pmcid [pmid]
+  (second (re-find pmc-re (slurp (str pmcid-url-stub pmid)))))
+
+(defn read-pmid [para]
+  (second (re-find #"PMID (\d*+)" (str para " "))))
+
+(defn append-pmcid [para]
+  (or
+    (when-not (has-pmcid para)
+      (when-let [pmid (read-pmid para)]
+        (when-let [pmcid (pmid-to-pmcid pmid)]
+          (str para
+            " {{PMCID|"
+            pmcid
+            "}}"))))
+    para)) 
+
+(defn doi-to-pmcid [doi]
+  (pmid-to-pmcid (doi-to-pmid doi)))
