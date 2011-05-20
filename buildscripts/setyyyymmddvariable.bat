5335f8b0b8f51333af99a5a20b2077beb7751942
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-05-20 20:56:32 +0000

    not necessary
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7269 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- buildscripts/setyyyymmddvariable.bat
+++ buildscripts/setyyyymmddvariable.bat
@@ -1,3 +1,3 @@
 rem set the day-stamp variable
 rem needs program yyyymmdd.exe installed in path
-FOR /F "tokens=*" %%i in ('\projects\micromanager\yyyymmdd') do SET YYYYMMDD=%%i
+FOR /F "tokens=*" %%i in ('yyyymmdd') do SET YYYYMMDD=%%i
