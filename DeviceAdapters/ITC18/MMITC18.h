1383f3de6197a17aeeb17d238cb3ac6fcab44419
karl <karl@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-05-24 14:40:28 +0000

    added new virtual DA sequence functions to return false
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7286 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/ITC18/MMITC18.h
+++ DeviceAdapters/ITC18/MMITC18.h
@@ -228,6 +228,17 @@ public:
     int OnVolts(MM::PropertyBase* pProp, MM::ActionType eAct);
     int OnMaxVolt(MM::PropertyBase* pProp, MM::ActionType eAct);
     int OnDACPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+    
+    // These commands are not supported on this device
+    // Sequence functions
+    int IsDASequenceable(bool& isSequenceable) const {isSequenceable = false; return DEVICE_OK;}
+    int GetDASequenceMaxLength(long& nrEvents) const  {nrEvents = 0; return DEVICE_OK;}
+    int StartDASequence() const {return DEVICE_OK;} 
+    int StopDASequence() const {return DEVICE_OK;}
+    int LoadDASequence(std::vector<double> voltages) const {return DEVICE_OK;}
+    
+   
+    
 private:
     int ITC18RunOnce(double value);
 
@@ -270,6 +281,15 @@ public:
      int OnVolts(MM::PropertyBase* pProp, MM::ActionType eAct);
      int OnRange(MM::PropertyBase* pProp, MM::ActionType eAct);
      int OnADCPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+     
+     // These commands are not supported on this device
+     // Sequence functions
+     int IsDASequenceable(bool& isSequenceable) const {isSequenceable = false; return DEVICE_OK;}
+     int GetDASequenceMaxLength(long& nrEvents) const  {nrEvents = 0; return DEVICE_OK;}
+     int StartDASequence() const {return DEVICE_OK;} 
+     int StopDASequence() const {return DEVICE_OK;}
+     int LoadDASequence(std::vector<double> voltages) const {return DEVICE_OK;}
+     
 private:
      int ITC18RunOnce(double &value);		
      std::string name_;
