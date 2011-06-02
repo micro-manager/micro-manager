a36cbb4cf43cecdc75b27feffb099c8a5bdf37a4
karlh <karlh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-06-02 19:21:19 +0000

    Fix missing java.library.path
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7332 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DataBrowser/build.bat
+++ plugins/DataBrowser/build.bat
@@ -1,7 +1,7 @@
 rmdir classes
 md classes
 xcopy /E /Y src classes\
-"%JDK6%\bin\java" -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/clojure-contrib.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
+"%JDK6%\bin\java" -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/clojure-contrib.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_Win32 -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
 "%JDK6%\bin\jar" cvf DataBrowser.jar -C classes\ .  
 copy /Y DataBrowser.jar "\Program Files\ImageJ\mmplugins\"
 copy /Y DataBrowser.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
