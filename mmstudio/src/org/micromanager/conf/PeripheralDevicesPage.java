2ea57f86ed1de5e26e7f0e60947b996b3661439e
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-06-03 16:18:26 +0000

    Formatted Peripheral Device pages so that I can edit them
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7337 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/conf/PeripheralDevicesPage.java
+++ mmstudio/src/org/micromanager/conf/PeripheralDevicesPage.java
@@ -42,150 +42,147 @@ import org.micromanager.utils.ReportingUtils;
  * Wizard page to add or remove devices.
  */
 public class PeripheralDevicesPage extends PagePanel {
-   private static final long serialVersionUID = 1L;
 
+   private static final long serialVersionUID = 1L;
    private static final int HUBCOLUMN = 0;
    private static final int NAMECOLUMN = 1;
    private static final int ADAPTERCOLUMN = 2;
    private static final int DESCRIPTIONCOLUMN = 3;
    private static final int SELECTIONCOLUMN = 4;
 
-
-
-
    class DeviceTable_TableModel extends AbstractTableModel {
-      private static final long serialVersionUID = 1L;
 
-      public final String[] COLUMN_NAMES = new String[] {
-            "Hub",
-            "Name",
-            "Adapter/Library",
-            "Description",
-            "Selected"
+      private static final long serialVersionUID = 1L;
+      public final String[] COLUMN_NAMES = new String[]{
+         "Hub",
+         "Name",
+         "Adapter/Library",
+         "Description",
+         "Selected"
       };
-      
       MicroscopeModel model_;
       Device devices_[];
       Vector<Device> peripheralDevices_;
       Vector<Boolean> selected_;
       Vector<String> masterDevices_;
-      
+
       public DeviceTable_TableModel(MicroscopeModel model) {
-         peripheralDevices_ = new Vector<Device>() ;
-         selected_ = new  Vector<Boolean>();
-         masterDevices_ = new Vector<String>() ;
+         peripheralDevices_ = new Vector<Device>();
+         selected_ = new Vector<Boolean>();
+         masterDevices_ = new Vector<String>();
          setMicroscopeModel(model);
       }
-      
+
       public void setMicroscopeModel(MicroscopeModel mod) {
          devices_ = mod.getDevices();
          model_ = mod;
 
          for (Device d : devices_) {
-         if (!d.getName().equals("Core")) {
-            StrVector disco = core_.getDiscoverableDevices(d.getName());
-            if (0 < disco.size()) {
-               for (String devvs : disco) {
-                  try{
-                     Device allPossiblePeripherals[] = Device.getLibraryContents(d.getLibrary(), core_);
-                     // find the device description
-                     String descr = "";
-                     String defaultName = "";
-                     for (int idd = 0; idd < allPossiblePeripherals.length; ++idd) {
+            if (!d.getName().equals("Core")) {
+               StrVector disco = core_.getDiscoverableDevices(d.getName());
+               if (0 < disco.size()) {
+                  for (String devvs : disco) {
+                     try {
+                        Device allPossiblePeripherals[] = Device.getLibraryContents(d.getLibrary(), core_);
+                        // find the device description
+                        String descr = "";
+                        String defaultName = "";
+                        for (int idd = 0; idd < allPossiblePeripherals.length; ++idd) {
 
-                        if (allPossiblePeripherals[idd].getAdapterName().equalsIgnoreCase(devvs)) {
-                           descr = allPossiblePeripherals[idd].getDescription();
-                           defaultName = allPossiblePeripherals[idd].getAdapterName();
-                           break;
+                           if (allPossiblePeripherals[idd].getAdapterName().equalsIgnoreCase(devvs)) {
+                              descr = allPossiblePeripherals[idd].getDescription();
+                              defaultName = allPossiblePeripherals[idd].getAdapterName();
+                              break;
+                           }
                         }
+                        if (0 < defaultName.length()) {
+                           Device newDev = new Device(defaultName, d.getLibrary(), defaultName, descr);
+                           newDev.setDiscoverable(true);
+                           masterDevices_.add(d.getName());
+                           selected_.add(true);
+                           peripheralDevices_.add(newDev);
+                        }
+                        // // ReportingUtils.displayNonBlockingMessage(message);
+                     } catch (Exception e) {
                      }
-                     if (0 < defaultName.length()) {
-                        Device newDev = new Device(defaultName, d.getLibrary(), defaultName, descr);
-                        newDev.setDiscoverable(true);
-                        masterDevices_.add(d.getName());
-                        selected_.add(true);
-                        peripheralDevices_.add(newDev);
-                     }
-                     // // ReportingUtils.displayNonBlockingMessage(message);
-                  }
-                  catch(Exception e){
-                     
                   }
                }
             }
-         }
 
          }
       }
-      
+
       public int getRowCount() {
          return peripheralDevices_.size();
       }
+
       public int getColumnCount() {
          return COLUMN_NAMES.length;
       }
+
       @Override
       public String getColumnName(int columnIndex) {
          return COLUMN_NAMES[columnIndex];
       }
 
       @Override
-      public Class getColumnClass(int c){
+      public Class getColumnClass(int c) {
          Class ret = String.class;
-         if( SELECTIONCOLUMN == c){
+         if (SELECTIONCOLUMN == c) {
             ret = Boolean.class;
          }
          return ret;
       }
+
       public Object getValueAt(int rowIndex, int columnIndex) {
 
-         if(HUBCOLUMN == columnIndex)
+         if (HUBCOLUMN == columnIndex) {
             return masterDevices_.get(rowIndex);
-         else if (columnIndex == NAMECOLUMN)
+         } else if (columnIndex == NAMECOLUMN) {
             return peripheralDevices_.get(rowIndex).getName();
-         else if (columnIndex == ADAPTERCOLUMN)
-            return new String (peripheralDevices_.get(rowIndex).getAdapterName() + "/" + peripheralDevices_.get(rowIndex).getLibrary());
-         else if (columnIndex == DESCRIPTIONCOLUMN)
+         } else if (columnIndex == ADAPTERCOLUMN) {
+            return new String(peripheralDevices_.get(rowIndex).getAdapterName() + "/" + peripheralDevices_.get(rowIndex).getLibrary());
+         } else if (columnIndex == DESCRIPTIONCOLUMN) {
             return peripheralDevices_.get(rowIndex).getDescription();
-         else if (SELECTIONCOLUMN == columnIndex )
+         } else if (SELECTIONCOLUMN == columnIndex) {
             return selected_.get(rowIndex);
-         else
+         } else {
             return null;
+         }
       }
 
       @Override
       public void setValueAt(Object value, int row, int col) {
-         switch( col){
-            case  HUBCOLUMN:
+         switch (col) {
+            case HUBCOLUMN:
                break;
-            case NAMECOLUMN:
-               {
-                  String n = (String)value;
-                  String o = peripheralDevices_.get(row).getName();
-                  try {
+            case NAMECOLUMN: {
+               String n = (String) value;
+               String o = peripheralDevices_.get(row).getName();
+               try {
 
-                     //  NOT YET!  model_.changeDeviceName(o, n);
-                     fireTableCellUpdated(row, col);
-                  } catch (Exception e) {
-                     handleError(e.getMessage());
-                  }
+                  //  NOT YET!  model_.changeDeviceName(o, n);
+                  fireTableCellUpdated(row, col);
+               } catch (Exception e) {
+                  handleError(e.getMessage());
                }
-               break;
+            }
+            break;
             case ADAPTERCOLUMN:
                break;
             case DESCRIPTIONCOLUMN:
                break;
             case SELECTIONCOLUMN:
-               selected_.set(row, (Boolean)value);
+               selected_.set(row, (Boolean) value);
                break;
          }
       }
-     
+
       @Override
       public boolean isCellEditable(int nRow, int nCol) {
          boolean ret = false;
-         switch( nCol){
-            case  HUBCOLUMN:
+         switch (nCol) {
+            case HUBCOLUMN:
                break;
             case NAMECOLUMN:
                ret = true;
@@ -200,38 +197,50 @@ public class PeripheralDevicesPage extends PagePanel {
          }
          return ret;
       }
-      
+
       public void refresh() {
          devices_ = model_.getDevices();
          this.fireTableDataChanged();
       }
 
-      Vector<Device> getPeripheralDevices() { return peripheralDevices_;} ;
-      Vector<Boolean> getSelected() { return selected_;};
-      Vector<String> getMasterDevices() { return masterDevices_;};
+      Vector<Device> getPeripheralDevices() {
+         return peripheralDevices_;
+      }
 
+      ;
 
+      Vector<Boolean> getSelected() {
+         return selected_;
+      }
+
+      ;
+
+      Vector<String> getMasterDevices() {
+         return masterDevices_;
+      }
+
+      ;
    }
-
    private JTable deviceTable_;
    private JScrollPane scrollPane_;
    private static final String HELP_FILE_NAME = "conf_devices_page.html";
+
    /**
     * Create the panel
     */
    public PeripheralDevicesPage(Preferences prefs) {
       super();
 
-      String hubColumn = Integer.toString(HUBCOLUMN+1);
-      String nameColumn = Integer.toString(NAMECOLUMN+1);
+      String hubColumn = Integer.toString(HUBCOLUMN + 1);
+      String nameColumn = Integer.toString(NAMECOLUMN + 1);
       title_ = "The following peripherals have been discovered";
-      helpText_ = "The list of detected peripheral devices is displayed above. " +
-      "You select and de-select the devices in the above list.\n" +
-      "Column " + hubColumn + " shows which hub the device was reported on\n" +
-      "Column " + nameColumn + " specified the device name, which you can edit\n" +
-      "In subsequent steps devices will be referred to by their assigned names.\n\n" +
-      "You can edit device names by double-clicking the name. Device name must be unique and should not contain any special characters.";
-      
+      helpText_ = "The list of detected peripheral devices is displayed above. "
+              + "You select and de-select the devices in the above list.\n"
+              + "Column " + hubColumn + " shows which hub the device was reported on\n"
+              + "Column " + nameColumn + " specified the device name, which you can edit\n"
+              + "In subsequent steps devices will be referred to by their assigned names.\n\n"
+              + "You can edit device names by double-clicking the name. Device name must be unique and should not contain any special characters.";
+
       setLayout(null);
       prefs_ = prefs;
       setHelpFileName(HELP_FILE_NAME);
@@ -244,22 +253,23 @@ public class PeripheralDevicesPage extends PagePanel {
       deviceTable_.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
       scrollPane_.setViewportView(deviceTable_);
    }
-   
+
    protected void removeDevice() {
       int sel = deviceTable_.getSelectedRow();
-      if (sel < 0)
-         return;      
-      String devName = (String)deviceTable_.getValueAt(sel, 0);
-      
+      if (sel < 0) {
+         return;
+      }
+      String devName = (String) deviceTable_.getValueAt(sel, 0);
+
       if (devName.contentEquals(new StringBuffer().append(MMCoreJ.getG_Keyword_CoreDevice()))) {
          handleError(MMCoreJ.getG_Keyword_CoreDevice() + " device can't be removed!");
          return;
       }
-      
+
       model_.removeDevice(devName);
       rebuildTable();
    }
-   
+
    public void rebuildTable() {
       TableModel tm = deviceTable_.getModel();
       DeviceTable_TableModel tmd;
@@ -273,11 +283,11 @@ public class PeripheralDevicesPage extends PagePanel {
       tmd.fireTableStructureChanged();
       tmd.fireTableDataChanged();
    }
-   
+
    public void refresh() {
       rebuildTable();
    }
-   
+
    public boolean enterPage(boolean fromNextPage) {
       boolean status = true;
       Container ancestor = getTopLevelAncestor();
@@ -322,33 +332,32 @@ public class PeripheralDevicesPage extends PagePanel {
             status = false;
          } finally {
             try {
-               core_.unloadAllDevices();
+               // core_.unloadAllDevices();
             } catch (Exception ex) {
-               ReportingUtils.logError(ex);
+               ReportingUtils.showError(ex);
             }
             Container ancestor = getTopLevelAncestor();
             Cursor oldc = null;
-            if (null != ancestor){
+            if (null != ancestor) {
                oldc = ancestor.getCursor();
                Cursor waitc = new Cursor(Cursor.WAIT_CURSOR);
                ancestor.setCursor(waitc);
             }
-            model_.loadModel(core_, false);
+            //model_.loadModel(core_, false);
 
-               if (null != ancestor){
-                  if( null != oldc)
-                     ancestor.setCursor(oldc);
+            if (null != ancestor) {
+               if (null != oldc) {
+                  ancestor.setCursor(oldc);
                }
+            }
          }
       }
       return status;
    }
 
    public void loadSettings() {
-      
    }
-   
+
    public void saveSettings() {
-      
    }
 }
--- mmstudio/src/org/micromanager/conf/PeripheralDevicesPreInitializationPropertiesPage.java
+++ mmstudio/src/org/micromanager/conf/PeripheralDevicesPreInitializationPropertiesPage.java
@@ -23,164 +23,155 @@
 // CVS:          $Id: PeripheralDevicesPreInitializationPropertiesPage.java 6968 2011-04-13 19:30:09Z karlh $
 //
 package org.micromanager.conf;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+
 import java.util.prefs.Preferences;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.TableColumn;
 import org.micromanager.utils.GUIUtils;
 import mmcorej.MMCoreJ;
-import mmcorej.DeviceDetectionStatus;
-import mmcorej.StrVector;
 import org.micromanager.utils.PropertyItem;
 import org.micromanager.utils.PropertyNameCellRenderer;
 import org.micromanager.utils.PropertyValueCellEditor;
 import org.micromanager.utils.PropertyValueCellRenderer;
 import org.micromanager.utils.ReportingUtils;
+
 /**
  * Wizard page to set device properties.
  *
  */
 public class PeripheralDevicesPreInitializationPropertiesPage extends PagePanel {
 
-    private static final long serialVersionUID = 1L;
-    private JTable propTable_;
-    private JScrollPane scrollPane_;
-    private static final String HELP_FILE_NAME = "conf_preinit_page.html";
-    /**
-     * Create the panel
-     */
-    public PeripheralDevicesPreInitializationPropertiesPage(Preferences prefs) {
-        super();
-        title_ = "Edit peripheral device pre-initialization settings";
-        helpText_ = "The list of device properties which must be defined prior to initialization is shown above. ";
-        setLayout(null);
-        prefs_ = prefs;
-        setHelpFileName(HELP_FILE_NAME);
-        scrollPane_ = new JScrollPane();
-        scrollPane_.setBounds(10, 9, 421, 262);
-        add(scrollPane_);
-        propTable_ = new JTable();
-        propTable_.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        propTable_.setAutoCreateColumnsFromModel(false);
-        scrollPane_.setViewportView(propTable_);
-        final PeripheralDevicesPreInitializationPropertiesPage thisPage = this;
-    }
-    private void rebuildTable() {
-        PropertyTableModel tm = new PropertyTableModel(this, model_, PropertyTableModel.PREINIT);
-        propTable_.setModel(tm);
-        PropertyValueCellEditor propValueEditor = new PropertyValueCellEditor();
-        PropertyValueCellRenderer propValueRenderer = new PropertyValueCellRenderer();
-        PropertyNameCellRenderer propNameRenderer = new PropertyNameCellRenderer();
-        if (propTable_.getColumnCount() == 0) {
-            TableColumn column;
-            column = new TableColumn(0, 200, propNameRenderer, null);
-            propTable_.addColumn(column);
-            column = new TableColumn(1, 200, propNameRenderer, null);
-            propTable_.addColumn(column);
-            column = new TableColumn(2, 200, propValueRenderer, propValueEditor);
-            propTable_.addColumn(column);
-        }
-        tm.fireTableStructureChanged();
-        tm.fireTableDataChanged();
-        boolean any = false;
-        Device devices[] = model_.getPeripheralDevices();
+   private static final long serialVersionUID = 1L;
+   private JTable propTable_;
+   private JScrollPane scrollPane_;
+   private static final String HELP_FILE_NAME = "conf_preinit_page.html";
 
-        //  build list of devices to look for on the serial ports
-        for (int i = 0; i < devices.length; i++) {
-            for (int j = 0; j < devices[i].getNumberOfProperties(); j++) {
-                PropertyItem p = devices[i].getProperty(j);
-                if (p.name.compareTo(MMCoreJ.getG_Keyword_Port()) == 0) {
-                    any = true;
-                    break;
-                }
+   /**
+    * Create the panel
+    */
+   public PeripheralDevicesPreInitializationPropertiesPage(Preferences prefs) {
+      super();
+      title_ = "Edit peripheral device pre-initialization settings";
+      helpText_ = "The list of device properties which must be defined prior to initialization is shown above. ";
+      setLayout(null);
+      prefs_ = prefs;
+      setHelpFileName(HELP_FILE_NAME);
+      scrollPane_ = new JScrollPane();
+      scrollPane_.setBounds(10, 9, 421, 262);
+      add(scrollPane_);
+      propTable_ = new JTable();
+      propTable_.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+      propTable_.setAutoCreateColumnsFromModel(false);
+      scrollPane_.setViewportView(propTable_);
+      final PeripheralDevicesPreInitializationPropertiesPage thisPage = this;
+   }
+
+   private void rebuildTable() {
+      PropertyTableModel tm = new PropertyTableModel(this, model_, PropertyTableModel.PREINIT);
+      propTable_.setModel(tm);
+      PropertyValueCellEditor propValueEditor = new PropertyValueCellEditor();
+      PropertyValueCellRenderer propValueRenderer = new PropertyValueCellRenderer();
+      PropertyNameCellRenderer propNameRenderer = new PropertyNameCellRenderer();
+      if (propTable_.getColumnCount() == 0) {
+         TableColumn column;
+         column = new TableColumn(0, 200, propNameRenderer, null);
+         propTable_.addColumn(column);
+         column = new TableColumn(1, 200, propNameRenderer, null);
+         propTable_.addColumn(column);
+         column = new TableColumn(2, 200, propValueRenderer, propValueEditor);
+         propTable_.addColumn(column);
+      }
+      tm.fireTableStructureChanged();
+      tm.fireTableDataChanged();
+      boolean any = false;
+      Device devices[] = model_.getPeripheralDevices();
+
+      //  build list of devices to look for on the serial ports
+      for (int i = 0; i < devices.length; i++) {
+         for (int j = 0; j < devices[i].getNumberOfProperties(); j++) {
+            PropertyItem p = devices[i].getProperty(j);
+            if (p.name.compareTo(MMCoreJ.getG_Keyword_Port()) == 0) {
+               any = true;
+               break;
             }
-            if (any) {
-                break;
+         }
+         if (any) {
+            break;
+         }
+      }
+      propTable_.repaint();
+   }
+
+   public boolean enterPage(boolean fromNextPage) {
+      if (fromNextPage) {
+         return true;
+      }
+      rebuildTable();
+      return true;
+   }
+
+   public boolean exitPage(boolean toNextPage) {
+      try {
+         if (toNextPage) {
+
+            // set the properties of only the 'peripheral' devices!!
+            Device devices[] = model_.getPeripheralDevices();
+
+            PropertyTableModel ptm = (PropertyTableModel) propTable_.getModel();
+            for (int i = 0; i < ptm.getRowCount(); i++) {
+               Setting s = ptm.getSetting(i);
+               boolean found = false;
+               for (Device d : devices) {
+                  if (s.deviceName_.equals(d.getName())) {
+                     found = true;
+                     break;
+                  }
+               }
+               if (found) {
+                  core_.setProperty(s.deviceName_, s.propertyName_, s.propertyValue_);
+                  Device dev = model_.findDevice(s.deviceName_);
+                  PropertyItem prop = dev.findSetupProperty(s.propertyName_);
+                  if (prop == null) {
+                     model_.addSetupProperty(s.deviceName_, new PropertyItem(s.propertyName_, s.propertyValue_, true));
+                  }
+                  model_.setDeviceSetupProperty(s.deviceName_, s.propertyName_, s.propertyValue_);
+               }
             }
-        }
-        propTable_.repaint();
-    }
-    public boolean enterPage(boolean fromNextPage) {
-        if (fromNextPage) {
-            return true;
-        }
-        rebuildTable();
-        return true;
-    }
-    public boolean exitPage(boolean toNextPage) {
-        try {
-            if (toNextPage) {
 
-                // set the properties of only the 'peripheral' devices!!
-                Device devices[] = model_.getPeripheralDevices();
-
-                PropertyTableModel ptm = (PropertyTableModel) propTable_.getModel();
-                for (int i = 0; i< ptm.getRowCount(); i++) {
-                    Setting s = ptm.getSetting(i);
-                    boolean found = false;
-                    for(Device d : devices){
-                        if( s.deviceName_.equals(d.getName())){
-                            found = true;
-                            break;
-                        }
-                    }
-                    if( found){
-                        core_.setProperty(s.deviceName_, s.propertyName_, s.propertyValue_);
-                        Device dev = model_.findDevice(s.deviceName_);
-                        PropertyItem prop = dev.findSetupProperty(s.propertyName_);
-                        if (prop == null) {
-                            model_.addSetupProperty(s.deviceName_, new PropertyItem(s.propertyName_, s.propertyValue_, true));
-                        }
-                        model_.setDeviceSetupProperty(s.deviceName_, s.propertyName_, s.propertyValue_);
-                    }
-                }
-
-                            try {
-
-
+            try {
                core_.initializeAllDevices();
                // create the post-initialization properties
                model_.loadDeviceDataFromHardware(core_);
                model_.loadStateLabelsFromHardware(core_);
             } catch (Exception ex) {
                ReportingUtils.logError(ex);
-            } finally{
-;
             }
 
+         } else {
+            GUIUtils.preventDisplayAdapterChangeExceptions();
+         }
+      } catch (Exception e) {
+         handleException(e);
+         if (toNextPage) {
+            return false;
+         }
+      }
+      return true;
+   }
 
+   public void refresh() {
+      rebuildTable();
+   }
 
-                
+   public void loadSettings() {
+   }
 
+   public void saveSettings() {
+   }
 
-            } else {
-                GUIUtils.preventDisplayAdapterChangeExceptions();
-            }
-        } catch (Exception e) {
-            handleException(e);
-            if (toNextPage) {
-                return false;
-            }
-        }
-        return true;
-    }
-    public void refresh() {
-        rebuildTable();
-    }
-    public void loadSettings() {
-    }
-    public void saveSettings() {
-    }
-    public JTable GetPropertyTable() {
-        return propTable_;
-    }
+   public JTable GetPropertyTable() {
+      return propTable_;
+   }
 }
