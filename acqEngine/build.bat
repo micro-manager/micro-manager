47013728abc70623d76a5454c9945f2226efd261
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-07-10 19:50:47 +0000

    missing stageControl build.xml file added build files for clj components set to be less verbose
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7469 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -2,7 +2,7 @@ if exist classes rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
 "%JAVA_HOME%\bin\java" -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;../../3rdpartypublic/classext/bsh-2.0b4.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
-"%JAVA_HOME%\bin\jar" cvf MMAcqEngine.jar -C classes\ .    
+"%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C classes\ .    
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
 copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
--- plugins/DataBrowser/build.bat
+++ plugins/DataBrowser/build.bat
@@ -2,5 +2,5 @@ if exist classes rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
 "%JAVA_HOME%\bin\java" -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/clojure-contrib.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_Win32 -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
-"%JAVA_HOME%\bin\jar" cvf DataBrowser.jar -C classes\ .  
+"%JAVA_HOME%\bin\jar" cf DataBrowser.jar -C classes\ .  
 copy /Y DataBrowser.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
