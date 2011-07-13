658b86b9be023f5fe77c53eefa1b2563f9becfc0
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-07-13 23:32:56 +0000

    simplify
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7477 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DataBrowser/src/org/micromanager/browser/core.clj
+++ plugins/DataBrowser/src/org/micromanager/browser/core.clj
@@ -147,7 +147,7 @@
 (defn create-browser-table-model [headings]
   (proxy [AbstractTableModel] []
     (getRowCount [] (count @current-data))
-    (getColumnCount [] (if-let [cols (first @current-data)] (count cols) (count headings)))
+    (getColumnCount [] (count headings))
     (getValueAt [row column] (when (pos? (count @current-data))
                                (nth (nth @current-data row) column)))
     (getColumnName [column] (nth headings column))))
