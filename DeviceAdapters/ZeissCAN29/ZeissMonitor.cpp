b344138c62e3843f6a585fab301b1759b71e0520
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-08-02 20:57:11 +0000

    Trying to avoid the message: Shutter reported that it was neither opened or closed, so I am confused
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7554 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/ZeissCAN29/ZeissMonitor.cpp
+++ DeviceAdapters/ZeissCAN29/ZeissMonitor.cpp
@@ -135,7 +135,9 @@ void ZeissMonitoringThread::interpretMessage(unsigned char* message)
                memcpy(&position, message + 8, 2);
                position = ntohs((unsigned short) position);
             }
-            hub_.SetModelPosition(message[7], position);
+            // NS, 2011-08-03:
+            // Since the device is moving, we will only confuse ourselves by recording the actual moving position
+            // hub_.SetModelPosition(message[7], position);
             hub_.SetModelBusy(message[7], true);
          }
          else if (message[6] == 0x03) { // target position settled
