3372caf2be345fb55874a7be7c955b7962266b13
karl <karl@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-08-02 19:16:24 +0000

    Adding stub functions for DA sequence calls
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7553 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/ITC18/MMITC18.h
+++ DeviceAdapters/ITC18/MMITC18.h
@@ -235,7 +235,11 @@ public:
     int GetDASequenceMaxLength(long& nrEvents) const  {nrEvents = 0; return DEVICE_OK;}
     int StartDASequence() const {return DEVICE_OK;} 
     int StopDASequence() const {return DEVICE_OK;}
-    int LoadDASequence(std::vector<double> voltages) const {return DEVICE_OK;}
+    int LoadDASequence(std::vector<double> voltages) const {return DEVICE_OK;} 
+    int ClearDASequence() {return DEVICE_OK;}
+    int AddToDASequence(double voltage) {return DEVICE_OK;}
+    int SendDASequence() const {return DEVICE_OK;}
+
     
    
     
@@ -289,6 +293,10 @@ public:
      int StartDASequence() const {return DEVICE_OK;} 
      int StopDASequence() const {return DEVICE_OK;}
      int LoadDASequence(std::vector<double> voltages) const {return DEVICE_OK;}
+     int ClearDASequence() {return DEVICE_OK;}
+     int AddToDASequence(double voltage) {return DEVICE_OK;}
+     int SendDASequence() const {return DEVICE_OK;}
+
      
 private:
      int ITC18RunOnce(double &value);		
