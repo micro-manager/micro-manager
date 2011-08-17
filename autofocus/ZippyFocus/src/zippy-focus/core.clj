555d56149ec26baf5fe78b04e8490b407bf48a3e
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-08-17 17:31:09 +0000

    zippy focus
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7601 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ autofocus/ZippyFocus/src/zippy-focus/core.clj
@@ -0,0 +1,68 @@
+(ns zippy-focus.core
+  (:import [org.micromanager.api Autofocus]
+           [org.micromanager.utils PropertyItem])
+  (:use [org.micromanager.mm
+            :only (load-mm gui mmc core double-vector)]))
+
+(defn triggerable-focus? []
+  (core isStageSequenceable (core getFocusDevice)))
+
+(defonce search-params (atom nil))
+
+(defn make-sweep-vector [z search-params]
+  (let [half-range (/ (search-params :range) 2)
+        tol (search-params :tolerance)]
+    (vec
+      (range (- z half-range)
+             (+ z half-range tol)
+             tol))))
+  
+(defn more-images-expected []
+  (or (pos? (core getRemainingImageCount))
+      (core isSequenceRunning)))
+
+(defn pop-burst-image []
+  (while (and (. mmc isSequenceRunning)
+              (zero? (. mmc getRemainingImageCount)))
+    (Thread/sleep 1))
+  (core popNextImage))
+
+(defn grab-images []
+  (doall
+    (for [_ (range) :while (more-images-expected)]
+      (pop-burst-image))))
+
+(defn score-image [score-type img]
+  (condp = score-type
+    "Sum" (apply + img)
+    0))
+
+(defn acquire-images [z-drive trigger exposure]
+    (core setExposure exposure)
+    (core setPosition z-drive (first trigger))
+    (core loadStageSequence z-drive (double-vector trigger))
+    (core startStageSequence z-drive)
+    (core waitForDevice z-drive)
+    (core startSequenceAcquisition (count trigger) 0 false)
+    (grab-images))
+
+(defn run-autofocus [search-params]
+  (let [z-drive (core getFocusDevice)
+        current-z (core getPosition z-drive)
+        trigger (make-sweep-vector current-z search-params)
+        current-exposure (core getExposure)]
+    (let [images (acquire-images z-drive trigger (:exposure search-params))
+          scores (map #(score-image "Sum" %) images)
+          best-index (apply max-key #(nth scores %) (range (count images)))
+          best-z (nth trigger best-index)]
+      (core setPosition z-drive best-z)
+      (core setExposure current-exposure)
+      best-z)))
+           
+(defn run-test []
+  (reset! search-params
+          {:range 10
+           :tolerance 1.00
+           :score-type "Sum"
+           :exposure 10})
+  (run-autofocus @search-params))
