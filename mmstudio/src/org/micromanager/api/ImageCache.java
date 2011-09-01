f10a5cba54ed0a876b839e43b7c0d047ea44589b
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-09-01 06:58:53 +0000

    added ImageCache.java
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7653 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/api/ImageCache.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.micromanager.api;
+
+import java.util.Set;
+import mmcorej.TaggedImage;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author arthur
+ */
+public interface ImageCache extends TaggedImageStorage {
+   void addImageStorageListener(ImageCacheListener l);
+   Set<String> getChangingKeys();
+   String getComment();
+   String getDiskLocation();
+   JSONObject getDisplayAndComments();
+   TaggedImage getImage(int channel, int slice, int frame, int position);
+   ImageCacheListener[] getImageStorageListeners();
+   JSONObject getLastImageTags();
+   JSONObject getSummaryMetadata();
+   boolean isFinished();
+   int lastAcquiredFrame();
+   void removeImageStorageListener(ImageCacheListener l);
+   void saveAs(TaggedImageStorage newImageFileManager);
+   void setComment(String text);
+   void setDisplayAndComments(JSONObject settings);
+   void setSummaryMetadata(JSONObject tags);
+   void setImageComment(String comment, JSONObject tags);
+   String getImageComment(JSONObject tags);
+}
