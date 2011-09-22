038c143971ba15d21e608a3c5fb142a921791cc8
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-09-22 03:09:49 +0000

    a utility for testing if the micromanager build succeeded
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7771 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ buildscripts/buildcheck/project.clj
@@ -0,0 +1,5 @@
+(defproject buildcheck "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [clj-http "0.2.0"]
+                 [local-file "0.0.6"]])
---
+++ buildscripts/buildcheck/src/buildcheck/core.clj
@@ -0,0 +1,56 @@
+(ns buildcheck.core
+  (:import (java.io File))
+  (:use [local-file :only (file*)])
+  (:require [clj-http.client :as client]))
+
+(def micromanager (file* "../../.."))
+
+(def MS-PER-HOUR (* 60 60 1000))
+
+(defn result-file [bits mode]
+  (File. micromanager (str "/result" bits (name mode) ".txt")))
+
+(defn visual-studio-errors [result-text]
+  (map first
+       (re-seq #"\n[^\n]+\b([1-9]|[0-9][1-9]|[0-9][0-9][1-9])\b\serror\(s\)[^\n]+\n" result-text)))
+
+;(defn jar-errors [result-text]
+;  (re-seq #"\nBUILD FAILED[^\n]+\n" result-text))
+
+(defn old-dlls [dir time-limit-hours]
+  (let [now (System/currentTimeMillis)
+        before (- now (* time-limit-hours MS-PER-HOUR))]
+    (filter
+      (fn [file]
+        (let [file-name (.getName file)]
+          (and (.endsWith file-name ".dll")
+               (.startsWith file-name "mmgr_dal")
+               (< (.lastModified file) before))))
+      (.listFiles dir))))
+
+(defn report-build-errors [bits mode]
+  (let [result-txt (slurp (result-file bits mode))
+        vs-errors (visual-studio-errors result-txt)
+        outdated-dlls (old-dlls (File. micromanager
+                                       (condp = bits
+                                         32 "bin_Win32"
+                                         64 "bin_x64")) 24)]
+    (when-not (or (empty? vs-errors) (empty? outdated-dlls))
+      (println (str "MICROMANAGER " bits "-bit "
+                    ({:inc "INCREMENTAL" :full "FULL"} mode)
+                    " BUILD ERROR REPORT"))
+      (println "\nVisual Studio reported errors:")
+      (dorun (map println vs-errors))
+      (println "\nOutdated device adapter DLLs:")
+      (dorun (map #(println (.getAbsolutePath %)) outdated-dlls)))
+    (println "\n\n")))
+
+(defn send-full-report [mode]
+  (let [report
+        (with-out-str
+          (report-build-errors 32 mode)
+          (report-build-errors 64 mode))]
+    (client/post "http://valelab.ucsf.edu/~MM/upload_corelog.php"
+                 {:body report})))
+    
+ 
