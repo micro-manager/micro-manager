84e0a4da64a606b3f18d00691bbfc38c3ee289df
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-09-26 18:28:24 +0000

    report an acquisition error
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7789 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/src/org/micromanager/acq_engine.clj
+++ acqEngine/src/org/micromanager/acq_engine.clj
@@ -33,6 +33,7 @@
            [org.micromanager.acquisition AcquisitionWrapperEngine LiveAcq TaggedImageQueue
                                          ProcessorStack SequenceSettings MMImageCache
                                          TaggedImageStorageRam VirtualAcquisitionDisplay]
+           [org.micromanager.utils ReportingUtils]
            [mmcorej TaggedImage Configuration Metadata]
            [java.util.prefs Preferences]
            [java.net InetAddress]
@@ -480,16 +481,19 @@
     (await-resume)))
 
 (defn run-acquisition [this settings out-queue]
-  (def acq-settings settings)
-  (prepare-state this)
-  (binding [state (.state this)]
-    (def last-state state) ; for debugging
-    (let [acq-seq (generate-acq-sequence settings @attached-runnables)]
-       (def acq-sequence acq-seq)
-      (execute (mapcat #(make-event-fns % out-queue) acq-seq))
-      (.put out-queue TaggedImageQueue/POISON)
-      (cleanup)
-     )))
+  (try
+    (def acq-settings settings)
+    (prepare-state this)
+    (binding [state (.state this)]
+      (def last-state state) ; for debugging
+      (let [acq-seq (generate-acq-sequence settings @attached-runnables)]
+        (def acq-sequence acq-seq)
+        (execute (mapcat #(make-event-fns % out-queue) acq-seq))
+        (.put out-queue TaggedImageQueue/POISON)
+        (cleanup)
+        ))
+    (catch Throwable t (do (ReportingUtils/showError t "Acquisition failed.")
+                           (cleanup)))))
 
 ;; generic metadata
 
