3ca6a5cda1982fb527724bd6871c13270a17c2e4
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-10-10 17:44:07 +0000

    Report java compiler errors
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7866 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- buildscripts/buildcheck/src/buildcheck/core.clj
+++ buildscripts/buildcheck/src/buildcheck/core.clj
@@ -15,8 +15,9 @@
   (map first
        (re-seq #"\n[^\n]+\b([1-9]|[0-9][1-9]|[0-9][0-9][1-9])\b\serror\(s\)[^\n]+\n" result-text)))
 
-;(defn jar-errors [result-text]
-;  (re-seq #"\nBUILD FAILED[^\n]+\n" result-text))
+(defn javac-errors [result-text]
+  (map first
+    (re-seq #"\[javac\]\s\b([1-9]|[0-9][1-9]|[0-9][0-9][1-9])\b\serrors?" result-text)))
 
 (defn old-dlls [dir time-limit-hours]
   (let [now (System/currentTimeMillis)
@@ -30,37 +31,41 @@
       (.listFiles dir))))
 
 (defn report-build-errors [bits mode]
-  (let [result-txt (slurp (result-file bits mode))
+  (let [f (result-file bits mode)
+        result-txt (slurp f)
         vs-errors (visual-studio-errors result-txt)
         outdated-dlls (old-dlls (File. micromanager
                                        (condp = bits
                                          32 "bin_Win32"
-                                         64 "bin_x64")) 24)]
-    (when-not (and (empty? vs-errors) (empty? outdated-dlls))
+                                         64 "bin_x64")) 24)
+        javac-errs (javac-errors result-txt)]
+    (when-not (and (empty? vs-errors) (empty? outdated-dlls) (empty? javac-errs))
       (println (str "MICROMANAGER " bits "-bit "
                     ({:inc "INCREMENTAL" :full "FULL"} mode)
                     " BUILD ERROR REPORT"))
+      (println (str "For a full report, see " (.getAbsolutePath f)))
       (println "\nVisual Studio reported errors:")
-      (dorun (map println vs-errors))
+      (if-not (empty? vs-errors) (dorun (map println vs-errors)) (println "None."))
       (println "\nOutdated device adapter DLLs:")
-      (dorun (map #(println (.getAbsolutePath %)) outdated-dlls))
+      (if-not (empty? outdated-dlls) (dorun (map #(println (.getAbsolutePath %)) outdated-dlls)) (println "None."))
+      (println "\nErrors reported by java compiler:")
+      (if-not (empty? javac-errs) (dorun (map println javac-errs)) (println "None."))
       (println "\n\n"))))
 
-(defn send-full-report [mode send?]
+(defn make-full-report [mode send?]
   (let [report
         (with-out-str
           (report-build-errors 32 mode)
           (report-build-errors 64 mode))]
-    (when send
+    (when send?
       (when-not (empty? report)
         (with-session
           "mmbuilderrors@gmail.com" (slurp "C:\\pass.txt") "smtp.gmail.com" 465 "smtp" true
-          (send-email (text-email ["info@micro-manager.org"] "mm build errors" report)))  
-        (println report)
-        ))))
+          (send-email (text-email ["info@micro-manager.org"] "mm build errors" report)))))  
+    (println report)))
 
 (defn -main [mode]
-  (send-full-report (get {"inc" :inc "full" :full} mode) true))
+  (make-full-report (get {"inc" :inc "full" :full} mode) true))
 
     
  
