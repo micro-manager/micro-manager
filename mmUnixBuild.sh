93fcdf8a3539702fe6772d2f577b8871495348dc
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-10-11 00:04:01 +0000

    Add DeviceKit to the mmUnixBuild script
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7878 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmUnixBuild.sh
+++ mmUnixBuild.sh
@@ -15,6 +15,12 @@ libtoolize --force
 automake --foreign --add-missing
 autoconf -I ../m4
 cd ..
+cd DeviceKit
+aclocal -I ../m4
+libtoolize --force
+automake --foreign --add-missing
+autoconf -I ../m4
+cd ..
 if test -r "SecretDeviceAdapters/configure.in" ;  then
    cd SecretDeviceAdapters
    aclocal -I ../m4
