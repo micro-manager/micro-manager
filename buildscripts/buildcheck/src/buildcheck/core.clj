b18334e4d66b8e35a89c1cfc2b242035d41d2aaf
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-10-14 22:22:53 +0000

    improve build check to see if jars are up to date
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@7918 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- buildscripts/buildcheck/src/buildcheck/core.clj
+++ buildscripts/buildcheck/src/buildcheck/core.clj
@@ -19,16 +19,27 @@
   (map first
     (re-seq #"\[javac\]\s\b([1-9]|[0-9][1-9]|[0-9][0-9][1-9])\b\serrors?" result-text)))
 
-(defn old-dlls [dir time-limit-hours]
+(defn old-files [files time-limit-hours]
   (let [now (System/currentTimeMillis)
         before (- now (* time-limit-hours MS-PER-HOUR))]
+    (filter #(< (.lastModified %) before) files)))
+    
+(defn old-dlls [dir time-limit-hours]
+  (old-files
     (filter
       (fn [file]
         (let [file-name (.getName file)]
           (and (.endsWith file-name ".dll")
-               (.startsWith file-name "mmgr_dal")
-               (< (.lastModified file) before))))
-      (.listFiles dir))))
+               (.startsWith file-name "mmgr_dal"))))
+      (.listFiles dir))
+    time-limit-hours))
+
+(defn old-jars [dir time-limit-hours]
+  (old-files
+    (filter
+      #(.. % getName (endsWith ".jar"))
+      (file-seq (File. dir)))
+    time-limit-hours))
 
 (defn report-build-errors [bits mode]
   (let [f (result-file bits mode)
@@ -38,31 +49,37 @@
                                        (condp = bits
                                          32 "bin_Win32"
                                          64 "bin_x64")) 24)
-        javac-errs (javac-errors result-txt)]
-    (when-not (and (empty? vs-errors) (empty? outdated-dlls) (empty? javac-errs))
-      (println (str "MICROMANAGER " bits "-bit "
+        javac-errs (javac-errors result-txt)
+        outdated-jars (old-jars (File. micromanager "Install_AllPlatforms") 24)]
+    (when-not (and (empty? vs-errors) (empty? outdated-dlls)
+                   (empty? javac-errs) (empty? outdated-jars))
+      (println (str "\n\nMICROMANAGER " bits "-bit "
                     ({:inc "INCREMENTAL" :full "FULL"} mode)
                     " BUILD ERROR REPORT"))
       (println (str "For a full report, see " (.getAbsolutePath f)))
       (println "\nVisual Studio reported errors:")
       (if-not (empty? vs-errors) (dorun (map println vs-errors)) (println "None."))
       (println "\nOutdated device adapter DLLs:")
-      (if-not (empty? outdated-dlls) (dorun (map #(println (.getAbsolutePath %)) outdated-dlls)) (println "None."))
+      (if-not (empty? outdated-dlls) (dorun (map #(println (.getName %)) outdated-dlls)) (println "None."))
       (println "\nErrors reported by java compiler:")
       (if-not (empty? javac-errs) (dorun (map println javac-errs)) (println "None."))
-      (println "\n\n"))))
+      (println "\nOutdated jar files:")
+      (if-not (empty? outdated-jars) (dorun (map #(println (.getName %)) outdated-jars)) (println "None."))
+      )))
 
 (defn make-full-report [mode send?]
   (let [report
         (with-out-str
           (report-build-errors 32 mode)
           (report-build-errors 64 mode))]
-    (when send?
-      (when-not (empty? report)
-        (with-session
-          "mmbuilderrors@gmail.com" (slurp "C:\\pass.txt") "smtp.gmail.com" 465 "smtp" true
-          (send-email (text-email ["info@micro-manager.org"] "mm build errors" report)))))  
-    (println report)))
+    (if-not (empty? report)
+      (do 
+        (when send?
+          (with-session
+            "mmbuilderrors@gmail.com" (slurp "C:\\pass.txt") "smtp.gmail.com" 465 "smtp" true
+            (send-email (text-email ["info@micro-manager.org"] "mm build errors" report))))
+        (println report))
+      (println "Nothing to report."))))
 
 (defn -main [mode]
   (make-full-report (get {"inc" :inc "full" :full} mode) true))
