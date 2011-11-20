d272de7852760b4592110eda5aa33af891c6ff6d
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-11-20 02:57:41 +0000

    Changed Stage Control plugin not to wait for device to become non-busy as requested by Stefan.  Will see if this causes problems
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8159 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/StageControl/src/org/micromanager/stagecontrol/StageControlFrame.java
+++ plugins/StageControl/src/org/micromanager/stagecontrol/StageControlFrame.java
@@ -1,12 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /*
  * StageControlFrame.java
  *
  * Created on Aug 19, 2010, 10:04:49 PM
+ * Nico Stuurman, copyright UCSF, 2010
+ * BSD license
  */
 
 package org.micromanager.stagecontrol;
@@ -19,7 +17,6 @@ import java.text.ParseException;
 import java.util.prefs.Preferences;
 
 import org.micromanager.api.ScriptInterface;
-import org.micromanager.utils.MMScriptException;
 
 /**
  *
@@ -558,100 +555,71 @@ public class StageControlFrame extends javax.swing.JFrame {
       pack();
    }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(-smallMovement_, 0.0);
-       } catch(MMScriptException e) {
+   private void setRelativeXYStagePosition(double x, double y) {
+      try {
+         if (!core_.deviceBusy(core_.getXYStageDevice()))
+            core_.setRelativeXYPosition(core_.getXYStageDevice(), x, y);
+       } catch(Exception e) {
           gui_.logError(e);
        }
+   }
+   
+    private void setRelativeStagePosition(double z)
+    {
+       try {
+          if (!core_.deviceBusy(core_.getFocusDevice()))
+             core_.setRelativePosition(core_.getFocusDevice(), z);
+       } catch (Exception e) {
+          gui_.logError(e);
+       }
+    }
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       setRelativeXYStagePosition(-smallMovement_, 0.0);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(-largeMovement_, 0.0);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(-largeMovement_, 0.0);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(-mediumMovement_, 0.0);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(-mediumMovement_, 0.0);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(smallMovement_, 0.0);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+      setRelativeXYStagePosition(smallMovement_, 0.0);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(largeMovement_, 0.0);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(largeMovement_, 0.0);
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(mediumMovement_, 0.0);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(mediumMovement_, 0.0);
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(0.0, -smallMovement_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(0.0, -smallMovement_);
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(0.0, -largeMovement_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(0.0, -largeMovement_);
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(0.0, -mediumMovement_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(0.0, -mediumMovement_);
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(0.0, smallMovement_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(0.0, smallMovement_);
     }//GEN-LAST:event_jButton10ActionPerformed
 
     private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(0.0, mediumMovement_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(0.0, mediumMovement_);
     }//GEN-LAST:event_jButton11ActionPerformed
 
     private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton12ActionPerformed
-       try {
-          gui_.setRelativeXYStagePosition(0.0, largeMovement_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeXYStagePosition(0.0, largeMovement_);
     }//GEN-LAST:event_jButton12ActionPerformed
 
     private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
@@ -731,35 +699,19 @@ public class StageControlFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_focusLostHandlerJTF3
 
     private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton16ActionPerformed
-       try {
-          gui_.setRelativeStagePosition(mediumMovementZ_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeStagePosition(mediumMovementZ_);
     }//GEN-LAST:event_jButton16ActionPerformed
 
     private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
-       try {
-          gui_.setRelativeStagePosition(smallMovementZ_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeStagePosition(smallMovementZ_);
     }//GEN-LAST:event_jButton17ActionPerformed
 
     private void jButton18ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton18ActionPerformed
-       try {
-          gui_.setRelativeStagePosition(-smallMovementZ_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeStagePosition(-smallMovementZ_);
     }//GEN-LAST:event_jButton18ActionPerformed
 
     private void jButton19ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton19ActionPerformed
-       try {
-          gui_.setRelativeStagePosition(-mediumMovementZ_);
-       } catch(MMScriptException e) {
-          gui_.logError(e);
-       }
+       setRelativeStagePosition(-mediumMovementZ_);
     }//GEN-LAST:event_jButton19ActionPerformed
 
     private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
