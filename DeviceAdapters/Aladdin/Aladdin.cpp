0d03ae855c0161334493fd201e1a273987858704
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-11-29 03:40:25 +0000

    Aladdin adapter from Kurt Thorn
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8211 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ DeviceAdapters/Aladdin/Aladdin.cpp
@@ -0,0 +1,589 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          Aladdin.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   PrecisExcite controller adapter
+// COPYRIGHT:     University of California, San Francisco, 2009
+//
+// AUTHOR:        Arthur Edelstein, arthuredelstein@gmail.com, 3/17/2009
+//
+// LICENSE:       This file is distributed under the BSD license.
+//                License text is included with the source distribution.
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
+// CVS:           
+//
+
+
+
+#ifdef WIN32
+   #include <windows.h>
+   #define snprintf _snprintf 
+#endif
+
+
+#include "../../MMDevice/MMDevice.h"
+#include "Aladdin.h"
+#include <string>
+#include <math.h>
+#include "../../MMDevice/ModuleInterface.h"
+#include "../../MMDevice/DeviceUtils.h"
+#include <sstream>
+
+// Controller
+const char* g_ControllerName = "Aladdin";
+const char* g_Keyword_Infuse = "Infuse";
+const char * g_Keyword_Withdraw = "Withdraw";
+const char * carriage_return = "\r";
+const char * line_feed = "\n";
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Exported MMDevice API
+///////////////////////////////////////////////////////////////////////////////
+MODULE_API void InitializeModuleData()
+{
+   AddAvailableDeviceName(g_ControllerName, "Aladdin Syringe Pump");
+   
+}
+
+MODULE_API MM::Device* CreateDevice(const char* deviceName)
+{
+   if (deviceName == 0)
+      return 0;
+
+   if (strcmp(deviceName, g_ControllerName) == 0)
+   {
+      // create AladdinController
+      AladdinController* pController = new AladdinController(g_ControllerName);
+      return pController;
+   }
+
+   return 0;
+}
+
+MODULE_API void DeleteDevice(MM::Device* pDevice)
+{
+   delete pDevice;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// AladdinController implementation
+// ~~~~~~~~~~~~~~~~~~~~
+
+AladdinController::AladdinController(const char* name) :
+   initialized_(false), 
+   volume_(0),
+   name_(name), 
+   busy_(false),
+   error_(0),
+   changedTime_(0.0)
+{
+   assert(strlen(name) < (unsigned int) MM::MaxStrLength);
+
+   InitializeDefaultErrorMessages();
+
+   // create pre-initialization properties
+   // ------------------------------------
+
+   // Name
+   CreateProperty(MM::g_Keyword_Name, name_.c_str(), MM::String, true);
+
+   // Description
+   CreateProperty(MM::g_Keyword_Description, "Aladdin Syringe Pump", MM::String, true);
+
+   // Port
+   CPropertyAction* pAct = new CPropertyAction (this, &AladdinController::OnPort);
+   CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
+
+   EnableDelay(); // signals that the delay setting will be used
+   UpdateStatus();
+}
+
+AladdinController::~AladdinController()
+{
+   Shutdown();
+}
+
+bool AladdinController::Busy()
+{
+   MM::MMTime interval = GetCurrentMMTime() - changedTime_;
+   MM::MMTime delay(GetDelayMs()*1000.0);
+   if (interval < delay)
+      return true;
+   else
+      return false;
+}
+
+void AladdinController::GetName(char* name) const
+{
+   assert(name_.length() < CDeviceUtils::GetMaxStringLength());
+   CDeviceUtils::CopyLimitedString(name, name_.c_str());
+}
+
+
+int AladdinController::Initialize()
+{
+   this->LogMessage("AladdinController::Initialize()");
+
+   GeneratePropertyVolume();
+   GeneratePropertyDiameter();
+   GeneratePropertyRate();
+   GeneratePropertyDirection();
+   GeneratePropertyRun();
+   
+   CreateDefaultProgram();
+
+   initialized_ = true;
+   return HandleErrors();
+
+}
+
+/////////////////////////////////////////////
+// Property Generators
+/////////////////////////////////////////////
+
+void AladdinController::GeneratePropertyVolume()
+{
+   CPropertyAction* pAct = new CPropertyAction (this, &AladdinController::OnVolume);
+   CreateProperty("Volume-uL", "0", MM::Float, false, pAct);
+}
+
+void AladdinController::GeneratePropertyDiameter()
+{
+   CPropertyAction* pAct = new CPropertyAction (this, &AladdinController::OnDiameter);
+   CreateProperty("SyringeDiameter", "4.699", MM::Float, false, pAct);
+}
+
+void AladdinController::GeneratePropertyRate()
+{
+   CPropertyAction* pAct = new CPropertyAction (this, &AladdinController::OnRate);
+   CreateProperty("FlowRate-uL/min", "0", MM::Float, false, pAct);
+}
+
+void AladdinController::GeneratePropertyDirection()
+{
+   CPropertyAction* pAct = new CPropertyAction (this, &AladdinController::OnDirection);
+   CreateProperty("Direction", g_Keyword_Infuse, MM::String, false, pAct);
+   AddAllowedValue("Direction", g_Keyword_Infuse);  
+   AddAllowedValue("Direction", g_Keyword_Withdraw);  
+}
+
+void AladdinController::GeneratePropertyRun()
+{
+   CPropertyAction* pAct = new CPropertyAction (this, &AladdinController::OnRun);
+   CreateProperty("Run", "0", MM::Integer, false, pAct);
+   AddAllowedValue("Run", "0");  
+   AddAllowedValue("Run", "1");  
+}
+
+int AladdinController::Shutdown()
+{
+   if (initialized_)
+   {
+      initialized_ = false;
+   }
+   return HandleErrors();
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// String utilities
+///////////////////////////////////////////////////////////////////////////////
+
+
+void AladdinController::StripString(string& StringToModify)
+{
+   if(StringToModify.empty()) return;
+
+   size_t startIndex = StringToModify.find_first_not_of(" ");
+   size_t endIndex = StringToModify.find_last_not_of(" ");
+   string tempString = StringToModify;
+   StringToModify.erase();
+
+   StringToModify = tempString.substr(startIndex, (endIndex-startIndex+ 1) );
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+
+
+int AladdinController::OnPort(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(port_.c_str());
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      if (initialized_)
+      {
+         // revert
+         pProp->Set(port_.c_str());
+         return ERR_PORT_CHANGE_FORBIDDEN;
+      }
+
+      pProp->Get(port_);
+   }
+
+   return HandleErrors();
+}
+
+int AladdinController::OnVolume(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+
+   double volume;
+   if (eAct == MM::BeforeGet)
+   {
+      GetVolume(volume);
+      pProp->Set(volume);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(volume);
+      SetVolume(volume);
+   }   
+
+   return HandleErrors();
+}
+
+int AladdinController::OnDiameter(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+
+   double diameter;
+   if (eAct == MM::BeforeGet)
+   {
+      GetDiameter(diameter);
+      pProp->Set(diameter);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(diameter);
+      SetDiameter(diameter);
+   }   
+
+   return HandleErrors();
+}
+
+int AladdinController::OnRate(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+
+   double rate;
+   if (eAct == MM::BeforeGet)
+   {
+      GetRate(rate);
+      pProp->Set(rate);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(rate);
+      SetRate(rate);
+   }   
+
+   return HandleErrors();
+}
+
+int AladdinController::OnDirection(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+
+   string dir;
+   if (eAct == MM::BeforeGet)
+   {
+      GetDirection(dir);
+      pProp->Set(dir.c_str());
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(dir);
+      SetDirection(dir);
+   }   
+
+   return HandleErrors();
+}
+
+int AladdinController::OnRun(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+
+   long run;
+   if (eAct == MM::BeforeGet)
+   {
+      GetRun(run);
+      pProp->Set(run);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      pProp->Get(run);
+      SetRun(run);
+   }   
+
+   return HandleErrors();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Utility methods
+///////////////////////////////////////////////////////////////////////////////
+
+
+void AladdinController::CreateDefaultProgram()
+//Set up the default program that we can run to do a simple infusion
+{
+   double rate;
+   GetRate(rate); //Get current rate and set it in the program
+   Purge();
+   Send("PHN1"); //Phase 1
+   ReceiveOneLine();
+   stringstream msg;
+   msg << "FUN RAT" << rate << "UM"; //set pumping rate in uL/min 
+   Purge();
+   Send(msg.str());
+   ReceiveOneLine();
+   Purge();
+   Send("PHN2"); //Phase 2
+   ReceiveOneLine();
+   Purge();
+   Send("FUN STP"); //Stop
+   ReceiveOneLine();
+   Send("PHN1"); //Set back to Phase 1
+   ReceiveOneLine();
+}
+
+void AladdinController::SetVolume(double volume)
+{
+   volume = volume/1000; //pump volume is always set in mL
+   stringstream msg;
+   msg << "VOL" << volume;
+   Purge();
+   Send(msg.str());
+   ReceiveOneLine();
+
+}
+
+void AladdinController::GetVolume(double& volume)
+{
+   stringstream msg;
+   string ans;
+   string units;
+   msg << "VOL";
+   Purge();
+   Send(msg.str());
+   buf_string_ = "";
+   GetSerialAnswer(port_.c_str(), "L", buf_string_); //Volume string ends in UL or ML
+
+   if (! buf_string_.empty())
+       {
+         volume = atof(buf_string_.substr(4,5).c_str());
+		 units = buf_string_.substr(9,1).c_str();
+		 if (units.compare("M") == 0)
+			 volume = volume*1000; //return volume in uL
+      }
+
+}
+
+void AladdinController::SetDiameter(double diameter)
+{
+   stringstream msg;
+   msg << "DIA" << diameter;
+   Purge();
+   Send(msg.str());
+   ReceiveOneLine();
+
+}
+
+void AladdinController::GetDiameter(double& diameter)
+{
+   stringstream msg;
+   msg << "DIA";
+   Purge();
+   Send(msg.str());
+   string answer = "";
+   GetUnterminatedSerialAnswer(answer, 9); //awaiting 9 response characters
+   if (! answer.empty())
+       {
+         diameter = atof(answer.substr(4,5).c_str());
+      }
+
+}
+
+void AladdinController::SetRate(double rate)
+{
+   stringstream msg;
+   msg << "RAT" << rate <<"UM"; //Always set rate in uL/min
+   Purge();
+   Send(msg.str());
+   ReceiveOneLine();
+
+}
+
+void AladdinController::GetRate(double& rate)
+{
+   stringstream msg;
+   msg << "RAT";
+   Purge();
+   Send(msg.str());
+   string answer = "";
+   string units;
+   GetUnterminatedSerialAnswer(answer, 10); //awaiting 9 response characters
+   if (! answer.empty())
+       {
+         rate = atof(answer.substr(4,5).c_str());
+		 units = answer.substr(9,2).c_str();
+      }
+   //units is UM uL/min, UH uL/hr, MM mL/min, MH, mL/hr
+   //we want to return it in uL/min
+   if (units.compare("UH") == 0)
+			 rate = rate/60; 
+   if (units.compare("MH") == 0)
+			 rate = (rate*1000)/60;
+   if (units.compare("MM") == 0)
+			 rate = rate*1000;
+}
+
+void AladdinController::SetDirection(string direction)
+{
+   stringstream msg;
+   if (direction.compare(g_Keyword_Infuse) == 0)
+   {
+	   msg << "DIR INF";
+	   Purge();
+	   Send(msg.str());
+	   ReceiveOneLine();
+   }
+   else if (direction.compare(g_Keyword_Withdraw) == 0)
+   {
+	   msg << "DIR WDR";
+	   Purge();
+	   Send(msg.str());
+	   ReceiveOneLine();
+   }
+
+}
+
+void AladdinController::GetDirection(string& direction)
+{
+   stringstream msg;
+   msg << "DIR";
+   Purge();
+   Send(msg.str());
+   string answer = "";
+   GetUnterminatedSerialAnswer(answer, 6); //awaiting 9 response characters
+   if (! answer.empty())
+       {
+         if (answer.substr(4,3).compare("INF") == 0)
+			 direction = g_Keyword_Infuse;
+		 else if (answer.substr(4,3).compare("WDR") == 0)
+			 direction = g_Keyword_Withdraw;
+      }
+
+}
+
+void AladdinController::GetRun(long& run)
+{
+   stringstream msg;
+   msg << ""; //A CR will get status
+   Purge();
+   Send(msg.str());
+   string answer = "";
+   string status = "";
+   GetUnterminatedSerialAnswer(answer, 4); //awaiting 9 response characters
+   if (! answer.empty())
+       {
+         if(answer.substr(3,1).compare("I") == 0 || answer.substr(3,1).compare("W") == 0)
+			run = 1;
+		 else run = 0;
+      }
+
+}
+
+void AladdinController::SetRun(long run)
+{
+   stringstream msg;
+   if (run == 0) //Stop pumping
+   {
+     msg << "STP";
+     Purge();
+     Send(msg.str());
+     ReceiveOneLine();
+   }
+   else if (run == 1) //Start pumping
+   {
+     msg << "RUN";
+     Purge();
+     Send(msg.str());
+     ReceiveOneLine();
+   }
+
+}
+
+int AladdinController::HandleErrors()
+{
+   int lastError = error_;
+   error_ = 0;
+   return lastError;
+}
+
+
+
+/////////////////////////////////////
+//  Communications
+/////////////////////////////////////
+
+
+void AladdinController::Send(string cmd)
+{
+   int ret = SendSerialCommand(port_.c_str(), cmd.c_str(), carriage_return);
+   if (ret!=DEVICE_OK)
+      error_ = DEVICE_SERIAL_COMMAND_FAILED;
+}
+
+
+void AladdinController::ReceiveOneLine()
+{
+   buf_string_ = "";
+   GetSerialAnswer(port_.c_str(), line_feed, buf_string_);
+
+}
+
+void AladdinController::Purge()
+{
+   int ret = PurgeComPort(port_.c_str());
+   if (ret!=0)
+      error_ = DEVICE_SERIAL_COMMAND_FAILED;
+}
+
+void AladdinController::GetUnterminatedSerialAnswer (std::string& ans, unsigned int count)
+{
+	const unsigned long MAX_BUFLEN = 20; 
+	char buf[MAX_BUFLEN];
+	unsigned char* cc = new unsigned char[MAX_BUFLEN];
+	int ret = 0;
+    unsigned long read = 0;
+    MM::MMTime startTime = GetCurrentMMTime();
+    // Time out of 1 sec.  Make this a property?
+    MM::MMTime timeOut(1000000);
+	unsigned long offset = 0;
+	while((offset < count) && ( (GetCurrentMMTime() - startTime) < timeOut))
+	{
+		ret = ReadFromComPort(port_.c_str(), cc, MAX_BUFLEN,read);
+		for(unsigned int i=0; i<read; i++)      
+		{
+			buf[offset+i] = cc[i];
+		}
+		offset += read;
+	}
+	ans = buf;
+}
---
+++ DeviceAdapters/Aladdin/Aladdin.h
@@ -0,0 +1,117 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          Aladdin.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   PrecisExcite controller adapter
+// COPYRIGHT:     University of California, San Francisco, 2009
+// LICENSE:       This file is distributed under the BSD license.
+//                License text is included with the source distribution.
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
+// AUTHOR:        Arthur Edelstein, arthuredelstein@gmail.com, 3/17/2009
+//
+//
+
+#ifndef _ALADDIN_H_
+#define _ALADDIN_H_
+
+#include "../../MMDevice/MMDevice.h"
+#include "../../MMDevice/DeviceBase.h"
+#include "../../MMDevice/DeviceUtils.h"
+#include <string>
+//#include <iostream>
+#include <vector>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////
+// Error codes
+//
+//#define ERR_UNKNOWN_POSITION         10002
+#define ERR_PORT_CHANGE_FORBIDDEN    10004
+
+//enum TriggerType {OFF, RISING_EDGES, FALLING_EDGES, BOTH_EDGES, FOLLOW_PULSE};
+//string TriggerLabels[] = {"Off","RisingEdges","FallingEdges","BothEdges","FollowPulse"};
+//char TriggerCmd[] = {'Z', '+', '-', '*', 'X'};
+
+class AladdinController : public CGenericBase<AladdinController>
+{
+public:
+   AladdinController(const char* name);
+   ~AladdinController();
+  
+   // MMDevice API
+   // ------------
+   int Initialize();
+   int Shutdown();
+  
+   void GetName(char* pszName) const;
+   bool Busy();
+   
+   // action interface
+   // ----------------
+   int OnPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnVolume(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnDiameter(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnRate(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnDirection(MM::PropertyBase* pProp, MM::ActionType eAct);
+   int OnRun(MM::PropertyBase* pProp, MM::ActionType eAct);
+
+private:
+
+   double volume_;
+   double diameter_;
+   int error_;
+
+   bool initialized_;
+   std::string name_;
+   std::string port_;
+   unsigned char buf_[1000];
+   string buf_string_;
+   vector<string> buf_tokens_;
+   unsigned long buf_bytes_;
+   long armState_;
+
+   bool busy_;
+   double answerTimeoutMs_;
+   MM::MMTime changedTime_;
+
+   void SetVolume(double volume);
+   void GetVolume(double& volume);
+   void SetDiameter(double diameter);
+   void GetDiameter(double& diameter);
+   void SetRate(double rate);
+   void GetRate(double& rate);
+   void SetRun(long run);
+   void GetRun(long& run);
+   void GetDirection (string& direction);
+   void SetDirection (string direction);
+   void GeneratePropertyVolume();
+   void GeneratePropertyDiameter();
+   void GeneratePropertyRate();
+   void GeneratePropertyDirection();
+   void GeneratePropertyRun();
+   void CreateDefaultProgram();
+   void StripString(string& StringToModify);
+   void Send(string cmd);
+   void ReceiveOneLine();
+   void Purge();
+   void GetUnterminatedSerialAnswer (std::string& ans, unsigned int count);
+   int HandleErrors();
+
+   static const int RCV_BUF_LENGTH = 1024;
+   char rcvBuf_[RCV_BUF_LENGTH];
+   
+
+   AladdinController& operator=(AladdinController& /*rhs*/) {assert(false); return *this;}
+};
+
+
+#endif // _ALADDIN_H_
