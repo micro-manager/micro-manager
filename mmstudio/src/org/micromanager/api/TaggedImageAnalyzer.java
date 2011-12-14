3a4d4218588b8878d07c9582fbbe4ee0cb6348ba
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-12-14 07:02:33 +0000

    Added a readable header
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8289 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/api/TaggedImageAnalyzer.java
+++ mmstudio/src/org/micromanager/api/TaggedImageAnalyzer.java
@@ -1,6 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Example implementation of DataProcessor
  */
 
 package org.micromanager.api;
