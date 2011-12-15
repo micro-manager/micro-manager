70c2c482b1625309e7846703e0fa19e9c17c5f98
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-12-15 20:17:00 +0000

    revert last change to VirtualAcquisitionDisplay.java
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8309 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/acquisition/VirtualAcquisitionDisplay.java
+++ mmstudio/src/org/micromanager/acquisition/VirtualAcquisitionDisplay.java
@@ -1,6 +1,5 @@
 package org.micromanager.acquisition;
 
-import java.lang.reflect.InvocationTargetException;
 import org.micromanager.api.ImageCacheListener;
 import ij.ImageStack;
 import ij.process.LUT;
@@ -64,7 +63,7 @@ public final class VirtualAcquisitionDisplay implements ImageCacheListener {
    private int[] channelInitiated_;
 
    private int numComponents_;
-   private MMCompositeImage hyperImage_;
+   private ImagePlus hyperImage_;
    private ScrollbarWithLabel pSelector_;
    private ScrollbarWithLabel tSelector_;
    private ScrollbarWithLabel zSelector_;
@@ -504,23 +503,6 @@ public final class VirtualAcquisitionDisplay implements ImageCacheListener {
       }
    }
 
-   public void updateDimensions() {
-      StackWindow win = (StackWindow) hyperImage_.getWindow();
-      //beanshell: hyperImage_.nChannels = 3;
-      hyperImage_.setNChannelsUnverified(3);
-      hyperImage_.setOpenAsHyperStack(true);
-      win.remove(hc_);
-      try {
-         StackWindow.class.getDeclaredMethod("addScrollbars", ImagePlus.class)
-                 .invoke(win, hyperImage_);
-      } catch (Exception ex) {
-         ReportingUtils.logError(ex);
-      }
-      win.add(hc_);
-      win.pack();
-   }
-
-
    private void setNumPositions(int n) {
       pSelector_.setMinimum(0);
       pSelector_.setMaximum(n);
@@ -547,10 +529,6 @@ public final class VirtualAcquisitionDisplay implements ImageCacheListener {
          ((IMMImagePlus) hyperImage_).setNSlicesUnverified(n);
          zSelector_.setMaximum(n + 1);
       }
-   }
-
-   private void setNumChannels(int n) {
-    //  hyperImage_.getWindow(
    }
 
    public ImagePlus getHyperImage() {
@@ -876,15 +854,20 @@ public final class VirtualAcquisitionDisplay implements ImageCacheListener {
       return img;
    }
 
-   final public MMCompositeImage createHyperImage(MMImagePlus mmIP, int channels, int slices,
+   final public ImagePlus createHyperImage(MMImagePlus mmIP, int channels, int slices,
            int frames, final AcquisitionVirtualStack virtualStack,
            HyperstackControls hc) {
-      final MMCompositeImage hyperImage;
+      final ImagePlus hyperImage;
       mmIP.setNChannelsUnverified(channels);
       mmIP.setNFramesUnverified(frames);
       mmIP.setNSlicesUnverified(slices);
-      hyperImage = new MMCompositeImage(mmIP, CompositeImage.COMPOSITE);
-      hyperImage.setOpenAsHyperStack(true);
+      if (channels > 1) {
+         hyperImage = new MMCompositeImage(mmIP, CompositeImage.COMPOSITE);
+         hyperImage.setOpenAsHyperStack(true);
+      } else {
+         hyperImage = mmIP;
+         mmIP.setOpenAsHyperStack(true);
+      }
       return hyperImage;
    }
 
