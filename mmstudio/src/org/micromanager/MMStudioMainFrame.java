046340e71eab7dc55de4e435ecf3297b8decc1d2
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2011-12-22 21:45:44 +0000

    Log JVM information
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8343 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/MMStudioMainFrame.java
+++ mmstudio/src/org/micromanager/MMStudioMainFrame.java
@@ -1963,7 +1963,10 @@ public class MMStudioMainFrame extends JFrame implements DeviceControlGUI, Scrip
             core_.logMessage(core_.getVersionInfo());
             core_.logMessage(core_.getAPIVersionInfo());
             core_.logMessage("Operating System: " + System.getProperty("os.name") + " " + System.getProperty("os.version"));
-
+            core_.logMessage("JVM: " + System.getProperty("java.vm.name")
+                    + ", version " + System.getProperty("java.version")
+                    + "; " + System.getProperty("sun.arch.data.model") + " bit");
+                    
             cameraLabel_ = "";
             shutterLabel_ = "";
             zStageLabel_ = "";
