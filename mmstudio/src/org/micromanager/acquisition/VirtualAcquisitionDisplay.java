c926f3ebe77476a19031eeaaa03083fd4ae21050
henry <henry@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-01-20 20:21:31 +0000

    ImageJ workaround.  Should stop the "Acquisition name in use" errors when opening datasets
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8563 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/acquisition/VirtualAcquisitionDisplay.java
+++ mmstudio/src/org/micromanager/acquisition/VirtualAcquisitionDisplay.java
@@ -2,6 +2,8 @@ package org.micromanager.acquisition;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.micromanager.api.ImageCacheListener;
 import ij.ImageStack;
 import ij.process.LUT;
@@ -24,6 +26,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentListener;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.io.File;
 import java.io.IOException;
 import java.util.prefs.Preferences;
@@ -130,6 +134,19 @@ public final class VirtualAcquisitionDisplay implements ImageCacheListener {
       MMCompositeImage(ImagePlus imgp, int type, VirtualAcquisitionDisplay disp) {
          super(imgp, type);
          display_ = disp;
+      }
+      
+      @Override
+      public void setChannelLut(LUT lut) {
+         super.setChannelLut(lut);
+         //ImageJ workaround: this function sets the currentChannel field to -1,
+         //which can lead to a null pointer exception if the function is called 
+         // while CompositeImage.updateImage is also running on a different thread
+         try {
+            JavaUtils.setRestrictedFieldValue(this, CompositeImage.class, "currentChannel", super.getChannel()-1);
+         } catch (Exception ex) {
+           ReportingUtils.logError(ex);
+         }
       }
 
       @Override
