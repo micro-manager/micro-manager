0468c3235e7571174e6ca995fbad7e6843b3eaa8
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-02-09 00:54:14 +0000

    Remove GentleLinkedBlockingQueue from acq engine and use a 10-node LinkedBlockingQueue
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8697 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/src/org/micromanager/acq_engine.clj
+++ acqEngine/src/org/micromanager/acq_engine.clj
@@ -38,8 +38,8 @@
            [mmcorej TaggedImage Configuration Metadata]
            [java.util.prefs Preferences]
            [java.net InetAddress]
-           [java.util.concurrent TimeUnit CountDownLatch]
-           [org.micromanager.utils GentleLinkedBlockingQueue MDUtils
+           [java.util.concurrent LinkedBlockingQueue TimeUnit CountDownLatch]
+           [org.micromanager.utils MDUtils
                                    ReportingUtils]
            [org.json JSONObject JSONArray]
            [java.util Date UUID]
@@ -738,7 +738,7 @@
   (def eng acq-eng)
   (load-mm)
   (swap! (.state this) assoc :stop false :pause false :finished false)
-  (let [out-queue (GentleLinkedBlockingQueue.)
+  (let [out-queue (LinkedBlockingQueue. 10)
         settings (convert-settings acq-settings)
         summary-metadata (make-summary-metadata settings)]
     (reset! (.state this) nil)
