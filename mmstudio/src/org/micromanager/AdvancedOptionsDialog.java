60dfdc78333acd0f94900aee3f37b58b5fea5557
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-02-16 05:19:42 +0000

    Cleanup imports
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@8770 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/AdvancedOptionsDialog.java
+++ mmstudio/src/org/micromanager/AdvancedOptionsDialog.java
@@ -1,42 +1,19 @@
 package org.micromanager;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dialog;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.SpringLayout;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-import javax.swing.plaf.basic.BasicBorders;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollPane;
 
 
 import org.micromanager.api.AcquisitionEngine;
 import org.micromanager.utils.MMDialog;
-import org.micromanager.utils.TooltipTextMaker;
-import org.micromanager.AcqControlDlg;
 
 public class AdvancedOptionsDialog extends MMDialog {
 
