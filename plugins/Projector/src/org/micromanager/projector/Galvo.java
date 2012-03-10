41a1f524deb9bb13f243ff0ef684748a30431078
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-03-10 00:49:25 +0000

    Finish implemented ExecutorService for galvo. Works like a charm!
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9004 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/Projector/src/org/micromanager/projector/Galvo.java
+++ plugins/Projector/src/org/micromanager/projector/Galvo.java
@@ -13,13 +13,8 @@ import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import mmcorej.CMMCore;
 import org.micromanager.utils.ReportingUtils;
 
@@ -111,62 +106,74 @@ public class Galvo implements ProjectionDevice {
    }
 
    public void turnOn() {
-      try {
-         mmc_.setProperty(galvo_, "CalibrationMode", 1);
-      } catch (Exception ex) {
-         ReportingUtils.showError(ex);
-      }
-   }
-
-   public void turnOff() {
-      try {
-         mmc_.setProperty(galvo_, "CalibrationMode", 0);
-      } catch (Exception ex) {
-         ReportingUtils.showError(ex);
-      }
-   }
-
-   public void setRois(Roi[] rois, AffineTransform trans, int reps) {
-      try {
-         mmc_.deleteGalvoPolygons(galvo_);
-      } catch (Exception ex) {
-         ReportingUtils.logError(ex);
-      }
-      int roiCount = 0;
-      for (Roi roi : rois) {
-         if (roi instanceof PointRoi) {
-            Point p = pointRoiToPoint((PointRoi) roi);
-            Point2D.Double pIn = new Point2D.Double(p.x, p.y);
-            Point2D.Double pOut = new Point2D.Double();
-            trans.transform(pIn, pOut);
-            displaySpot(pOut.x, pOut.y);
-         } else if (roi instanceof PolygonRoi) {
-            Polygon poly = ((PolygonRoi) roi).getPolygon();
+      galvoExecutor_.submit(new Runnable() {
+         public void run() {
             try {
-               Point2D lastGalvoPoint = null;
-               for (int i = 0; i < poly.npoints; ++i) {
-                  Point2D.Double imagePoint = new Point2D.Double(poly.xpoints[i], poly.ypoints[i]);
-                  Point2D galvoPoint = trans.transform(imagePoint, null);
-                  if (i == 0) {
-                     lastGalvoPoint = galvoPoint;
-                  }
-                  mmc_.addGalvoPolygonVertex(galvo_, roiCount, galvoPoint.getX(), galvoPoint.getY());
-               }
-               mmc_.addGalvoPolygonVertex(galvo_, roiCount, lastGalvoPoint.getX(), lastGalvoPoint.getY());
+               mmc_.setProperty(galvo_, "CalibrationMode", 1);
             } catch (Exception ex) {
                ReportingUtils.showError(ex);
             }
-            ++roiCount;
-         } else {
-            ReportingUtils.showError("Not able to handle this type of Roi.");
-            return;
          }
-      }
-      try {
-         mmc_.loadGalvoPolygons(galvo_, reps);
-      } catch (Exception ex) {
-         ReportingUtils.logError(ex);
-      }
+      });
+   }
+
+   public void turnOff() {
+      galvoExecutor_.submit(new Runnable() {
+         public void run() {
+            try {
+               mmc_.setProperty(galvo_, "CalibrationMode", 0);
+            } catch (Exception ex) {
+               ReportingUtils.showError(ex);
+            }
+         }
+      });
+   }
+
+   public void setRois(final Roi[] rois, final AffineTransform trans, final int reps) {
+      galvoExecutor_.submit(new Runnable() {
+         public void run() {
+            try {
+               mmc_.deleteGalvoPolygons(galvo_);
+            } catch (Exception ex) {
+               ReportingUtils.logError(ex);
+            }
+            int roiCount = 0;
+            for (Roi roi : rois) {
+               if (roi instanceof PointRoi) {
+                  Point p = pointRoiToPoint((PointRoi) roi);
+                  Point2D.Double pIn = new Point2D.Double(p.x, p.y);
+                  Point2D.Double pOut = new Point2D.Double();
+                  trans.transform(pIn, pOut);
+                  displaySpot(pOut.x, pOut.y);
+               } else if (roi instanceof PolygonRoi) {
+                  Polygon poly = ((PolygonRoi) roi).getPolygon();
+                  try {
+                     Point2D lastGalvoPoint = null;
+                     for (int i = 0; i < poly.npoints; ++i) {
+                        Point2D.Double imagePoint = new Point2D.Double(poly.xpoints[i], poly.ypoints[i]);
+                        Point2D galvoPoint = trans.transform(imagePoint, null);
+                        if (i == 0) {
+                           lastGalvoPoint = galvoPoint;
+                        }
+                        mmc_.addGalvoPolygonVertex(galvo_, roiCount, galvoPoint.getX(), galvoPoint.getY());
+                     }
+                     mmc_.addGalvoPolygonVertex(galvo_, roiCount, lastGalvoPoint.getX(), lastGalvoPoint.getY());
+                  } catch (Exception ex) {
+                     ReportingUtils.showError(ex);
+                  }
+                  ++roiCount;
+               } else {
+                  ReportingUtils.showError("Not able to handle this type of Roi.");
+                  return;
+               }
+            }
+            try {
+               mmc_.loadGalvoPolygons(galvo_, reps);
+            } catch (Exception ex) {
+               ReportingUtils.logError(ex);
+            }
+         }
+      });
    }
 
    private static Point pointRoiToPoint(PointRoi roi) {
@@ -175,18 +182,16 @@ public class Galvo implements ProjectionDevice {
    }
 
    public void runPolygons() {
-
-      Runnable polygonRunner = new Runnable() {
+      galvoExecutor_.submit(new Runnable() {
          public void run() {
-         try {
-            mmc_.runGalvoPolygons(galvo_);
-         } catch (Exception ex) {
-            ReportingUtils.logError(ex);
-         }
-      }
 
-      };
-      polygonRunner.run();
-      //galvoAgent_.
+            try {
+               mmc_.runGalvoPolygons(galvo_);
+            } catch (Exception ex) {
+               ReportingUtils.logError(ex);
+            }
+         }
+      });
+
    }
 }
