5c40ff3a17775e793e829b11ca1a2651b87ca4cc
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-03-17 03:57:58 +0000

    get DAtaBrowser building again
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9080 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DataBrowser/build.bat
+++ plugins/DataBrowser/build.bat
@@ -1,9 +1,7 @@
 rem We need Java 6 for this plugin. If you have Java 6, installed, set an environment variable, JDK6, to its location.
-if not "%JDK6%"=="" (
 if exist classes rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
-"%JDK6%\bin\java" -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/data.json.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_Win32 -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
-"%JDK6%\bin\jar" cf DataBrowser.jar -C classes\ .  
+java -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/data.json.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_Win32 -Dclojure.compile.path=classes -server clojure.lang.Compile org.micromanager.browser.plugin
+jar cf DataBrowser.jar -C classes\ .  
 copy /Y DataBrowser.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
-)
