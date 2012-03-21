61395f0380811662ae341160fe13fcbc7e425af9
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-03-21 23:26:57 +0000

    add missing file
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9123 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ plugins/Projector/src/org/micromanager/projector/OnStateListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.micromanager.projector;
+
+/**
+ *
+ * @author Valelab
+ */
+interface OnStateListener {
+   public void turnedOn();
+   public void turnedOff();
+}
