aad390d82393b2bc099bd95c56e94b029839ee4f
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-03-21 18:03:53 +0000

    seems to be working
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9118 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/LocalWeightedMean/src/org/micromanager/lwm/LocalWeightedMean.java
+++ plugins/LocalWeightedMean/src/org/micromanager/lwm/LocalWeightedMean.java
@@ -1,7 +1,9 @@
 package org.micromanager.lwm;
 
 import ags.utils.KdTree;
+import ags.utils.KdTree.Entry;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,12 +14,13 @@ import org.apache.commons.math.linear.LUDecompositionImpl;
 import org.apache.commons.math.linear.RealMatrix;
 
 public class LocalWeightedMean {
-
    final private ExponentPairs exponentPairs_;
    final private ControlPoints controlPoints_;
-   final private KdTree kdTree_;
+   final private EnhancedKDTree kdTree_;
    final private int order_;
-   final private Map<Point2D.Double, Point2D.Double> pointMap_;
+   final private PointMap pointMap_;
+
+   public static class PointMap extends HashMap<Point2D.Double, Point2D.Double> {}
 
    public static class ExponentPair
    {
@@ -25,7 +28,7 @@ public class LocalWeightedMean {
       public int yExponent;
    }
 
-   public static interface ExponentPairs extends List<ExponentPair> {}
+   public static class ExponentPairs extends ArrayList<ExponentPair> {}
 
    public static class PolynomialCoefficients {
       public double[] polyX;
@@ -37,7 +40,7 @@ public class LocalWeightedMean {
    }
 
    public static ExponentPairs polynomialExponents(int order) {
-      final ExponentPairs exponents = (ExponentPairs) new ArrayList<ExponentPair>();
+      final ExponentPairs exponents = new ExponentPairs();
       for (int j=0; j<=order; ++j) {
          for (int k = j; k>=0; k--) {
             final ExponentPair pair = new ExponentPair();
@@ -49,31 +52,65 @@ public class LocalWeightedMean {
       return exponents;
    }
 
+   public static class EnhancedKDTree extends KdTree.SqrEuclid {
+      final Point2D.Double[] points_;
+
+      EnhancedKDTree(Point2D.Double[] points) {
+         super(2, Integer.MAX_VALUE);
+         points_ = points;
+         for (int i = 0; i < points.length; ++i) {
+            final Point2D.Double point = points[i];
+            addPoint(new double[]{point.x, point.y}, i);
+         }
+      }
+
+      public List<Point2D.Double> nearestNeighbor(Point2D.Double testPoint,
+              int size, boolean ordered) {
+         List<Entry<Integer>> neighbors = super.nearestNeighbor(new double[] {testPoint.x, testPoint.y},
+                 size, ordered);
+         List<Point2D.Double> neighborList = new ArrayList<Point2D.Double>();
+         for (int i=0; i<neighbors.size(); ++i) {
+            neighborList.add(points_[neighbors.get(i).value]);
+         }
+         return neighborList;
+      }
+      
+   }
+
+   public static PointMap selectPoints(PointMap points, List<Point2D.Double> srcPoints) {
+      PointMap selectPointMap = new PointMap();
+      for (Double srcPoint:srcPoints) {
+         selectPointMap.put(srcPoint, points.get(srcPoint));
+      }
+      return selectPointMap;
+   }
+   
+
    public static class ControlPoint {
       final public Point2D.Double point;
       final public double Rnormalized;
       final public PolynomialCoefficients polynomialCoefficients;
 
-      public ControlPoint(KdTree kdTree, Point2D.Double srcPoint,
-              int order, Map<Point2D.Double, Point2D.Double> pointMap) {
+      public ControlPoint(EnhancedKDTree kdTree, Point2D.Double srcPoint,
+              int order, PointMap pointMap) {
          point = srcPoint;
          ExponentPairs exponentPairs = polynomialExponents(order);
-         List<Point2D.Double> neighbors = kdTree.nearestNeighbor(new double[] {srcPoint.x, srcPoint.y},
+         List<Point2D.Double> neighbors = kdTree.nearestNeighbor(srcPoint,
                  exponentPairs.size(), true);
          Rnormalized = neighbors.get(0).distance(srcPoint);
-         polynomialCoefficients = fitPolynomial(exponentPairs, pointMap);
+         polynomialCoefficients = fitPolynomial(exponentPairs, selectPoints(pointMap, neighbors));
       }
    }
 
-   public static interface ControlPoints extends Map<Point2D.Double, ControlPoint> {}
+   public static class ControlPoints extends HashMap<Point2D.Double, ControlPoint> {}
 
    public static double[] powerTerms(double x, double y,
            final ExponentPairs exponentPairs) {
       final double[] powerTerms = new double[exponentPairs.size()];
       int i = 0;
       for (ExponentPair exponentPair:exponentPairs) {
-         ++i;
          powerTerms[i] = Math.pow(x, exponentPair.xExponent)*Math.pow(y, exponentPair.yExponent);
+         ++i;
       }
       return powerTerms;
    }
@@ -81,7 +118,7 @@ public class LocalWeightedMean {
    public static PolynomialCoefficients fitPolynomial(ExponentPairs exponentPairs,
            Map<Point2D.Double, Point2D.Double> pointPairs) {
       final List<Point2D.Double> srcPoints = new ArrayList(pointPairs.keySet());
-      final RealMatrix matrix = new Array2DRowRealMatrix();
+      final RealMatrix matrix = new Array2DRowRealMatrix(srcPoints.size(), exponentPairs.size());
       for (int i=0; i<srcPoints.size(); ++i) {
          matrix.setRow(i, powerTerms(srcPoints.get(i).x, srcPoints.get(i).y, exponentPairs));
       }
@@ -108,17 +145,17 @@ public class LocalWeightedMean {
       return result;
    }
 
-   public static ControlPoints createControlPoints(KdTree kdTree, int order,
-           Map<Point2D.Double, Point2D.Double> pointMap) {
-      final ControlPoints controlPointMap = (ControlPoints) new HashMap<Point2D.Double, ControlPoint>();
+   public static ControlPoints createControlPoints(EnhancedKDTree kdTree, int order,
+           PointMap pointMap) {
+      final ControlPoints controlPointMap = new ControlPoints();
       for (Point2D.Double srcPoint:pointMap.keySet()) {
          controlPointMap.put(srcPoint, new ControlPoint(kdTree, srcPoint, order, pointMap));
       }
       return controlPointMap;
    }
 
-   public static Point2D.Double computeTransformation(KdTree kdTree, Point2D.Double testPoint, ControlPoints controlPoints, ExponentPairs exponentPairs) {
-      final List<Point2D.Double> neighbors = kdTree.nearestNeighbor(new double [] {testPoint.x, testPoint.y}, 20, false);
+   public static Point2D.Double computeTransformation(EnhancedKDTree kdTree, Point2D.Double testPoint, ControlPoints controlPoints, ExponentPairs exponentPairs) {
+      final List<Point2D.Double> neighbors = kdTree.nearestNeighbor(testPoint, 20, false);
       double sumWeights = 0;
       double sumWeightedPolyX = 0;
       double sumWeightedPolyY = 0;
@@ -127,34 +164,27 @@ public class LocalWeightedMean {
          final double r = testPoint.distance(controlPoint.point) / controlPoint.Rnormalized;
          final double weight = weightFunction(r);
          sumWeights += weight;
-         sumWeightedPolyX += evaluatePolynomial(testPoint.x, testPoint.y,
+         sumWeightedPolyX += weight * evaluatePolynomial(testPoint.x, testPoint.y,
                  controlPoint.polynomialCoefficients.polyX, exponentPairs);
-         sumWeightedPolyY += evaluatePolynomial(testPoint.x, testPoint.y,
+         sumWeightedPolyY += weight * evaluatePolynomial(testPoint.x, testPoint.y,
                  controlPoint.polynomialCoefficients.polyY, exponentPairs);
       }
       return new Point2D.Double(sumWeightedPolyX / sumWeights,
                                 sumWeightedPolyY / sumWeights);
    }
 
-   public static KdTree setupKdTree(Point2D.Double[] points) {
-      final KdTree kdTree = new KdTree.SqrEuclid<Double>(2, Integer.MAX_VALUE);
-      for (int i = 0; i<points.length; ++i) {
-         final Point2D.Double point = points[i];
-         kdTree.addPoint(new double[] {point.x, point.y}, i);
-      }
-      return kdTree;
-   }
-
-   public LocalWeightedMean(int order, Map<Point2D.Double, Point2D.Double> pointMap) {
-      pointMap_ = pointMap;
-      order_ = order;
-      kdTree_ = setupKdTree((Point2D.Double []) pointMap.keySet().toArray());
-      exponentPairs_ = polynomialExponents(order);
-      controlPoints_ = createControlPoints(kdTree_, order_, pointMap_);
-   }
-
    public Point2D.Double transform(Point2D.Double srcTestPoint) {
       return computeTransformation(kdTree_, srcTestPoint, controlPoints_, exponentPairs_);
    }
-         
+
+   public LocalWeightedMean(int order, PointMap pointMap) {
+      pointMap_ = pointMap;
+      order_ = order;
+      exponentPairs_ = polynomialExponents(order);
+      final ArrayList<Point2D.Double> keys = new ArrayList<Point2D.Double>();
+      keys.addAll(pointMap.keySet());
+      final Point2D.Double[] keyArray = keys.toArray(new Point2D.Double[]{});
+      kdTree_ = new EnhancedKDTree(keyArray);
+      controlPoints_ = createControlPoints(kdTree_, order_, pointMap_);
+   }   
 }
