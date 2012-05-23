c0ec6c8a0f633e176aa3ff2ff959d0aa95263c0e
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-05-23 01:18:38 +0000

    implement my own lru cache
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9449 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/SlideExplorer2/src/slide_explorer/cache.clj
+++ plugins/SlideExplorer2/src/slide_explorer/cache.clj
@@ -1,70 +1,86 @@
 (ns slide-explorer.cache
   (:refer-clojure :exclude [get])
-  (:require [clojure.core.cache :as cache]
-            [slide-explorer.image :as image])
-  (:use [clojure.core.cache :only (defcache CacheProtocol)])
+  (:require [slide-explorer.image :as image])
   (:import (java.util.concurrent Executors)
            (java.io File)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Immutable LRU cache
+;; based on, but different from, clojure.core.cache by @fogus
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn lru-map [base limit]
+  (with-meta base
+             {::lru {:limit limit
+                     :tick 0
+                     :priority {}}}))
+
+(defn remove-lru-excess [lru-map]
+  (let [{:keys [priority tick limit]} (::lru (meta lru-map))]
+    (if (<= (count lru-map) limit)
+      lru-map
+      (let [old-key (apply min-key priority (keys priority))]
+        (vary-meta (dissoc lru-map old-key)
+                   update-in [::lru :priority] dissoc old-key)))))
+        
+(defn hit-lru-metadata [lru-meta key]
+  (let [tick+ (inc (get-in lru-meta [::lru :tick]))]
+    (-> lru-meta
+        (assoc-in [::lru :tick] tick+)
+        (update-in [::lru :priority] assoc key tick+))))
+
+(defn remove-item [lru-map key]
+  (-> lru-map
+      (dissoc key val)
+      (vary-meta update-in [::lru :priority] dissoc key)))
+
+(defn add-item [lru-map key val]
+  (-> lru-map
+      (assoc key val)
+      (vary-meta hit-lru-metadata key)
+      remove-lru-excess))
+
+(defn hit-item [lru-map key]
+  (vary-meta lru-map hit-lru-metadata key))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; image-cache consists of an agent
+;; that contains a map
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def file-service (Executors/newFixedThreadPool 1))
   
-(defn- key-map-to-file-name [dir key-map]
-  (str dir "/"
-    (apply str 
-           (for [[k v] key-map]
-             (str (name k) "_" v "_")))
-       ".tif"))
+(defn- key-map-to-file-name [cache-agent key-map]
+  (let [dir (-> cache-agent meta ::cache :directory)]
+    (str dir "/"
+         (apply str 
+                (for [[k v] key-map]
+                  (str (name k) "_" v "_")))
+         ".tif")))
 
-(defn- add-image
-  [cache-agent dir key image]
-  (send-off cache-agent assoc key image)
-    (.submit file-service
-             #(image/write-processor (key-map-to-file-name dir key) image))
+(defn add-image
+  [cache-agent key image]
+  (send-off cache-agent add-item key image)
+  (.submit file-service #(image/write-processor (key-map-to-file-name cache-agent key) image))
   nil)
   
-(defn- get-image
-  [cache-agent dir key]
-  (send-off cache-agent #(.hit %1 %2) key) 
+(defn get-image
+  [cache-agent key]
+  (send-off cache-agent hit-item key) 
   (or (clojure.core/get @cache-agent key)
       (do
         (.submit file-service
-                 #(let [image (image/read-processor (key-map-to-file-name dir key))]
-                    (send-off cache-agent assoc key image)))
+                 #(let [image (image/read-processor (key-map-to-file-name cache-agent key))]
+                    (send-off cache-agent add-item key image)))
         nil)))
 
-(defn- has-image
-  [cache-value dir key]
-  (or (.has? cache-value key)
-      (.exists (File. (key-map-to-file-name dir key)))))
-
-(defprotocol ImageCacheProtocol
-  "An image cache that stores images."
-  (add [this key image]
-       "Put an image into cache.")
-  (get [this key]
-       "Read an image from cache.")
-  (has [this key]
-       "Checks if image is available in cache.")
-  (memory [this]
-       "Returns the current value stored in memory.")
-  (different [this other]
-             "Checks if the memory value is different."))
-
-(deftype ImageCache [cache-agent dir]
-  ImageCacheProtocol
-    (add [_ key image] (add-image cache-agent dir key image))
-    (get [_ key] (get-image cache-agent dir key))
-    (has [_ key] (has-image @cache-agent dir key))
-    (memory [_] @cache-agent)
-    (different [this other] (memory this) (.memory other))
-  clojure.lang.IRef
-    (addWatch [this Object IFn] (do (.addWatch cache-agent Object IFn) this))
-    (deref [_] (.deref cache-agent))
-    (getValidator [_] (.getValidator cache-agent))
-    (getWatches [_] (.getWatches cache-agent))
-    (removeWatch [this Object] (do (.removeWatch cache-agent Object) this))
-    (setValidator [_ IFn] (.setValidator cache-agent IFn)))
-
+(defn has-image
+  [cache-agent key]
+  (or (not (nil? (@cache-agent key)))
+      (.exists (File. (key-map-to-file-name cache-agent key)))))
+                 
 (defn image-cache
   "Creates an image cache that uses a LRU policy to store memory-cache-size
    images in memory, and also stores all images on disk in TIFF files in the
@@ -75,4 +91,11 @@
    be used to provide a callback when the image has been loaded into
    memory."
   [dir memory-cache-size]
-  (ImageCache. (agent (cache/lru-cache-factory memory-cache-size {})) dir))
+  (doto (agent (lru-map {} memory-cache-size))
+    (reset-meta! {::cache {:directory dir}})))
+
+;; test
+
+(import ij.ImageJ)
+
+(import ij.process.ByteProcessor)
