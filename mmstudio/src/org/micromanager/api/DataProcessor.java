f41f9c382c4cce1bdd3344356ab674965c737dfa
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-07-11 03:52:25 +0000

    documentation for DataProcessor; completes documentation of MM APIs
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9656 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/api/DataProcessor.java
+++ mmstudio/src/org/micromanager/api/DataProcessor.java
@@ -25,13 +25,26 @@ public abstract class DataProcessor<E> extends Thread {
    private boolean stopRequested_ = false;
    private boolean started_ = false;
 
-   protected abstract void process();
-   /*    The "Identity" process method:
-    * {
+   /*
+    * The process method should be overridden by classes implementing
+    * DataProcessor, to provide a processing function.
+    * 
+    * For example, an "Identity" DataProcessor (where nothing is
+    * done to the data) would override process() thus:
+    *
+    * @Override
+    * public void process() {
     *    produce(poll());
     * }
     */
+   protected abstract void process();
+ 
 
+   /*
+    * The run method that causes images to be processed. As DataProcessor
+    * extends java's Thread class, this method will be executed whenever
+    * DataProcessor.start() is called.
+    */
    @Override
    public void run() {
       setStarted(true);
@@ -40,28 +53,54 @@ public abstract class DataProcessor<E> extends Thread {
       }
    }
 
+   /*
+    * Request that the data processor stop processing. The current
+    * processing event will continue, but no others will be started.
+    */
    public synchronized void requestStop() {
       stopRequested_ = true;
    }
-   
+
+   /*
+    * Private method for tracking when processing has started.
+    */
    private synchronized void setStarted(boolean started) {
       started_ = started;
    }
-   
+
+   /*
+    * Returns true if the DataProcessor has started up and objects
+    * are being processed as they arrive.
+    */
    public synchronized boolean isStarted() {
       return started_;
    }
 
+   /*
+    * The constructor.
+    */
    public DataProcessor() {}
 
+   /*
+    * Sets the input queue where objects to be processed
+    * are received by the DataProcessor.
+    */
    public void setInput(BlockingQueue<E> input) {
       input_ = input;
    }
 
+   /*
+    * Sets the output queue where objects that have been processed
+    * exit the DataProcessor.
+    */
    public void setOutput(BlockingQueue<E> output) {
       output_ = output;
    }
 
+   /*
+    * A protected method that reads the next object from the input
+    * queue.
+    */
    protected E poll() {
       while (!stopRequested()) {
          try {
@@ -76,10 +115,17 @@ public abstract class DataProcessor<E> extends Thread {
       return null;
    }
 
+   /*
+    * A convenience method for draining all available data objects
+    * on the input queue to a collection.
+    */
    protected void drainTo(Collection<E> data) {
       input_.drainTo(data);
    }
 
+   /*
+    * A convenience method for posting a data object to the output queue.
+    */
    protected void produce(E datum) {
       try {
          output_.put(datum);
@@ -88,6 +134,9 @@ public abstract class DataProcessor<E> extends Thread {
       }
    };
 
+   /*
+    * Returns true if stop has been requested.
+    */
    protected synchronized boolean stopRequested() {
       return stopRequested_;
    }
