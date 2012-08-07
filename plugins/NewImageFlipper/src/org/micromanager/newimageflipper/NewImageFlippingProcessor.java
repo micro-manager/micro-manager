e4fdc7ccfbf55e1a81ebc9eaecc16dfa1887b186
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-08-07 04:45:31 +0000

    Added new version of image flipper
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9717 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ plugins/NewImageFlipper/src/org/micromanager/newimageflipper/NewImageFlippingProcessor.java
@@ -0,0 +1,71 @@
+package org.micromanager.newimageflipper;
+
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+import mmcorej.TaggedImage;
+import org.json.JSONObject;
+import org.micromanager.api.DataProcessor;
+import org.micromanager.utils.ImageUtils;
+import org.micromanager.utils.MDUtils;
+import org.micromanager.utils.ReportingUtils;
+
+public class NewImageFlippingProcessor extends DataProcessor<TaggedImage> {
+
+   NewImageFlipperControls controls_;
+
+   public NewImageFlippingProcessor(NewImageFlipperControls controls) {
+      this.controls_ = controls;
+   }
+
+   @Override
+   public void process() {
+      try {
+         TaggedImage nextImage = poll();
+         try {
+            String camera = nextImage.tags.getString("Core-Camera");
+            if (!camera.equals(controls_.getCamera())) {
+               if (nextImage.tags.has("CameraChannelIndex")) {
+                  camera = MDUtils.getChannelName(nextImage.tags);
+               }
+            }
+            if (!camera.equals(controls_.getCamera())) {
+               produce(nextImage);
+               return;
+
+            }
+            int width = MDUtils.getWidth(nextImage.tags);
+            int height = MDUtils.getHeight(nextImage.tags);
+            String type = MDUtils.getPixelType(nextImage.tags);
+            int ijType = ImagePlus.GRAY8;
+            if (type.equals("GRAY16")) {
+               ijType = ImagePlus.GRAY16;
+            }
+
+            ImageProcessor proc = ImageUtils.makeProcessor(ijType, width, height, nextImage.pix);
+
+            if (controls_.getMirror()) {
+               proc.flipHorizontal();
+            }
+            if (controls_.getRotate() == 1) {
+               proc = proc.rotateLeft();
+            }
+            if (controls_.getRotate() == 2) {
+               proc.flipVertical();
+            }
+            if (controls_.getRotate() == 3) {
+               proc = proc.rotateRight();
+            }
+            JSONObject newTags = nextImage.tags;
+            MDUtils.setWidth(newTags, proc.getWidth());
+            MDUtils.setHeight(newTags, proc.getHeight());
+
+            produce(new TaggedImage(proc.getPixels(), newTags));
+         } catch (Exception ex) {
+            produce(nextImage);
+            ReportingUtils.logError(ex);
+         }
+      } catch (Exception ex) {
+         ReportingUtils.logError(ex);
+      }
+   }
+}
---
+++ plugins/NewImageFlipper/src/org/micromanager/newimageflipper/NewTaggedImageFlipper.java
@@ -0,0 +1,85 @@
+///////////////////////////////////////////////////////////////////////////////
+//FILE:          NewTaggedImageFlipper.java
+//PROJECT:       Micro-Manager
+//SUBSYSTEM:     mmstudio
+//-----------------------------------------------------------------------------
+//
+// AUTHOR:       Arthur Edelstein, Nico Stuurman
+//
+// COPYRIGHT:    University of California, San Francisco, 2011, 2012
+//
+// LICENSE:      This file is distributed under the BSD license.
+//               License text is included with the source distribution.
+//
+//               This file is distributed in the hope that it will be useful,
+//               but WITHOUT ANY WARRANTY; without even the implied warranty
+//               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+
+package org.micromanager.newimageflipper;
+
+import org.micromanager.api.AcquisitionEngine;
+import org.micromanager.api.MMPlugin;
+import org.micromanager.api.ScriptInterface;
+
+/**
+ * Example demonstrating the use of DataProcessors.  DataProcessors can 
+ * get hold of images coming out of the acquisition engine before they 
+ * are inserted into the ImageCache.  DataProcessors can modify images 
+ * or even generate totally new ones.
+ * 
+ * This specific example has grown out to modify images only from a specific camera
+ * and is therefore very useful when using multiple cameras
+ * 
+ * @author arthur
+ */
+public class NewTaggedImageFlipper implements MMPlugin {
+   public static String menuName = "Image Flipper";
+   public static String tooltipDescription = "Mirrors, flips and rotates images on the fly";
+   private ScriptInterface gui_;
+   private NewImageFlipperControls ctls_;
+   
+
+   public void dispose() {
+      if (ctls_ != null)
+         ctls_.safePrefs();
+   }
+
+   public void setApp(ScriptInterface app) {
+      gui_ = app;
+   }
+
+   public void show() {
+      if (ctls_ == null)
+         ctls_ = new NewImageFlipperControls();
+      else
+         ctls_.updateCameras();
+      AcquisitionEngine eng = gui_.getAcquisitionEngine();
+      eng.addImageProcessor(ctls_.getProcessor());
+      ctls_.setVisible(true);
+   }
+
+   public void configurationChanged() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getDescription() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getInfo() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getVersion() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   public String getCopyright() {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+}
