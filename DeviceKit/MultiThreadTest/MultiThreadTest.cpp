c2c10fdb07040627e585da038c82e7bc764f9182
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-09-12 01:03:25 +0000

    Skeleton for multi-threaded test
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9897 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceKit/MultiThreadTest/MultiThreadTest.cpp
+++ DeviceKit/MultiThreadTest/MultiThreadTest.cpp
@@ -27,9 +27,6 @@
 #include "../../MMCore/MMCore.h"
 #include "../../MMDevice/ImageMetadata.h"
 
-#ifdef WIN32
-//#include <windows.h>
-#endif
 #include <iostream>
 #include <iomanip>
 #include <assert.h>
@@ -37,14 +34,22 @@
 
 using namespace std;
 
-// declaration of test methods
-void TestDemoDevices(CMMCore& core);
-void TestCameraStreaming(CMMCore& core);
-void TestPixelSize(CMMCore& core);
-void TestColorMode(CMMCore& core);
-void TestHam(CMMCore& core);
-void TestCameraLive(CMMCore& core);
-void TestAF(CMMCore& core);
+class TestThread : public MMDeviceThreadBase
+{
+public:
+   TestThread(CMMCore* c) : core(c) {}
+   ~TestThread() {}
+
+   int svc();
+
+private:
+   CMMCore* core;
+};
+
+int TestThread::svc()
+{
+   return 0;
+}
 
 // main routine
 int main(int argc, char* argv[])
@@ -54,19 +59,17 @@ int main(int argc, char* argv[])
    if (argc != 2)
    {
       cout << "Invalid number of command-line parameters." << endl;
-      cout << "Use: Test_MMCore <configuration file name>" << endl;
+      cout << "Use: MultiThreadTest <configuration file name>" << endl;
       return 1;
    }
 
    try {
-      // Create CMMCore      object
       CMMCore core;
 
       // load system configuration
       core.loadSystemConfiguration(argv[1]);
-      core.enableStderrLog(false); // supress console echo of log/debug messages
-      std::cerr.flush();
-
+      core.enableStderrLog(false);
+ 
       // print current device status
       // (this should work for any configuration)
       vector<string> devices = core.getLoadedDevices();
@@ -87,18 +90,7 @@ int main(int argc, char* argv[])
             std::cout.flush();
          }
       }
-
-      cout << "Pixel size: " << core.getPixelSizeUm() << " um" << endl;
-
       // add any testing routines here...
-
-      // TestDemoDevices is just an example for a testing rountine
-      // It assumes that specific demo configuration is already loaded
-      TestDemoDevices(core);
-      TestAF(core);
-      // TestColorMode(core);
-      TestCameraLive(core);
-      TestPixelSize(core);
 
       // clean-up before exiting
       core.unloadAllDevices();
@@ -107,214 +99,9 @@ int main(int argc, char* argv[])
    {
       cout << "Exception: " << err.getMsg() << endl;
       cout << "Exiting now." << endl;
-      retval = 1;
+      return 2;
    }
 
-   cout << (0==retval?"Test_MMCore ended OK.":"See errors above") << "\nPress Enter to terminate this program." << endl;
-   std::cout.flush();
-   std::cerr.flush();
-   getchar();
    return retval;
-}
-
-/**
-* Test routine for the MMConfig_Demo.cfg.
-* Device names must match
-*/
-void TestDemoDevices(CMMCore& core)
-{
-
-   std::string  XYStageName = core.getXYStageDevice();
-   const char* wheelName = "Emission";
-
-   // Example 1: move filter wheel to state(position) 3
-   // -------------------------------------------------
-   core.setState(wheelName, 3);
-   core.waitForDevice(wheelName);
-
-   long state = core.getState(wheelName);
-   cout << "State device " << wheelName << " in state " << state << endl;
-
-   // Example 2: move filter wheel to specific label (must be previously defined)
-   // ---------------------------------------------------------------------------
-   core.setStateLabel(wheelName, "Chroma-HQ620");
-   core.waitForDevice(wheelName);
-
-   state = core.getState(wheelName);
-   string stateLabel = core.getStateLabel(wheelName);
-   cout << "State device " << wheelName << " in state " << state << ", labeled as " << stateLabel << endl;
-
-   // Example 3: move multiple filters at once using one of the predefined configurations
-   // -----------------------------------------------------------------------------------
-   core.setConfig("Channel", "DAPI");
-   core.waitForSystem();
-
-   // print current status for all state devices
-   vector<string> stateDevices = core.getLoadedDevicesOfType(MM::StateDevice);
-   for (size_t i=0; i<stateDevices.size(); i++)
-   {
-      state = core.getState(stateDevices[i].c_str());
-      stateLabel = core.getStateLabel(stateDevices[i].c_str());
-      cout << "State device " << stateDevices[i] << " in state " << state << ", labeled as " << stateLabel << endl;
-   }
-
-   // Example 4: snap an image
-   // ------------------------
-   core.setExposure(100.0);
-   core.setProperty("Camera", "PixelType", "8bit");
-   core.snapImage();
-   cout << "Image snapped." << endl;
-
-   // Example 5: move XYStage
-   // -----------------------
-   core.setXYPosition(XYStageName.c_str(), 0.0, 0.0);
-   core.waitForDevice(XYStageName.c_str());
-
-   core.setXYPosition(XYStageName.c_str(), 10000.0, 10000.0);
-   core.waitForDevice(XYStageName.c_str());
-
-   double x,y;
-   core.getXYPosition(XYStageName.c_str(), x, y);
-
-   cout << "XY position = " << x << "," << y << endl;
-}
-
-
-
-/**
-* Test routine for the 
-*/
-void TestCameraStreaming(CMMCore& core)
-{
-   const long numFrames = 20;
-   const int memoryFootprintMB = 100;
-   const double intervalMs = 300.0; // ms
-
-   core.setCircularBufferMemoryFootprint(memoryFootprintMB);
-
-   cout << "Buffer capacity: " << core.getBufferTotalCapacity() << endl;
-   string camera = core.getCameraDevice();
-   core.setExposure(200.0);
-
-   // test normal mode
-   core.snapImage();
-   core.getImage();
-
-   core.startSequenceAcquisition(numFrames, intervalMs, true);
-
-   CDeviceUtils::SleepMs(5000);
-
-   core.stopSequenceAcquisition();
-
-   int count=0;
-   while (core.deviceBusy(camera.c_str()))
-   {
-      Metadata md;
-      core.getLastImageMD(0, 0, md);
-      double interval = core.getBufferIntervalMs();
-      printf("Displaying current image, %ld in que, %.0f ms interval.\n", core.getRemainingImageCount(), interval);
-      MetadataSingleTag mdst = md.GetSingleTag(MM::g_Keyword_Elapsed_Time_ms);
-      printf("Elapsed time %s, device %s\n", mdst.GetValue().c_str(), mdst.GetDevice().c_str());
-   }
-   printf("Camera finished with %.0f ms interval.\n", core.getBufferIntervalMs());
-   core.setProperty(camera.c_str(), "ShutterMode", "Auto");
-
-
-
-   cout << "Done! Free space =" << core.getBufferFreeCapacity() << endl;
-
-   core.startSequenceAcquisition(numFrames, intervalMs, true);
-
-
-   count=0;
-   while (core.deviceBusy(camera.c_str()))
-   {
-      Metadata md;
-      core.getLastImageMD(0, 0, md);
-      double interval = core.getBufferIntervalMs();
-      printf("Displaying current image, %ld in que, %.0f ms interval.\n", core.getRemainingImageCount(), interval);
-      MetadataSingleTag mdst = md.GetSingleTag(MM::g_Keyword_Elapsed_Time_ms);
-      printf("Elapsed time %s, device %s\n", mdst.GetValue().c_str(), mdst.GetDevice().c_str());
-   }
-   printf("Camera finished with %.0f ms interval.\n", core.getBufferIntervalMs());
-   core.setProperty(camera.c_str(), "ShutterMode", "Auto");
-
-}
-
-void TestPixelSize(CMMCore& core)
-{
-   core.definePixelSizeConfig("Resolution10", "Objective", "State", "1");
-   core.definePixelSizeConfig("Resolution20", "Objective", "State", "3");
-   core.definePixelSizeConfig("Resolution40", "Objective", "State", "0");
-   core.setPixelSizeUm("Resolution10", 1.0);
-   core.setPixelSizeUm("Resolution20", 0.5);
-   core.setPixelSizeUm("Resolution40", 0.25);
-
-   core.setState("Objective", 2);
-   cout << "Pixel size = " << core.getPixelSizeUm() << " um" << endl;
-
-   core.setState("Objective", 1);
-   cout << "Pixel size = " << core.getPixelSizeUm() << " um" << endl;
-
-   core.setState("Objective", 3);
-   cout << "Pixel size = " << core.getPixelSizeUm() << " um" << endl;
-
-   core.setState("Objective", 0);
-   cout << "Pixel size = " << core.getPixelSizeUm() << " um" << endl;
-
-}
-
-void TestColorMode(CMMCore& core)
-{
-   string camera = core.getCameraDevice();
-   core.setProperty(camera.c_str(), "ColorMode", "RGB-32bit");
-   core.snapImage();
-   core.getImage();
-   cout << "image: " << core.getImageWidth() << " X " << core.getImageHeight() << " X " << core.getNumberOfComponents() << endl;
-}
-
-/**
-* Test routine for the MMConfig_Demo.cfg.
-*/
-void TestCameraLive(CMMCore& core)
-{
-   const int memoryFootprintMB = 100;
-
-   core.setCircularBufferMemoryFootprint(memoryFootprintMB);
-
-   cout << "Buffer capacity: " << core.getBufferTotalCapacity() << endl;
-   string camera = core.getCameraDevice();
-   //core.setProperty(camera.c_str(), "ShutterMode", "Open");
-   core.setProperty(camera.c_str(), "Binning", "4");
-   core.setExposure(200.0);
-
-   // test normal mode
-   core.snapImage();
-   core.getImage();
-
-   core.startContinuousSequenceAcquisition(0);
-
-   int count=0;
-   while (core.deviceBusy(camera.c_str()))
-   {
-      core.getLastImage();
-      double interval = core.getBufferIntervalMs();
-      printf("Displaying image %d, %ld in que, %.0f ms interval.\n", count+1, core.getRemainingImageCount(), interval);
-      count++;
-      if (count >= 100)
-         core.stopSequenceAcquisition();
-   }
-   printf("Camera finished with %.0f ms interval.\n", core.getBufferIntervalMs());
-   //core.setProperty(camera.c_str(), "ShutterMode", "Auto");
-
-}
-
-/**
-* Test routine for the autofocus.
-*/
-void TestAF(CMMCore& core)
-{
-   std::cout << "Testing selected Autofocus device " << core.getAutoFocusDevice() << std::endl;
-   core.fullFocus();
 }
 
