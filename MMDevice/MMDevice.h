222c3073682b84f9fbaca3ff2bf3101d76aa8f09
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-09-25 01:28:30 +0000

    bump device interface version number
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9957 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- MMDevice/MMDevice.h
+++ MMDevice/MMDevice.h
@@ -34,7 +34,7 @@
 // Header version
 // If any of the class declarations changes, the interface version
 // must be incremented
-#define DEVICE_INTERFACE_VERSION 51
+#define DEVICE_INTERFACE_VERSION 52
 ///////////////////////////////////////////////////////////////////////////////
 
 #pragma once
