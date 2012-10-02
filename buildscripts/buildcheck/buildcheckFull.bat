4553d48094c36f93941bf61702c82e4e8f7c1231
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-10-02 00:01:16 +0000

    point to the correct version
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@9987 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- buildscripts/buildcheck/buildcheckFull.bat
+++ buildscripts/buildcheck/buildcheckFull.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jre6\bin\java" -cp buildcheck-1.0.0.jar;lib\*; buildcheck.core full > output.txt
+"C:\Program Files\Java\jre6\bin\java" -cp buildcheck-0.0.1.jar;lib\*; buildcheck.core full > output.txt
