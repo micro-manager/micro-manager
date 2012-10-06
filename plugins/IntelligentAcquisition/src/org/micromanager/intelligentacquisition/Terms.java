df00bb8f1186163bd91c7637c456d3dc855203d0
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-10-06 04:33:57 +0000

    Split out static String to its own class
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10052 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ plugins/IntelligentAcquisition/src/org/micromanager/intelligentacquisition/Terms.java
@@ -0,0 +1,21 @@
+package org.micromanager.intelligentacquisition;
+
+/**
+ * Strings needed in various classes in this plugin
+ * Guarantees that these strings are the same and that they can be changed
+ * 
+ * 
+ * @author nico
+ */
+public class Terms {
+
+   public static final String FRAME = "Frame";
+   public static final String POSITION = "Position";
+   public static final String CHANNEL = "Channel";
+   public static final String SLICE = "Slice";
+   public static final String X = "X";
+   public static final String Y = "Y";
+   public static final String RESULTTABLENAME = "IA Test Results";
+   
+   
+}
