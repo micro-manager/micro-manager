ac9362cf278a40875030f1f5c10f430bb1198854
henry <henry@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-10-09 16:59:23 +0000

    correct image label ordering
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10062 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/utils/ImageLabelComparator.java
+++ mmstudio/src/org/micromanager/utils/ImageLabelComparator.java
@@ -23,26 +23,25 @@ package org.micromanager.utils;
 
 import java.util.Comparator;
 
-
 public class ImageLabelComparator implements Comparator<String> {
 
    private final boolean slicesFirst_;
    private final boolean timeFirst_;
-   
+
    public ImageLabelComparator() {
-      this(false,false);
+      this(false, false);
    }
-   
+
    public ImageLabelComparator(boolean slicesFirst, boolean timeFirst) {
       super();
       slicesFirst_ = slicesFirst;
       timeFirst_ = timeFirst;
    }
-   
+
    public boolean getSlicesFirst() {
       return slicesFirst_;
    }
-   
+
    public boolean getTimeFirst() {
       return timeFirst_;
    }
@@ -53,46 +52,36 @@ public class ImageLabelComparator implements Comparator<String> {
       String[] indices1 = s1.split("_");
       String[] indices2 = s2.split("_");
       if (timeFirst_) {
-         int position = indices1[3].compareTo(indices2[3]);
-         if (position != 0) {
-            return position;
+         int position1 = Integer.parseInt(indices1[3]), position2 = Integer.parseInt(indices2[3]);
+         if (position1 != position2) {
+            return position1 - position2;
          }
-         int time = indices1[2].compareTo(indices2[2]);
-         if (time != 0) {
-            return time;
+         int frame1 = Integer.parseInt(indices1[2]), frame2 = Integer.parseInt(indices2[2]);
+         if (frame1 != frame2) {
+            return frame1 - frame2;
          }
       } else {
-         int time = indices1[2].compareTo(indices2[2]);
-         if (time != 0) {
-            return time;
+         int frame1 = Integer.parseInt(indices1[2]), frame2 = Integer.parseInt(indices2[2]);
+         if (frame1 != frame2) {
+            return frame1 - frame2;
          }
-         int position = indices1[3].compareTo(indices2[3]);
-         if (position != 0) {
-            return position;
+         int position1 = Integer.parseInt(indices1[3]), position2 = Integer.parseInt(indices2[3]);
+         if (position1 != position2) {
+            return position1 - position2;
          }
       }
       if (slicesFirst_) {
-         int channel = indices1[0].compareTo(indices2[0]);
-         if (channel != 0) {
-            return channel;
+         int channel1 = Integer.parseInt(indices1[0]), channel2 = Integer.parseInt(indices2[0]);
+         if (channel1 != channel2) {
+            return channel1 - channel2;
          }
-         int slice = indices1[1].compareTo(indices2[1]);
-         if (slice != 0) {
-            return slice;
-         }
+         return Integer.parseInt(indices1[1]) - Integer.parseInt(indices2[1]);
       } else {
-         int slice = indices1[1].compareTo(indices2[1]);
-         if (slice != 0) {
-            return slice;
+         int slice1 = Integer.parseInt(indices1[1]), slice2 = Integer.parseInt(indices2[1]);
+         if (slice1 != slice2) {
+            return slice1 - slice2;
          }
-         int channel = indices1[0].compareTo(indices2[0]);
-         if (channel != 0) {
-            return channel;
-         }
+         return Integer.parseInt(indices1[0]) - Integer.parseInt(indices2[0]);
       }
-      return 0;
    }
-   
- 
-   
 }
