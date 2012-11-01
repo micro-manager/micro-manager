e962766faee696560fedc9d22383f05a22ac14d7
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-11-01 23:54:19 +0000

    change name to DLLAutoReloader
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10216 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/DLLAutoReloader/build.bat
+++ plugins/DLLAutoReloader/build.bat
@@ -2,6 +2,6 @@ rem We need Java 6 for this plugin. If you have Java 6, installed, set an enviro
 if exist classes rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
-java -cp src;classes;../../../3rdpartypublic/classext/*;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_Win32 -Dclojure.compile.path=classes -server clojure.lang.Compile DeviceAdapterHelper.plugin
+java -cp src;classes;../../../3rdpartypublic/classext/*;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_Win32 -Dclojure.compile.path=classes -server clojure.lang.Compile DLLAutoReloader.plugin
 jar cf DLLAutoReloader.jar -C classes\ .  
 copy /Y DLLAutoReloader.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
