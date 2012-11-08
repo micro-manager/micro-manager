6c453d19ba5372dfd17933ab162b5cfed73392ba
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-11-08 04:05:19 +0000

    Add AcqSettingsListener internal interface
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10240 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/internalinterfaces/AcqSettingsListener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.micromanager.internalinterfaces;
+
+/**
+ *
+ * @author Arthur
+ */
+public interface AcqSettingsListener {
+    public void settingsChanged();
+}
