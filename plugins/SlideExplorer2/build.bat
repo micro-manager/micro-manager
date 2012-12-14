a3df85d993875a967cba1ca0be8239b2a9bb3c30
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2012-12-14 20:09:19 +0000

    remove unnecessary files
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10374 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/SlideExplorer2/build.bat
+++ plugins/SlideExplorer2/build.bat
@@ -2,6 +2,7 @@ rem We need Java 6 for this plugin. If you have Java 6, installed, set an enviro
 if exist classes rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
-java -cp src;classes;../../../3rdpartypublic/classext/*;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_x64 -Dclojure.compile.path=classes -server clojure.lang.Compile slide-explorer.plugin
+java -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/core.cache.jar;../../../3rdpartypublic/classext/core.memoize.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_x64 -Dclojure.compile.path=classes -server clojure.lang.Compile slide-explorer.plugin
+rmdir /S /Q classes\clojure
 jar cf SlideExplorer2.jar -C classes\ .  
 copy /Y SlideExplorer2.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
