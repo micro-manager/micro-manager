e68f9322922f085f6238df506b058f1a375e598a
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-02-06 20:29:30 +0000

    Use mostly JIT compiling for acq eng.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10516 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -2,7 +2,9 @@ if exist classes rmdir classes /s /q
 mkdir classes
 xcopy /E /y src classes\
 "%JAVA_HOME%\bin\java" -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;../../3rdpartypublic/classext/bsh-2.0b4.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
-"%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C classes\ .    
+xcopy /E /y src build\
+copy classes\org\micromanager\AcquisitionEngine2010.class build\org\micromanager
+"%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C build\ .    
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
 copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
