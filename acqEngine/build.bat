1c06f59d49b0a84b6527f77649c8a1c93f7dd0ed
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-02-06 21:23:22 +0000

    fix bugs in JIT compiling build scripts.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10518 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -1,10 +1,11 @@
 if exist classes rmdir classes /s /q
 if exist build rmdir build /s /q
 mkdir classes
+mkdir build
 xcopy /E /y src classes\
 "%JAVA_HOME%\bin\java" -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;../../3rdpartypublic/classext/bsh-2.0b4.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
 xcopy /E /y src build\
-copy classes\org\micromanager\AcquisitionEngine2010.class build\org\micromanager
+xcopy classes\org\micromanager\AcquisitionEngine2010.class build\org\micromanager
 "%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C build\ .    
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
--- plugins/SlideExplorer2/build.bat
+++ plugins/SlideExplorer2/build.bat
@@ -1,10 +1,12 @@
 rem We need Java 6 for this plugin. If you have Java 6, installed, set an environment variable, JDK6, to its location.
 if exist classes rmdir classes /s /q
+if exist build rmdir build /s /q
 mkdir classes
+mkdir build
+mkdir 
 xcopy /E /y src classes\
 java -cp src;classes;../../../3rdpartypublic/classext/clojure.jar;../../../3rdpartypublic/classext/core.cache.jar;../../../3rdpartypublic/classext/core.memoize.jar;../../../3rdpartypublic/classext/ij.jar;../../../3rdpartypublic/classext/bsh-2.0b4.jar;../../mmstudio/MMJ_.jar;../../MMCoreJ_wrap/MMCoreJ.jar;../../acqEngine/MMAcqEngine.jar -Djava.library.path=../../bin_x64 -Dclojure.compile.path=classes -server clojure.lang.Compile slide-explorer.plugin
-rmdir /S /Q classes\clojure
-rmdir /S /Q classes\slide_explorer
-xcopy /E /y src classes\
-jar cf SlideExplorer2.jar -C classes\ .  
+xcopy /E /y src build\
+xcopy classes\org\micromanager\SlideExplorer2.class build\org\micromanager\
+jar cf SlideExplorer2.jar -C build\ .  
 copy /Y SlideExplorer2.jar ..\..\Install_AllPlatforms\micro-manager\mmplugins\
