4a0e995bf3eb99ea1673ebb80181636662511420
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-04-13 03:47:01 +0000

    Beginnings of UIMonitor for logging user inputs
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@10790 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/src/org/micromanager/utils/UIMonitor.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.micromanager.utils;
+
+import java.awt.AWTEvent;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.AWTEventListener;
+import java.awt.event.MouseEvent;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JMenuItem;
+import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Arthur
+ */
+public class UIMonitor {
+
+   private static AWTEventListener clickListener_ = null;
+
+   public static void enableUIMonitor() {
+      if (clickListener_ != null) {
+         return;
+      }
+      Toolkit toolkit = Toolkit.getDefaultToolkit();
+      clickListener_ = new AWTEventListener() {
+         @Override
+         public void eventDispatched(AWTEvent event) {
+            MouseEvent mouseEvent = (MouseEvent) event;
+            String message = "";
+            if (event.getID() == MouseEvent.MOUSE_CLICKED) {
+               Object source = mouseEvent.getSource();
+               if (source instanceof AbstractButton) {
+                  AbstractButton button = (AbstractButton) source;
+                  message += "\n\"" + button.getText() + "\" ";
+                  boolean selectable = (source instanceof JToggleButton)
+                             || (source instanceof JMenuItem);
+                  if (selectable) { 
+                    message += "toggled " + (button.isSelected() ? "on " : "off ");
+                  } else {
+                    message += "clicked ";
+                  }
+               }
+               if (!message.isEmpty()) {
+                  message += "in " + ((Frame) SwingUtilities.getAncestorOfClass(Frame.class, (JComponent) source)).getClass().getSimpleName() + ".";
+                  ReportingUtils.logMessage(message);
+               }            
+            }
+         }
+      };
+      toolkit.addAWTEventListener(clickListener_, AWTEvent.MOUSE_EVENT_MASK);
+   }
+
+   public static void disableUIMonitor() {
+      Toolkit.getDefaultToolkit().removeAWTEventListener(clickListener_);
+      clickListener_ = null;
+   }
+}
