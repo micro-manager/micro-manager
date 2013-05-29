acf3570ebeb734805e1c66a0f138d7d761a2625d
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-05-29 17:20:49 +0000

    Bring back AOT compiling for acquisition engine
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@11162 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -4,9 +4,7 @@ mkdir classes
 mkdir build
 xcopy /E /y src classes\
 "%JAVA_HOME%\bin\java" -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;../../3rdpartypublic/classext/bsh-2.0b4.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
-xcopy /E /y src build\
-xcopy classes\org\micromanager\AcquisitionEngine2010.class build\org\micromanager
-"%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C build\ .    
+"%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C classes\ .    
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
 copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
