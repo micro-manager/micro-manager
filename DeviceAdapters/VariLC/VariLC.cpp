ca2caa4b8c2cde055745c6e38743f784ce1a9525
amitabh <amitabh@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-06-26 21:46:57 +0000

    Added State commands to implement Palette elements Version info COM port search Compatibility for Abrio systems
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@11290 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/VariLC/VariLC.cpp
+++ DeviceAdapters/VariLC/VariLC.cpp
@@ -8,7 +8,22 @@
 // AUTHOR:        Rudolf Oldenbourg, MBL, w/ Arthur Edelstein and Karl Hoover, UCSF, Sept, Oct 2010
 // COPYRIGHT:     
 // LICENSE:       
-// 
+
+// Change Log - Amitabh Verma, Grant Harris - Nov. 10, 2012
+// 1. Implemented Number of Active LCs, Total No. of LCs and baud value for searching appropriate VariLC (9600) or Abrio (115200)
+
+// Change Log - Amitabh Verma, Grant Harris - Oct 10, 2012
+// 1. Implemented COM port Search ability using Micro-Manager Hardware Wizard
+
+// Change Log - Amitabh Verma, Grant Harris - Aug 08, 2012
+// 1. State commands 0-5 which implement Palette
+
+// Change Log - Amitabh Verma, Grant Harris - Apr 27, 2012
+// 1. Added Version numbering
+// 2. Serial number device property
+// 3. Total Number of LCs default value to "2" instead of "4"
+// 4. wavelength default 546
+
 
 #ifdef WIN32
 //   #include <windows.h>
@@ -16,8 +31,9 @@
 #endif
 
 #include "VariLC.h"
+#include <cstdio>
 #include <string>
-//#include <math.h>
+#include <math.h>
 #include "../../MMDevice/ModuleInterface.h"
 #include <sstream>
 
@@ -27,17 +43,20 @@ const char* g_ControllerName    = "VariLC";
 const char* g_TxTerm            = "\r"; //unique termination
 const char* g_RxTerm            = "\r"; //unique termination
 
+const char* g_BaudRate_key        = "Baud Rate";
+const char* g_Baud9600            = "9600";
+const char* g_Baud115200          = "115200";
+
+
 
 using namespace std;
-
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // Exported MMDevice API
 ///////////////////////////////////////////////////////////////////////////////
 MODULE_API void InitializeModuleData()
 {
-   AddAvailableDeviceName(g_ControllerName,    "VariLC");             
+   AddAvailableDeviceName(g_ControllerName,    "VariLC");
 }                                                                            
 
 MODULE_API MM::Device* CreateDevice(const char* deviceName)                  
@@ -57,7 +76,7 @@ MODULE_API void DeleteDevice(MM::Device* pDevice)
 int ClearPort(MM::Device& device, MM::Core& core, std::string port)
 {
    // Clear contents of serial port 
-   const int bufSize = 255;
+   const int bufSize = 2048;
    unsigned char clear[bufSize];                                                        
    unsigned long read = bufSize;                                               
    int ret;                                                                    
@@ -82,29 +101,38 @@ VariLC::VariLC() :
   answerTimeoutMs_(1000),
   initialized_(false),
   numActiveLCs_(2),
-  numTotalLCs_(4),
-  numPalEls_(5)
+  numTotalLCs_(2),
+  numPalEls_(5),
+  baud_(g_Baud9600)
 {
    InitializeDefaultErrorMessages();
-
    
    // pre-initialization properties
+
 
    // Port:
    CPropertyAction* pAct = new CPropertyAction(this, &VariLC::OnPort);
    CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
-   
+   SetProperty(MM::g_Keyword_Port, port_.c_str());
+
    pAct = new CPropertyAction (this, &VariLC::OnNumActiveLCs);
    CreateProperty("Number of Active LCs", "2", MM::Integer, false, pAct, true); 
    
    pAct = new CPropertyAction (this, &VariLC::OnNumTotalLCs);
-   CreateProperty("Total Number of LCs", "4", MM::Integer, false, pAct, true); 
+   CreateProperty("Total Number of LCs", "2", MM::Integer, false, pAct, true); 
    
    pAct = new CPropertyAction (this, &VariLC::OnNumPalEls);
    CreateProperty("Total Number of Palette Elements", "5", MM::Integer, false, pAct, true); 
    
+   pAct = new CPropertyAction(this, &VariLC::OnBaud);
+   CreateProperty(g_BaudRate_key, "Undefined", MM::String, false, pAct, true);  
+   
+   AddAllowedValue(g_BaudRate_key, g_Baud115200, (long) 115200);
+   AddAllowedValue(g_BaudRate_key, g_Baud9600, (long) 9600);
+
    EnableDelay();
 }
+
 
 VariLC::~VariLC()
 {
@@ -116,6 +144,81 @@ void VariLC::GetName(char* name) const
    CDeviceUtils::CopyLimitedString(name, g_ControllerName);
 }
 
+
+MM::DeviceDetectionStatus VariLC::DetectDevice(void)
+{
+   // all conditions must be satisfied...
+   MM::DeviceDetectionStatus result = MM::Misconfigured;
+
+   try
+   {	   
+	   long baud;
+	   GetProperty(g_BaudRate_key, baud);
+
+      std::string transformed = port_;
+      for( std::string::iterator its = transformed.begin(); its != transformed.end(); ++its)
+      {
+         *its = (char)tolower(*its);
+      }	  	     
+
+      if( 0< transformed.length() &&  0 != transformed.compare("undefined")  && 0 != transformed.compare("unknown") )
+      {
+		int ret = 0;	  
+		MM::Device* pS;
+
+		
+			 // the port property seems correct, so give it a try
+			 result = MM::CanNotCommunicate;
+			 // device specific default communication parameters
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_AnswerTimeout, "2000.0");			 
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_BaudRate, baud_.c_str() );
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_DelayBetweenCharsMs, "0.0");
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_Handshaking, "Off");
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_Parity, "None");
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_StopBits, "1");
+			 GetCoreCallback()->SetDeviceProperty(port_.c_str(), "Verbose", "1");
+			 pS = GetCoreCallback()->GetDevice(this, port_.c_str());
+			 pS->Initialize();
+	         
+			 ClearPort(*this, *GetCoreCallback(), port_);
+			 ret = SendSerialCommand(port_.c_str(), "V?", "\r");     
+			 GetSerialAnswer (port_.c_str(), "\r", serialnum_);
+			 GetSerialAnswer (port_.c_str(), "\r", serialnum_);
+				 if (ret!=DEVICE_OK || serialnum_.length() < 5)
+				 {
+					LogMessageCode(ret,true);
+					LogMessage(std::string("VariLC not found on ")+port_.c_str(), true);
+					LogMessage(std::string("VariLC serial no:")+serialnum_, true);
+					ret = 1;
+					serialnum_ = "0";
+					pS->Shutdown();	
+				 } else
+				 {
+					// to succeed must reach here....
+					LogMessage(std::string("VariLC found on ")+port_.c_str(), true);
+					LogMessage(std::string("VariLC serial no:")+serialnum_, true);
+					result = MM::CanCommunicate;	
+					GetCoreCallback()->SetSerialProperties(port_.c_str(),
+											  "600.0",
+											  baud_.c_str(),
+											  "0.0",
+											  "Off",
+											  "None",
+											  "1");
+					serialnum_ = "0";
+					pS->Initialize();
+					ret = SendSerialCommand(port_.c_str(), "R 1", "\r");
+					ret = SendSerialCommand(port_.c_str(), "C 0", "\r");
+					pS->Shutdown();					
+				}
+      }
+   }
+   catch(...)
+   {
+      LogMessage("Exception in DetectDevice!",false);
+   }
+   return result;
+}
 
 int VariLC::Initialize()
 {
@@ -136,11 +239,30 @@ int VariLC::Initialize()
    ret = CreateProperty(MM::g_Keyword_Name, g_ControllerName, MM::String, true);
    if (DEVICE_OK != ret)
       return ret;
+    
+    // Version number
+   CPropertyAction* pAct = new CPropertyAction (this, &VariLC::OnSerialNumber);
+   ret = CreateProperty("Version Number", "Version Number Not Found", MM::String, true, pAct);
+   if (ret != DEVICE_OK)
+      return ret;
 
-   CPropertyAction* pAct = new CPropertyAction (this, &VariLC::OnBriefMode);
+   // Active LC number
+   pAct = new CPropertyAction (this, &VariLC::OnNumActiveLCs);
+   ret = CreateProperty("Active LCs", "0", MM::Integer, true, pAct);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   // Total LC number
+   pAct = new CPropertyAction (this, &VariLC::OnNumTotalLCs);
+   ret = CreateProperty("Total LCs", "0", MM::Integer, true, pAct);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   pAct = new CPropertyAction (this, &VariLC::OnBriefMode);
    ret = CreateProperty("Mode, 1=Brief, 0=Standard", "", MM::String, true, pAct); 
    if (ret != DEVICE_OK)
 	   return ret;
+
    //Set VariLC to Standard mode
    briefModeQ_ = false;
    ret = SendSerialCommand(port_.c_str(), "B 0", "\r");
@@ -154,8 +276,9 @@ int VariLC::Initialize()
    if (getFromVariLC_.length() == 0)
 	   return DEVICE_NOT_CONNECTED;
 
+   // Wavelength
    pAct = new CPropertyAction (this, &VariLC::OnWavelength);
-   ret = CreateProperty("Wavelength", "550.0", MM::Float, false, pAct); 
+   ret = CreateProperty("Wavelength", "546.0", MM::Float, false, pAct); 
    if (ret != DEVICE_OK)
       return ret;
    SetPropertyLimits("Wavelength", 400., 800.);
@@ -169,12 +292,29 @@ int VariLC::Initialize()
 	   s << "Retardance LC-" << char(65+i);
 	   pActX = new CPropertyActionEx(this, &VariLC::OnRetardance, i);
 	   CreateProperty(s.str().c_str(), "0.5", MM::Float, false, pActX);
-	   SetPropertyLimits(s.str().c_str(), 0.14, 1.2);
+	   SetPropertyLimits(s.str().c_str(), 0.0001, 3);
    }
  
+//   for (long i=0;i<numPalEls_;++i) {
+//	   ostringstream s;
+//	   s << "Pal. elem. " << char(48+i) << ", enter 0 to define, 1 to activate";
+//	   pActX = new CPropertyActionEx(this, &VariLC::OnPalEl, i);
+//	   CreateProperty(s.str().c_str(), "", MM::String, false, pActX);
+//   }
+
    for (long i=0;i<numPalEls_;++i) {
 	   ostringstream s;
-	   s << "Pal. elem. " << char(48+i) << ", enter 0 to define, 1 to activate";
+	   std::string number;
+
+	   std::stringstream strstream;
+	   strstream << i;
+	   strstream >> number;
+	   if (i < 10) {
+			number = "0"+number;
+		}
+	   
+
+	   s << "Pal. elem. " << number << ", enter 0 to define, 1 to activate";
 	   pActX = new CPropertyActionEx(this, &VariLC::OnPalEl, i);
 	   CreateProperty(s.str().c_str(), "", MM::String, false, pActX);
    }
@@ -190,7 +330,8 @@ int VariLC::Initialize()
       return ret;
 
    // Needed for Busy flag
-   changedTime_ = GetCurrentMMTime() - GetDelayMs();
+   changedTime_ = GetCurrentMMTime();
+   SetErrorText(99, "Device set busy for ");
 
    return DEVICE_OK;
 }
@@ -220,9 +361,30 @@ int VariLC::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
          return DEVICE_INVALID_INPUT_PARAM;
       }
       pProp->Get(port_);
-   }
+   }   
+      
    return DEVICE_OK;
 }
+
+int VariLC::OnBaud(MM::PropertyBase* pProp, MM::ActionType eAct)
+{	
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(baud_.c_str());
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      if (initialized_)
+      {
+         pProp->Set(baud_.c_str());
+         return DEVICE_INVALID_INPUT_PARAM;
+      }
+      pProp->Get(baud_);
+   }
+
+   return DEVICE_OK;
+}
+
 
  int VariLC::OnBriefMode(MM::PropertyBase* pProp, MM::ActionType eAct)
 {
@@ -231,7 +393,8 @@ int VariLC::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
          if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
      std::string ans;
      GetSerialAnswer (port_.c_str(), "\r", ans);
-     GetSerialAnswer (port_.c_str(), "\r", ans);
+	 GetSerialAnswer (port_.c_str(), "\r", ans);	 
+     
 	 if (ans == "1") {
 		 briefModeQ_ = true;
 	 } else {
@@ -270,11 +433,12 @@ int VariLC::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
      pProp->Set(numActiveLCs_);
     }
    else if (eAct == MM::AfterSet)
-   {
+   {		
 	  pProp->Get(numActiveLCs_);
    }
    return DEVICE_OK;
 }
+
 
  int VariLC::OnNumPalEls(MM::PropertyBase* pProp, MM::ActionType eAct)
 {
@@ -289,6 +453,20 @@ int VariLC::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
    return DEVICE_OK;
 }
 
+ int VariLC::OnSerialNumber (MM::PropertyBase* pProp, MM::ActionType eAct)
+ {
+   if (eAct == MM::BeforeGet)
+  {
+     int ret = SendSerialCommand(port_.c_str(), "V?", "\r");
+         if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
+	 GetSerialAnswer (port_.c_str(), "\r", serialnum_);
+	 GetSerialAnswer (port_.c_str(), "\r", serialnum_);
+	 
+	 pProp->Set(serialnum_.c_str());
+   }
+   return DEVICE_OK;
+ }
+
 int VariLC::OnWavelength (MM::PropertyBase* pProp, MM::ActionType eAct)
  {
    if (eAct == MM::BeforeGet)
@@ -297,7 +475,7 @@ int VariLC::OnWavelength (MM::PropertyBase* pProp, MM::ActionType eAct)
          if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
      std::string ans;
 	 GetSerialAnswer (port_.c_str(), "\r", ans);
-     GetSerialAnswer (port_.c_str(), "\r", ans);
+	 GetSerialAnswer (port_.c_str(), "\r", ans);
 
      vector<double> numbers = getNumbersFromMessage(ans,briefModeQ_);
      pProp->Set(numbers[0]);
@@ -315,8 +493,8 @@ int VariLC::OnWavelength (MM::PropertyBase* pProp, MM::ActionType eAct)
      if (ret!=DEVICE_OK)
 	     return DEVICE_SERIAL_COMMAND_FAILED;
      std::string ans;
-	 GetSerialAnswer (port_.c_str(), "\r", ans);
-
+     GetSerialAnswer (port_.c_str(), "\r", ans);
+	 
      wavelength_ = wavelength;
 // Clear palette elements after change of wavelength
    }
@@ -363,7 +541,7 @@ int VariLC::OnRetardance(MM::PropertyBase* pProp, MM::ActionType eAct, long inde
    		return DEVICE_SERIAL_COMMAND_FAILED;
       
       std::string ans;
-	  GetSerialAnswer (port_.c_str(), "\r", ans);
+	  GetSerialAnswer (port_.c_str(), "\r", ans);	  
 
       retardance_[index] = retardance;
 
@@ -388,7 +566,7 @@ int VariLC::OnRetardance(MM::PropertyBase* pProp, MM::ActionType eAct, long inde
      vector<double> numbers = getNumbersFromMessage(ans,briefModeQ_);
      int elemNr = (int) numbers[0];
     if (elemNr == 0) {
-		for (int i=0;i<5;i++) {
+		for (int i=0;i<numPalEls_+1;i++) {
 			palEl_[i] = "";
 		}
 	 } else {
@@ -421,13 +599,13 @@ int VariLC::OnRetardance(MM::PropertyBase* pProp, MM::ActionType eAct, long inde
 		   cmd << "D " << index;
 			int ret = SendSerialCommand(port_.c_str(), cmd.str().c_str(), "\r");
 				if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
-				GetSerialAnswer (port_.c_str(), "\r", ans);
+				GetSerialAnswer (port_.c_str(), "\r", ans);				
 		}
 		if (setPalEl == 1) {
 		   cmd << "P " << index;
 			int ret = SendSerialCommand(port_.c_str(), cmd.str().c_str(), "\r");
-				if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
-				GetSerialAnswer (port_.c_str(), "\r", ans);
+				if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;				
+				GetSerialAnswer (port_.c_str(), "\r", ans);				 
 		}
    }
    return DEVICE_OK;
@@ -445,22 +623,86 @@ int VariLC::OnSendToVariLC(MM::PropertyBase* pProp, MM::ActionType eAct)
       // read value from property
       pProp->Get(sendToVariLC_);
       // write retardance out to device....
+	  
+	  int len = strlen(sendToVariLC_.c_str());
+	  char state[5]; 
+
+	  if (len > 5) {		  
+		  strncpy(state, sendToVariLC_.c_str(), 5);
+		  state[5] = '\0';
+	  }
+
       if (sendToVariLC_=="Escape") {
          char command[2];
          command[0]=27;
          command[1]=0;
-         int ret = SendSerialCommand(port_.c_str(), command, 0);
+         int ret = SendSerialCommand(port_.c_str(), command, "\r");
          if (ret!=DEVICE_OK)
-		   return DEVICE_SERIAL_COMMAND_FAILED;
-		 GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);
-         getFromVariLC_ = "";
-      } else {
-         int ret = SendSerialCommand(port_.c_str(), sendToVariLC_.c_str(), "\r");
+		   return DEVICE_SERIAL_COMMAND_FAILED;		 
+      } 
+	  else if (sendToVariLC_=="@") {         
+		 int ret = SendSerialCommand(port_.c_str(), sendToVariLC_.c_str(), "\r");
          if (ret!=DEVICE_OK)
 		      return DEVICE_SERIAL_COMMAND_FAILED;
-		 GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);
-         GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);
       }
+	  else if (sendToVariLC_=="!") {                  
+		 int ret = SendSerialCommand(port_.c_str(), sendToVariLC_.c_str(), "\r");		 
+         if (ret!=DEVICE_OK)
+		      return DEVICE_SERIAL_COMMAND_FAILED;		
+      }
+	  else if ((std::string)state=="State") {   
+		  	  
+			  std::vector<char> val(len-5);
+			  for (int i=5; i < len; i++) {				  
+					val[5-i] = sendToVariLC_[i];
+			  }
+			  val[len] = '\0';
+
+			  std::stringstream ss;
+				for(size_t i = 0; i < val.size(); ++i)
+				{
+				  if(i != 0)
+					ss << ",";
+				  ss << val[i];
+				}
+			 std::string s = ss.str();
+
+			 sendToVariLC_ = "P" + s;
+
+			 changedTime_ = GetCurrentMMTime();
+			 int ret = SendSerialCommand(port_.c_str(), sendToVariLC_.c_str(), "\r");
+				 if (ret!=DEVICE_OK) {
+				   return DEVICE_SERIAL_COMMAND_FAILED;
+				 }
+      }
+	  else if (sendToVariLC_=="W ?" || sendToVariLC_=="W?") {
+		  int ret = SendSerialCommand(port_.c_str(), "W?", "\r");
+			 if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
+		 GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);		 
+	  }
+	  else if (sendToVariLC_=="V ?" || sendToVariLC_=="V?") {
+		  int ret = SendSerialCommand(port_.c_str(), "V?", "\r");
+			 if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
+		 GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);		 
+	  }
+	  else if (sendToVariLC_=="B ?" || sendToVariLC_=="B?") {
+		  int ret = SendSerialCommand(port_.c_str(), "B?", "\r");
+			 if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
+		 GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);		 
+	  }
+	  else if (sendToVariLC_=="L ?" || sendToVariLC_=="L?") {
+		  int ret = SendSerialCommand(port_.c_str(), "L?", "\r");
+			 if (ret!=DEVICE_OK)return DEVICE_SERIAL_COMMAND_FAILED;
+		 GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);		 
+	  }
+	  else {
+         int ret = SendSerialCommand(port_.c_str(), sendToVariLC_.c_str(), "\r");
+         if (ret!=DEVICE_OK)
+		      return DEVICE_SERIAL_COMMAND_FAILED;		 
+      }
+	  
+		GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);  
+	  
    }
    return DEVICE_OK;
 }
@@ -469,6 +711,7 @@ int VariLC::OnGetFromVariLC(MM::PropertyBase* pProp, MM::ActionType eAct)
 {
    if (eAct == MM::BeforeGet)
    {
+	//   GetSerialAnswer (port_.c_str(), "\r", getFromVariLC_);
       pProp->Set(getFromVariLC_.c_str());
    }
    else if (eAct == MM::AfterSet)
@@ -512,4 +755,3 @@ std::vector<double> VariLC::getNumbersFromMessage(std::string variLCmessage, boo
 
 	return values;
 }
-
--- DeviceAdapters/VariLC/VariLC.h
+++ DeviceAdapters/VariLC/VariLC.h
@@ -7,6 +7,7 @@
 //
 //
 // AUTHOR:        Rudolf Oldenbourg, MBL, w/ Arthur Edelstein and Karl Hoover, UCSF, Sept, Oct 2010
+//				  modified Amitabh Verma Apr. 17, 2012
 // COPYRIGHT:     
 // LICENSE:       
 //
@@ -17,11 +18,12 @@
 
 #include "../../MMDevice/MMDevice.h"
 #include "../../MMDevice/DeviceBase.h"
+
 #include <string>
 #include <map>
 
 //////////////////////////////////////////////////////////////////////////////
-
+#define ERR_PORT_CHANGE_FORBIDDEN 109
 
 // MMCore name of serial port
 std::string port_;
@@ -41,45 +43,52 @@ class VariLC : public CGenericBase<VariLC>
 
       void GetName(char* pszName) const;
       bool Busy();
+	  int GetVLCSerialAnswer (const char* portName, const char* term, std::string& ans);
 
 //      int Initialize(MM::Device& device, MM::Core& core);
       int DeInitialize() {initialized_ = false; return DEVICE_OK;};
       bool Initialized() {return initialized_;};
+	  
+	  // device discovery
+      MM::DeviceDetectionStatus DetectDevice(void);
 
       // action interface
       // ---------------
       int OnPort    (MM::PropertyBase* pProp, MM::ActionType eAct);
+	  int OnBaud	(MM::PropertyBase* pProp, MM::ActionType eAct);
       int OnBriefMode (MM::PropertyBase* pProp, MM::ActionType eAct);
       int OnWavelength (MM::PropertyBase* pProp, MM::ActionType eAct);
+	  int OnSerialNumber (MM::PropertyBase* pProp, MM::ActionType eAct);
       int OnNumTotalLCs (MM::PropertyBase* pProp, MM::ActionType eAct);
-      int OnNumActiveLCs (MM::PropertyBase* pProp, MM::ActionType eAct);
+      int OnNumActiveLCs (MM::PropertyBase* pProp, MM::ActionType eAct);	  
       int OnRetardance (MM::PropertyBase* pProp, MM::ActionType eAct, long index);
 //      int OnEpilogueL (MM::PropertyBase* pProp, MM::ActionType eAct);
       int OnNumPalEls (MM::PropertyBase* pProp, MM::ActionType eAct);
       int OnPalEl (MM::PropertyBase* pProp, MM::ActionType eAct, long index);
       int OnSendToVariLC (MM::PropertyBase* pProp, MM::ActionType eAct);
       int OnGetFromVariLC (MM::PropertyBase* pProp, MM::ActionType eAct);
-
+	  
    private:
       // Command exchange with MMCore
       std::string command_;
-      double answerTimeoutMs_;
+	  std::string baud_;
       bool initialized_;
-	   bool briefModeQ_;
+      double answerTimeoutMs_;
+	  bool briefModeQ_;
       double wavelength_; // the cached value
+      long numTotalLCs_;  // total number of LCs	  
       long numActiveLCs_;  // number of actively controlled LCs (the actively controlled LCs appear first in the list of retardance values in the L-command)
-      long numTotalLCs_;  // total number of LCs
-      double retardance_[8]; // retardance values of total number of LCs; I made the index 8, a high number unlikely to be exceeded by the variLC hardware
+      double retardance_[25]; // retardance values of total number of LCs; I made the index 8, a high number unlikely to be exceeded by the variLC hardware
       std::string epilogueL_; // added at the end of every L command to account for uncontrolled LCs
       long numPalEls_;  // total number of palette elements
-	  std::string palEl_[5]; // array of palette elements, index is total number of elements
+	  std::string palEl_[99]; // array of palette elements, index is total number of elements
 //      std::string currRet_;
+	  std::string serialnum_;
       std::string sendToVariLC_;
       std::string getFromVariLC_;
-
-      std::vector<double> getNumbersFromMessage(std::string variLCmessage, bool prefixQ);
 	  MM::MMTime changedTime_;
-
+	  
+      std::vector<double> getNumbersFromMessage(std::string variLCmessage, bool prefixQ);
 };
 
 
