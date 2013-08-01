adc779120351162c00cd1f8862fb6e6fd3e079b0
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-08-01 03:04:13 +0000

    Fix Windows acqEngine build script.
    Avoid copying to a nonexistent directory, thereby creating a file with that
    name.
    
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@11489 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -7,5 +7,7 @@ xcopy /E /y src classes\
 "%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C classes\ .    
 copy /Y MMAcqEngine.jar ..\bin_Win32\
 copy /Y MMAcqEngine.jar ..\bin_x64\
-copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
-copy /Y MMAcqEngine.jar ..\Install_x64\micro-manager\plugins\Micro-Manager"
+mkdir ..\Install_Win32\micro-manager\plugins\Micro-Manager
+copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager\
+mkdir ..\Install_x64\micro-manager\plugins\Micro-Manager
+copy /Y MMAcqEngine.jar ..\Install_x64\micro-manager\plugins\Micro-Manager\
