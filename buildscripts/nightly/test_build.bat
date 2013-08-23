ae4bf2f10e5bdb48bca0315ecddbe47a3e1cd1b6
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-08-23 22:29:22 +0000

    Batch file to run simulated nightly build.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@11664 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ buildscripts/nightly/test_build.bat
@@ -0,0 +1,40 @@
+@echo off
+
+rem This batch file is not actually used for the nightly build, but performs
+rem similar steps and is handy for testing the build with similar settings.
+
+rem It also demonstrates how to set up an automated build by calling Ant
+rem directly.
+
+rem Note, however, that this batch file will not clean the source tree before
+rem building.
+
+
+set batch_file_dir=%~dp0
+set src_root=%batch_file_dir%\..\..
+set log_filename=%src_root%\buildlog.xml
+set report_filename=%src_root%\buildreport.html
+
+if exist %log_filename% (
+  del %log_filename%
+)
+
+call ant -f %src_root%\buildscripts\nightly\nightlybuild_Windows.xml ^
+  -listener org.apache.tools.ant.XmlLogger ^
+  -logger org.apache.tools.ant.listener.SimpleBigProjectLogger ^
+  -DXmlLogger.file=%log_filename% ^
+  -Dmm.versionstring=test_build
+
+if not exist %log_filename% (
+  echo Ant failed without producing XML log file
+  exit /b 1
+)
+
+python -c "import sys" -c "if sys.version_info.major != 3 or sys.version_info.minor < 3: sys.exit(1)"
+if errorlevel 1 (
+  echo Python 3.3 or later required to generate build error report
+  exit /b
+)
+
+python %src_root%\buildscripts\nightly\genreport_Windows.py %src_root% %log_filename% %report_filename%
+start %report_filename%
