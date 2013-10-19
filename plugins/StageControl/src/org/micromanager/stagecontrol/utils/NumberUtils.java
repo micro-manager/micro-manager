a6523252a3911060d63ed8ebbf3848d35adf8115
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-10-19 01:20:34 +0000

    Fix build problem in StageControl plugin build
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@11982 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/StageControl/src/org/micromanager/stagecontrol/utils/NumberUtils.java
+++ plugins/StageControl/src/org/micromanager/stagecontrol/utils/NumberUtils.java
@@ -1,13 +1,30 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Class emulating Micro-Manager's ReportingUtils
+ * Goal is to avoid dependencies on MMJ_.jar
+ * 
  */
 package org.micromanager.stagecontrol.utils;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 /**
  *
+ * 
+ * 
  * @author nico
  */
 public class NumberUtils {
+   private static final NumberFormat format_;
    
+   static {
+		// The display is supposed to use local formating (e.g., switch commas with periods in Locale.GERMANY).
+		format_ = NumberFormat.getInstance();
+      format_.setMaximumFractionDigits(4);
+   }
+   
+   public static String doubleToDisplayString(double number) {
+		return format_.format(number);
+	}
+
 }
