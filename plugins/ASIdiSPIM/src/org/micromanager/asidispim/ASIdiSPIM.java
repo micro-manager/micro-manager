95f28d3b8eafe6ab928829f294fed36cfee090f0
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-11-26 05:26:56 +0000

    Start of ASIdiSPIM plugin
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@12194 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ plugins/ASIdiSPIM/src/org/micromanager/asidispim/ASIdiSPIM.java
@@ -0,0 +1,64 @@
+package org.micromanager.asidispim;
+
+import java.awt.Frame;
+import mmcorej.CMMCore;
+import org.micromanager.api.MMPlugin;
+import org.micromanager.api.ScriptInterface;
+import org.micromanager.utils.ReportingUtils;
+
+
+public class ASIdiSPIM implements MMPlugin {
+   public static String menuName = "ASI diSPIM";
+   public static String tooltipDescription = "Control the ASI diSPIM ";
+   private CMMCore core_;
+   private ScriptInterface gui_;
+   private Frame myFrame_;
+
+    @Override
+   public void setApp(ScriptInterface app) {
+      gui_ = app;                                        
+      core_ = app.getMMCore();
+      if (myFrame_ == null) {
+         try {
+            //myFrame_ = new MultiCameraFrame(gui_);
+            //gui_.addMMListener(myFrame_);
+            //gui_.addMMBackgroundListener(myFrame_);
+         } catch (Exception e) {
+            ReportingUtils.showError(e);
+            return;
+         }
+      }
+      //myFrame_.setVisible(true);
+   }
+
+    @Override
+   public void dispose() {
+      //if (myFrame_ != null)
+         //myFrame_.safePrefs();
+   }
+
+    @Override
+   public void show() {
+         String ig = "ASI diSPIM";
+   }
+
+    @Override
+   public String getInfo () {
+      return "ASI diSPIM";
+   }
+
+    @Override
+   public String getDescription() {
+      return tooltipDescription;
+   }
+   
+    @Override
+   public String getVersion() {
+      return "0.1";
+   }
+   
+    @Override
+   public String getCopyright() {
+      return "University of California and ASI, 2013";
+   }
+}
