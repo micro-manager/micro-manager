afcbd037542c21b87f24de46d34eb9fd209d8041
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-11-26 05:49:32 +0000

    Start of ASIdiSPIMFrame
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@12195 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ plugins/ASIdiSPIM/src/org/micromanager/asidispim/ASIdiSPIMFrame.java
@@ -0,0 +1,35 @@
+
+package org.micromanager.asidispim;
+
+import javax.swing.JTabbedPane;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ *
+ * @author nico
+ */
+public class ASIdiSPIMFrame {
+   
+   public ASIdiSPIMFrame() {
+      
+      JTabbedPane tabbedPane = new JTabbedPane();
+      
+      //JPanel p1 = createTabPanel(new MigLayout("fill"));
+      
+      
+   }
+   /*
+   private TabItem createTabPanel(TabFolder parent, String text, Layout lm)      
+   {
+      Composite panel = new Composite(parent, DOUBLE_BUFFER);
+ //                panel.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+      TabItem tab = new TabItem(parent, DOUBLE_BUFFER);
+      tab.setControl(panel);
+      tab.setText(text);
+      if (lm != null)
+         panel.setLayout(lm);
+ //                configureActiveComponet(panel);
+      return tab;
+   }
+   */
+}
