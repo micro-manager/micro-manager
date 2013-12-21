2ba4feb5b278ef7f41ba4c8e3766939d43e787f6
jondaniels <jondaniels@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2013-12-21 00:50:19 +0000

    ASIdiSPIM: adding new interface file forgotten last checkin
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@12434 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/UpdateFromPropertyListenerInterface.java
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+//FILE:          UpdateFromPropertyListenerInterface.java
+//PROJECT:       Micro-Manager 
+//SUBSYSTEM:     ASIdiSPIM plugin
+//-----------------------------------------------------------------------------
+//
+// AUTHOR:       Nico Stuurman, Jon Daniels
+//
+// COPYRIGHT:    University of California, San Francisco, & ASI, 2013
+//
+// LICENSE:      This file is distributed under the BSD license.
+//               License text is included with the source distribution.
+//
+//               This file is distributed in the hope that it will be useful,
+//               but WITHOUT ANY WARRANTY; without even the implied warranty
+//               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+
+package org.micromanager.asidispim.Utils;
+
+/**
+ *
+ * @author Jon
+ */
+public interface UpdateFromPropertyListenerInterface {
+    /**
+    * This function will be called when we refresh state from device property
+    */
+   public void updateFromProperty();
+}
