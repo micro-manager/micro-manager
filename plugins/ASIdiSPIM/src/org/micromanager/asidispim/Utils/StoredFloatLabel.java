79b05c022426fe39c8eb3df22831c77c5b98d942
jondaniels <jondaniels@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-07-01 20:09:34 +0000

    ASIdiSPIM: formatting changes only
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@13773 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/StoredFloatLabel.java
+++ plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/StoredFloatLabel.java
@@ -21,83 +21,86 @@
 package org.micromanager.asidispim.Utils;
 
 import java.text.ParseException;
+
 import javax.swing.JLabel;
+
 import org.micromanager.api.ScriptInterface;
 import org.micromanager.asidispim.Data.Prefs;
 import org.micromanager.utils.NumberUtils;
 
 
-  /**
-    * Utility class that ties a JLabel to a value stored in the preferences
-    * @author Nico
+/**
+ * Utility class that ties a JLabel to a value stored in the preferences
+ * @author Nico
+ */
+@SuppressWarnings("serial")
+public class StoredFloatLabel extends JLabel {
+   private final String prefNode_;
+   private final String prefKey_;
+   private final Prefs prefs_;
+   private final ScriptInterface gui_;
+
+   /**
+    * Creates a JLabel and overrides the setText methods so that 
+    * changed in the Label will be written to the preferences
+    * Adds a setFloat method for convenience
+    * 
+    * @param prefNode - Node used to store the value in preferences
+    * @param prefKey - Key used to store the value in preferences
+    * @param defaultValue - default value in case nothing is found in prefs
+    * @param prefs - Global preferences object used in this plugin
+    * @param gui - MM ScriptInterface instance
     */
-   public class StoredFloatLabel extends JLabel {
-      private final String prefNode_;
-      private final String prefKey_;
-      private final Prefs prefs_;
-      private final ScriptInterface gui_;
-      
-      /**
-       * Creates a JLabel and overrides the setText methods so that 
-       * changed in the Label will be written to the preferences
-       * Adds a setFloat method for convenience
-       * 
-       * @param prefNode - Node used to store the value in preferences
-       * @param prefKey - Key used to store the value in preferences
-       * @param defaultValue - default value in case nothing is found in prefs
-       * @param prefs - Global preferences object used in this plugin
-       * @param gui - MM ScriptInterface instance
-       */
-      public StoredFloatLabel(String prefNode, String prefKey, float defaultValue, 
-              Prefs prefs, ScriptInterface gui) {               
-          super();
-          prefNode_ = prefNode;
-          prefKey_ = prefKey;
-          prefs_ = prefs;
-          gui_ = gui;
-          super.setText("" + (prefs_.getFloat(prefNode, prefKey, defaultValue)));
-      }
-      
-      /**
-       * Sets the text of the JLabel and stores the value in Preferences
-       * @param txt 
-       */
-      @Override
-      public void setText(String txt) {
-         super.setText(txt);
-         if (prefNode_ != null && prefKey_ != null) {
-            try {
-               prefs_.putFloat(prefNode_, prefKey_,
-                       ((Double) NumberUtils.displayStringToDouble(txt)).floatValue());
-            } catch (ParseException e) {
-               gui_.showError(e);
-            }
-         }
-      }
-      
-      /**
-       * Convenience method to display a float and store value in prefs 
-       * @param val 
-       */
-      public void setFloat(float val) {
-         super.setText(NumberUtils.doubleToDisplayString(val));
-         if (prefNode_ != null && prefKey_ != null) {
-            prefs_.putFloat(prefNode_, prefKey_, val);
-         }
-      }
-      
-      /**
-       * @return - displayed value as a float
-       */
-      public float getFloat() {
-         String txt = getText();
-         float result = 0;
+   public StoredFloatLabel(String prefNode, String prefKey, float defaultValue, 
+         Prefs prefs, ScriptInterface gui) {               
+      super();
+      prefNode_ = prefNode;
+      prefKey_ = prefKey;
+      prefs_ = prefs;
+      gui_ = gui;
+      super.setText("" + (prefs_.getFloat(prefNode, prefKey, defaultValue)));
+   }
+
+   /**
+    * Sets the text of the JLabel and stores the value in Preferences
+    * @param txt 
+    */
+   @Override
+   public void setText(String txt) {
+      super.setText(txt);
+      if (prefNode_ != null && prefKey_ != null) {
          try {
-            result = (float) NumberUtils.displayStringToDouble(txt);
-         } catch (ParseException pe) {
-            // do nothing
+            prefs_.putFloat(prefNode_, prefKey_,
+                  ((Double) NumberUtils.displayStringToDouble(txt)).floatValue());
+         } catch (ParseException e) {
+            gui_.showError(e);
          }
-         return result;
       }
    }
-     
+
+   /**
+    * Convenience method to display a float and store value in prefs 
+    * @param val 
+    */
+   public void setFloat(float val) {
+      super.setText(NumberUtils.doubleToDisplayString(val));
+      if (prefNode_ != null && prefKey_ != null) {
+         prefs_.putFloat(prefNode_, prefKey_, val);
+      }
+   }
+
+   /**
+    * @return - displayed value as a float
+    */
+   public float getFloat() {
+      String txt = getText();
+      float result = 0;
+      try {
+         result = (float) NumberUtils.displayStringToDouble(txt);
+      } catch (ParseException pe) {
+         // do nothing
+      }
+      return result;
+   }
+   
+}
