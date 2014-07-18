0a56634bb3ee7d93381298bc205a056e5b3cf397
cweisiger <cweisiger@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-07-18 19:35:47 +0000

    Unregister the display from the EventBus on close.
    This prevents some pointless error messages from getting logged.
    
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@13866 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/imagedisplay/VirtualAcquisitionDisplay.java
+++ mmstudio/src/org/micromanager/imagedisplay/VirtualAcquisitionDisplay.java
@@ -1161,6 +1161,7 @@ public class VirtualAcquisitionDisplay implements ImageCacheListener {
       // Tell our display thread to stop what it's doing.
       shouldStopDisplayThread_.set(true);
       displayThread_.interrupt();
+      bus_.unregister(this);
       imageCache_.finished();
       imageCache_.close();
 
