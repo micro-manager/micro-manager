538c4ff0d6e9d4d747f09f59ffac5bcba5969063
Chris Weisiger <cweisiger@msg.ucsf.edu>
2014-07-22 09:30:21 -0700

    Modify Coords to be immutable.

--- mmstudio/src/org/micromanager/data/DefaultCoords.java
+++ mmstudio/src/org/micromanager/data/DefaultCoords.java
@@ -12,8 +12,58 @@ import org.micromanager.utils.ReportingUtils;
 
 /**
  * DefaultCoords indicate the position of a given image within a dataset.
+ * They are immutable, constructed using a Builder pattern.
  */
 public class DefaultCoords implements Coords {
+
+   public static class DefaultCoordsBuilder implements Coords.CoordsBuilder {
+      // Maps axis labels to our position along those axes.
+      private HashMap<String, Integer> axisToPos_;
+      // Axes for which we are the last image along that axis.
+      private Set<String> terminalAxes_;
+      // Convenience/optimization: we maintain a sorted list of our axes.
+      private ArrayList<String> sortedAxes_;
+
+      public DefaultCoordsBuilder() {
+         axisToPos_ = new HashMap<String, Integer>();
+         terminalAxes_ = new HashSet<String>();
+         sortedAxes_ = new ArrayList<String>();
+      }
+
+      public DefaultCoords build() {
+         return new DefaultCoords(this);
+      }
+      
+      public CoordsBuilder position(String axis, int position) {
+         axisToPos_.put(axis, new Integer(position));
+         sortedAxes_.add(axis);
+         try {
+            Collections.sort(sortedAxes_);
+         }
+         catch (UnsupportedOperationException e) {
+            ReportingUtils.logError(e, "Unable to sort coordinate axes");
+         }
+         return this;
+      }
+
+      public CoordsBuilder offset(String axis, int offset) throws IllegalArgumentException {
+         if (!axisToPos_.containsKey(axis)) {
+            throw new IllegalArgumentException("Axis " + axis + " is not a part of this CoordsBuilder.");
+         }
+         int curVal = axisToPos_.get(axis);
+         if (curVal + offset < 0) {
+            throw new IllegalArgumentException("Adding offset " + offset + " to current position " + curVal + " for axis " + axis + " would result in a negative position.");
+         }
+         position(axis, curVal + offset);
+         return this;
+      }
+      
+      public CoordsBuilder isAxisEndFor(String axis) {
+         terminalAxes_.add(axis);
+         return this;
+      }
+   }
+
    // Maps axis labels to our position along those axes.
    private HashMap<String, Integer> axisToPos_;
    // Axes for which we are the last image along that axis.
@@ -21,32 +71,12 @@ public class DefaultCoords implements Coords {
    // Convenience/optimization: we maintain a sorted list of our axes.
    private ArrayList<String> sortedAxes_;
 
-   public DefaultCoords() {
-      initMembers();
+   public DefaultCoords(DefaultCoordsBuilder builder) {
+      axisToPos_ = builder.axisToPos_;
+      terminalAxes_ = builder.terminalAxes_;
+      sortedAxes_ = builder.sortedAxes_;
    }
-
-   /**
-    * Generate a new DefaultCoords that's a copy of the provided Coords. 
-    * It may be null, in which case this is functionally identical to the
-    * default constructor.
-    */
-   public DefaultCoords(Coords alt) {
-      initMembers();
-      if (alt != null) {
-         // Copy positional information over.
-         for (String axis : alt.getAxes()) {
-            setPosition(axis, alt.getPositionAt(axis));
-         }
-      }
-   }
-
-   /** Initialize member fields. */
-   private void initMembers() {
-      axisToPos_ = new HashMap<String, Integer>();
-      terminalAxes_ = new HashSet<String>();
-      sortedAxes_ = new ArrayList<String>();
-   }
-
+   
    public int getPositionAt(String axis) {
       if (axisToPos_.containsKey(axis)) {
          return axisToPos_.get(axis);
@@ -54,23 +84,8 @@ public class DefaultCoords implements Coords {
       return -1;
    }
    
-   public void setPosition(String axis, int position) {
-      axisToPos_.put(axis, new Integer(position));
-      sortedAxes_.add(axis);
-      try {
-         Collections.sort(sortedAxes_);
-      }
-      catch (UnsupportedOperationException e) {
-         ReportingUtils.logError(e, "Unable to sort coordinate axes");
-      }
-   }
-
    public boolean getIsAxisEndFor(String axis) {
       return terminalAxes_.contains(axis);
-   }
-
-   public void setIsAxisEndFor(String axis) {
-      terminalAxes_.add(axis);
    }
 
    public Set<String> getTerminalAxes() {
@@ -81,11 +96,13 @@ public class DefaultCoords implements Coords {
       return new ArrayList<String>(sortedAxes_);
    }
 
-   public Coords makeOffsetCopy(HashMap<String, Integer> offsets) {
-      Coords result = new DefaultCoords(this);
-      for (String axis : offsets.keySet()) {
-         result.setPosition(axis, 
-               result.getPositionAt(axis) + offsets.get(axis));
+   public CoordsBuilder copy() {
+      DefaultCoordsBuilder result = new DefaultCoordsBuilder();
+      for (String axis : axisToPos_.keySet()) {
+         result.position(axis, axisToPos_.get(axis));
+      }
+      for (String axis : terminalAxes_) {
+         result.isAxisEndFor(axis);
       }
       return result;
    }
