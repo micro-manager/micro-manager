8725816a533e4d945bf5597adc71f04085dd1079
Chris Weisiger <cweisiger@msg.ucsf.edu>
2014-07-23 12:40:46 -0700

    Split some attributes from summary metadata to display settings.
    Specifically: channelNames, channelColors, channelContrast[Mins|Maxes].

---
+++ mmstudio/src/org/micromanager/api/data/DisplaySettings.java
@@ -0,0 +1,39 @@
+package org.micromanager.api.data;
+
+import java.awt.Color;
+
+import org.micromanager.api.MultiStagePosition;
+
+/**
+ * This class defines the parameters that control how a given dataset is
+ * displayed.
+ * It is immutable; construct it with a DisplaySettingsBuilder.
+ */
+public interface DisplaySettings {
+
+   interface DisplaySettingsBuilder {
+      /**
+       * Construct a DisplaySettings from the DisplaySettingsBuilder. Call 
+       * this once you are finished setting all DisplaySettings parameters.
+       */
+      DisplaySettings build();
+
+      // The following functions each set the relevant value for the 
+      // DisplaySettings.
+      DisplaySettingsBuilder channelNames(String[] channelNames);
+      DisplaySettingsBuilder channelColors(Color[] channelColors);
+      DisplaySettingsBuilder channelContrastMins(Integer[] channelContrastMins);
+      DisplaySettingsBuilder channelContrastMaxes(Integer[] channelContrastMaxes);
+   }
+
+   /**
+    * Generate a new DisplaySettingsBuilder whose values are initialized to be
+    * the values of this DisplaySettings.
+    */
+   DisplaySettingsBuilder copy();
+
+   public String[] getChannelNames();
+   public Color[] getChannelColors();
+   public Integer[] getChannelContrastMins();
+   public Integer[] getChannelContrastMaxes();
+}
