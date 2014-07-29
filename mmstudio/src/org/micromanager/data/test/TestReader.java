6b1b8faa79e0e81ca748bd43fc951e66f0045ebf
Derakon <derakon@gmail.com>
2014-07-29 14:15:39 -0700

    Remember images we have generated in the past; set "imageNumber" metadata.

--- mmstudio/src/org/micromanager/data/test/TestReader.java
+++ mmstudio/src/org/micromanager/data/test/TestReader.java
@@ -1,26 +1,52 @@
 package org.micromanager.data.test;
 
+import java.util.HashMap;
+
 import org.micromanager.api.data.Coords;
 import org.micromanager.api.data.DisplaySettings;
 import org.micromanager.api.data.Image;
+import org.micromanager.api.data.Metadata;
 import org.micromanager.api.data.Reader;
 import org.micromanager.api.data.SummaryMetadata;
 
-import org.micromanager.data.DefaultImage;
+import org.micromanager.data.DefaultCoords;
 import org.micromanager.data.DefaultDisplaySettings;
+import org.micromanager.data.DefaultImage;
+import org.micromanager.data.DefaultMetadata;
 import org.micromanager.data.DefaultSummaryMetadata;
+
+import org.micromanager.utils.ReportingUtils;
 
 /**
  * Dummy class that provides blank Images/Metadatas/etc. whenever asked.
  */
 public class TestReader implements Reader {
+   private HashMap<Coords, Image> coordsToImage_;
+
+   public TestReader() {
+      coordsToImage_ = new HashMap<Coords, Image>();
+   }
+
    @Override
    public Image getImage(Coords coords) {
-      short[] pixelData = new short[128*128];
-      for (short i = 0; i < 128*128; ++i) {
-         pixelData[i] = i;
+      ReportingUtils.logError("Asked for image at " + coords);
+      if (coordsToImage_.containsKey(coords)) {
+         ReportingUtils.logError("Already have it");
+         return coordsToImage_.get(coords);
       }
-      return new DefaultImage(pixelData, 128, 128, 2);
+      short[] pixelData = new short[4*4];
+      int index = coordsToImage_.size();
+      for (short i = 0; i < 4*4; ++i) {
+         pixelData[i] = (short) index;
+      }
+      Metadata metadata = (new DefaultMetadata.Builder())
+            .imageNumber(index)
+            .build();
+      Image result = new DefaultImage(pixelData, 4, 4, 2,
+            coords, metadata);
+      coordsToImage_.put(coords, result);
+      ReportingUtils.logError("Generated it");
+      return result;
    }
 
    @Override
