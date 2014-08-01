0fbb6d24e5fd18b350adf47b7c8a8d3029de9706
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-08-01 18:47:55 +0000

    Remove mmUnixBuild.sh
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14031 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmUnixBuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-echo "Micro-Manager has a new set of autoconf/automake build scripts, and the" 1>&2
-echo "mmUnixBuild.sh script has been retired. It will be removed in the near" 1>&2
-echo "future. Please run ./autogen.sh instead." 1>&2
-echo "Note that the ./configure options have changed. See the build documentation" 1>&2
-echo "for details." 1>&2
-exit 1
