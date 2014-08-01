13477ce479b4d2ba276bead213b2b40c6c6f71c7
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-08-01 17:52:06 +0000

    Set up JUnit for Java unit testing
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14028 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ mmstudio/test/SanityTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Confirm that the unit testing setup is working.
+ */
+public class SanityTest {
+   @Test
+   public void testSetupIsSane() {
+      Assert.assertTrue("must be true", true);
+   }
+}
