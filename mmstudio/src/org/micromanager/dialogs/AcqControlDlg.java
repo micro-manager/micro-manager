203cb5b28e6093a05da2fa0433999b892e502163
cweisiger <cweisiger@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-08-01 15:54:43 +0000

    Split out ChannelCellRenderer to its own module, separate from AcqControlDlg.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@13985 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/dialogs/AcqControlDlg.java
+++ mmstudio/src/org/micromanager/dialogs/AcqControlDlg.java
@@ -196,105 +196,6 @@ public class AcqControlDlg extends JFrame implements PropertyChangeListener,
    private ArrayList<JPanel> panelList_;
    private boolean disableGUItoSettings_ = false;
 
-   /**
-    * Renderer class for the channel table.
-    */
-   public class ChannelCellRenderer extends JLabel implements TableCellRenderer {
-
-      private static final long serialVersionUID = -4328340719459382679L;
-      private AcquisitionEngine acqEng_;
-
-      // This method is called each time a cell in a column
-      // using this renderer needs to be rendered.
-      public ChannelCellRenderer(AcquisitionEngine acqEng) {
-         super();
-         acqEng_ = acqEng;
-      }
-
-      @Override
-      public Component getTableCellRendererComponent(JTable table, Object value,
-              boolean isSelected, boolean hasFocus, int rowIndex, int colIndex) {
-
-         ChannelTableModel model = (ChannelTableModel) table.getModel();
-         ArrayList<ChannelSpec> channels = model.getChannels();
-         ChannelSpec channel = channels.get(rowIndex);
-         this.setEnabled(table.isEnabled() /*&&
-                 ((Boolean) model.getValueAt(rowIndex, 0) || colIndex == 0)*/);
-
-         if (hasFocus) {
-            // this cell is the anchor and the table has the focus
-         }
-
-         colIndex = table.convertColumnIndexToModel(colIndex);
-
-         setOpaque(false);
-         if (colIndex == 0) {
-            JCheckBox check = new JCheckBox("", channel.useChannel);
-            check.setEnabled(table.isEnabled());
-            check.setOpaque(true);
-            if (isSelected) {
-               check.setBackground(table.getSelectionBackground());
-               check.setOpaque(true);
-            } else {
-               check.setOpaque(false);
-               check.setBackground(table.getBackground());
-            }
-            return check;
-         } else if (colIndex == 1) {
-            setText(channel.config);
-         } else if (colIndex == 2) {
-            setText(NumberUtils.doubleToDisplayString(channel.exposure));
-         } else if (colIndex == 3) {
-            setText(NumberUtils.doubleToDisplayString(channel.zOffset));
-         } else if (colIndex == 4) {
-            JCheckBox check = new JCheckBox("", channel.doZStack);
-            check.setEnabled(acqEng_.isZSliceSettingEnabled() && table.isEnabled());
-            if (isSelected) {
-               check.setBackground(table.getSelectionBackground());
-               check.setOpaque(true);
-            } else {
-               check.setOpaque(false);
-               check.setBackground(table.getBackground());
-            }
-            return check;
-         } else if (colIndex == 5) {
-            setText(Integer.toString(channel.skipFactorFrame));
-         } else if (colIndex == 6) {
-            setText("");
-            setBackground(channel.color);
-            setOpaque(true);
-         }
-
-         if (isSelected) {
-            setBackground(table.getSelectionBackground());
-            setOpaque(true);
-         } else {
-            setOpaque(false);
-            setBackground(table.getBackground());
-         }
-
-         // Since the renderer is a component, return itself
-         return this;
-      }
-
-      // The following methods override the defaults for performance reasons
-      @Override
-      public void validate() {
-      }
-
-      @Override
-      public void revalidate() {
-      }
-
-      @Override
-      protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
-      }
-
-      @Override
-      public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {
-      }
-   }
-
    public final void createChannelTable() {
       model_ = new ChannelTableModel(studio_, acqEng_, 
             exposurePrefs_, colorPrefs_, options_);
@@ -2168,7 +2069,6 @@ public class AcqControlDlg extends JFrame implements PropertyChangeListener,
          final CheckBoxPanel thisPanel = this;
 
          checkBox.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                boolean enable = checkBox.isSelected();
@@ -2178,14 +2078,10 @@ public class AcqControlDlg extends JFrame implements PropertyChangeListener,
             public void writeObject(java.io.ObjectOutputStream stream) throws MMException  {
                throw new MMException("Do not serialize this class");
             }
-            
          });
-
-         
       }
 
       public void setChildrenEnabled(boolean enabled) {
-
          Component comp[] = this.getComponents();
          for (int i = 0; i < comp.length; i++) {
             if (comp[i].getClass().equals(JPanel.class)) {
---
+++ mmstudio/src/org/micromanager/dialogs/ChannelCellRenderer.java
@@ -0,0 +1,107 @@
+package org.micromanager.dialogs;
+
+import java.awt.Component;
+import java.util.ArrayList;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+import org.micromanager.acquisition.AcquisitionEngine;
+import org.micromanager.utils.ChannelSpec;
+import org.micromanager.utils.NumberUtils;
+
+/**
+ * Renderer class for the channel table.
+ */
+public class ChannelCellRenderer extends JLabel implements TableCellRenderer {
+
+   private static final long serialVersionUID = -4328340719459382679L;
+   private AcquisitionEngine acqEng_;
+
+   // This method is called each time a cell in a column
+   // using this renderer needs to be rendered.
+   public ChannelCellRenderer(AcquisitionEngine acqEng) {
+      super();
+      acqEng_ = acqEng;
+   }
+
+   @Override
+   public Component getTableCellRendererComponent(JTable table, Object value,
+           boolean isSelected, boolean hasFocus, int rowIndex, int colIndex) {
+
+      ChannelTableModel model = (ChannelTableModel) table.getModel();
+      ArrayList<ChannelSpec> channels = model.getChannels();
+      ChannelSpec channel = channels.get(rowIndex);
+      this.setEnabled(table.isEnabled());
+
+      colIndex = table.convertColumnIndexToModel(colIndex);
+
+      setOpaque(false);
+      if (colIndex == 0) {
+         JCheckBox check = new JCheckBox("", channel.useChannel);
+         check.setEnabled(table.isEnabled());
+         check.setOpaque(true);
+         if (isSelected) {
+            check.setBackground(table.getSelectionBackground());
+            check.setOpaque(true);
+         } else {
+            check.setOpaque(false);
+            check.setBackground(table.getBackground());
+         }
+         return check;
+      } else if (colIndex == 1) {
+         setText(channel.config);
+      } else if (colIndex == 2) {
+         setText(NumberUtils.doubleToDisplayString(channel.exposure));
+      } else if (colIndex == 3) {
+         setText(NumberUtils.doubleToDisplayString(channel.zOffset));
+      } else if (colIndex == 4) {
+         JCheckBox check = new JCheckBox("", channel.doZStack);
+         check.setEnabled(acqEng_.isZSliceSettingEnabled() && table.isEnabled());
+         if (isSelected) {
+            check.setBackground(table.getSelectionBackground());
+            check.setOpaque(true);
+         } else {
+            check.setOpaque(false);
+            check.setBackground(table.getBackground());
+         }
+         return check;
+      } else if (colIndex == 5) {
+         setText(Integer.toString(channel.skipFactorFrame));
+      } else if (colIndex == 6) {
+         setText("");
+         setBackground(channel.color);
+         setOpaque(true);
+      }
+
+      if (isSelected) {
+         setBackground(table.getSelectionBackground());
+         setOpaque(true);
+      } else {
+         setOpaque(false);
+         setBackground(table.getBackground());
+      }
+
+      // Since the renderer is a component, return itself
+      return this;
+   }
+
+   // The following methods override the defaults for performance reasons
+   @Override
+   public void validate() {
+   }
+
+   @Override
+   public void revalidate() {
+   }
+
+   @Override
+   protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+   }
+
+   @Override
+   public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {
+   }
+}
