0bd04a7492f91c9da62b1ac8954453fb394dd3b7
Derakon <derakon@gmail.com>
2014-08-19 13:53:43 -0700

    Minor code cleanups.

--- mmstudio/src/org/micromanager/data/test/TestDisplay.java
+++ mmstudio/src/org/micromanager/data/test/TestDisplay.java
@@ -13,8 +13,6 @@ import org.micromanager.imagedisplay.HyperstackControls;
 import org.micromanager.imagedisplay.MMCompositeImage;
 import org.micromanager.imagedisplay.MMImagePlus;
 
-import org.micromanager.imagedisplay.DisplayWindow;
-
 import org.micromanager.utils.ReportingUtils;
 
 public class TestDisplay {
@@ -24,6 +22,7 @@ public class TestDisplay {
    private MMImagePlus plus_;
    private MMVirtualStack stack_;
    private HyperstackControls controls_;
+   private HistogramsPanel histograms_;
 
    private EventBus bus_;
    
@@ -35,14 +34,15 @@ public class TestDisplay {
       plus_ = new MMImagePlus("foo", stack_, bus_);
       stack_.setImagePlus(plus_);
       ijImage_ = new MMCompositeImage(plus_, 1, "foo", bus_);
+      ijImage_.updateImage();
 //      controls_ = new HyperstackControls(bus_, false, false);
       // TODO: For now, stuffing the histograms into the display window. The 
       // display window takes a Panel but the histograms are a JPanel, hence
       // the wrapper.
       Panel temp = new Panel();
-      HistogramsPanel histograms = new HistogramsPanel(store_, ijImage_, bus_);
-      temp.add(histograms);
-      histograms.setVisible(true);
+      HistogramsPanel histograms_ = new HistogramsPanel(store_, ijImage_, bus_);
+      temp.add(histograms_);
+      histograms_.setVisible(true);
       temp.validate();
       window_ = new DisplayWindow(ijImage_, temp, bus_);
    }
@@ -71,6 +71,8 @@ public class TestDisplay {
     */
    @Subscribe
    public void onDrawEvent(DrawEvent event) {
+      ijImage_.updateImage();
       ijImage_.drawWithoutUpdate();
+      histograms_.calcAndDisplayHistAndStats(true);
    }
 }
