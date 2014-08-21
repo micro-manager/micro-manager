8b392d64e1663257c07fd0e5febd9d36f97d92ee
franz <franz@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-08-21 09:00:22 +0000

    FRE / PCO AG / 21.08.14
    - PCO_Kamlib(64).dll: fixed timout for exp/del > 2000ms
    
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14204 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/PCO_Generic/lib/pco_generic/Camera.h
+++ DeviceAdapters/PCO_Generic/lib/pco_generic/Camera.h
@@ -38,7 +38,7 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#define KAMLIBVERSION           237
+#define KAMLIBVERSION           238
 
 typedef unsigned char byte;     /* 8-bit  */
 typedef unsigned short word;    /* 16-bit */
