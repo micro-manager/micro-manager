7bb16d811d87515223bd4d24551eec3e78fb8757
Derakon <derakon@gmail.com>
2014-09-05 14:17:23 -0700

    Add some classes needed for further display layout development work.

---
+++ mmstudio/src/org/micromanager/data/test/LayoutChangedEvent.java
@@ -0,0 +1,7 @@
+package org.micromanager.data.test;
+
+/**
+ * This class signifies that the layout of the display window has changed and
+ * the window needs to adjust itself.
+ */
+public class LayoutChangedEvent {}
