45c1d3e29dd5f004ffca03eab62907a7a04d2636
Derakon <derakon@gmail.com>
2014-09-11 12:37:02 -0700

    Remove hardcoded limits on image format.
    Previously it was locked to 16-bit 512x512 images; now it should properly
    derive the format from the Image object.

--- mmstudio/src/org/micromanager/data/test/MMVirtualStack.java
+++ mmstudio/src/org/micromanager/data/test/MMVirtualStack.java
@@ -2,6 +2,7 @@ package org.micromanager.data.test;
 
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
+import ij.process.ByteProcessor;
 
 import org.micromanager.api.data.Coords;
 import org.micromanager.api.data.Datastore;
@@ -35,12 +36,8 @@ public class MMVirtualStack extends ij.VirtualStack {
    }
 
    /**
-    * Retrieve the image at the specified index, which we map into a
-    * channel/frame/z offset. This will also update curCoords_ as needed.
-    * Note that we only pay attention to a given offset if we already have
-    * a position along that axis (e.g. so that we don't try to ask the
-    * Datastore for an image at time=0 when none of the images in the Datastore
-    * have a time axis whatsoever).
+    * Retrieve the pixel buffer for the image at the specified offset. See
+    * getImage() for more details.
     */
    @Override
    public Object getPixels(int flatIndex) {
@@ -48,6 +45,23 @@ public class MMVirtualStack extends ij.VirtualStack {
          ReportingUtils.logError("Asked to get pixels when I don't have an ImagePlus");
          return null;
       }
+      DefaultImage image = getImage(flatIndex);
+      if (image != null) {
+         return image.getRawPixels();
+      }
+      ReportingUtils.logError("Null image at " + curCoords_);
+      return null;
+   }
+
+   /**
+    * Retrieve the image at the specified index, which we map into a
+    * channel/frame/z offset. This will also update curCoords_ as needed.
+    * Note that we only pay attention to a given offset if we already have
+    * a position along that axis (e.g. so that we don't try to ask the
+    * Datastore for an image at time=0 when none of the images in the Datastore
+    * have a time axis whatsoever).
+    */
+   private DefaultImage getImage(int flatIndex) {
       // Note: index is 1-based.
       if (flatIndex > plus_.getStackSize()) {
          ReportingUtils.logError("Stack asked for image at " + flatIndex + 
@@ -79,18 +93,26 @@ public class MMVirtualStack extends ij.VirtualStack {
          builder.position("frame", frame);
       }
       curCoords_ = builder.build();
-      DefaultImage image = (DefaultImage) store_.getImage(curCoords_);
-      if (image != null) {
-         return image.getRawPixels();
-      }
-      ReportingUtils.logError("Null image at " + curCoords_);
-      return null;
+      return (DefaultImage) store_.getImage(curCoords_);
    }
 
    @Override
    public ImageProcessor getProcessor(int flatIndex) {
-      // TODO: defaulting to 16-bit grayscale 512x512 images.
-      ImageProcessor result = ImageUtils.makeProcessor(ImagePlus.GRAY16, 512, 512, getPixels(flatIndex));
+      if (plus_ == null) {
+         // We have to supply a processor, even if it's totally bogus.
+         return new ByteProcessor(1, 1);
+      }
+      DefaultImage image = getImage(flatIndex);
+      int width = image.getWidth();
+      int height = image.getHeight();
+      int depth = image.getMetadata().getBitDepth();
+      // TODO: find a better way of setting the mode.
+      int mode = ImagePlus.GRAY8;
+      if (depth > 8) {
+         mode = ImagePlus.GRAY16;
+      }
+      Object pixels = image.getRawPixels();
+      ImageProcessor result = ImageUtils.makeProcessor(mode, width, height, pixels);
       return result;
    }
 
