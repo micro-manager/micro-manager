5a051b137b6f81f9a1a3e400e3fd6f869456608a
jondaniels <jondaniels@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-09-15 16:35:47 +0000

    PatternOverlay: fixed typo in comment
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14349 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/PatternOverlay/src/org/micromanager/patternoverlay/PatternOverlayFrame.java
+++ plugins/PatternOverlay/src/org/micromanager/patternoverlay/PatternOverlayFrame.java
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//FILE:          ImageOverlay.java
+//FILE:          PatternOverlayFrame.java
 //PROJECT:       Micro-Manager
 //SUBSYSTEM:     Image Overlay plugin
 //-----------------------------------------------------------------------------
@@ -52,8 +52,9 @@ import net.miginfocom.swing.MigLayout;
  *  Enables the user to set preferences like overlay type and size.
  *  
  *  To make a new pattern:
- *  1. Derive from class GenericOverlay and then create and return the overlay.
- *     See existing overlays as examples (e.g. CrosshairOverlay.java, GridOverlay.java).
+ *  1. Derive from class GenericOverlay and implement getOverlay() which
+ *     creates and return the overlay. See existing overlays as examples
+ *     (e.g. CrosshairOverlay.java, GridOverlay.java).
  *  2. Add an entry to the OverayOption.Keys enum
  *  3. Create/add the object along with associated key to the overlayModel
  *     variable in the constructor of PatternOverlayFrame.
