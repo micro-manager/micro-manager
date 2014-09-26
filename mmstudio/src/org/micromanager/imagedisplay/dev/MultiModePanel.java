b028bdd05a8038af5880742557a91b3ccbce3061
Derakon <derakon@gmail.com>
2014-09-26 15:28:15 -0700

    Reduce wasted space in the button panel.

--- mmstudio/src/org/micromanager/imagedisplay/dev/MultiModePanel.java
+++ mmstudio/src/org/micromanager/imagedisplay/dev/MultiModePanel.java
@@ -34,7 +34,12 @@ class MultiModePanel extends JPanel {
       bus_ = bus;
       widgetToButton_ = new HashMap<Component, VerticalButton>();
       orderedWidgets_ = new ArrayList<Component>();
-      buttonPanel_ = new JPanel(new MigLayout("insets 0, flowy"));
+      // TODO: I can't figure out a way in MiG to specify grid gaps without
+      // making assumptions about how many entries are in the layout. Except
+      // I *can* specify gaps when adding components, if I use the "wrap"
+      // command...which means the button panel technically has a horizontal
+      // layout, but every single button added to it is added via "wrap 0px".
+      buttonPanel_ = new JPanel(new MigLayout("insets 0"));
       modePanel_ = new JPanel(new MigLayout("insets 0, flowy"));
       setLayout(new MigLayout("insets 0"));
       add(buttonPanel_, "growy");
@@ -47,7 +52,7 @@ class MultiModePanel extends JPanel {
       final VerticalButton button = new VerticalButton(label);
       widgetToButton_.put(widget, button);
       orderedWidgets_.add(widget);
-      buttonPanel_.add(button);
+      buttonPanel_.add(button, "wrap 0px");
       // Add an event to show/hide the appropriate widget.
       button.addActionListener(new ActionListener() {
          @Override
