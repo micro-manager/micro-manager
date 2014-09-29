4b74e6f565c7ef3c191552909115cf7f15aa1187
Chris Weisiger <cweisiger@msg.ucsf.edu>
2014-09-29 14:37:55 -0700

    Strip out all non-scroller controls from the HyperstackControls class.
    They'll be added back in from external sources in future commits.

--- mmstudio/src/org/micromanager/imagedisplay/dev/HyperstackControls.java
+++ mmstudio/src/org/micromanager/imagedisplay/dev/HyperstackControls.java
@@ -42,15 +42,13 @@ import org.micromanager.data.AbortEvent;
 import org.micromanager.data.DefaultCoords;
 import org.micromanager.data.NewImageEvent;
 import org.micromanager.imagedisplay.MouseIntensityEvent;
-import org.micromanager.internalinterfaces.DisplayControls;
-import org.micromanager.internalinterfaces.LiveModeListener;
 import org.micromanager.MMStudio;
 import org.micromanager.utils.MDUtils;
 import org.micromanager.utils.NumberUtils;
 import org.micromanager.utils.ReportingUtils;
 
 
-public class HyperstackControls extends DisplayControls implements LiveModeListener {
+public class HyperstackControls extends JPanel {
 
    private final static int DEFAULT_FPS = 10;
    private final static double MAX_FPS = 5000;
@@ -101,7 +99,6 @@ public class HyperstackControls extends DisplayControls implements LiveModeListe
       stack_ = stack;
       initComponents(shouldUseLiveControls);
       displayBus_.register(this);
-      MMStudio.getInstance().getSnapLiveManager().addLiveModeListener(this);
    }
 
    private void initComponents(final boolean shouldUseLiveControls) {
@@ -128,69 +125,8 @@ public class HyperstackControls extends DisplayControls implements LiveModeListe
 
       scrollerPanel_ = new ScrollerPanel(store_, displayBus_, DEFAULT_FPS);
       subPanel_.add(scrollerPanel_, "span, growx, wrap 0px");
-
-      // Hacky layout to minimize gaps between components. 
-      JPanel buttonPanel = new JPanel(new MigLayout("insets 0", 
-               "[]0[]0[]0[]0[]0[]"));
-
-      showFolderButton_ = new JButton();
-      saveButton_ = new JButton();
-
-      buttonPanel.add(showFolderButton_);
-      buttonPanel.add(saveButton_);
-
-      showFolderButton_.setBackground(new java.awt.Color(255, 255, 255));
-      showFolderButton_.setIcon(
-            new javax.swing.ImageIcon(
-               getClass().getResource("/org/micromanager/icons/folder.png")));
-      showFolderButton_.setToolTipText("Show containing folder");
-      showFolderButton_.setFocusable(false);
-      showFolderButton_.setHorizontalTextPosition(
-            javax.swing.SwingConstants.CENTER);
-      showFolderButton_.setMaximumSize(new java.awt.Dimension(30, 28));
-      showFolderButton_.setMinimumSize(new java.awt.Dimension(30, 28));
-      showFolderButton_.setPreferredSize(new java.awt.Dimension(30, 28));
-      showFolderButton_.setVerticalTextPosition(
-            javax.swing.SwingConstants.BOTTOM);
-      showFolderButton_.addActionListener(new java.awt.event.ActionListener() {
-         public void actionPerformed(java.awt.event.ActionEvent evt) {
-            showFolderButtonActionPerformed(evt);
-         }
-      });
-
-      saveButton_.setBackground(new java.awt.Color(255, 255, 255));
-      saveButton_.setIcon(
-            new javax.swing.ImageIcon(
-               getClass().getResource("/org/micromanager/icons/disk.png")));
-      saveButton_.setToolTipText("Save as...");
-      saveButton_.setFocusable(false);
-      saveButton_.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-      saveButton_.setMaximumSize(new java.awt.Dimension(30, 28));
-      saveButton_.setMinimumSize(new java.awt.Dimension(30, 28));
-      saveButton_.setPreferredSize(new java.awt.Dimension(30, 28));
-      saveButton_.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-      saveButton_.addActionListener(new java.awt.event.ActionListener() {
-         public void actionPerformed(java.awt.event.ActionEvent evt) {
-            saveButtonActionPerformed(evt, shouldUseLiveControls);
-         }
-      });
-
-      // This control is added by both Snap/Live, and Standard, but in 
-      // different places on each. 
-      fpsLabel_ = new JLabel("                      ", SwingConstants.RIGHT);
-      fpsLabel_.setFont(new java.awt.Font("Lucida Grande", 0, 10));
-      fpsLabel_.setFocusable(false);
-
-      if (shouldUseLiveControls) {
-         makeSnapLiveControls(buttonPanel);
-      }
-      else {
-         makeStandardControls(buttonPanel);
-      }
-      
-      subPanel_.add(buttonPanel);
       add(subPanel_);
-      
+
       // Propagate resizing through to our JPanel, adjusting slightly the 
       // amount of space we give them to create a border.
       addComponentListener(new ComponentAdapter() {
@@ -201,147 +137,6 @@ public class HyperstackControls extends DisplayControls implements LiveModeListe
             validate();
          }
       });
-   }
-
-   /**
-    * Generate the controls used for the "Snap/Live" window.
-    */
-   private void makeSnapLiveControls(JPanel buttonPanel) {
-      snapButton_ = new JButton();
-      snapButton_.setFocusable(false);
-      snapButton_.setIconTextGap(6);
-      snapButton_.setText("Snap");
-      snapButton_.setMinimumSize(new Dimension(90,28));
-      snapButton_.setPreferredSize(new Dimension(90,28));
-      snapButton_.setMaximumSize(new Dimension(90,28));
-      snapButton_.setIcon(SwingResourceManager.getIcon(
-            MMStudio.class, "/org/micromanager/icons/camera.png"));
-      snapButton_.setFont(new Font("Arial", Font.PLAIN, 10));
-      snapButton_.setToolTipText("Snap single image");
-      snapButton_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent e) {
-            MMStudio.getInstance().doSnap();
-         }
-
-      });
-
-      liveButton_ = new JButton();
-      liveButton_.setIcon(SwingResourceManager.getIcon(
-            MMStudio.class,
-            "/org/micromanager/icons/camera_go.png"));
-      liveButton_.setIconTextGap(6);
-      liveButton_.setText("Live");
-      liveButton_.setMinimumSize(new Dimension(90,28));
-      liveButton_.setPreferredSize(new Dimension(90,28));
-      liveButton_.setMaximumSize(new Dimension(90,28));
-      liveButton_.setFocusable(false);
-      liveButton_.setToolTipText("Continuous live view");
-      liveButton_.setFont(new Font("Arial", Font.PLAIN, 10));
-      liveButton_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent e) {
-            liveButtonAction();
-         }
-      });
-    
-      snapToAlbumButton_ = new JButton("Album");
-      snapToAlbumButton_.setIcon(SwingResourceManager.getIcon(MMStudio.class,
-              "/org/micromanager/icons/arrow_right.png"));
-      snapToAlbumButton_.setIconTextGap(6);
-      snapToAlbumButton_.setToolTipText("Add current image to album");
-      snapToAlbumButton_.setFocusable(false);
-      snapToAlbumButton_.setMaximumSize(new Dimension(90, 28));
-      snapToAlbumButton_.setMinimumSize(new Dimension(90, 28));
-      snapToAlbumButton_.setPreferredSize(new Dimension(90, 28));
-      snapToAlbumButton_.setFont(new Font("Arial", Font.PLAIN, 10));
-      snapToAlbumButton_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent evt) {
-            snapToAlbumButtonActionPerformed();
-         }
-      });
-
-      buttonPanel.add(snapButton_);
-      buttonPanel.add(liveButton_);
-      buttonPanel.add(snapToAlbumButton_);
-      fpsLabel_.setText("                          ");
-      buttonPanel.add(fpsLabel_, "span, wrap, width 120px, align right");
-   }
-
-   /**
-    * Generate the controls used on a standard dataset display (i.e. not the 
-    * snap/live window).
-    */
-   private void makeStandardControls(JPanel buttonPanel) {
-      fpsField_ = new javax.swing.JTextField(String.valueOf(DEFAULT_FPS), 4);
-      abortButton_ = new JButton();
-      pauseAndResumeToggleButton_ = new javax.swing.JToggleButton();
-      
-      buttonPanel.add(abortButton_);
-      buttonPanel.add(pauseAndResumeToggleButton_);
-      // Make a new panel to hold the FPS info, since they need to be 
-      // together.
-      JPanel fpsPanel = new JPanel(new MigLayout("insets 0"));
-      fpsPanel.add(fpsLabel_);
-      fpsPanel.add(fpsField_);
-
-      buttonPanel.add(fpsPanel, "span, gapleft push, wrap");
-
-      fpsField_.setToolTipText(
-            "Set the speed at which the acquisition is played back.");
-      fpsField_.addFocusListener(new java.awt.event.FocusAdapter() {
-         @Override
-         public void focusLost(java.awt.event.FocusEvent evt) {
-            fpsField_FocusLost(evt);
-         }
-      });
-      fpsField_.addKeyListener(new java.awt.event.KeyAdapter() {
-         @Override
-         public void keyReleased(java.awt.event.KeyEvent evt) {
-            fpsField_KeyReleased(evt);
-         }
-      });
-
-      abortButton_.setBackground(new java.awt.Color(255, 255, 255));
-      abortButton_.setIcon(
-            new javax.swing.ImageIcon(
-               getClass().getResource("/org/micromanager/icons/cancel.png")));
-      abortButton_.setToolTipText("Stop acquisition");
-      abortButton_.setFocusable(false);
-      abortButton_.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-      abortButton_.setMaximumSize(new java.awt.Dimension(30, 28));
-      abortButton_.setMinimumSize(new java.awt.Dimension(30, 28));
-      abortButton_.setPreferredSize(new java.awt.Dimension(30, 28));
-      abortButton_.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-      abortButton_.addActionListener(new java.awt.event.ActionListener() {
-         public void actionPerformed(java.awt.event.ActionEvent evt) {
-            abortButtonActionPerformed(evt);
-         }
-      });
-
-      pauseAndResumeToggleButton_.setIcon(
-            new javax.swing.ImageIcon(getClass().getResource(
-                  "/org/micromanager/icons/control_pause.png")));
-      pauseAndResumeToggleButton_.setToolTipText("Pause acquisition");
-      pauseAndResumeToggleButton_.setFocusable(false);
-      pauseAndResumeToggleButton_.setMargin(new java.awt.Insets(0, 0, 0, 0));
-      pauseAndResumeToggleButton_.setMaximumSize(new java.awt.Dimension(30, 28));
-      pauseAndResumeToggleButton_.setMinimumSize(new java.awt.Dimension(30, 28));
-      pauseAndResumeToggleButton_.setPreferredSize(
-            new java.awt.Dimension(30, 28));
-      pauseAndResumeToggleButton_.setPressedIcon(
-            new javax.swing.ImageIcon(getClass().getResource(
-                  "/org/micromanager/icons/resultset_next.png")));
-      pauseAndResumeToggleButton_.setSelectedIcon(
-            new javax.swing.ImageIcon(getClass().getResource(
-                  "/org/micromanager/icons/resultset_next.png")));
-      pauseAndResumeToggleButton_.addActionListener(
-            new java.awt.event.ActionListener() {
-               public void actionPerformed(java.awt.event.ActionEvent evt) {
-                  pauseAndResumeToggleButtonActionPerformed(evt);
-               }
-            });
    }
 
    /**
@@ -406,66 +201,7 @@ public class HyperstackControls extends DisplayControls implements LiveModeListe
       validate();
    }
 
-   private void showFolderButtonActionPerformed(java.awt.event.ActionEvent evt) {
-      ReportingUtils.logError("TODO: show folder button");
-   }
-
-   private void fpsField_FocusLost(java.awt.event.FocusEvent evt) {
-      updateFPS();
-   }
-
-   private void fpsField_KeyReleased(java.awt.event.KeyEvent evt) {
-      updateFPS();
-   }
-
-   private void abortButtonActionPerformed(java.awt.event.ActionEvent evt) {
-      store_.publishEvent(new AbortEvent());
-   }
-
-   private void pauseAndResumeToggleButtonActionPerformed(java.awt.event.ActionEvent evt) {
-      ReportingUtils.logError("TODO: pause button");
-   }
-
-   private void saveButtonActionPerformed(java.awt.event.ActionEvent evt, final boolean isSimpleDisplay) {
-      new Thread() {
-         @Override
-         public void run() {
-            // We don't want to tie the Snap/Live display to a specific
-            // file since its contents get overwritten regularly.
-            ReportingUtils.logError("TODO: save button");
-         }
-      }.start();
-   }
-
-   private void snapToAlbumButtonActionPerformed() {
-      try {
-         MMStudio.getInstance().addToAlbum(store_.getImage(stack_.getCurrentImageCoords()));
-      } catch (Exception ex) {
-         ReportingUtils.logError(ex);
-      }
-   }
-
-   private void liveButtonAction() {
-       MMStudio.getInstance().enableLiveMode(!MMStudio.getInstance().isLiveModeOn());
-    }
-
-   private void updateFPS() {
-      // There's no FPS field when using the Snap/Live window
-      if (fpsField_ != null) {
-         try {
-            double fps = NumberUtils.displayStringToDouble(fpsField_.getText());
-            // Constrain the FPS to a sane range.
-            fps = Math.max(1.0, Math.min(fps, MAX_FPS));
-            scrollerPanel_.setFramesPerSecond(fps);
-         } catch (ParseException ex) {
-            // No recognizable number (e.g. because the field is empty); just
-            // do nothing.
-         }
-      }
-   }
-
-   @Override
-   public synchronized void setImageInfoLabel(String text) {
+   public void setImageInfoLabel(String text) {
       imageInfoLabel_.setText(text);
    }
 
@@ -543,92 +279,10 @@ public class HyperstackControls extends DisplayControls implements LiveModeListe
       return hoursString + minutesString + secondsString;
    }
 
-   @Override
-   public void newImageUpdate(JSONObject tags) {
-      ReportingUtils.logError("TODO: new image update");
-   }
-
-
-   @Override
-   public void acquiringImagesUpdate(boolean state) {
-      // NB currently there's no situation in which one of these will be null
-      // when the other isn't, but on the other hand who knows what the future
-      // will bring?
-      if (abortButton_ != null) {
-         abortButton_.setEnabled(state);
-      }
-      if (pauseAndResumeToggleButton_ != null) {
-         pauseAndResumeToggleButton_.setEnabled(state);
-      }
-   }
-
-   @Override
-   public void imagesOnDiskUpdate(boolean enabled) {
-      showFolderButton_.setEnabled(enabled);
-   }
-
    public void prepareForClose() {
       scrollerPanel_.prepareForClose();
       displayBus_.unregister(this);
       store_.unregisterForEvents(this);
       scrollerPanel_.prepareForClose();
-      MMStudio.getInstance().getSnapLiveManager().removeLiveModeListener(this);
-   }
-
-   @Override
-   public void setPosition(int p) {
-      scrollerPanel_.setPosition("position", p);
-   }
-
-   @Override
-   public int getPosition() {
-      return scrollerPanel_.getPosition("position");
-   }
-
-   @Override
-   public void setChannel(int c) {
-      scrollerPanel_.setPosition("channel", c);
-   }
-
-   // TODO: implement after this replaces the existing display system.
-//   /**
-//    * New information on our FPS; update a label.
-//    */
-//   @Subscribe
-//   public void onFPSUpdate(FPSEvent event) {
-//      // Default to assuming we'll be blanking the label.
-//      String newLabel = "";
-//      if (event.getDataFPS() != 0) {
-//         newLabel = String.format("FPS: %.1f (display %.1f)", 
-//               event.getDataFPS(), event.getDisplayFPS());
-//      }
-//      else if (fpsField_ != null) {
-//         // No new data, but we do have an FPS text field for animations, so
-//         // switch fpsLabel_ to being an indicator for that. 
-//         newLabel = "Playback FPS:";
-//      }
-//      fpsLabel_.setText(newLabel);
-//   }
-
-   /**
-    * Live mode was toggled; if we have a "live mode" button, it needs to be 
-    * toggled on/off; likewise, the Snap button should be disabled/enabled.
-    */
-   public void liveModeEnabled(boolean isEnabled) {
-      if (liveButton_ == null) {
-         return;
-      }
-      String label = isEnabled ? "Stop Live" : "Live";
-      String iconPath = isEnabled ? "/org/micromanager/icons/cancel.png" : "/org/micromanager/icons/camera_go.png";
-      liveButton_.setIcon(
-            SwingResourceManager.getIcon(MMStudio.class, iconPath));
-      liveButton_.setText(label);
-      if (snapButton_ != null) {
-         snapButton_.setEnabled(!isEnabled);
-      }
-   }
-
-   public int getNumPositions() {
-      return scrollerPanel_.getMaxPosition("position");
    }
 }
