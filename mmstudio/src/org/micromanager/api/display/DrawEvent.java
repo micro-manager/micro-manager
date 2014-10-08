b71c02e93ecd99fce11fba9ea9acb55ab174428f
Derakon <derakon@gmail.com>
2014-10-08 08:26:08 -0700

    Add image coords to DrawEvent.

--- mmstudio/src/org/micromanager/api/display/DrawEvent.java
+++ mmstudio/src/org/micromanager/api/display/DrawEvent.java
@@ -1,7 +1,12 @@
 package org.micromanager.api.display;
 
+import org.micromanager.api.data.Coords;
+
 /**
  * By posting this event on an EventBus, you can request that an image window
- * refresh its display.
+ * refresh its display. Optionally, you can specify the coordinates of an
+ * Image to display.
  */
-public class DrawEvent {}
+public interface DrawEvent {
+   public Coords getCoords();
+}
