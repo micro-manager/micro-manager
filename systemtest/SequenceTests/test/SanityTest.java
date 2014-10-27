4768d3e78645ab86a2fff23c00f055d88b2a3278
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-10-27 18:37:33 +0000

    SequenceTests: Turn Core setup into JUnit rule
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14449 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- systemtest/SequenceTests/test/SanityTest.java
+++ systemtest/SequenceTests/test/SanityTest.java
@@ -5,7 +5,9 @@ import java.util.concurrent.TimeUnit;
 import mmcorej.CMMCore;
 import mmcorej.StrVector;
 import mmcorej.TaggedImage;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
 import org.micromanager.AcquisitionEngine2010;
 import org.micromanager.api.IAcquisitionEngine2010;
 import org.micromanager.api.PositionList;
@@ -17,12 +19,46 @@ import static org.micromanager.testing.TestImageDecoder.InfoPacket;
 
 
 public class SanityTest {
+   CMMCore mmc;
+
+   @Rule
+   public ExternalResource coreResource = new ExternalResource() {
+      @Override protected void before() throws Exception {
+         String jniLibPath = System.getenv("MMCOREJ_LIBRARY_PATH");
+         String adapterPath = System.getenv("MMTEST_ADAPTER_PATH");
+
+         System.setProperty("mmcorej.library.loading.stderr.log", "yes");
+         if (jniLibPath != null) {
+            System.setProperty("mmcorej.library.path", jniLibPath);
+         }
+
+         mmc = new CMMCore();
+
+         mmc.enableStderrLog(true);
+         mmc.enableDebugLog(true);
+
+         StrVector paths = new StrVector();
+         if (adapterPath != null) {
+            paths.add(adapterPath);
+         }
+         mmc.setDeviceAdapterSearchPaths(paths);
+
+         mmc.loadDevice("THub", "SequenceTester", "THub");
+         mmc.initializeDevice("THub");
+      }
+
+      @Override protected void after() {
+         // Ensure CoreLog is finished
+         mmc.delete();
+      }
+   };
+
    @Test
    public void sequenceTestCameraWorks() throws Exception {
       // Prefix matching allows us to create arbitrary devices.
       // Device name and label always match, to simplify things.
       String camera = "TCamera-0";
-      CMMCore mmc = prepareTestDevices(camera);
+      prepareTestDevices(mmc, camera);
       mmc.setCameraDevice(camera);
 
       // Default SequenceSettings should do a single snap.
@@ -41,43 +77,13 @@ public class SanityTest {
       assertEquals(0, packet.camera.serialNumber);
       assertEquals(0, packet.camera.frameNumber);
       assertTrue(packet.startCounter < packet.currentCounter);
-
-      mmc.delete(); // TODO Put this in teardown.
    }
 
-   public CMMCore prepareTestDevices(String... devices) throws Exception {
-      CMMCore mmc = createMMCore();
-      mmc.loadDevice("THub", "SequenceTester", "THub");
-      mmc.initializeDevice("THub");
-
+   public void prepareTestDevices(CMMCore mmc, String... devices) throws Exception {
       for (String device : devices) {
          mmc.loadDevice(device, "SequenceTester", device);
          mmc.initializeDevice(device);
       }
-
-      return mmc;
-   }
-
-   public CMMCore createMMCore() {
-      String jniLibPath = System.getenv("MMCOREJ_LIBRARY_PATH");
-      String adapterPath = System.getenv("MMTEST_ADAPTER_PATH");
-
-      System.setProperty("mmcorej.library.loading.stderr.log", "yes");
-      if (jniLibPath != null) {
-         System.setProperty("mmcorej.library.path", jniLibPath);
-      }
-      CMMCore mmc = new CMMCore();
-
-      mmc.enableStderrLog(true);
-      mmc.enableDebugLog(true);
-
-      StrVector paths = new StrVector();
-      if (adapterPath != null) {
-         paths.add(adapterPath);
-      }
-      mmc.setDeviceAdapterSearchPaths(paths);
-
-      return mmc;
    }
 
    public List<InfoPacket>
@@ -85,15 +91,23 @@ public class SanityTest {
       throws InterruptedException, java.io.IOException
    {
       List<InfoPacket> packets = new ArrayList<InfoPacket>();
+      int packetCount = 0;
       for (;;) {
          TaggedImage tim = q.poll(1, TimeUnit.SECONDS);
          assertNotNull(tim);
          if (tim == POISON) {
+            assertTrue("images should not remain in queue after POISON",
+                  q.isEmpty());
             break;
          }
+
          // For now, we always get 8-bit images.
          byte[] data = (byte[]) tim.pix;
+
+         System.out.print("Packet " + packetCount + ": ");
          TestImageDecoder.dumpJSON(data, System.out);
+         System.out.println();
+
          packets.add(TestImageDecoder.decode(data));
       }
       return packets;
