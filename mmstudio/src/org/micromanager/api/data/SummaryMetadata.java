9deda7b912639a890e577f7af841869c9c2dfc0f
Chris Weisiger <cweisiger@msg.ucsf.edu>
2014-11-11 09:26:17 -0800

    Add documentation of the SummaryMetadata fields.

--- mmstudio/src/org/micromanager/api/data/SummaryMetadata.java
+++ mmstudio/src/org/micromanager/api/data/SummaryMetadata.java
@@ -25,7 +25,8 @@ public interface SummaryMetadata {
       SummaryMetadata build();
 
       // The following functions each set the relevant value for the 
-      // SummaryMetadata.
+      // SummaryMetadata. See the corresponding getter methods of
+      // SummaryMetadata, below, for the meaning of these properties.
       SummaryMetadataBuilder acquisitionName(String acquisitionName);
       SummaryMetadataBuilder fileName(String fileName);
       SummaryMetadataBuilder prefix(String prefix);
@@ -49,19 +50,40 @@ public interface SummaryMetadata {
     */
    SummaryMetadataBuilder copy();
 
+   /** The name of the acquisition. TODO: are we deprecating this? */
    public String getAcquisitionName();
+   /** The complete filename for this file, including any suffix attached
+    * by Micro-Manager */
    public String getFileName();
+   /** The user-supplied portion of the filename, not including Micro-Manager's
+     * additional suffix */
    public String getPrefix();
+   /** The signed-in user of the machine that collected this data */
    public String getUserName();
+   /** The version of Micro-Manager used to collect the data */
    public String getMicroManagerVersion();
+   /** The version of the metadata when the data was collected */
    public String getMetadataVersion();
+   /** The name of the computer the data was collected on */
    public String getComputerName();
+   /** The directory the data was originally saved to */
    public String getDirectory();
+   /** Any comments attached to the acquisition as a whole (not to
+     * individual images within the acquisition) */
    public String getComments();
+   /** Amount of time to wait between timepoints */
    public Double getWaitInterval();
+   /** When using a variable amount of time between timepoints, this array
+     * has the list of wait times */
    public Double[] getCustomIntervalsMs();
+   /** The expected number of images along each axis that were to be collected.
+     * The actual dimensions may differ if the acquisition was aborted partway
+     * through for any reason.
+     */
    public Coords getIntendedDimensions();
+   /** The date and time at which the acquisition started */
    public String getStartDate();
+   /** The stage positions that were to be visited in the acquisition */
    public MultiStagePosition[] getStagePositions();
 
    /**
