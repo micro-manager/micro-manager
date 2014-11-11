3f655953298c5a4df54b39106a586c05cf030fd1
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-11-11 18:15:08 +0000

    MMDevice: Unit tests for float property truncation
    Turns out all these pass already.
    
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14602 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ MMDevice/unittest/FloatPropertyTruncation-Tests.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+
+#include "Property.h"
+
+using namespace MM;
+
+
+TEST(FloatPropertyTruncationTests, SetValueIsTruncatedTo4Digits)
+{
+   FloatProperty fp;
+   double v;
+
+   ASSERT_TRUE(fp.Set(0.00004));
+   ASSERT_TRUE(fp.Get(v));
+   ASSERT_DOUBLE_EQ(0.0, v);
+
+   ASSERT_TRUE(fp.Set(0.00005));
+   ASSERT_TRUE(fp.Get(v));
+   ASSERT_DOUBLE_EQ(0.0001, v);
+
+   ASSERT_TRUE(fp.Set(-0.00004));
+   ASSERT_TRUE(fp.Get(v));
+   ASSERT_DOUBLE_EQ(0.0, v);
+
+   ASSERT_TRUE(fp.Set(-0.00005));
+   ASSERT_TRUE(fp.Get(v));
+   ASSERT_DOUBLE_EQ(-0.0001, v);
+}
+
+
+TEST(FloatPropertyTruncationTests, LowerLimitIsTruncatedUp)
+{
+   FloatProperty fp;
+
+   ASSERT_TRUE(fp.SetLimits(0.0, 1000.0));
+   ASSERT_DOUBLE_EQ(0.0, fp.GetLowerLimit());
+   ASSERT_TRUE(fp.SetLimits(0.00001, 1000.0));
+   ASSERT_DOUBLE_EQ(0.0001, fp.GetLowerLimit());
+   ASSERT_TRUE(fp.SetLimits(0.00011, 1000.0));
+   ASSERT_DOUBLE_EQ(0.0002, fp.GetLowerLimit());
+
+   ASSERT_TRUE(fp.SetLimits(-0.0, 1000.0));
+   ASSERT_DOUBLE_EQ(0.0, fp.GetLowerLimit());
+   ASSERT_TRUE(fp.SetLimits(-0.00001, 1000.0));
+   ASSERT_DOUBLE_EQ(0.0, fp.GetLowerLimit());
+   ASSERT_TRUE(fp.SetLimits(-0.00011, 1000.0));
+   ASSERT_DOUBLE_EQ(-0.0001, fp.GetLowerLimit());
+}
+
+
+TEST(FloatPropertyTruncationTests, UpperLimitIsTruncatedDown)
+{
+   FloatProperty fp;
+
+   ASSERT_TRUE(fp.SetLimits(-1000.0, 0.0));
+   ASSERT_DOUBLE_EQ(0.0, fp.GetUpperLimit());
+   ASSERT_TRUE(fp.SetLimits(-1000.0, 0.00001));
+   ASSERT_DOUBLE_EQ(0.0, fp.GetUpperLimit());
+   ASSERT_TRUE(fp.SetLimits(-1000.0, 0.00011));
+   ASSERT_DOUBLE_EQ(0.0001, fp.GetUpperLimit());
+
+   ASSERT_TRUE(fp.SetLimits(-1000.0, -0.0));
+   ASSERT_DOUBLE_EQ(0.0, fp.GetUpperLimit());
+   ASSERT_TRUE(fp.SetLimits(-1000.0, -0.00001));
+   ASSERT_DOUBLE_EQ(-0.0001, fp.GetUpperLimit());
+   ASSERT_TRUE(fp.SetLimits(-1000.0, -0.00011));
+   ASSERT_DOUBLE_EQ(-0.0002, fp.GetUpperLimit());
+}
+
+
+int main(int argc, char **argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
