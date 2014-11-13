8b0ee72a8664b5fcea1880061ab8ef12c3f17f34
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-11-13 17:42:19 +0000

    PositionList: Added missing header to PositionTableModel
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14612 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/positionlist/PositionTableModel.java
+++ mmstudio/src/org/micromanager/positionlist/PositionTableModel.java
@@ -1,3 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+//FILE:          PositionTableModel.java
+//PROJECT:       Micro-Manager  
+//SUBSYSTEM:     mmstudio
+//-----------------------------------------------------------------------------
+//
+// AUTHOR:       Chris WEisiger
+//
+// COPYRIGHT:    University of California, San Francisco 2014
+//
+// LICENSE:      This file is distributed under the BSD license.
+//               License text is included with the source distribution.
+//
+//               This file is distributed in the hope that it will be useful,
+//               but WITHOUT ANY WARRANTY; without even the implied warranty
+//               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+
 package org.micromanager.positionlist;
 
 import javax.swing.table.AbstractTableModel;
