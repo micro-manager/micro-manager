e2e446f96f66fd83e10b03b8aa07cc7037407aee
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2014-12-17 19:20:10 +0000

    TIScam: Fixes to device adapter (Johannes Schindelin)
    TIScam: initialize previously unintialized pointers
    TIScam: avoid crashes by allocating/releasing the buffer properly
    
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14790 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/TISCam/TIScamera.cpp
+++ DeviceAdapters/TISCam/TIScamera.cpp
@@ -156,6 +156,8 @@ perform most of the initialization in the Initialize() method.
 CTIScamera::CTIScamera() : CCameraBase<CTIScamera> (),
    initialized_(false),
 
+   pSelectDevice(NULL),
+   pShowProperties(NULL),
    lCCD_Width(0),
    lCCD_Height(0),
    uiCCD_BitsPerPixel(8),
@@ -197,6 +199,11 @@ currentExpMS_(12.34), //ms
 
    m_pSimpleProperties = NULL;
    seqThread_ = new AcqSequenceThread(this); 
+
+   for (int ii = 0; ii < NUMBER_OF_BUFFERS; ++ii)
+   {
+      pBuf[ii] = NULL;
+   }
 
    // create a pre-initialization property and list all the available cameras
    char szPath[MAX_PATH];
@@ -1020,7 +1027,10 @@ int CTIScamera::Shutdown()
    }
    for (int ii = 0; ii < NUMBER_OF_BUFFERS; ++ii)
    {
-      delete pBuf[ii];
+      if (pBuf[ii]) {
+         delete pBuf[ii];
+         pBuf[ii] = NULL;
+      }
    }
    delete pGrabber;
    pGrabber = 0;
@@ -1421,11 +1431,6 @@ void CTIScamera::RecalculateROI()
 
       pGrabber->startLive(ACTIVEMOVIE);
 
-
-      for (int ii = 0; ii < NUMBER_OF_BUFFERS; ++ii)
-      {
-         delete pBuf[ii];
-      }
 
       // Retrieve the output type and dimension of the handler sink.
       // The dimension of the sink could be different from the VideoFormat, when
