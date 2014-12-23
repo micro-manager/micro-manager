0fe1bb4b1b91da6c26d5ea1c39b1d3bcb62e408c
Chris Weisiger <cweisiger@msg.ucsf.edu>
2014-12-23 12:12:28 -0800

    [Link button] Replace icon; improve button size.

--- mmstudio/src/org/micromanager/imagedisplay/LinkButton.java
+++ mmstudio/src/org/micromanager/imagedisplay/LinkButton.java
@@ -13,16 +13,21 @@ import javax.swing.JToggleButton;
  * attributes across multiple DisplayWindows.
  */
 public class LinkButton extends JToggleButton {
+   // This icon is courtesy of
+   // http://icon-park.com/icon/black-link-icon-vector-data/
    private static final ImageIcon LINK_ICON = SwingResourceManager.getIcon(
-         LinkButton.class, "/org/micromanager/icons/linkflat30.png");
+         LinkButton.class, "/org/micromanager/icons/linkflat.png");
    public LinkButton() {
       super(LINK_ICON);
       setMinimumSize(new Dimension(1, 1));
       setMargin(new Insets(0, 0, 0, 0));
    }
 
+   /**
+    * We want to be slightly larger than the icon we contain.
+    */
    public Dimension getPreferredSize() {
-      return new Dimension(LINK_ICON.getIconWidth() + 2,
+      return new Dimension(LINK_ICON.getIconWidth() + 6,
             LINK_ICON.getIconHeight() + 2);
    }
 }
