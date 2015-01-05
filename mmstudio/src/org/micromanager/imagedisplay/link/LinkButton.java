91562573b6a5adb81193c26a131f94847041b8b7
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-01-05 14:16:49 -0800

    Move LinkButton to the imagedisplay.link package.

--- mmstudio/src/org/micromanager/imagedisplay/link/LinkButton.java
+++ mmstudio/src/org/micromanager/imagedisplay/link/LinkButton.java
@@ -1,5 +1,6 @@
-package org.micromanager.imagedisplay;
+package org.micromanager.imagedisplay.link;
 
+import com.google.common.eventbus.EventBus;
 import com.swtdesigner.SwingResourceManager;
 
 import java.awt.Dimension;
@@ -8,19 +9,33 @@ import java.awt.Insets;
 import javax.swing.ImageIcon;
 import javax.swing.JToggleButton;
 
+import org.micromanager.api.display.DisplayWindow;
+
 /**
  * This class provides the GUI for a button that links DisplaySettings
  * attributes across multiple DisplayWindows.
  */
 public class LinkButton extends JToggleButton {
-   // This icon is courtesy of
+   // This icon is a modified version of
    // http://icon-park.com/icon/black-link-icon-vector-data/
    private static final ImageIcon LINK_ICON = SwingResourceManager.getIcon(
          LinkButton.class, "/org/micromanager/icons/linkflat.png");
-   public LinkButton() {
+
+   public LinkButton(final SettingsLinker linker,
+         final DisplayWindow display) {
       super(LINK_ICON);
       setMinimumSize(new Dimension(1, 1));
       setMargin(new Insets(0, 0, 0, 0));
+
+      linker.setButton(this);
+      final LinkButton finalThis = this;
+      addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            display.postEvent(new LinkButtonEvent(linker, display,
+                  finalThis));
+         }
+      });
    }
 
    /**
