524cde9d9bd7c5e02ec0a6ae26ae5befc7f3dcf2
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2015-01-21 23:52:38 +0000

    TriggerScope: Update license notice
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14957 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/TriggerScope/TriggerScope.cpp
+++ DeviceAdapters/TriggerScope/TriggerScope.cpp
@@ -10,17 +10,24 @@
 //
 // COPYRIGHT:     Advanced Research Consulting. (2014)
 //
-// LICENSE:       
-
+// LICENSE:       This library is free software; you can redistribute it and/or
+//                modify it under the terms of the GNU Lesser General Public
+//                License as published by the Free Software Foundation.
 //
-//                This file is distributed in the hope that it will be useful,
-//                but WITHOUT ANY WARRANTY; without even the implied warranty
-//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//                This library is distributed in the hope that it will be
+//                useful, but WITHOUT ANY WARRANTY; without even the implied
+//                warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//                PURPOSE. See the GNU Lesser General Public License for more
+//                details.
 //
-//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+//                LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//                EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
 //
+//                You should have received a copy of the GNU Lesser General
+//                Public License along with this library; if not, write to the
+//                Free Software Foundation, Inc., 51 Franklin Street, Fifth
+//                Floor, Boston, MA 02110-1301 USA.
 
 #include "TriggerScope.h"
 #include <cstdio>
