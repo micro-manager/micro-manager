f5990af64b2efa2c177f922ecc88afae1dd40d2e
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2015-01-21 23:52:39 +0000

    TriggerScope: Remove unnecessary DllMain function
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14958 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/TriggerScope/TriggerScope.cpp
+++ DeviceAdapters/TriggerScope/TriggerScope.cpp
@@ -52,27 +52,6 @@ const char* line_feed = "\n";
 const char * g_TriggerScope_Version = "v1.0.2, 28/9/2014";
 
 
-// TODO: linux entry code
-
-// windows DLL entry code
-#ifdef WIN32
-BOOL APIENTRY DllMain( HANDLE /*hModule*/, 
-                      DWORD  ul_reason_for_call, 
-                      LPVOID /*lpReserved*/
-                      )
-{
-   switch (ul_reason_for_call)
-   {
-   case DLL_PROCESS_ATTACH:
-   case DLL_THREAD_ATTACH:
-   case DLL_THREAD_DETACH:
-   case DLL_PROCESS_DETACH:
-      break;
-   }
-   return TRUE;
-}
-#endif
-
 ///////////////////////////////////////////////////////////////////////////////
 // Exported MMDevice API
 ///////////////////////////////////////////////////////////////////////////////
