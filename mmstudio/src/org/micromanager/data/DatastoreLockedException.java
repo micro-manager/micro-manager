6911c686b71b6facb200ba7dcfd151dc75b690ae
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-02-02 12:30:54 -0800

    [data] Add a TODO.

--- mmstudio/src/org/micromanager/data/DatastoreLockedException.java
+++ mmstudio/src/org/micromanager/data/DatastoreLockedException.java
@@ -3,5 +3,6 @@ package org.micromanager.data;
 /**
  * This exception is raised when an attempt is made to modify a Datastore after
  * its lock() method has been called.
+ * TODO: make an interface and move implementation to data/internal.
  */
 public class DatastoreLockedException extends Exception {}
