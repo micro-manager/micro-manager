101e6a82c10773608104a459675ca3c807ce61ef
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-02-03 14:08:17 -0800

    [Data] Remove pointless TODO.
    Can't make interfaces out of exceptions, alas.

--- mmstudio/src/org/micromanager/data/DatastoreLockedException.java
+++ mmstudio/src/org/micromanager/data/DatastoreLockedException.java
@@ -3,6 +3,5 @@ package org.micromanager.data;
 /**
  * This exception is raised when an attempt is made to modify a Datastore after
  * its lock() method has been called.
- * TODO: make an interface and move implementation to data/internal.
  */
 public class DatastoreLockedException extends Exception {}
