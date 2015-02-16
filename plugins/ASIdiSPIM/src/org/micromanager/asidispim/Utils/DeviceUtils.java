2d720314480f7aefb87d72b942094b0579b4c507
jondaniels <jondaniels@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2015-02-16 20:30:11 +0000

    ASIdiSPIM: changed recommended minimum PLogic firmware
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@15053 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/DeviceUtils.java
+++ plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/DeviceUtils.java
@@ -93,7 +93,7 @@ public class DeviceUtils {
             }
             break;
          case PLOGIC:
-            if (firmwareVersion < 3.039) {
+            if (firmwareVersion < 3.069) {
                MyDialogUtils.showError("Device " + devices_.getMMDevice(key)
                      + ": PLogic firmware is old; some features may not work."
                      + " Contact ASI for updated firmware.");
