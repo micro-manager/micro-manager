6d81aabee9d8b24ea4f742f762ecd01c3aff1d01
Nico Stuurman <nico.stuurman@ucsf.edu>
2015-03-12 19:19:17 -0700

    [mmstudio] Fix deprecated annotation in utils.GUIUpdater

--- mmstudio/src/org/micromanager/internal/utils/GUIUpdater.java
+++ mmstudio/src/org/micromanager/internal/utils/GUIUpdater.java
@@ -32,6 +32,7 @@ import javax.swing.SwingUtilities;
  * 
  * @deprecated
  */
+@Deprecated
 public class GUIUpdater {
 
    final AtomicReference<Runnable> latestTask = new AtomicReference<Runnable>();
