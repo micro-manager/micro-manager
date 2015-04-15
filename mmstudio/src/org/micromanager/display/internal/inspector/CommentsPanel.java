928d283a50f538ff37fdbc8a9e17231f4ec56716
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-04-15 11:05:16 -0700

    [Comments] Many bugfixes; comments UI is now usable.
    No word yet on if saving/loading works. And the way we update the images in the
    Datastore causes the displayed image to jump around sometimes, which is
    suboptimal. Probably need to add a replaceImage() method to the Datastore that
    doesn't provoke display updates...

--- mmstudio/src/org/micromanager/display/internal/inspector/CommentsPanel.java
+++ mmstudio/src/org/micromanager/display/internal/inspector/CommentsPanel.java
@@ -26,6 +26,8 @@ import ij.gui.ImageWindow;
 
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -63,16 +65,18 @@ public class CommentsPanel extends InspectorPanel {
    private Datastore store_;
    private MMVirtualStack stack_;
    private Timer updateTimer_;
+   private HashMap<Image, Timer> imageToSaveTimer_;
 
    /** Creates new form CommentsPanel */
    public CommentsPanel() {
+      imageToSaveTimer_ = new HashMap<Image, Timer>();
       initialize();
       addTextChangeListeners();
       addFocusListeners();
    }
 
    private void initialize() {
-      JPanel summaryPanel = new JPanel(new MigLayout("flowy"));
+      JPanel summaryPanel = new JPanel(new MigLayout("flowy, insets 0"));
 
       summaryPanel.add(new JLabel("Acquisition comments:"));
 
@@ -110,29 +114,80 @@ public class CommentsPanel extends InspectorPanel {
       return result;
    }
 
+   /**
+    * Every time we change the metadata on an Image, we have to call putImage()
+    * on the Datastore to replace the old image, which in turn causes the
+    * displayed image to "change" and our imageChangedUpdate method to be
+    * called. To avoid flicker, we delay saves using a timer to wait until
+    * after the user is done typing (5s).
+    */
    private void recordCommentsChanges() {
       Image curImage = store_.getImage(stack_.getCurrentImageCoords());
+      // Determine if anything has actually changed.
+      String imageText = null;
+      String summaryText = null;
       Metadata metadata = curImage.getMetadata();
-      try {
-         String oldComments = metadata.getComments();
-         if (oldComments == null ||
-               !oldComments.equals(imageCommentsTextArea_.getText())) {
-            metadata = metadata.copy().comments(imageCommentsTextArea_.getText()).build();
-            store_.putImage(curImage.copyWithMetadata(metadata));
-         }
-         SummaryMetadata summary = store_.getSummaryMetadata();
-         String oldSummary = summary.getComments();
-         if (oldSummary == null ||
-               !oldSummary.equals(summaryCommentsTextArea_.getText())) {
-            summary = summary.copy().comments(summaryCommentsTextArea_.getText()).build();
-            store_.setSummaryMetadata(summary);
-         }
+      String oldComments = metadata.getComments();
+      if (oldComments == null ||
+            !oldComments.equals(imageCommentsTextArea_.getText())) {
+         // Comments have changed.
+         imageText = imageCommentsTextArea_.getText();
       }
-      catch (DatastoreFrozenException e) {
-         ReportingUtils.showError("Comments cannot be changed because the datastore has been locked.");
+      SummaryMetadata summary = store_.getSummaryMetadata();
+      String oldSummary = summary.getComments();
+      if (oldSummary == null ||
+            !oldSummary.equals(summaryCommentsTextArea_.getText())) {
+         // Summary comments have changed.
+         summaryText = summaryCommentsTextArea_.getText();
+      }
+      synchronized(imageToSaveTimer_) {
+         if (imageToSaveTimer_.containsKey(curImage)) {
+            // Cancel the current timer. Since the task is synchronized around
+            // imageToSaveTimer_, there should be no race condition here.
+            imageToSaveTimer_.get(curImage).cancel();
+         }
+         Timer timer = new Timer("Ephemeral comments save timer");
+         timer.schedule(makeSaveTask(curImage, imageText, summaryText), 5000);
+         imageToSaveTimer_.put(curImage, timer);
       }
    }
 
+   /**
+    * Create a task that will record changes in the comments text for the
+    * given image. Only makes changes if the input strings are non-null.
+    */
+   private TimerTask makeSaveTask(final Image image,
+         final String imageText, final String summaryText) {
+      return new TimerTask() {
+         @Override
+         public void run() {
+            synchronized(imageToSaveTimer_) {
+               Metadata metadata = image.getMetadata();
+               try {
+                  if (imageText != null) {
+                     // Comments have changed.
+                     metadata = metadata.copy().comments(imageText).build();
+                     store_.putImage(image.copyWithMetadata(metadata));
+                  }
+                  if (summaryText != null) {
+                     // Summary comments have changed.
+                     SummaryMetadata summary = store_.getSummaryMetadata();
+                     summary = summary.copy().comments(summaryText).build();
+                     store_.setSummaryMetadata(summary);
+                  }
+               }
+               catch (DatastoreFrozenException e) {
+                  ReportingUtils.showError("Comments cannot be changed because the datastore has been locked.");
+               }
+               imageToSaveTimer_.remove(image);
+            }
+         }
+      };
+   }
+
+   /**
+    * TODO: why do we care about losing focus?
+    */
    private void addFocusListeners() {
       FocusListener listener = new FocusListener() {
          @Override
@@ -168,13 +223,17 @@ public class CommentsPanel extends InspectorPanel {
    }
 
    /**
-    * We postpone metadata display updates slightly in case the image display
+    * We postpone comments display updates slightly in case the image display
     * is changing rapidly, to ensure that we don't end up with a race condition
     * that causes us to display the wrong metadata.
     */
    public synchronized void imageChangedUpdate(final Image image) {
-      if (updateTimer_ == null) {
-         updateTimer_ = new Timer("Metadata update");
+      // Do nothing if the new image's comments match our current contents,
+      // to avoid reseting the cursor position.
+      String newComments = image.getMetadata().getComments();
+      if (newComments != null &&
+            newComments.contentEquals(imageCommentsTextArea_.getText())) {
+         return;
       }
       TimerTask task = new TimerTask() {
          @Override
@@ -186,13 +245,21 @@ public class CommentsPanel extends InspectorPanel {
       };
       // Cancel all pending tasks and then schedule our task for execution
       // 125ms in the future.
-      updateTimer_.purge();
+      if (updateTimer_ != null) {
+         updateTimer_.cancel();
+      }
+      updateTimer_ = new Timer("Metadata update");
       updateTimer_.schedule(task, 125);
    }
 
    @Subscribe
    public void onPixelsSet(PixelsSetEvent event) {
-      imageChangedUpdate(event.getImage());
+      try {
+         imageChangedUpdate(event.getImage());
+      }
+      catch (Exception e) {
+         ReportingUtils.logError(e, "Error on pixels set");
+      }
    }
 
    @Override
@@ -206,6 +273,10 @@ public class CommentsPanel extends InspectorPanel {
       stack_ = ((DefaultDisplayWindow) display).getStack();
       display_ = display;
       display_.registerForEvents(this);
+      List<Image> images = display_.getDisplayedImages();
+      if (images.size() > 0) {
+         imageChangedUpdate(images.get(0));
+      }
    }
 
    @Override
