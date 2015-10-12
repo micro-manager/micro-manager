ae65d1f79322ae57c6c8c922672f9f4c3836e643
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-10-12 13:06:13 -0700

    [RGB images] Update RGBColorModel to not cause error spew.

--- mmstudio/src/main/java/org/micromanager/display/internal/RGBColorModel.java
+++ mmstudio/src/main/java/org/micromanager/display/internal/RGBColorModel.java
@@ -21,8 +21,6 @@
 package org.micromanager.display.internal;
 
 import java.awt.color.ColorSpace;
-import java.awt.color.ICC_ColorSpace;
-import java.awt.color.ICC_Profile;
 import java.awt.image.ColorModel;
 import java.awt.image.ComponentColorModel;
 import java.awt.image.DataBuffer;
@@ -32,13 +30,17 @@ import java.awt.image.SinglePixelPackedSampleModel;
 import java.awt.image.WritableRaster;
 import java.awt.Point;
 
+/**
+ * This class is used to give us control over how the individual components
+ * in an RGB image are displayed.
+ */
 public class RGBColorModel extends ComponentColorModel {
    private int[] mins_ = new int[] {0, 0, 0};
    private int[] maxes_ = new int[] {0, 255, 255};
 
    public RGBColorModel() {
-      super(new ICC_ColorSpace(ICC_Profile.getInstance(ColorSpace.CS_sRGB)),
-            false, false, java.awt.Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+      super(ColorSpace.getInstance(ColorSpace.CS_sRGB),
+            false, false, java.awt.Transparency.OPAQUE, DataBuffer.TYPE_INT);
    }
 
    @Override
@@ -48,16 +50,57 @@ public class RGBColorModel extends ComponentColorModel {
 
    @Override
    public int getRed(int pixel) {
-      return 255;
+      return 0;
    }
 
    @Override
    public int getGreen(int pixel) {
-      return 128;
+      return 255;
    }
 
    @Override
    public int getBlue(int pixel) {
       return 0;
+   }
+
+   /**
+    * ComponentColorModel only expects to work with byte- and short-based
+    * image packings, but ImageJ uses packed int[] arrays instead, so we need
+    * to adapt the "compatible"-related methods to work with int[]s instead.
+    */
+   @Override
+   public WritableRaster createCompatibleWritableRaster(int width, int height) {
+      return Raster.createPackedRaster(DataBuffer.TYPE_INT,
+            width, height, 3, 8, null);
+   }
+
+   /**
+    * For the same reason that we have to override
+    * createCompatibleWritableRaster, we have to override this method.
+    */
+   @Override
+   public SampleModel createCompatibleSampleModel(int width, int height) {
+      return new SinglePixelPackedSampleModel(DataBuffer.TYPE_INT,
+            width, height, new int[] {0xff000000, 0xff0000, 0xff00});
+   }
+
+   /**
+    * For the same reason that we have to override
+    * createCompatibleWritableRaster, we have to override this method.
+    */
+   @Override
+   public boolean isCompatibleRaster(Raster raster) {
+      return isCompatibleSampleModel(raster.getSampleModel());
+   }
+
+   /**
+    * For the same reason that we have to override
+    * createCompatibleWritableRaster, we have to override this method.
+    */
+   @Override
+   public boolean isCompatibleSampleModel(SampleModel model) {
+      return (model instanceof SinglePixelPackedSampleModel &&
+            model.getNumBands() == getNumComponents() &&
+            model.getTransferType() == getTransferType());
    }
 }
