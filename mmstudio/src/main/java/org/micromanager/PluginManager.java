c7bf6b15db79942bf5edb1a25bc00e5985d8c6cc
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-10-22 14:31:38 -0700

    PluginManager: tweak documentation.

--- mmstudio/src/main/java/org/micromanager/PluginManager.java
+++ mmstudio/src/main/java/org/micromanager/PluginManager.java
@@ -32,23 +32,23 @@ import org.micromanager.quickaccess.QuickAccessPlugin;
  */
 public interface PluginManager {
    /**
-    * Return a HashMap that maps plugin names to ProcessorPlugin instances.
-    * ProcessorPlugins are used to construct pipelines that modify images
-    * as they are acquired (see DataManager.createPipeline()).
-    * @return HashMap that maps plugin names to ProcessorPlugin instances
+    * Return a HashMap that maps plugin class names to ProcessorPlugin
+    * instances. ProcessorPlugins are used to construct pipelines that modify
+    * images as they are acquired (see DataManager.createPipeline()).
+    * @return HashMap that maps plugin class names to ProcessorPlugin instances
     */
    public HashMap<String, ProcessorPlugin> getProcessorPlugins();
 
    /**
-    * Return a HashMap that maps plugin names to OverlayPlugin instances.
+    * Return a HashMap that maps plugin class names to OverlayPlugin instances.
     * OverlayPlugins are used to draw on top of images in the image display
     * window.
-    * @return HashMap that maps plugin names to OverlayPlugin instances
+    * @return HashMap that maps plugin class names to OverlayPlugin instances
     */
    public HashMap<String, OverlayPlugin> getOverlayPlugins();
 
    /**
-    * Return a HashMap that maps plugin names to MenuPlugin instances.
+    * Return a HashMap that maps plugin class names to MenuPlugin instances.
     * MenuPlugins are normally available under the "Plugins" menu; this method
     * allows plugins to talk to each other without explicitly depending on
     * each other.
@@ -56,13 +56,13 @@ public interface PluginManager {
    public HashMap<String, MenuPlugin> getMenuPlugins();
 
    /**
-    * Return a HashMap that maps plugin names to AutofocusPlugin instances.
-    * AutofocusPlugins are used to perform autofocus operations.
+    * Return a HashMap that maps plugin class names to AutofocusPlugin
+    * instances. AutofocusPlugins are used to perform autofocus operations.
     */
    public HashMap<String, AutofocusPlugin> getAutofocusPlugins();
 
    /**
-    * Return a HashMap that maps plugin names to IntroPlugin instances.
+    * Return a HashMap that maps plugin class names to IntroPlugin instances.
     * IntroPlugins are used to customize the intro dialog at the start of the
     * program. Ordinarily there should only be at most one of this kind of
     * plugin installed at a time.
@@ -70,9 +70,9 @@ public interface PluginManager {
    public HashMap<String, IntroPlugin> getIntroPlugins();
 
    /**
-    * Return a HashMap that maps plugin names to QuickAccessPlugin instances.
-    * QuickAccessPlugins are for controls that can be included in the Quick-
-    * Access Window.
+    * Return a HashMap that maps plugin class names to QuickAccessPlugin
+    * instances. QuickAccessPlugins are for controls that can be included in
+    * the Quick- Access Window.
     */
    public HashMap<String, QuickAccessPlugin> getQuickAccessPlugins();
 }
