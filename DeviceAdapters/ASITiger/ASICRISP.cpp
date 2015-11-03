micro-manager/micro-manager

commit 2b8d20c7d3ef98f23b79895fa6c670cca79249ec

ASITiger: bugfix for CRISP GetContinuousFocusing() (thanks to Forrest Collman) and improved implementation of SetContinuousFocusing()
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@15829 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

DeviceAdapters/ASITiger/ASICRISP.cpp
@@ -152,19 +152,29 @@ bool CCRISP::Busy()
 int CCRISP::SetContinuousFocusing(bool state)
 {
    ostringstream command; command.str("");
-   command << addressChar_ << "LK F=";
-   if(state)
-      command << "83";
-   else
-      command << "85";
-   return hub_->QueryCommandVerify(command.str(), ":A");
+   bool focusingOn;
+   RETURN_ON_MM_ERROR( GetContinuousFocusing(focusingOn) );  // will update focusState_
+   if (focusingOn && !state) {
+   	// was on, turning off
+   	return ForceSetFocusState(g_CRISP_R);
+   } else if (!focusingOn && state) {
+   	// was off, turning on
+   	if (focusState_ == g_CRISP_R ) {
+   		return ForceSetFocusState(g_CRISP_K);
+   	} else {  // need to move to ready state, then turn on
+   		RETURN_ON_MM_ERROR( ForceSetFocusState(g_CRISP_R) );
+   		RETURN_ON_MM_ERROR( ForceSetFocusState(g_CRISP_K) );
+   	}
+   }
+   // if was already in state requested we don't need to do anything
+   return DEVICE_OK;
 }
 
 int CCRISP::GetContinuousFocusing(bool& state)
 {
-   // this returns true if trying to focus but not yet locked, not sure if that is intended use
+   // this returns true if focusing is turned on, whether or not we are locked yet
    RETURN_ON_MM_ERROR( UpdateFocusState() );
-   state = (focusState_ == g_CRISP_K);
+   state = ((focusState_ == g_CRISP_K) || (focusState_ == g_CRISP_F));
    return DEVICE_OK;
 }
 
@@ -270,6 +280,32 @@ int CCRISP::UpdateFocusState()
    return DEVICE_OK;
 }
 
+int CCRISP::ForceSetFocusState(string focusState)
+{
+   ostringstream command; command.str("");
+   if (focusState_ == g_CRISP_R)
+   	command << addressChar_ << "LK F=85";
+   else if (focusState == g_CRISP_K)
+   	command << addressChar_ << "LK F=83";
+   else if (focusState == g_CRISP_SSZ) // save settings to controller
+      command << addressChar_ << "SS Z";
+   else if (focusState == g_CRISP_I)  // Idle (switch off LED)
+      command << addressChar_ << "LK F=79";
+   else if (focusState == g_CRISP_G) // log-amp calibration
+         command << addressChar_ << "LK F=72";
+   else if (focusState == g_CRISP_SG) // gain_cal (servo) calibration
+         command << addressChar_ << "LK F=67";
+   else if (focusState == g_CRISP_f) // dither
+         command << addressChar_ << "LK F=102";
+   else if (focusState == g_CRISP_RFO) // reset focus offset
+         command << addressChar_ << "LK F=108";
+
+   if (command.str() == "")
+      return DEVICE_OK;  // don't complain if we try to set to something else
+   else
+      return hub_->QueryCommandVerify(command.str(), ":A");
+}
+
 int CCRISP::SetFocusState(string focusState)
 {
    RETURN_ON_MM_ERROR ( UpdateFocusState() );
@@ -277,30 +313,7 @@ int CCRISP::SetFocusState(string focusState)
    if (focusState == focusState_)
       return DEVICE_OK;
 
-   if (focusState == g_CRISP_R)  // Unlock
-      return SetContinuousFocusing(false);
-
-   if (focusState == g_CRISP_K)  // Unlock
-      return SetContinuousFocusing(true);
-
-   ostringstream command; command.str("");
-   if (focusState == g_CRISP_SSZ) // save settings to controller
-      command << addressChar_ << "SS Z";
-   else if (focusState == g_CRISP_I)  // Idle (switch off LED)
-      command << addressChar_ << "LK F=" << "79";
-   else if (focusState == g_CRISP_G) // log-amp calibration
-         command << addressChar_ << "LK F=" << "72";
-   else if (focusState == g_CRISP_SG) // gain_cal (servo) calibration
-         command << addressChar_ << "LK F=" << "67";
-   else if (focusState == g_CRISP_f) // dither
-         command << addressChar_ << "LK F=" << "102";
-   else if (focusState == g_CRISP_RFO) // reset focus offset
-         command << addressChar_ << "LK F=" << "108";
-
-   if (command.str() == "")
-      return DEVICE_OK;  // don't complain if we try to set to something else
-   else
-      return hub_->QueryCommandVerify(command.str(), ":A");
+   return ForceSetFocusState(focusState);
 }
 
 

DeviceAdapters/ASITiger/ASICRISP.h
@@ -74,6 +74,7 @@ private:
 
    int UpdateFocusState();
    int SetFocusState(string focusState);
+   int ForceSetFocusState(string focusState);
 };
 
 #endif //_ASICRISP_H_

DeviceAdapters/ASITiger/ASITiger.h
@@ -573,7 +573,7 @@ const char* const g_AxisPolarityReversed = "Reversed";  // used for other stages
 // CRISP states
 const char* const g_CRISPState = "CRISP State";
 const char* const g_CRISP_I = "Idle";
-const char* const g_CRISP_R = "Ready";
+const char* const g_CRISP_R = "Ready";  // LED on and ready to move to K/lock state
 const char* const g_CRISP_D = "Dim";
 const char* const g_CRISP_K = "Lock";  // enter this state to try to lock, system will move to F when locked
 const char* const g_CRISP_F = "In Focus";  // a "read only" state, don't go to directly but via K state

commit 89f30befcf5af485dce9e06e331dd49cef0adc16

ASITiger: fix CRISP bug introduced in r15829
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@15836 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

DeviceAdapters/ASITiger/ASICRISP.cpp
@@ -296,7 +296,7 @@ int CCRISP::UpdateFocusState()
 int CCRISP::ForceSetFocusState(string focusState)
 {
    ostringstream command; command.str("");
-   if (focusState_ == g_CRISP_R)
+   if (focusState == g_CRISP_R)
    	command << addressChar_ << "LK F=85";
    else if (focusState == g_CRISP_K)
    	command << addressChar_ << "LK F=83";