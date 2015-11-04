6deedb433a71c506dc9bb1f4fafe19e4f4809d80
Chris Weisiger <cweisiger@msg.ucsf.edu>
2015-11-04 09:44:45 -0800

    PixelCalibrator: apply Falk Dettmar's patch for another travel radius option.

--- plugins/PixelCalibrator/src/main/java/org/micromanager/pixelcalibrator/PixelCalibratorDialog.java
+++ plugins/PixelCalibrator/src/main/java/org/micromanager/pixelcalibrator/PixelCalibratorDialog.java
@@ -76,7 +76,7 @@ public class PixelCalibratorDialog extends MMFrame {
 
       jLabel1.setText("Safe travel radius, um:");
 
-      safeTravelRadiusComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1000", "10000" }));
+      safeTravelRadiusComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1000", "10000", "100000" }));
       safeTravelRadiusComboBox.addActionListener(new java.awt.event.ActionListener() {
          public void actionPerformed(java.awt.event.ActionEvent evt) {
             safeTravelRadiusComboBoxActionPerformed(evt);
