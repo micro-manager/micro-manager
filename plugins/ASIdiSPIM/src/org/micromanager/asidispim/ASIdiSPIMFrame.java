micro-manager/micro-manager

commit def12ee1d5b6b52def202f6bdf6e3e1d504df0d5

ASIdiSPIM: fix automatic update of offset with autofocus
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@15842 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/SetupPanel.java
@@ -101,7 +101,7 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
    private final JLabel imagingPiezoPositionLabel_;
    private final JLabel illuminationPiezoPositionLabel_;
    private final JLabel sheetPositionLabel_;
-
+   private AutofocusUtils.FocusResult lastFocusResult_;
 
 
    public SetupPanel(ScriptInterface gui, 
@@ -241,9 +241,10 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
       tmp_but.setToolTipText("Autofocus at current piezo position");
       tmp_but.setBackground(Color.green);
       tmp_but.addActionListener(new ActionListener() {
+
          @Override
          public void actionPerformed(ActionEvent e) {
-            autofocus_.runFocus(setupPanel, side_, true,
+            lastFocusResult_ = autofocus_.runFocus(setupPanel, side_, true,
                     ASIdiSPIM.getFrame().getAcquisitionPanel().getSliceTiming(),
                     true, true);
          }
@@ -635,7 +636,7 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
          return;
       }
       double rate = (Double) rateField_.getValue();
-      double newOffset = score.getPiezoPosition() - rate * score.getGalvoPosition();         
+      double newOffset = score.getPiezoFocusPosition() - rate * score.getGalvoFocusPosition();         
       offsetField_.setValue((Double) newOffset);
       ReportingUtils.logMessage("autofocus updated offset for side " + side_ + "; new value is " + newOffset);
    }
@@ -804,16 +805,14 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
    }
    
    @Override
-   // if this gets called other than after autofocus may need to change code
+   // currently only called after autofocus, so do autofocus-specific tasks here
+   // if this gets called otherwise in future then will need to change code
    public void refreshSelected() {
-      // currently only called after autofocus, so do an autofocus-specific task here
-      // cannot put this where we call runFocus because it runs on a separate
-      //   asynchronous thread
-      // be sure to update the calibration offset if needed before possibly
-      //   changing beam state using beamPanel_
       if (prefs_.getBoolean(MyStrings.PanelNames.AUTOFOCUS.toString(), 
             Properties.Keys.PLUGIN_AUTOFOCUS_AUTOUPDATE_OFFSET, false)) {
-         updateCalibrationOffset();
+         // cannot put this where we call runFocus because it runs on a separate
+         //   asynchronous thread
+         updateCalibrationOffset(lastFocusResult_);
       }
       cameraPanel_.gotSelected();
       if (beamPanel_.isUpdateOnTab()) {
@@ -856,5 +855,4 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
       imagingCenterPosLabel_.setFloat((float)imagingCenterPos_);
    }
 
-   
 }

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/AutofocusUtils.java
@@ -63,6 +63,7 @@ import org.micromanager.utils.ReportingUtils;
 
 /**
  * @author nico
+ * @author Jon
  */
 public class AutofocusUtils {
 
@@ -99,9 +100,9 @@ public class AutofocusUtils {
          piezoPosition_ = piezoPosition;
       }
       
-      public boolean getFocusSuccess() { return focusSuccess_; } 
-      public double getGalvoPosition() { return galvoPosition_; }
-      public double getPiezoPosition() { return piezoPosition_; }
+      public boolean getFocusSuccess()      { return focusSuccess_; } 
+      public double getGalvoFocusPosition() { return galvoPosition_; }
+      public double getPiezoFocusPosition() { return piezoPosition_; }
    }
    
    /**
@@ -141,6 +142,11 @@ public class AutofocusUtils {
 
          @Override
          protected FocusResult doInBackground() throws Exception {
+            
+            if (ASIdiSPIM.getFrame().getAcquisitionPanel().isAcquisitionRequested()) {
+               throw new ASIdiSPIMException("Cannot run autofocus while another"
+                     + " autofocus or acquisition is ongoing.");
+            }
 
             // Score indicating goodness of the fit
             double r2 = 0;

commit 47cf5e75ecc567fed60f581fada2036e386b6f7f

ASIdiSPIM: restructure range checking of autofocus offset to correct bug (also cleaner code), also fixed bug with autofocus during acquisition introduced in r15842
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@15848 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/ASIdiSPIMFrame.java
@@ -34,6 +34,7 @@ import org.micromanager.asidispim.Utils.ListeningJTabbedPane;
 import org.micromanager.asidispim.Utils.MyDialogUtils;
 
 import java.awt.Container;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.prefs.Preferences;
 
 import javax.swing.JFrame;
@@ -112,6 +113,8 @@ public class ASIdiSPIMFrame extends MMFrame
    private final StatusSubPanel statusSubPanel_;
    private final StagePositionUpdater stagePosUpdater_;
    private final ListeningJTabbedPane tabbedPane_;
+   
+   private final AtomicBoolean hardwareInUse_ = new AtomicBoolean(false);   // true if acquisition or autofocus running
    
    private static final String MAIN_PREF_NODE = "Main"; 
    
@@ -253,6 +256,14 @@ public class ASIdiSPIMFrame extends MMFrame
             "[" + this.getHeight() + "]"));
       glassPane.add(statusSubPanel_, "dock south");
       
+   }
+   
+   public void setHardwareInUse(boolean inuse) {
+      hardwareInUse_.set(inuse);
+   }
+   
+   public boolean getHardwareInUse() {
+      return hardwareInUse_.get();
    }
    
    /**

plugins/ASIdiSPIM/src/org/micromanager/asidispim/AcquisitionPanel.java
@@ -820,7 +820,16 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
    
    private void updateCalibrationOffset(final Sides side, 
            final AutofocusUtils.FocusResult score) {
-         ASIdiSPIM.getFrame().getSetupPanel(side).updateCalibrationOffset(score);
+      if (score.getFocusSuccess()) {
+         double offsetDelta = score.getOffsetDelta();
+         double maxDelta = props_.getPropValueFloat(Devices.Keys.PLUGIN,
+               Properties.Keys.PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE);
+         if (Math.abs(offsetDelta) <= maxDelta) {
+            ASIdiSPIM.getFrame().getSetupPanel(side).updateCalibrationOffset(score);
+         } else {
+            ReportingUtils.logMessage("autofocus successful for side " + side + " but offset change too much to automatically update");
+         }
+      }
    }
    
    public SliceTiming getSliceTiming() {
@@ -1454,6 +1463,11 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          return false;
       }
       
+      if (ASIdiSPIM.getFrame().getHardwareInUse()) {
+         MyDialogUtils.showError("Hardware is being used by something else (maybe autofocus?)");
+         return false;
+      }
+      
       boolean liveModeOriginally = gui_.isLiveModeOn();
       if (liveModeOriginally) {
          gui_.enableLiveMode(false);
@@ -2055,6 +2069,9 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
                   // loop over all the times we trigger the controller
                   for (int channelNum = 0; channelNum < nrChannelsSoftware; channelNum++) {
                      try {
+                        // flag that we are using the cameras/controller
+                        ASIdiSPIM.getFrame().setHardwareInUse(true);
+                        
                         // deal with shutter before starting acquisition
                         shutterOpen = core_.getShutterOpen();
                         if (autoShutter) {
@@ -2211,6 +2228,8 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
                         MyDialogUtils.showError(ex);
                      } finally {
                         // cleanup at the end of each time we trigger the controller
+                        
+                        ASIdiSPIM.getFrame().setHardwareInUse(false);
 
                         // put shutter back to original state
                         core_.setShutterOpen(shutterOpen);

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Properties.java
@@ -210,7 +210,7 @@ public class Properties {
       PLUGIN_AUTOFOCUS_WINDOW_HEIGHT("AutofocusWIndowHeight"),
       PLUGIN_AUTOFOCUS_ACQBEFORESTART("AutofocusDoBeforeACQStart"),
       PLUGIN_AUTOFOCUS_EACHNIMAGES("AutofocusEachNTimePoints"),
-      PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE("AutofocusMaxOffsetChange"),
+      PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE("AutofocusMaxOffsetChange"),  // during acquisition
       PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE_SETUP("AutofocusMaxOffsetChangeSetup"),
       PLUGIN_AUTOFOCUS_AUTOUPDATE_OFFSET("AutofocusAutoUpdateOffset"),
       PLUGIN_AUTOFOCUS_CHANNEL("AutofocusChannel"),

plugins/ASIdiSPIM/src/org/micromanager/asidispim/SetupPanel.java
@@ -603,6 +603,7 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
    /**
     * Performs "1-point" calibration updating the offset
     * (but not the slope) based on current piezo/galvo positions.
+    * Does not perform any error/range checking except to ensure beam is on.
     */
    private void updateCalibrationOffset() {
       try {
@@ -616,6 +617,7 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
             double currentPiezo = positions_.getUpdatedPosition(piezoImagingDeviceKey_);
             double newOffset = currentPiezo - rate * currentScanner;
             offsetField_.setValue((Double) newOffset);
+            ReportingUtils.logMessage("updated offset for side " + side_ + "; new value is " + newOffset);
          }
       } catch (Exception ex) {
          MyDialogUtils.showError(ex);
@@ -623,17 +625,12 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
    }
    
    /**
-    * Give autofocus and acquisition the opportunity to update the offset.
-    * Only happens if the focus is deemed a "success" by autofocus
-    * (currently means R^2 value is sufficiently high and best focus
-    * position is in the middle 80% of the range sampled)
+    * Update the calibration offset according to the autofocus score.
+    * Caller should make sure to apply any required criteria before calling this.
+    * method because no error/range checking is done here.
     * @param newValue - new value for the piezo/slice calibration offset
     */
    public void updateCalibrationOffset(AutofocusUtils.FocusResult score) {
-      if (!score.getFocusSuccess()) {
-         ReportingUtils.logMessage("autofocus offset for side " + side_ + " not updated because focus not successful");
-         return;
-      }
       double rate = (Double) rateField_.getValue();
       double newOffset = score.getPiezoFocusPosition() - rate * score.getGalvoFocusPosition();         
       offsetField_.setValue((Double) newOffset);
@@ -809,9 +806,19 @@ public final class SetupPanel extends ListeningJPanel implements LiveModeListene
    public void refreshSelected() {
       if (prefs_.getBoolean(MyStrings.PanelNames.AUTOFOCUS.toString(), 
             Properties.Keys.PLUGIN_AUTOFOCUS_AUTOUPDATE_OFFSET, false)) {
-         // cannot put this where we call runFocus because it runs on a
+         // cannot put this where we call runFocus because autofocus runs on a
          //   separate asynchronous thread
-         updateCalibrationOffset(autofocus_.getLastFocusResult());
+         AutofocusUtils.FocusResult score = autofocus_.getLastFocusResult();
+         if (score.getFocusSuccess()) {
+            double offsetDelta = score.getOffsetDelta();
+            double maxDelta = props_.getPropValueFloat(Devices.Keys.PLUGIN,
+                  Properties.Keys.PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE_SETUP);
+            if (Math.abs(offsetDelta) <= maxDelta) {
+               updateCalibrationOffset(score);
+            } else {
+               ReportingUtils.logMessage("autofocus successful for side " + side_ + " but offset change too much to automatically update");
+            }
+         }
       }
       cameraPanel_.gotSelected();
       if (beamPanel_.isUpdateOnTab()) {

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/AutofocusUtils.java
@@ -88,7 +88,7 @@ public class AutofocusUtils {
       posUpdater_ = stagePosUpdater;
       positions_ = positions;
       controller_ = controller;
-      lastFocusResult_ = new FocusResult(false, 0.0, 0.0);
+      lastFocusResult_ = new FocusResult(false, 0.0, 0.0, 0.0);
       
    }
 
@@ -96,16 +96,19 @@ public class AutofocusUtils {
       private final boolean focusSuccess_;
       private final double galvoPosition_;
       private final double piezoPosition_;
+      private final double offsetDelta_;
       
-      public FocusResult(boolean focusSuccess, double galvoPosition, double piezoPosition) {
+      public FocusResult(boolean focusSuccess, double galvoPosition, double piezoPosition, double offsetDelta) {
          focusSuccess_ = focusSuccess;
          galvoPosition_ = galvoPosition;
          piezoPosition_ = piezoPosition;
+         offsetDelta_ = offsetDelta;  // amount in um that the offset will shift, could be positive or negative
       }
       
-      public boolean getFocusSuccess()      { return focusSuccess_; } 
+      public boolean getFocusSuccess()      { return focusSuccess_; }
       public double getGalvoFocusPosition() { return galvoPosition_; }
       public double getPiezoFocusPosition() { return piezoPosition_; }
+      public double getOffsetDelta()        { return offsetDelta_; }
    }
    
    public FocusResult getLastFocusResult() {
@@ -151,7 +154,7 @@ public class AutofocusUtils {
          @Override
          protected FocusResult doInBackground() throws Exception {
             
-            if (ASIdiSPIM.getFrame().getAcquisitionPanel().isAcquisitionRequested()) {
+            if (ASIdiSPIM.getFrame().getHardwareInUse()) {
                throw new ASIdiSPIMException("Cannot run autofocus while another"
                      + " autofocus or acquisition is ongoing.");
             }
@@ -294,6 +297,8 @@ public class AutofocusUtils {
                   gui_.enableLiveMode(false);
                   gui_.getMMCore().waitForDevice(originalCamera);
                }
+               
+               ASIdiSPIM.getFrame().setHardwareInUse(true);
                
                // deal with shutter before starting acquisition
                // needed despite core's handling because of DemoCamera
@@ -457,9 +462,11 @@ public class AutofocusUtils {
                throw new ASIdiSPIMException(ex);
             } finally {
                
+               ASIdiSPIM.getFrame().setHardwareInUse(false);
+               
                // set result to be a dummy value for now; we will overwrite it later
                //  unless we encounter an exception in the meantime
-               lastFocusResult_ = new FocusResult(false, galvoPosition, piezoPosition);
+               lastFocusResult_ = new FocusResult(false, galvoPosition, piezoPosition, 0.0);
                
                try {
                   caller.setCursor(Cursor.getDefaultCursor());
@@ -481,55 +488,30 @@ public class AutofocusUtils {
                      positions_.setPosition(galvoDevice, Directions.Y, originalGalvoPosition);
                   }
                   
-                  // determine if we found a "maximal focus" position
-                  // cannot be outside the center 80% of the search range
-                  // R^2 value of fit must be greater than the minimum specified
-                  // if for some reason we were unsuccessful then restore to original position
-                  // don't bother moving anything if we are doing this during acquisition
-                  boolean focusSuccess = false;
+                  // determine if it was "successful" which for now we define as
+                  //   - "maximal focus" position is inside the center 80% of the search range
+                  //   - curve fit has sufficient R^2 value
+                  // if we are successful then stay at that position, otherwise restore the original position
+                  //   (don't bother moving anything if we are doing this during acquisition)
+                  // the caller may (and usually will) apply further logic to decide whether or not this was a
+                  //   successful autofocus, e.g. before automatically updating the offset
+                  boolean focusSuccess = false; 
                   if (isPiezoScan) {
                      final double end1 = piezoStart + 0.1*piezoRange;
                      final double end2 = piezoStart + 0.9*piezoRange;
-                     if (r2 > minimumRSquare
+                     focusSuccess = (r2 > minimumRSquare
                            && bestPiezoPosition > Math.min(end1, end2)
-                           && bestPiezoPosition < Math.max(end1, end2)) {
-                        // passed initial tests, now make sure change is within allowed range
-                        double focusDelta = Math.abs(piezoCenter-bestPiezoPosition);
-                        final double maxDelta;
-                        if (restoreCameraMode) {  // proxy for "running from setup"
-                           maxDelta = props_.getPropValueFloat(Devices.Keys.PLUGIN,
-                                 Properties.Keys.PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE_SETUP);
-                        } else {
-                           maxDelta = props_.getPropValueFloat(Devices.Keys.PLUGIN,
-                                 Properties.Keys.PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE);
-                        }
-                        focusSuccess = (focusDelta <= maxDelta);
-                     }
-                     lastFocusResult_ = new FocusResult(focusSuccess, galvoPosition, bestPiezoPosition);
+                           && bestPiezoPosition < Math.max(end1, end2));
+                     double focusDelta = piezoCenter-bestPiezoPosition;
+                     lastFocusResult_ = new FocusResult(focusSuccess, galvoPosition, bestPiezoPosition, focusDelta);
                   } else { // slice scan
                      final double end1 = galvoStart + 0.1*galvoRange;
                      final double end2 = galvoStart + 0.9*galvoRange;
-                     if (r2 > minimumRSquare
+                     focusSuccess = (r2 > minimumRSquare
                            && bestGalvoPosition > Math.min(end1, end2)
-                           && bestGalvoPosition < Math.max(end1, end2)) {
-                        double focusDelta = Math.abs((galvoCenter-bestGalvoPosition) * calibrationRate);
-                        final double maxDelta;
-                        if (restoreCameraMode) {  // proxy for "running from setup"
-                           if (prefs_.getBoolean(MyStrings.PanelNames.AUTOFOCUS.toString(), 
-                                 Properties.Keys.PLUGIN_AUTOFOCUS_AUTOUPDATE_OFFSET, false)) {
-                              maxDelta = props_.getPropValueFloat(Devices.Keys.PLUGIN,
-                                    Properties.Keys.PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE_SETUP);
-                           } else {
-                              // effectively have no maximum delta if we aren't automatically updating
-                              maxDelta = 100000;
-                           }
-                        } else {
-                           maxDelta = props_.getPropValueFloat(Devices.Keys.PLUGIN,
-                                 Properties.Keys.PLUGIN_AUTOFOCUS_MAXOFFSETCHANGE);
-                        }
-                        focusSuccess = (focusDelta <= maxDelta);
-                     }
-                     lastFocusResult_ = new FocusResult(focusSuccess, bestGalvoPosition, piezoPosition);
+                           && bestGalvoPosition < Math.max(end1, end2));
+                     double focusDelta = (galvoCenter-bestGalvoPosition) * calibrationRate;
+                     lastFocusResult_ = new FocusResult(focusSuccess, bestGalvoPosition, piezoPosition, focusDelta);
                   }
                   
                   // if we are in Setup panel, move to either the best-focus position (if found)
@@ -561,6 +543,8 @@ public class AutofocusUtils {
                   throw new ASIdiSPIMException(ex, "Error while restoring hardware state after autofocus.");
                }
             }
+            ReportingUtils.logMessage("finished autofocus: " + (lastFocusResult_.getFocusSuccess() ? "successful" : "not successful")
+               + " with galvo position " + lastFocusResult_.getGalvoFocusPosition() + " and piezo position " + lastFocusResult_.getPiezoFocusPosition());
             return lastFocusResult_;
          }
 
@@ -593,7 +577,7 @@ public class AutofocusUtils {
       }
       
       // we can only return a bogus score 
-      return new FocusResult(false, 0.0, 0.0);
+      return new FocusResult(false, 0.0, 0.0, 0.0);
    }
 
    public static ImageProcessor makeProcessor(TaggedImage taggedImage)