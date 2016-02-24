b4c8a49fd9718c5dc5c0a52e37d9d965c6607f78
Chris Weisiger <chris@open-imaging.com>
2016-02-24 13:32:14 -0800

    JavaUtils: add getBuildTime() to get the time at which the jar was built.
    This is copied from one of the answers to this StackOverflow question:
    http://stackoverflow.com/questions/3336392/java-print-time-of-last-compilation

--- mmstudio/src/main/java/org/micromanager/internal/utils/JavaUtils.java
+++ mmstudio/src/main/java/org/micromanager/internal/utils/JavaUtils.java
@@ -16,14 +16,18 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.jar.JarInputStream;
+import java.util.zip.ZipEntry;
 
 public class JavaUtils {
 
@@ -241,5 +245,37 @@ public class JavaUtils {
          ReportingUtils.logError(e, "Unable to get path of jar");
          return "";
       }
+   }
+
+   // Copied from http://stackoverflow.com/questions/3336392/java-print-time-of-last-compilation
+   public static Date getBuildTime() {
+      Date d = null;
+      Class<?> currentClass = new Object() {}.getClass().getEnclosingClass();
+      URL resource = currentClass.getResource(currentClass.getSimpleName() + ".class");
+      if (resource != null) {
+         if (resource.getProtocol().equals("file")) {
+            try {
+               d = new Date(new File(resource.toURI()).lastModified());
+            } catch (URISyntaxException ignored) { }
+         } else if (resource.getProtocol().equals("jar")) {
+            String path = resource.getPath();
+            d = new Date( new File(path.substring(5, path.indexOf("!"))).lastModified() );
+         } else if (resource.getProtocol().equals("zip")) {
+            String path = resource.getPath();
+            File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
+            //long jfodLastModifiedLong = jarFileOnDisk.lastModified ();
+            //Date jfodLasModifiedDate = new Date(jfodLastModifiedLong);
+            try {
+               JarFile jf = new JarFile (jarFileOnDisk);
+               ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2))
+;//Skip the ! and the /
+               long zeTimeLong = ze.getTime ();
+               Date zeTimeDate = new Date(zeTimeLong);
+               d = zeTimeDate;
+            } catch (IOException ignored) {}
+            catch (RuntimeException ignored) {}
+         }
+      }
+      return d;
    }
 }
