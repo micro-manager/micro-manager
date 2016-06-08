54bfee61f5ac3710775570ed70c48270b5ad85f2
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2016-06-08 23:09:27 +0000

    Mark InsertMultiChannel() as deprecated
    - All known uses of this function actually only deal with single-channel
      images; therefore they can be rewritten with InsertImage()
    - Multi-channel images provided by this mechanism aren't supported by
      downstream app
    - Passing unserialized Metadata* breaks binary compatibility between
      DLLs (e.g. between MSVC Release and Debug, or different compiler
      versions, or even different compiler flags)
    
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16044 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- MMDevice/MMDevice.h
+++ MMDevice/MMDevice.h
@@ -1294,6 +1294,7 @@ namespace MM {
       virtual int InsertImage(const Device* caller, const unsigned char* buf, unsigned width, unsigned height, unsigned byteDepth, const char* serializedMetadata, const bool doProcess = true) = 0;
       virtual void ClearImageBuffer(const Device* caller) = 0;
       virtual bool InitializeImageBuffer(unsigned channels, unsigned slices, unsigned int w, unsigned int h, unsigned int pixDepth) = 0;
+      /// \deprecated Use the other forms instead.
       virtual int InsertMultiChannel(const Device* caller, const unsigned char* buf, unsigned numChannels, unsigned width, unsigned height, unsigned byteDepth, Metadata* md = 0) = 0;
 
       // autofocus
