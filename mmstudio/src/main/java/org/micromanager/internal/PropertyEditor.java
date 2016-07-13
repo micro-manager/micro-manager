43ebe66adc4c42cce9a01c6e04ef517bc4f02dd5
Chris Weisiger <chris@open-imaging.com>
2016-07-13 12:41:10 -0700

    Device Property Browser: use ShowFlagsPanel (makes it searchable).
    Also switched to using MigLayout instead of SpringLayout, and numerous code
    style/layout changes.

--- mmstudio/src/main/java/org/micromanager/internal/PropertyEditor.java
+++ mmstudio/src/main/java/org/micromanager/internal/PropertyEditor.java
@@ -17,9 +17,6 @@
 //               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
 //               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 //               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
-//
-// CVS:          $Id$
-//
 
 package org.micromanager.internal;
 
@@ -44,12 +41,13 @@ import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.SpringLayout;
 import javax.swing.border.BevelBorder;
 import javax.swing.table.TableColumn;
 
 import mmcorej.CMMCore;
 import mmcorej.StrVector;
+
+import net.miginfocom.swing.MigLayout;
 
 import org.micromanager.events.PropertiesChangedEvent;
 import org.micromanager.events.PropertyChangedEvent;
@@ -62,6 +60,7 @@ import org.micromanager.internal.utils.PropertyValueCellRenderer;
 import org.micromanager.internal.utils.PropertyItem;
 import org.micromanager.internal.utils.PropertyTableData;
 import org.micromanager.internal.utils.ShowFlags;
+import org.micromanager.internal.utils.ShowFlagsPanel;
 import org.micromanager.internal.utils.PropertyNameCellRenderer;
 import org.micromanager.internal.utils.ReportingUtils;
 
@@ -73,13 +72,12 @@ import org.micromanager.internal.utils.ReportingUtils;
  * aka the "Device/Property Browser"
  */
 public class PropertyEditor extends MMFrame {
-   private final SpringLayout springLayout;
    private static final long serialVersionUID = 1507097881635431043L;
-   
+
    private JTable table_;
    private PropertyEditorTableData data_;
    private ShowFlags flags_;
-   
+
    private static final String PREF_SHOW_READONLY = "show_readonly";
    private JCheckBox showCamerasCheckBox_;
    private JCheckBox showShuttersCheckBox_;
@@ -87,46 +85,48 @@ public class PropertyEditor extends MMFrame {
    private JCheckBox showStateDevicesCheckBox_;
    private JCheckBox showOtherCheckBox_;
    private JCheckBox showReadonlyCheckBox_;
-   private final JScrollPane scrollPane_;
-   private Studio gui_;
-   
+   private JScrollPane scrollPane_;
+   private Studio studio_;
+   private CMMCore core_;
 
-   @Subscribe
-   public void onPropertiesChanged(PropertiesChangedEvent event) {
-      // avoid re-executing a refresh because of callbacks while we are
-      // updating
-      if (!data_.updating()) {
-         refresh();
-      }
-   }
-
-   @Subscribe
-   public void onPropertyChanged(PropertyChangedEvent event) {
-      String device = event.getDevice();
-      String property = event.getProperty();
-      String value = event.getValue();
-      data_.update(device, property, value);
-   }
-
-   public void setGui(Studio gui) {
-      gui_ = gui;
-      gui_.events().registerForEvents(this);
-   }
-
-
-
-   public PropertyEditor() {
+   public PropertyEditor(Studio studio) {
       super("property editor");
-      
-      final DefaultUserProfile profile = DefaultUserProfile.getInstance();
+
+      studio_ = studio;
+      studio_.events().registerForEvents(this);
+      core_ = studio_.core();
+
       flags_ = new ShowFlags();
       flags_.load(PropertyEditor.class);
-      
+
+      createTable();
+      createComponents();
+   }
+
+   private void createTable() {
+      data_ = new PropertyEditorTableData(
+            core_, "", "", 1, 2, getContentPane());
+      data_.gui_ = studio_;
+      data_.flags_ = flags_;
+      data_.showUnused_ = true;
+      data_.setColumnNames("Property", "Value", "");
+
+      table_ = new DaytimeNighttime.Table();
+      table_.setAutoCreateColumnsFromModel(false);
+      table_.setModel(data_);
+
+      table_.addColumn(new TableColumn(0, 200, new PropertyNameCellRenderer(), null));
+      table_.addColumn(new TableColumn(1, 200, new PropertyValueCellRenderer(false), new PropertyValueCellEditor(false)));
+   }
+
+   private void createComponents() {
+      final DefaultUserProfile profile = DefaultUserProfile.getInstance();
+
       setIconImage(Toolkit.getDefaultToolkit().getImage(
               getClass().getResource("/org/micromanager/icons/microscope.gif") ) );
-      springLayout = new SpringLayout();
-      getContentPane().setLayout(springLayout);
-      setSize(551, 514);
+
+      setLayout(new MigLayout("fill, insets 2"));
+
       addWindowListener(new WindowAdapter() {
          @Override
          public void windowClosing(WindowEvent e) {
@@ -149,35 +149,14 @@ public class PropertyEditor extends MMFrame {
       loadAndRestorePosition(100, 100, 400, 300);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-      scrollPane_ = new JScrollPane();
-      scrollPane_.setFont(new Font("Arial", Font.PLAIN, 10));
-      scrollPane_.setBorder(new BevelBorder(BevelBorder.LOWERED));
-      getContentPane().add(scrollPane_);
-      springLayout.putConstraint(SpringLayout.EAST, scrollPane_, -5, SpringLayout.EAST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, scrollPane_, 5, SpringLayout.WEST, getContentPane());
-      
-      table_ = new DaytimeNighttime.Table();
-      table_.setAutoCreateColumnsFromModel(false);
-      
-      final JButton refreshButton = new JButton();
-      refreshButton.setIcon(new ImageIcon(getClass().getResource( 
-              "/org/micromanager/icons/arrow_refresh.png")) );
-      refreshButton.setFont(new Font("Arial", Font.PLAIN, 10));
-      getContentPane().add(refreshButton);
-      springLayout.putConstraint(SpringLayout.EAST, refreshButton, 285, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, refreshButton, 185, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.SOUTH, refreshButton, 32, SpringLayout.NORTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.NORTH, refreshButton, 9, SpringLayout.NORTH, getContentPane());
-      refreshButton.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent e) {
-            refresh();
-         }
-      });
-      refreshButton.setText("Refresh! ");
+      add(new ShowFlagsPanel(data_, flags_, core_,
+               core_.getSystemStateCache()),
+            "split 2, flowy");
 
-      showReadonlyCheckBox_ = new JCheckBox();
-      showReadonlyCheckBox_.setFont(new Font("Arial", Font.PLAIN, 10));
+      Font defaultFont = new Font("Arial", Font.PLAIN, 10);
+
+      showReadonlyCheckBox_ = new JCheckBox("Show Read-Only Properties");
+      showReadonlyCheckBox_.setFont(defaultFont);
       showReadonlyCheckBox_.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
@@ -187,154 +166,74 @@ public class PropertyEditor extends MMFrame {
             data_.fireTableStructureChanged();
           }
       });
-      showReadonlyCheckBox_.setText("Show read-only properties");
-      getContentPane().add(showReadonlyCheckBox_);
-      springLayout.putConstraint(SpringLayout.EAST, showReadonlyCheckBox_, 358, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, showReadonlyCheckBox_, 185, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.SOUTH, showReadonlyCheckBox_, 63, SpringLayout.NORTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.NORTH, showReadonlyCheckBox_, 40, SpringLayout.NORTH, getContentPane());
-      
       // restore values from the previous session
       showReadonlyCheckBox_.setSelected(profile.getBoolean(
                PropertyEditor.class, PREF_SHOW_READONLY, true));
+      data_.setShowReadOnly(showReadonlyCheckBox_.isSelected());
+      add(showReadonlyCheckBox_);
 
-      showCamerasCheckBox_ = new JCheckBox();
-      showCamerasCheckBox_.setFont(new Font("", Font.PLAIN, 10));
-      showCamerasCheckBox_.addActionListener(new ActionListener() {
+      final JButton refreshButton = new JButton("Refresh",
+            new ImageIcon(getClass().getResource(
+              "/org/micromanager/icons/arrow_refresh.png")));
+      refreshButton.setFont(defaultFont);
+      refreshButton.addActionListener(new ActionListener() {
          @Override
-         public void actionPerformed(ActionEvent arg0) {
-            flags_.cameras_ = showCamerasCheckBox_.isSelected();
-            data_.update(false);
+         public void actionPerformed(ActionEvent e) {
+            refresh();
          }
       });
-      showCamerasCheckBox_.setText("Show cameras");
-      getContentPane().add(showCamerasCheckBox_);
-      springLayout.putConstraint(SpringLayout.SOUTH, showCamerasCheckBox_, 28, SpringLayout.NORTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, showCamerasCheckBox_, 10, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.EAST, showCamerasCheckBox_, 111, SpringLayout.WEST, getContentPane());
+      add(refreshButton, "width 100!, alignx left, aligny top, gaptop 20, gapbottom push, wrap");
 
-      showShuttersCheckBox_ = new JCheckBox();
-      showShuttersCheckBox_.setFont(new Font("", Font.PLAIN, 10));
-      showShuttersCheckBox_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent arg0) {
-            flags_.shutters_ = showShuttersCheckBox_.isSelected();
-            data_.update(false);
-         }
-      });
-      showShuttersCheckBox_.setText("Show shutters");
-      getContentPane().add(showShuttersCheckBox_);
-      springLayout.putConstraint(SpringLayout.EAST, showShuttersCheckBox_, 111, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, showShuttersCheckBox_, 10, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.SOUTH, showShuttersCheckBox_, 50, SpringLayout.NORTH, getContentPane());
-
-      showStagesCheckBox_ = new JCheckBox();
-      showStagesCheckBox_.setFont(new Font("", Font.PLAIN, 10));
-      showStagesCheckBox_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent arg0) {
-            flags_.stages_ = showStagesCheckBox_.isSelected();
-            data_.update(false);
-         }
-      });
-      showStagesCheckBox_.setText("Show stages");
-      getContentPane().add(showStagesCheckBox_);
-      springLayout.putConstraint(SpringLayout.EAST, showStagesCheckBox_, 111, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, showStagesCheckBox_, 10, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.SOUTH, showStagesCheckBox_, 73, SpringLayout.NORTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.NORTH, showStagesCheckBox_, 50, SpringLayout.NORTH, getContentPane());
-
-      showStateDevicesCheckBox_ = new JCheckBox();
-      showStateDevicesCheckBox_.setFont(new Font("", Font.PLAIN, 10));
-      showStateDevicesCheckBox_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent arg0) {
-            flags_.state_ = showStateDevicesCheckBox_.isSelected();
-            data_.update(false);
-         }
-      });
-      showStateDevicesCheckBox_.setText("Show discrete changers");
-      getContentPane().add(showStateDevicesCheckBox_);
-      springLayout.putConstraint(SpringLayout.EAST, showStateDevicesCheckBox_, 200, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, showStateDevicesCheckBox_, 10, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.SOUTH, showStateDevicesCheckBox_, 95, SpringLayout.NORTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.NORTH, showStateDevicesCheckBox_, 72, SpringLayout.NORTH, getContentPane());
-
-      showOtherCheckBox_ = new JCheckBox();
-      showOtherCheckBox_.setFont(new Font("", Font.PLAIN, 10));
-      showOtherCheckBox_.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent arg0) {
-            flags_.other_ = showOtherCheckBox_.isSelected();
-            data_.update(false);
-         }
-      });
-      showOtherCheckBox_.setText("Show other devices");
-      getContentPane().add(showOtherCheckBox_);
-      springLayout.putConstraint(SpringLayout.EAST, showOtherCheckBox_, 155, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.WEST, showOtherCheckBox_, 10, SpringLayout.WEST, getContentPane());
-      springLayout.putConstraint(SpringLayout.NORTH, showOtherCheckBox_, 95, SpringLayout.NORTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.SOUTH, scrollPane_, -5, SpringLayout.SOUTH, getContentPane());
-      springLayout.putConstraint(SpringLayout.NORTH, scrollPane_, 5, SpringLayout.SOUTH, showOtherCheckBox_);
+      scrollPane_ = new JScrollPane();
+      scrollPane_.setViewportView(table_);
+      scrollPane_.setFont(defaultFont);
+      scrollPane_.setBorder(new BevelBorder(BevelBorder.LOWERED));
+      add(scrollPane_, "span, grow, wrap");
    }
-   
-
 
    protected void refresh() {
-      data_.gui_ = gui_;
+      data_.gui_ = studio_;
       data_.flags_ = flags_;
       data_.showUnused_ = true;
       data_.refresh(false);
    }
 
-   public void updateStatus() {
-      if (data_ != null)
-         data_.update(false);
+   @Subscribe
+   public void onPropertiesChanged(PropertiesChangedEvent event) {
+      // avoid re-executing a refresh because of callbacks while we are
+      // updating
+      if (!data_.updating()) {
+         refresh();
+      }
    }
 
-    public void setCore(CMMCore core) {
-        data_ = new PropertyEditorTableData(core, "", "", 1, 2, getContentPane());
-        data_.gui_ = gui_;
-        data_.flags_ = flags_;
-        data_.showUnused_ = true;
-        data_.setColumnNames("Property", "Value", "");
+   @Subscribe
+   public void onPropertyChanged(PropertyChangedEvent event) {
+      String device = event.getDevice();
+      String property = event.getProperty();
+      String value = event.getValue();
+      data_.update(device, property, value);
+   }
 
-        table_ = new DaytimeNighttime.Table();
-        table_.setAutoCreateColumnsFromModel(false);
-        table_.setModel(data_);
-        scrollPane_.setViewportView(table_);
-
-        table_.addColumn(new TableColumn(0, 200, new PropertyNameCellRenderer(), null));
-        table_.addColumn(new TableColumn(1, 200, new PropertyValueCellRenderer(false), new PropertyValueCellEditor(false)));
-
-        showCamerasCheckBox_.setSelected(flags_.cameras_);
-        showStagesCheckBox_.setSelected(flags_.stages_);
-        showShuttersCheckBox_.setSelected(flags_.shutters_);
-        showStateDevicesCheckBox_.setSelected(flags_.state_);
-        showOtherCheckBox_.setSelected(flags_.other_);
-
-        data_.setShowReadOnly(showReadonlyCheckBox_.isSelected());
-    }
-   
     public class PropertyEditorTableData extends PropertyTableData {
       public PropertyEditorTableData(CMMCore core, String groupName, String presetName,
          int PropertyValueColumn, int PropertyUsedColumn, Component parentComponent) {
 
          super(core, groupName, presetName, PropertyValueColumn, PropertyUsedColumn, false);
       }
-   
+
       private static final long serialVersionUID = 1L;
 
       @Override
       public void setValueAt(Object value, int row, int col) {
          PropertyItem item = propListVisible_.get(row);
-         gui_.logs().logMessage("Setting value " + value + " at row " + row);
+         studio_.logs().logMessage("Setting value " + value + " at row " + row);
          if (col == PropertyValueColumn_) {
             setValueInCore(item,value);
          }
          core_.updateSystemStateCache();
          refresh(true);
-         gui_.compat().refreshGUIFromCache();
+         studio_.compat().refreshGUIFromCache();
          fireTableCellUpdated(row, col);
       }
 
@@ -346,7 +245,7 @@ public class PropertyEditor extends MMFrame {
             fireTableDataChanged();
          }
       }
-      
+
       @Override
       public void update(ShowFlags flags, String groupName, String presetName, boolean fromCache) {
          try {
@@ -356,12 +255,12 @@ public class PropertyEditor extends MMFrame {
             if (!fromCache) {
                // Some properties may not be readable if we are
                // mid-acquisition.
-               gui_.live().setSuspended(true);
+               studio_.live().setSuspended(true);
             }
-            for (int i=0; i<devices.size(); i++) { 
+            for (int i = 0; i < devices.size(); i++) {
                if (data_.showDevice(flags, devices.get(i))) {
                   StrVector properties = core_.getDevicePropertyNames(devices.get(i));
-                  for (int j=0; j<properties.size(); j++){
+                  for (int j = 0; j < properties.size(); j++){
                      PropertyItem item = new PropertyItem();
                      item.readFromCore(core_, devices.get(i), properties.get(j), fromCache);
 
@@ -372,23 +271,17 @@ public class PropertyEditor extends MMFrame {
                }
             }
 
-            updateRowVisibility(flags); 
+            updateRowVisibility(flags);
 
             if (!fromCache) {
-               gui_.live().setSuspended(false);
+               studio_.live().setSuspended(false);
             }
          } catch (Exception e) {
-            handleException(e);
+            ReportingUtils.showError(e, "Error updating Device Property Browser");
          }
          this.fireTableStructureChanged();
 
       }
    }
-
- 
-   private void handleException (Exception e) {
-      ReportingUtils.showError(e, this);
-   }
-   
 }
 
