8a1c2c9439b8a587091d83848a51c68b1404a854
Chris Weisiger <chris@open-imaging.com>
2016-08-03 14:55:00 -0700

    ExamplePlugin: provide better example GUI (issue #377)

--- plugins/Example/src/main/java/org/micromanager/plugins/example/Example.java
+++ plugins/Example/src/main/java/org/micromanager/plugins/example/Example.java
@@ -1,22 +1,22 @@
 /**
- * A very simple Micro-Manager plugin
- * 
- * This plugin does nothing, its sole function is to make it easier for developers
- * to start writing Micro-Manager plugins.
- * 
+ * A very simple Micro-Manager plugin, intended to be used as an example for
+ * developers wishing to create their own, actually useful plugins. This one
+ * demonstrates performing various common tasks, but does not do anything
+ * really useful.
+ *
  * Copy this code to a location of your choice, change the name of the project
  * (and the classes), build the jar file and copy it to the mmplugins folder
  * in your Micro-Manager directory.
- * 
+ *
  * Once you have it loaded and running, you can attach the NetBean debugger
- * and use all of NetBean's functionality to debug your code.  If you make a 
+ * and use all of NetBean's functionality to debug your code.  If you make a
  * generally useful plugin, please do not hesitate to send a copy to
- * info@micro-managaer.org for inclusion in the Micro-Manager source code 
+ * info@micro-managaer.org for inclusion in the Micro-Manager source code
  * repository.
- * 
+ *
  * Nico Stuurman, 2012
  * copyright University of California
- *  
+ *
  * LICENSE:      This file is distributed under the BSD license.
  *               License text is included with the source distribution.
  *
@@ -32,7 +32,6 @@
 
 package org.micromanager.example;
 
-import mmcorej.CMMCore;
 import org.micromanager.MenuPlugin;
 import org.micromanager.Studio;
 
@@ -41,31 +40,47 @@ import org.scijava.plugin.SciJavaPlugin;
 
 @Plugin(type = MenuPlugin.class)
 public class Example implements SciJavaPlugin, MenuPlugin {
-   public static String menuName = "Example";
-   public static String tooltipDescription = "This Micro-Manager plugin does nothing. "
-		   +"Its only purpose is to be an example for developers wishing to write their own plugin.";
-   private CMMCore core_;
-   private Studio gui_;
-   private ExampleFrame myFrame_;
+   private Studio studio_;
+   private ExampleFrame frame_;
 
+   /**
+    * This method receives the Studio object, which is the gateway to the
+    * Micro-Manager API. You should retain a reference to this object for the
+    * lifetime of your plugin. This method should not do anything except for
+    * store that reference, as Micro-Manager is still busy starting up at the
+    * time that this is called.
+    */
    @Override
-   public void setContext(Studio app) {
-      gui_ = app;                                        
-      core_ = app.getCMMCore();
+   public void setContext(Studio studio) {
+      studio_ = studio;
    }
 
+   /**
+    * This method is called when your plugin is selected from the Plugins menu.
+    * Typically at this time you should show a GUI (graphical user interface)
+    * for your plugin.
+    */
    @Override
    public void onPluginSelected() {
-      if (myFrame_ == null)
-         myFrame_ = new ExampleFrame(gui_);
-      myFrame_.setVisible(true);
+      if (frame_ == null) {
+         // We have never before shown our GUI, so now we need to create it.
+         frame_ = new ExampleFrame(studio_);
+      }
+      frame_.setVisible(true);
    }
 
+   /**
+    * This string is the sub-menu that the plugin will be displayed in, in the
+    * Plugins menu.
+    */
    @Override
    public String getSubMenu() {
       return "Developer Tools";
    }
 
+   /**
+    * The name of the plugin in the Plugins menu.
+    */
    @Override
    public String getName() {
       return "Example plugin";
@@ -73,14 +88,14 @@ public class Example implements SciJavaPlugin, MenuPlugin {
 
    @Override
    public String getHelpText() {
-      return tooltipDescription;
+      return "Help text that is displayed in certain contexts to tell the user what the plugin does.";
    }
-   
+
    @Override
    public String getVersion() {
-      return "1.1";
+      return "2.0";
    }
-   
+
    @Override
    public String getCopyright() {
       return "University of California, 2012-2015";
