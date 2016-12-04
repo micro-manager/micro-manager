84bd6d5e6b2997120de2c23dca4909b667b239ab
Hadrien Mary <hadrien.mary@gmail.com>
2016-12-04 01:26:55 -0500

    Fix FrameCombiner UI

--- plugins/FrameCombiner/src/main/java/org/micromanager/plugins/framecombiner/FrameCombinerConfigurator.java
+++ plugins/FrameCombiner/src/main/java/org/micromanager/plugins/framecombiner/FrameCombinerConfigurator.java
@@ -28,112 +28,80 @@ public class FrameCombinerConfigurator extends MMFrame implements ProcessorConfi
    }
 
    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+   private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        NumberFormat format = NumberFormat.getInstance();
-        NumberFormatter formatter = new NumberFormatter(format);
-        formatter.setValueClass(Integer.class);
-        formatter.setMinimum(1);
-        formatter.setMaximum(Integer.MAX_VALUE);
-        formatter.setCommitsOnValidEdit(true);
-        formatter.setAllowsInvalid(false);
-        numerOfImagesToProcessField_ = new javax.swing.JFormattedTextField(formatter);
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        processorAlgoBox_ = new javax.swing.JComboBox();
-        processorDimensionBox_ = new javax.swing.JComboBox();
-        channelsToAvoidField_ = new javax.swing.JFormattedTextField();
-        jLabel3 = new javax.swing.JLabel();
-       jLabel4 = new javax.swing.JLabel();
+      jPanel1 = new javax.swing.JPanel();
+      jLabel4 = new javax.swing.JLabel();
+      processorDimensionBox_ = new javax.swing.JComboBox();
+      jLabel1 = new javax.swing.JLabel();
+      processorAlgoBox_ = new javax.swing.JComboBox();
+      jLabel2 = new javax.swing.JLabel();
+      NumberFormat format = NumberFormat.getInstance();
+      NumberFormatter formatter = new NumberFormatter(format);
+      formatter.setValueClass(Integer.class);
+      formatter.setMinimum(1);
+      formatter.setMaximum(Integer.MAX_VALUE);
+      formatter.setCommitsOnValidEdit(true);
+      formatter.setAllowsInvalid(false);
+      numerOfImagesToProcessField_ = new javax.swing.JFormattedTextField(formatter);
+      jLabel3 = new javax.swing.JLabel();
+      channelsToAvoidField_ = new javax.swing.JFormattedTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("FrameCombiner Processor Configuration");
+      setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+      setTitle("FrameCombiner Processor Configuration");
 
-        numerOfImagesToProcessField_.setName("_"); // NOI18N
+      jPanel1.setAutoscrolls(true);
+      jPanel1.setLayout(new java.awt.GridLayout(4, 2, 10, 10));
 
-        jLabel1.setText("<html>Number of images to process<br>( &lt; total frame/slices if using MDA)</html>");
+      jLabel4.setText("Dimension to process");
+      jPanel1.add(jLabel4);
 
-        jLabel2.setText("Algorithm to apply on image stack");
+      processorDimensionBox_.addItem(FrameCombinerPlugin.PROCESSOR_DIMENSION_TIME);
+      processorDimensionBox_.addItem(FrameCombinerPlugin.PROCESSOR_DIMENSION_Z);
+      jPanel1.add(processorDimensionBox_);
 
-       //no channel option, because mm2 already provides a channel overlapping view option
-       processorDimensionBox_.addItem(FrameCombinerPlugin.PROCESSOR_DIMENSION_TIME);
-       processorDimensionBox_.addItem(FrameCombinerPlugin.PROCESSOR_DIMENSION_Z);
+      jLabel1.setText("Number of images to process");
+      jPanel1.add(jLabel1);
 
-        processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MEAN);
-        //processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MEDIAN);
-        processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_SUM);
-        processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MAX);
-        processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MIN);
+      processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MEAN);
+      //processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MEDIAN);
+      processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_SUM);
+      processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MAX);
+      processorAlgoBox_.addItem(FrameCombinerPlugin.PROCESSOR_ALGO_MIN);
+      jPanel1.add(processorAlgoBox_);
 
-        channelsToAvoidField_.setName("_"); // NOI18N
+      jLabel2.setText("Algorithm to apply on image stack");
+      jPanel1.add(jLabel2);
 
-        jLabel3.setText("<html>Avoid Channel(s) (zero-based)<br/><p style=\"text-align: center;\">eg. 1,2 or 1-5 (no space)</p></html>");
+      numerOfImagesToProcessField_.setName("_"); // NOI18N
+      jPanel1.add(numerOfImagesToProcessField_);
 
-        jLabel4.setText("Dimension to process");
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(numerOfImagesToProcessField_)
-                            .addComponent(channelsToAvoidField_))
-                        .addContainerGap(156, Short.MAX_VALUE))
-                        .addComponent(processorDimensionBox_)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(processorAlgoBox_, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(139, Short.MAX_VALUE))))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(processorDimensionBox_, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(processorAlgoBox_, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(numerOfImagesToProcessField_, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(channelsToAvoidField_, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3)))
-        );
+      jLabel3.setText("<html>Avoid Channel(s) (zero-based)<br/><p style=\"text-align: center;\">eg. 1,2 or 1-5 (no space)</p></html>");
+      jPanel1.add(jLabel3);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.DEFAULT_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+      channelsToAvoidField_.setName("_"); // NOI18N
+      jPanel1.add(channelsToAvoidField_);
 
-//        pack();
-    }// </editor-fold>//GEN-END:initComponents
+      javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+      getContentPane().setLayout(layout);
+      layout.setHorizontalGroup(
+         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(layout.createSequentialGroup()
+            .addContainerGap()
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 468, Short.MAX_VALUE)
+            .addContainerGap())
+      );
+      layout.setVerticalGroup(
+         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(layout.createSequentialGroup()
+            .addGap(24, 24, 24)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
+            .addGap(24, 24, 24))
+      );
+
+      pack();
+   }// </editor-fold>//GEN-END:initComponents
 
    private void loadSettingValue() {
       processorDimensionBox_.setSelectedItem(settings_.getString(
@@ -214,16 +182,15 @@ public class FrameCombinerConfigurator extends MMFrame implements ProcessorConfi
               CHANNEL_TO_AVOID, channelsToAvoid);
    }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JFormattedTextField channelsToAvoidField_;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JFormattedTextField numerOfImagesToProcessField_;
-    private javax.swing.JComboBox processorAlgoBox_;
-    // End of variables declaration//GEN-END:variables
-
-    private javax.swing.JComboBox processorDimensionBox_;
+   // Variables declaration - do not modify//GEN-BEGIN:variables
+   private javax.swing.JFormattedTextField channelsToAvoidField_;
+   private javax.swing.JLabel jLabel1;
+   private javax.swing.JLabel jLabel2;
+   private javax.swing.JLabel jLabel3;
+   private javax.swing.JLabel jLabel4;
+   private javax.swing.JPanel jPanel1;
+   private javax.swing.JFormattedTextField numerOfImagesToProcessField_;
+   private javax.swing.JComboBox processorAlgoBox_;
+   private javax.swing.JComboBox processorDimensionBox_;
+   // End of variables declaration//GEN-END:variables
 }
