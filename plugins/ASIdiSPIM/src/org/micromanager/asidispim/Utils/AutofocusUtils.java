micro-manager/micro-manager

commit 8291ad761e273d8f5a9dc1be45c04dfbdcff1bbf

ASIdiSPIM: fix bugs in restoring camera exposure times after acquisition, also refactor code around setting up cameras for acquisition
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16191 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/AutofocusUtils.java
@@ -201,9 +201,11 @@ public class AutofocusUtils {
             );
 
             String camera = devices_.getMMDevice(Devices.Keys.CAMERAA);
+            Devices.Keys cameraDevice = Devices.Keys.CAMERAB;
             boolean usingDemoCam = devices_.getMMDeviceLibrary(Devices.Keys.CAMERAA).equals(Devices.Libraries.DEMOCAM);
             if (side.equals(Devices.Sides.B)) {
                camera = devices_.getMMDevice(Devices.Keys.CAMERAB);
+               cameraDevice = Devices.Keys.CAMERAB;
                usingDemoCam = devices_.getMMDeviceLibrary(Devices.Keys.CAMERAB).equals(Devices.Libraries.DEMOCAM);
             }
             Devices.Keys galvoDevice = Devices.getSideSpecificKey(Devices.Keys.GALVOA, side);
@@ -326,8 +328,12 @@ public class AutofocusUtils {
                }
                gui_.getMMCore().clearCircularBuffer();
                gui_.getMMCore().initializeCircularBuffer();
-               cameras_.setSPIMCamerasForAcquisition(true);
+               cameras_.setCameraForAcquisition(cameraDevice, true);
+               prefs_.putFloat(MyStrings.PanelNames.SETTINGS.toString(),
+                     Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE_FIRST.toString(),
+                     (float)gui_.getMMCore().getExposure());
                gui_.getMMCore().setExposure((double) sliceTiming.cameraExposure);
+               gui_.refreshGUIFromCache();
                gui_.getMMCore().startSequenceAcquisition(camera, nrImages, 0, true);
 
                boolean success = controller_.triggerControllerStartAcquisition(
@@ -479,8 +485,13 @@ public class AutofocusUtils {
 
                   controller_.cleanUpControllerAfterAcquisition(1, acqSettings.firstSideIsA, false);
                   
-                  if (runAsynchronously)
-                     cameras_.setSPIMCamerasForAcquisition(false);
+                  if (runAsynchronously) {
+                     // when run from Setup panels then put things back to live mode settings, but not if run during acquisition
+                     cameras_.setCameraForAcquisition(cameraDevice, false);
+                     gui_.getMMCore().setExposure(camera, prefs_.getFloat(MyStrings.PanelNames.SETTINGS.toString(),
+                           Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE_FIRST.toString(), 10f));
+                     gui_.refreshGUIFromCache();
+                  }
 
                   // move back to original position if needed
                   if (!centerAtCurrentZ) {

plugins/ASIdiSPIM/src/org/micromanager/asidispim/AcquisitionPanel.java
@@ -1617,17 +1617,22 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       
       // get MM device names for first/second cameras to acquire
       String firstCamera, secondCamera;
+      Devices.Keys firstCameraKey, secondCameraKey;
       boolean firstSideA = acqSettings.firstSideIsA; 
       if (firstSideA) {
          firstCamera = devices_.getMMDevice(Devices.Keys.CAMERAA);
+         firstCameraKey = Devices.Keys.CAMERAA;
          secondCamera = devices_.getMMDevice(Devices.Keys.CAMERAB);
+         secondCameraKey = Devices.Keys.CAMERAB;
       } else {
          firstCamera = devices_.getMMDevice(Devices.Keys.CAMERAB);
+         firstCameraKey = Devices.Keys.CAMERAB;
          secondCamera = devices_.getMMDevice(Devices.Keys.CAMERAA);
+         secondCameraKey = Devices.Keys.CAMERAA;
       }
       
       boolean sideActiveA, sideActiveB;
-      boolean twoSided = acqSettings.numSides > 1;
+      final boolean twoSided = acqSettings.numSides > 1;
       if (twoSided) {
          sideActiveA = true;
          sideActiveB = true;
@@ -1723,8 +1728,8 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          return false;
       }
       
-      float cameraReadoutTime = computeCameraReadoutTime();
-      double exposureTime = acqSettings.sliceTiming.cameraExposure;
+      final float cameraReadoutTime = computeCameraReadoutTime();
+      final double exposureTime = acqSettings.sliceTiming.cameraExposure;
       
       final boolean save = saveCB_.isSelected() && !testAcq;
       final String rootDir = rootField_.getText();
@@ -1896,6 +1901,40 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          }
       }
       
+      cameras_.setCameraForAcquisition(firstCameraKey, true);
+      if (twoSided) {
+         cameras_.setCameraForAcquisition(secondCameraKey, true);
+      }
+
+      // save exposure time, will restore at end of acquisition
+      // TODO use new getExposure(label) method once it's in MMCore to avoid having to set active camera
+      try {
+         cameras_.setCamera(firstCameraKey);
+         gui_.refreshGUIFromCache();
+         prefs_.putFloat(MyStrings.PanelNames.SETTINGS.toString(),
+               Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE_FIRST.toString(),
+               (float)core_.getExposure());
+         if (twoSided) {
+            cameras_.setCamera(secondCameraKey);
+            gui_.refreshGUIFromCache();
+            prefs_.putFloat(MyStrings.PanelNames.SETTINGS.toString(),
+                  Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE_SECOND.toString(),
+                  (float)core_.getExposure());
+         }
+      } catch (Exception ex) {
+         MyDialogUtils.showError(ex, "could not cache exposure");
+      }
+
+      try {
+         core_.setExposure(firstCamera, exposureTime);
+         if (twoSided) {
+            core_.setExposure(secondCamera, exposureTime);
+         }
+         gui_.refreshGUIFromCache();
+      } catch (Exception ex) {
+         MyDialogUtils.showError(ex, "could not get set exposure");
+      }
+      
       // seems to have a problem if the core's camera has been set to some other
       // camera before we start doing things, so set to a SPIM camera
       try {
@@ -1926,8 +1965,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          }
       }
       
-      cameras_.setSPIMCamerasForAcquisition(true);
-
       numTimePointsDone_ = 0;
       
       // force saving as image stacks, not individual files
@@ -2013,16 +2050,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
             } else {
                gui_.openAcquisition(acqName, rootDir, nrFrames, acqSettings.numSides * acqSettings.numChannels,
                   acqSettings.numSlices, nrPositions, true, save);
-            }
-            
-            // save exposure time, will restore at end of acquisition
-            prefs_.putFloat(MyStrings.PanelNames.SETTINGS.toString(),
-                    Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE.toString(),
-                    (float)core_.getExposure());
-            
-            core_.setExposure(firstCamera, exposureTime);
-            if (twoSided) {
-               core_.setExposure(secondCamera, exposureTime);
             }
             
             channelNames_ = new String[acqSettings.numSides * acqSettings.numChannels];
@@ -2541,11 +2568,22 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       // TODO be more careful and always do these if we actually started acquisition, 
       // even if exception happened
       
-      // restore camera
+      cameras_.setCameraForAcquisition(firstCameraKey, false);
+      if (twoSided) {
+         cameras_.setCameraForAcquisition(secondCameraKey, false);
+      }
+      
+      // restore exposure times of SPIM cameras
       try {
-         core_.setCameraDevice(originalCamera);
+         core_.setExposure(firstCamera, prefs_.getFloat(MyStrings.PanelNames.SETTINGS.toString(),
+               Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE_FIRST.toString(), 10f));
+         if (twoSided) {
+            core_.setExposure(secondCamera, prefs_.getFloat(MyStrings.PanelNames.SETTINGS.toString(),
+                  Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE_SECOND.toString(), 10f));
+         }
+         gui_.refreshGUIFromCache();
       } catch (Exception ex) {
-         MyDialogUtils.showError("Could not restore camera after acquisition");
+         MyDialogUtils.showError("Could not restore exposure after acquisition");
       }
       
       // reset channel to original if we clobbered it
@@ -2592,7 +2630,14 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       if (separateImageFilesOriginally) {
          ImageUtils.setImageStorageClass(TaggedImageStorageDiskDefault.class);
       }
-      cameras_.setSPIMCamerasForAcquisition(false);
+      
+      // restore camera
+      try {
+         core_.setCameraDevice(originalCamera);
+      } catch (Exception ex) {
+         MyDialogUtils.showError("Could not restore camera after acquisition");
+      }
+      
       if (liveModeOriginally) {
          gui_.enableLiveMode(true);
       }

plugins/ASIdiSPIM/src/org/micromanager/asidispim/CameraSubPanel.java
@@ -159,7 +159,7 @@ public final class CameraSubPanel extends ListeningJPanel implements LiveModeLis
          @Override
          public void actionPerformed(ActionEvent e) {
             setLiveButtonAppearance(!gui_.isLiveModeOn());
-            cameras_.enableLiveMode(!gui_.isLiveModeOn());
+            gui_.enableLiveMode(!gui_.isLiveModeOn());
          }
       });
       add(toggleButtonLive_, "center, width " + columnWidth + "px, span 2");

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Cameras.java
@@ -146,15 +146,15 @@ public class Cameras {
       String mmDevice = devices_.getMMDevice(key);
       if (mmDevice != null) {
          try {
-            boolean liveEnabled = gui_.isLiveModeOn();
+            final boolean liveEnabled = gui_.isLiveModeOn();
             if (liveEnabled) {
-               enableLiveMode(false);
+               gui_.enableLiveMode(false);
             }
             currentCameraKey_ = key;
             core_.setCameraDevice(mmDevice);
             gui_.refreshGUIFromCache();
             if (liveEnabled) {
-               enableLiveMode(true);
+               gui_.enableLiveMode(true);
             }
          } catch (Exception ex) {
             MyDialogUtils.showError("Failed to set Core Camera property");
@@ -192,17 +192,6 @@ public class Cameras {
     */
    public boolean isCurrentCameraValid() {
       return !((currentCameraKey_ == null) || (currentCameraKey_ == Devices.Keys.NONE));
-   }
-
-   /**
-    * Turns live mode on or off via core call
-    * @param enable true: switch on live mode, false: switch it off
-    */
-   public void enableLiveMode(boolean enable) {
-      if (enable) {
-         setSPIMCamerasForAcquisition(false);
-      }
-      gui_.enableLiveMode(enable);
    }
 
    /**
@@ -597,41 +586,23 @@ public class Cameras {
    }
    
    /**
-    * private utility/shortcut function to set both SPIM cameras
-    * @param trigMode
+    * Sets the specified camera for acquisition if acq is true or for live mode if acq is false
+    * @param camKey
+    * @param acq
     */
-   private void setSPIMTriggerMode(CameraModes.Keys trigMode) {
-      // TODO look at whether both sides are active before setting cameras up
-      setCameraTriggerMode(Devices.Keys.CAMERAA, trigMode);
-      setCameraTriggerMode(Devices.Keys.CAMERAB, trigMode);
-   }
-
-   /**
-    * Sets up SPIM cameras in correct mode for acquisition when called with true.
-    * Uses the camera mode setting to see which external trigger mode.
-    * @param acq true if setting for acquisition, false if setting for live
-    */
-   public void setSPIMCamerasForAcquisition(boolean acq) {
+   public void setCameraForAcquisition(Devices.Keys camKey, boolean acq) {
       if (acq) {
          CameraModes.Keys cameraMode = CameraModes.getKeyFromPrefCode(
                // could also get from props_ with PLUGIN device
                prefs_.getInt(MyStrings.PanelNames.SETTINGS.toString(),
                      Properties.Keys.PLUGIN_CAMERA_MODE, 0));
-         setSPIMTriggerMode(cameraMode);
+         setCameraTriggerMode(camKey, cameraMode);
          // exposure time set by acquisition setup code
       } else { // for Live mode
-         setSPIMTriggerMode(CameraModes.Keys.INTERNAL);
-         // also set exposure time to the live mode value
-         float exposure = prefs_.getFloat(MyStrings.PanelNames.SETTINGS.toString(),
-               Properties.Keys.PLUGIN_CAMERA_LIVE_EXPOSURE.toString(), 100f);
-         try {
-            core_.setExposure(exposure);
-         } catch (Exception e) {
-            MyDialogUtils.showError("Could not change exposure setting for live mode");
-         }
+         setCameraTriggerMode(camKey, CameraModes.Keys.INTERNAL);
       }
    }
-   
+
    /**
     * Gets the camera ROI in actual pixels used (i.e. if binning is 4x with 2k sensor return 2k instead of 512).
     * This is because reset and readout times depend on actual pixels and only data transfer time (usually less

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Properties.java
@@ -199,7 +199,8 @@ public class Properties {
       AUTOFOCUS_SCORING_ALGORITHM("AutofocusScoringAlgorithm"),
       PLUGIN_ACQUSITION_USE_AUTOFOCUS("UseAutofocusInAcquisition"),
       PLUGIN_CAMERA_MODE("CameraMode"),
-      PLUGIN_CAMERA_LIVE_EXPOSURE("CameraLiveExposureMs"),  // used to be use setting, now used internally to save/restore live exposure time
+      PLUGIN_CAMERA_LIVE_EXPOSURE_FIRST("CameraLiveExposureMs_First"),  // used internally to save/restore live exposure time
+      PLUGIN_CAMERA_LIVE_EXPOSURE_SECOND("CameraLiveExposureMs_Second"),  // used internally to save/restore live exposure time
       PLUGIN_CAMERA_LIVE_SCAN("CameraLiveScanMs"),
       PREFS_ENABLE_POSITION_UPDATES("EnablePositionUpdates"),
       PREFS_AUTO_SHEET_WIDTH("AutomaticSheetWidth"),

commit 7be938fd1d8ed9c1080d8751a71416d94c4a271c

ASIdiSPIM: fix bug introduced by typo in r16191 that affected autofocus on side A
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16212 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/AutofocusUtils.java
@@ -201,7 +201,7 @@ public class AutofocusUtils {
             );
 
             String camera = devices_.getMMDevice(Devices.Keys.CAMERAA);
-            Devices.Keys cameraDevice = Devices.Keys.CAMERAB;
+            Devices.Keys cameraDevice = Devices.Keys.CAMERAA;
             boolean usingDemoCam = devices_.getMMDeviceLibrary(Devices.Keys.CAMERAA).equals(Devices.Libraries.DEMOCAM);
             if (side.equals(Devices.Sides.B)) {
                camera = devices_.getMMDevice(Devices.Keys.CAMERAB);