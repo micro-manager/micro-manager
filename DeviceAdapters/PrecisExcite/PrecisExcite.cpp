6cf350fab56732b780b83f79c7c7a07aafe1eb1d
mark <mark@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2016-12-16 17:26:51 +0000

    PrecisExcite: Skip reading from invalid channels and switching the other channels off when selecting a new channel (Egor Zindy)
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16231 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/PrecisExcite/PrecisExcite.cpp
+++ DeviceAdapters/PrecisExcite/PrecisExcite.cpp
@@ -184,14 +184,17 @@ int Controller::ReadChannelLabels()
    }
       while(! buf_string_.empty());
    
-   
    for (unsigned int i=0;i<buf_tokens_.size();i++)
    {
       if (buf_tokens_[i].substr(0,3).compare("LAM")==0) {
-         channelLetters_.push_back(buf_tokens_[i][4]); // Read 4th character
          string label = buf_tokens_[i].substr(6);
          StripString(label);
 
+         //This skips invalid channels
+         if (label.compare("----") == 0)
+            continue;
+
+         channelLetters_.push_back(buf_tokens_[i][4]); // Read 4th character
          // This is a temporary log entry to debug an issue with channel labels
          // that appear to contain an invalid character at the end.
          std::ostringstream ss;
@@ -358,7 +361,7 @@ int Controller::OnChannelLabel(MM::PropertyBase* pProp, MM::ActionType eAct)
    {
       GetState(state_);
       pProp->Get(currentChannelLabel_);
-      for (unsigned i=0;i<channelLabels_.size();i++)
+      for (unsigned int i=0;i<channelLabels_.size();i++)
          if (channelLabels_[i].compare(currentChannelLabel_) == 0)
          {
             currentChannel_ = i;
@@ -440,7 +443,7 @@ void Controller::SetTrigger()
    stringstream msg;
    msg << "SQX" << carriage_return;
 
-   for (unsigned i=0;i<triggerSequence_.size();i++)
+   for (unsigned int i=0;i<triggerSequence_.size();i++)
    {
       msg << "SQ" << triggerSequence_[i] << carriage_return;
    }
@@ -463,8 +466,17 @@ void Controller::Illuminate()
    }
    else if (state_==1)
    {
-      if (triggerMode_ == OFF)
-         msg << "SQZ" << carriage_return << "C" << channelLetters_[currentChannel_] << "N";
+      if (triggerMode_ == OFF) {
+         msg << "SQZ" << carriage_return;
+         for (unsigned int i=0; i<channelLetters_.size(); i++) {
+            msg << "C" << channelLetters_[i];
+            if (i == currentChannel_)
+               msg << "N";
+            else
+               msg << "F";
+            msg << carriage_return;
+         }
+      }
       else if (triggerMode_ == FOLLOW_PULSE)
          msg << "SQZ" << carriage_return << "A" << channelLetters_[currentChannel_] << "#";
       else
@@ -518,7 +530,7 @@ void Controller::GetState(long &state)
       Send("C?");
       long stateTmp = 0;
 
-      for (unsigned int i=1;i<=channelLetters_.size();i++)
+      for (unsigned int i=0;i<channelLetters_.size();i++)
       {
          ReceiveOneLine();
 
