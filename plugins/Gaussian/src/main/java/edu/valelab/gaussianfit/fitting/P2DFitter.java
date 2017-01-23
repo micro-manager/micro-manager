de025736da15323a54b8d901e28483a195d14e36
nico <nico.stuurman@ucsf.edu>
2017-01-22 20:18:58 -0800

    Gaussian: Deleted superfluous code

--- plugins/Gaussian/src/main/java/edu/valelab/gaussianfit/fitting/P2DFitter.java
+++ plugins/Gaussian/src/main/java/edu/valelab/gaussianfit/fitting/P2DFitter.java
@@ -173,49 +173,5 @@ public class P2DFitter {
          }
       }
    }
-    
-        
+            
 }
-
-
-/*
-
-
-
-
-/**
- *
- * @author nico
- *
-public class P2DFitter extends AbstractCurveFitter{
-
-   @Override
-   protected LeastSquaresProblem getProblem(Collection<WeightedObservedPoint> points) {
-      final int len = points.size();
-      final double[] target  = new double[len];
-      final double[] weights = new double[len];
-      final double[] initialGuess = { 1.0, 1.0, 1.0 };
-
-      int i = 0;
-      for(WeightedObservedPoint point : points) {
-         target[i]  = point.getY();
-         weights[i] = point.getWeight();
-         i += 1;
-      }
-
-      final AbstractCurveFitter.TheoreticalValuesFunction model = new
-            AbstractCurveFitter.TheoreticalValuesFunction(new MyFunc(), points);
-
-      return new LeastSquaresBuilder().
-            maxEvaluations(Integer.MAX_VALUE).
-            maxIterations(Integer.MAX_VALUE).
-            start(initialGuess).
-            target(target).
-            weight(new DiagonalMatrix(weights)).
-            model(model.getModelFunction(), model.getModelFunctionJacobian()).
-            build();
-   }
-   
-}
-
-*/
