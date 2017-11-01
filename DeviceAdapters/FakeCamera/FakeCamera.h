b08f6e6de991f5cdc21a65f326347ebbf4a07b12
lukaslang <lukaslang@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2017-11-01 09:38:14 +0000

    Fixed some errors occurring before a valid image has been loaded
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16523 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/FakeCamera/FakeCamera.h
+++ DeviceAdapters/FakeCamera/FakeCamera.h
@@ -44,72 +44,72 @@ extern const char* label_CV_16UC4;
 class FakeCamera : public CCameraBase<FakeCamera>
 {
 public:
-  FakeCamera();
-  ~FakeCamera();
+	FakeCamera();
+	~FakeCamera();
 
-  // Inherited via CCameraBase
-  int Initialize();
-  int Shutdown();
-  void GetName(char * name) const;
-  long GetImageBufferSize() const;
-  unsigned GetBitDepth() const;
-  int GetBinning() const;
-  int SetBinning(int binSize);
-  void SetExposure(double exp_ms);
-  double GetExposure() const;
-  int SetROI(unsigned x, unsigned y, unsigned xSize, unsigned ySize);
-  int GetROI(unsigned & x, unsigned & y, unsigned & xSize, unsigned & ySize);
-  int ClearROI();
-  int IsExposureSequenceable(bool & isSequenceable) const;
-  const unsigned char * GetImageBuffer();
-  unsigned GetImageWidth() const;
-  unsigned GetImageHeight() const;
-  unsigned GetImageBytesPerPixel() const;
-  int SnapImage();
-  int StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow);
-  int StopSequenceAcquisition();
-  void OnThreadExiting() throw();
+	// Inherited via CCameraBase
+	int Initialize();
+	int Shutdown();
+	void GetName(char * name) const;
+	long GetImageBufferSize() const;
+	unsigned GetBitDepth() const;
+	int GetBinning() const;
+	int SetBinning(int binSize);
+	void SetExposure(double exp_ms);
+	double GetExposure() const;
+	int SetROI(unsigned x, unsigned y, unsigned xSize, unsigned ySize);
+	int GetROI(unsigned & x, unsigned & y, unsigned & xSize, unsigned & ySize);
+	int ClearROI();
+	int IsExposureSequenceable(bool & isSequenceable) const;
+	const unsigned char * GetImageBuffer();
+	unsigned GetImageWidth() const;
+	unsigned GetImageHeight() const;
+	unsigned GetImageBytesPerPixel() const;
+	int SnapImage();
+	int StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow);
+	int StopSequenceAcquisition();
+	void OnThreadExiting() throw();
 
-  unsigned GetNumberOfComponents() const;
-  const unsigned int* GetImageBufferAsRGB32();
+	unsigned GetNumberOfComponents() const;
+	const unsigned int* GetImageBufferAsRGB32();
 
-  int OnPath(MM::PropertyBase* pProp, MM::ActionType eAct);
-  int OnPixelType(MM::PropertyBase* pProp, MM::ActionType eAct);
+	int OnPath(MM::PropertyBase* pProp, MM::ActionType eAct);
+	int OnPixelType(MM::PropertyBase* pProp, MM::ActionType eAct);
 
-  std::string buildPath() const throw(error_code);
-  void getImg() const throw (error_code);
-  void updateROI() const;
+	std::string buildPath() const throw(error_code);
+	void getImg() const;
+	void updateROI() const;
 
-  void initSize(bool loadImg = true) const;
+	void initSize(bool loadImg = true) const;
 
 private:
-  bool initialized_;
+	bool initialized_;
 
-  std::string path_;
+	std::string path_;
 
-  bool capturing_;
-  mutable bool initSize_;
-  mutable unsigned width_;
-  mutable unsigned height_;
-  unsigned byteCount_;
-  unsigned type_;
-  bool color_;
+	bool capturing_;
+	mutable bool initSize_;
+	mutable unsigned width_;
+	mutable unsigned height_;
+	unsigned byteCount_;
+	unsigned type_;
+	bool color_;
 
-  mutable unsigned roiX_;
-  mutable unsigned roiY_;
-  mutable unsigned roiWidth_;
-  mutable unsigned roiHeight_;
+	mutable unsigned roiX_;
+	mutable unsigned roiY_;
+	mutable unsigned roiWidth_;
+	mutable unsigned roiHeight_;
 
-  cv::Mat emptyImg;
+	cv::Mat emptyImg;
 
-  mutable cv::Mat curImg_;
-  mutable cv::Mat alphaChannel_;
-  mutable cv::Mat lastFailedImg_;
-  mutable cv::Mat roi_;
-  mutable std::string curPath_;
-  mutable std::string lastFailedPath_;
+	mutable cv::Mat curImg_;
+	mutable cv::Mat alphaChannel_;
+	mutable cv::Mat lastFailedImg_;
+	mutable cv::Mat roi_;
+	mutable std::string curPath_;
+	mutable std::string lastFailedPath_;
 
-  void resetCurImg();
+	void resetCurImg();
 
-  double exposure_;
+	double exposure_;
 };
