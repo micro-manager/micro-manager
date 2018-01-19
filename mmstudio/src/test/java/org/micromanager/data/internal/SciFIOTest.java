c225faca964f748ad170054b8022ff0acd563966
Nico Stuurman <nico.stuurman@ucsf.edu>
2018-01-18 16:01:59 -0800

    Testing framework for SciFIO

---
+++ mmstudio/src/test/java/org/micromanager/data/internal/SciFIOTest.java
@@ -0,0 +1,57 @@
+
+package org.micromanager.data.internal;
+
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.micromanager.data.Coords;
+
+/**
+ * tests opening data through the SciFIO library
+ * @author nico
+ */
+public class SciFIOTest {
+      final int nrZs_ = 6;
+      final int nrChannels_ = 2; 
+      final int nrTimePoints_ = 8;
+      final int XSize_ = 250;
+      final int YSize_ = 350;
+   
+   @Test
+   public void test8bit() {
+      SciFIODataProvider sdp = new SciFIODataProvider(null, 
+              "8bit-unsigned&pixelType=uint8&lengths=" +
+                      XSize_ + "," + YSize_ + "," + nrZs_ + "," + nrChannels_ + 
+                      "," + nrTimePoints_ + "&axes=X,Y,Z,Channel,Time.fake");
+      try {
+         Assert.assertEquals("Image X Size is wrong", XSize_, sdp.getAnyImage().getWidth());
+         Assert.assertEquals("Image Y Size is wrong", YSize_, sdp.getAnyImage().getHeight());
+         Assert.assertEquals("Bytes Per Pixel is wrong", 1, sdp.getAnyImage().getBytesPerPixel());
+         Assert.assertEquals("Nr Zs is wrong" , nrZs_, sdp.getAxisLength(Coords.Z));
+         Assert.assertEquals("nrChannels is wrong", nrChannels_, sdp.getAxisLength(Coords.C));
+         Assert.assertEquals("nrTime Points is wrong", nrTimePoints_, sdp.getAxisLength(Coords.T));
+      } catch (IOException ex) {
+         Assert.fail("IOException while testing SciFIODataProvider");
+      }
+   }
+   
+   @Test
+   public void test16bit() {
+      SciFIODataProvider sdp = new SciFIODataProvider(null, 
+              "16bit-unsigned&pixelType=uint16&lengths=" +
+                      XSize_ + "," + YSize_ + "," + nrZs_ + "," + nrChannels_ + 
+                      "," + nrTimePoints_ + "&axes=X,Y,Z,Channel,Time.fake");
+      try {
+         Assert.assertEquals("Image X Size is wrong", XSize_, sdp.getAnyImage().getWidth());
+         Assert.assertEquals("Image Y Size is wrong", YSize_, sdp.getAnyImage().getHeight());
+         Assert.assertEquals("Bytes Per Pixel is wrong", 2, sdp.getAnyImage().getBytesPerPixel());
+         Assert.assertEquals("Nr Zs is wrong" , nrZs_, sdp.getAxisLength(Coords.Z));
+         Assert.assertEquals("nrChannels is wrong", nrChannels_, sdp.getAxisLength(Coords.C));
+         Assert.assertEquals("nrTime Points is wrong", nrTimePoints_, sdp.getAxisLength(Coords.T));
+      } catch (IOException ex) {
+         Assert.fail("IOException while testing SciFIODataProvider");
+      }
+   }
+   
+}
