c2b96c5611f1c05955bda0d1dff9a5a74c806175
Nico Stuurman <nico.stuurman@ucsf.edu>
2018-02-03 11:48:03 -0800

    Studio: Catch exceptions in EventManager. It is common to unsubscribe mutliple times, resulting in uncaught exceptions.

--- mmstudio/src/main/java/org/micromanager/events/internal/DefaultEventManager.java
+++ mmstudio/src/main/java/org/micromanager/events/internal/DefaultEventManager.java
@@ -1,8 +1,10 @@
 package org.micromanager.events.internal;
 
 import com.google.common.eventbus.EventBus;
+import org.micromanager.LogManager;
 import org.micromanager.events.EventManager;
 import org.micromanager.internal.utils.EventBusExceptionLogger;
+import org.micromanager.internal.utils.ReportingUtils;
 
 // This is a singleton wrapper around the Guava library's EventBus. It exposes
 // a system-wide EventBus for certain general-purpose events.
@@ -14,23 +16,43 @@ public final class DefaultEventManager implements EventManager {
    }
 
    private final EventBus bus_;
+   private final LogManager logger_;
+   
+   /**
+    * DefaultEventManager is basically a pass-through to the Google Eventbus
+    * Todo: make non-static
+    * 
+    */
    public DefaultEventManager() {
       bus_ = new EventBus(EventBusExceptionLogger.getInstance());
+      logger_ = ReportingUtils.getWrapper();
    }
 
    @Override
    public void registerForEvents(Object obj) {
-      bus_.register(obj);
+      try {
+         bus_.register(obj);
+      } catch (IllegalArgumentException iae) {
+         logger_.logError(iae, "Error while registering");
+      }
    }
 
    @Override
    public void unregisterForEvents(Object obj) {
-      bus_.unregister(obj);
+      try {
+         bus_.unregister(obj);
+      } catch (IllegalArgumentException iae) {
+         logger_.logError(iae, "Error while unregistering");
+      }
    }
 
    @Override
    public void post(Object obj) {
-      bus_.post(obj);
+      try {
+         bus_.post(obj);
+      } catch (IllegalArgumentException iae) {
+         logger_.logError(iae, "Error while posting");
+      }
    }
 
    public static DefaultEventManager getInstance() {
