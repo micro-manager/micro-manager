df20841a0ce9a1838a4a1b2a1fea007f426c520b
langl <langl@ethz.ch>
2018-07-26 16:28:13 +0200

    Make Serial port communication faster for USB based serial ports
    See https://stackoverflow.com/q/48976397

--- DeviceAdapters/SerialManager/SerialManager.cpp
+++ DeviceAdapters/SerialManager/SerialManager.cpp
@@ -641,7 +641,10 @@ int SerialPort::OpenWin32SerialPort(const std::string& portName,
 
    COMMTIMEOUTS timeouts;
    memset(&timeouts, 0, sizeof(timeouts));
-   timeouts.ReadIntervalTimeout = 1;
+   timeouts.ReadIntervalTimeout = MAXDWORD;
+   timeouts.ReadTotalTimeoutMultiplier = MAXDWORD;
+   timeouts.ReadTotalTimeoutConstant = 1;
+
    if (!SetCommTimeouts(portHandle, &timeouts))
    {
       DWORD err = GetLastError();
