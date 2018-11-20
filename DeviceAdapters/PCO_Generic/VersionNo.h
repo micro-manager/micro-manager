893e3470720ba782e31b06d1509c84c81c9d5f9f
Nico Stuurman <nico.stuurman@ucsf.edu>
2018-11-19 17:09:17 -0800

    franz svn r16831: FRE / pco ag / 31.10.2018 / Fixed warnings

--- DeviceAdapters/PCO_Generic/VersionNo.h
+++ DeviceAdapters/PCO_Generic/VersionNo.h
@@ -2,10 +2,10 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        2,0,0,25
-#define PRODUCTVER     2,0,0,25
-#define STRFILEVER     "2,0,0,25\0"
-#define STRPRODUCTVER  "2,0,0,25\0"
+#define FILEVER        2,0,0,30
+#define PRODUCTVER     2,0,0,30
+#define STRFILEVER     "2,0,0,30\0"
+#define STRPRODUCTVER  "2,0,0,30\0"
 #define FILEVER_MAJOR  2
 #define FILEVER_MINOR  0
 
