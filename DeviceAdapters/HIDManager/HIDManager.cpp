b1e2a4d759c3155c7e15d6b296466bafd14d6f95
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2019-02-12 18:10:42 +0000

    Windows build: hopefully now builds BaslerPylon and ThorlabsElliptecSlider
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16911 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/HIDManager/HIDManager.cpp
+++ DeviceAdapters/HIDManager/HIDManager.cpp
@@ -64,7 +64,8 @@ static HIDDeviceInfo g_knownDevices[] = {
    {"LMM5-HID", 0x1bdb, 0x0300},
    {"LMM5-HID-Andor", 0x1428, 0x0300},
    {"Andor-AMH200-FOS", 0x136e, 0x1001},
-   {"MJC-SaimScan", 0x0461, 0x0004}
+   {"MJC-SaimScan", 0x0461, 0x0004},
+   {"SAIMScannerV3", 0x04d8, 0xf11a}
 };
 static int g_numberKnownDevices = sizeof(g_knownDevices) / sizeof(HIDDeviceInfo);
 
