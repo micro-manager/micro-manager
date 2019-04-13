049b981928fcd27e4ec23109c6183c396cec4065
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2019-04-13 22:22:43 +0000

    TSI: correction to white balance processing
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@16985 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/TSI/ColorProcess.cpp
+++ DeviceAdapters/TSI/ColorProcess.cpp
@@ -494,6 +494,7 @@ int Tsi3Cam::ClearWhiteBalance()
 
 int Tsi3Cam::ApplyWhiteBalance(double redScaler, double greenScaler, double blueScaler)
 {
+	/*
 	// reset current color matrix
 	if (!tl_color_clear_matrix(colorProcessor))
 		return ERR_INTERNAL_ERROR;
@@ -510,6 +511,19 @@ int Tsi3Cam::ApplyWhiteBalance(double redScaler, double greenScaler, double blue
 	tl_camera_get_color_correction_matrix(camHandle, merged_camera_correction_sRGB_matrix);
 	if (!tl_color_append_matrix(colorProcessor, merged_camera_correction_sRGB_matrix))
 		return ERR_INTERNAL_ERROR;
+	*/
+	// reset current color matrix
+	tl_color_clear_matrix(colorProcessor);
+
+	// apply wb scaling
+	float grey_world_balance_matrix [9] = { (float)redScaler, 0.0f, 0.0f, 0.0f, (float)greenScaler, 0.0f, 0.0f, 0.0f, (float)blueScaler };
+	tl_color_append_matrix (colorProcessor, grey_world_balance_matrix);
+
+	tl_color_append_matrix(colorProcessor, grey_world_balance_matrix);
+
+	float merged_camera_correction_sRGB_matrix[9] = { 1.25477f, -0.15359f, -0.10118f, -0.07011f, 1.13723f, -0.06713f, 0.0f, -0.26641f, 1.26641f };
+	tl_camera_get_color_correction_matrix(camHandle, merged_camera_correction_sRGB_matrix);
+	tl_color_append_matrix(colorProcessor, merged_camera_correction_sRGB_matrix);
 
 	return DEVICE_OK;
 }
