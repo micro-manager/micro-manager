25464e7075aee1818122f7d7b931d2724ce2ab42
Henry Pinkard <henry.pinkard@gmail.com>
2019-05-19 11:35:49 -0700

    working version of a fast server for sending data between java and python

---
+++ plugins/Magellan/src/main/java/org/micromanager/magellan/acq/MagellanServer.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.micromanager.magellan.acq;
+
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.micromanager.magellan.misc.Log;
+
+/**
+ *
+ * @author henrypinkard
+ */
+public class MagellanServer {
+
+   private final ServerSocket socket_;
+   private Socket client_;
+   private InputStream inStream_;
+   private OutputStream outStream_;
+   private final ExecutorService executor_ = Executors.newSingleThreadExecutor((Runnable r)
+           -> new Thread(r, "Acquisition control server"));
+
+   public static void main(String[] args) throws IOException {
+      new MagellanServer();
+   }
+
+   public MagellanServer() throws IOException {
+      int port = 8080;
+      socket_ = new ServerSocket(port);
+      executor_.submit(listen());
+   }
+
+   private Runnable listen() {
+      return new Runnable() {
+         @Override
+         public void run() {
+
+            connectToClient();
+            while (true) {
+               //Read incoming messages
+               try {
+                  //first read the number of bytes of this message
+                  if (inStream_.available() >= 4) {
+                     byte[] buff = new byte[4];
+                     inStream_.read(buff, 0, 4);
+                     int numBytes = ByteBuffer.wrap(buff).order(ByteOrder.nativeOrder()).asIntBuffer().get(0);
+                     //read message
+                     byte[] message = new byte[numBytes];
+                     ByteStreams.readFully(inStream_, message);
+                     parseMessage(message);
+                     //send back an answer
+                     if (false) {
+                        //nothing to send for now
+                     } else {
+                        //send a message alerting that there's nothing to send
+                        outStream_.write(new byte[]{0, 0, 0, 0}, 0, 4);
+                        outStream_.flush();
+                     }
+                  }
+               } catch (IOException ex) {
+                  ex.printStackTrace();
+                  Log.log("Exception whule trying to read message from client");
+                  Log.log(ex);
+               }
+            }
+         }
+      };
+   }
+
+   /**
+    * Parse incoming message and take appropriate action
+    */
+   private void parseMessage(byte[] message) {
+      System.out.println("Got message of length " + message.length);
+      System.out.println(message[3]);
+//      System.out.println(new String(message));
+   }
+
+   private void connectToClient() {
+      try {
+         client_ = socket_.accept();
+         System.out.println("got connection on port 8080");
+         inStream_ = client_.getInputStream();
+         outStream_ = client_.getOutputStream();
+      } catch (IOException ex) {
+         Log.log(ex);
+      }
+   }
+
+}
---
+++ plugins/Magellan/src/main/python/magellanclient.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+import numpy as np
+import time
+
+def initialize(host='localhost', port=8080):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+
+def sendMessage(sock, data):
+    if type(data) == str:
+        length = len(data).to_bytes(4, sys.byteorder, signed=True)
+        message = length + bytes(data, 'utf-8')
+    elif type(data) == np.ndarray:
+        length = data.nbytes.to_bytes(4, sys.byteorder, signed=True)
+        message = length + data.tobytes()
+    sock.sendall(message)
+
+def recieveMessage(sock):
+    message_length_bytes = sock.recv(4)
+    message_length = int.from_bytes(message_length_bytes, sys.byteorder, signed=True)
+    if message_length > 0:
+        #decode the message and take action
+        pass
+
+def communicate(sock, message):
+    sendMessage(sock, message)
+    return recieveMessage(sock)
+
+sock = initialize()
+
+
+test_img = np.random.randint(0, 65535, (2048, 2048), dtype=np.uint16)
+
+
+
+start = time.time()
+communicate(sock, test_img)
+print(time.time() - start)
+
+start = time.time()
+communicate(sock, 'hello')
+print(time.time() - start)
