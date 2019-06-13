7c6e21b12d9a35ceceabb6a6bd4993ffe5d69175
Henry Pinkard <henry.pinkard@gmail.com>
2019-06-12 19:07:15 -0700

    fixed bug with resaving configs from GUI that caused focusdirections of focus drives to be forgotton

--- mmstudio/src/main/java/org/micromanager/internal/hcwizard/MicroscopeModel.java
+++ mmstudio/src/main/java/org/micromanager/internal/hcwizard/MicroscopeModel.java
@@ -36,6 +36,7 @@ import java.util.Vector;
 import javax.swing.JOptionPane;
 import mmcorej.CMMCore;
 import mmcorej.Configuration;
+import mmcorej.DeviceType;
 import mmcorej.DoubleVector;
 import mmcorej.MMCoreJ;
 import mmcorej.PropertySetting;
@@ -563,7 +564,7 @@ public final class MicroscopeModel {
                devices_.add(dev);
             } else if (tokens[0].contentEquals(new StringBuffer()
                   .append(MMCoreJ.getG_CFGCommand_Property()))) {
-
+                  
                // -------------------------------------------------------------
                // "PropertyItem" command
                // -------------------------------------------------------------
@@ -721,6 +722,8 @@ public final class MicroscopeModel {
                Device dev = findDevice(tokens[1]);
                if (dev != null) {
                   dev.setFocusDirection(Integer.parseInt(tokens[2]));
+                  //Set type manually or else focus direction wont get resaved
+                  dev.setTypeByInt(DeviceType.StageDevice.swigValue());
                }
             } else if (tokens[0].contentEquals(new StringBuffer().append(MMCoreJ.getG_CFGCommand_ParentID()))) {
                if (tokens.length != 3) {
