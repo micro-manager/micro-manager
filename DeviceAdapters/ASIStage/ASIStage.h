c87dc94fdccf2535ae79199eca8e3d6499c48d80
jondaniels <jondaniels@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2020-08-27 22:49:26 +0000

    ASIStage: added support for magnifier and TIRF devices
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@17345 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/ASIStage/ASIStage.h
+++ DeviceAdapters/ASIStage/ASIStage.h
@@ -480,5 +480,83 @@ private:
 };
 
 
+class Magnifier : public CMagnifierBase<Magnifier>, public ASIBase
+{
+public:
+    Magnifier();
+    ~Magnifier();
+
+    // Device API
+    // ----------
+    int Initialize(); //
+    int Shutdown();   //
+
+    void GetName(char* pszName) const;
+    bool Busy();
+
+    bool SupportsDeviceDetection(void);
+    MM::DeviceDetectionStatus DetectDevice(void);
+
+    // Magnifier API
+    //------------
+
+    MM::DeviceType GetType();
+    double GetMagnification();
+
+    // action interface
+    // ----------------
+
+    int OnPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+    int OnAxis(MM::PropertyBase* pProp, MM::ActionType eAct);
+    int OnMagnification(MM::PropertyBase* pProp, MM::ActionType eAct);
+
+private:
+    int SetMagnification(double mag);
+
+    std::string axis_;
+    int answerTimeoutMs_;
+};
+
+
+
+class ASI_TIRF : public CGenericBase<ASI_TIRF>, public ASIBase
+{
+public:
+    ASI_TIRF();
+    ~ASI_TIRF();
+
+    // Device API
+    int Initialize(); //Generic
+    int Shutdown();   //Generic
+
+    void GetName(char* pszName) const;
+    bool Busy();
+    bool SupportsDeviceDetection(void);
+    MM::DeviceDetectionStatus DetectDevice(void);
+
+    // Generic API
+    //------------
+    MM::DeviceType GetType();
+   
+
+    // action interface
+    // ----------------
+
+    int OnPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+    int OnAxis(MM::PropertyBase* pProp, MM::ActionType eAct);
+    int OnAngle(MM::PropertyBase* pProp, MM::ActionType eAct);
+    int OnScaleFactor(MM::PropertyBase* pProp, MM::ActionType eAct);
+    
+
+//private:
+
+    int SetAngle(double angle);
+    double GetAngle();
+
+    std::string axis_;
+    int answerTimeoutMs_;
+    double scaleFactor_;
+    double unitFactor_;
+};
 
 #endif //_ASIStage_H_
