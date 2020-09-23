d4130a9c4ea9afba682decc0378264e7f3a63c56
Henry Pinkard <henry.pinkard@gmail.com>
2020-09-23 15:12:34 -0500

    bump versions

--- plugins/Magellan/src/main/java/org/micromanager/magellan/internal/magellanacq/MagellanGUIAcquisition.java
+++ plugins/Magellan/src/main/java/org/micromanager/magellan/internal/magellanacq/MagellanGUIAcquisition.java
@@ -285,30 +285,14 @@ public class MagellanGUIAcquisition extends Acquisition implements MagellanAcqui
     */
    public static boolean isZAboveImagingVolume(int spaceMode, MagellanGUIAcquisitionSettings settings,
            Point2D.Double[] positionCorners, double zPos, double zOrigin) {
-      boolean towardsSampleIsPositive;
-      try {
-         String zDevice = Magellan.getCore().getFocusDevice();
-         int dir = Magellan.getCore().getFocusDirection(zDevice);
-
-         if (dir > 0) {
-            towardsSampleIsPositive = true;
-         } else if (dir < 0) {
-            towardsSampleIsPositive = false;
-         } else {
-            throw new Exception();
-         }
-      } catch (Exception e) {
-         Log.log("Couldn't get focus direction of Z drive. Configre using Tools--Hardware Configuration Wizard");
-         throw new RuntimeException();
-      }
       if (spaceMode == MagellanGUIAcquisitionSettings.SURFACE_FIXED_DISTANCE_Z_STACK) {
          boolean extrapolate = settings.fixedSurface_ != settings.xyFootprint_;
          //extrapolate only if different surface used for XY positions than footprint
          return settings.fixedSurface_.isPositionCompletelyAboveSurface(positionCorners, settings.fixedSurface_,
-                 zPos + settings.distanceAboveFixedSurface_, extrapolate, towardsSampleIsPositive);
+                 zPos + settings.distanceAboveFixedSurface_, extrapolate);
       } else if (spaceMode == MagellanGUIAcquisitionSettings.VOLUME_BETWEEN_SURFACES_Z_STACK) {
          return settings.topSurface_.isPositionCompletelyAboveSurface(positionCorners, settings.topSurface_,
-                 zPos + settings.distanceAboveTopSurface_, false, towardsSampleIsPositive);
+                 zPos + settings.distanceAboveTopSurface_, false);
       } else if (spaceMode == MagellanGUIAcquisitionSettings.CUBOID_Z_STACK) {
          return zPos < settings.zStart_;
       } else {
@@ -319,32 +303,16 @@ public class MagellanGUIAcquisition extends Acquisition implements MagellanAcqui
 
    public static boolean isZBelowImagingVolume(int spaceMode, MagellanGUIAcquisitionSettings settings,
            Point2D.Double[] positionCorners, double zPos, double zOrigin) {
-      boolean towardsSampleIsPositive;
-      try {
-         String zDevice = Magellan.getCore().getFocusDevice();
-         int dir = Magellan.getCore().getFocusDirection(zDevice);
-
-         if (dir > 0) {
-            towardsSampleIsPositive = true;
-         } else if (dir < 0) {
-            towardsSampleIsPositive = false;
-         } else {
-            throw new Exception();
-         }
-      } catch (Exception e) {
-         Log.log("Couldn't get focus direction of Z drive. Configre using Tools--Hardware Configuration Wizard");
-         throw new RuntimeException();
-      }
       if (spaceMode == MagellanGUIAcquisitionSettings.SURFACE_FIXED_DISTANCE_Z_STACK) {
          boolean extrapolate = settings.fixedSurface_ != settings.xyFootprint_;
          //extrapolate only if different surface used for XY positions than footprint
          return settings.fixedSurface_.isPositionCompletelyBelowSurface(
                  positionCorners, settings.fixedSurface_, zPos - settings.distanceBelowFixedSurface_,
-                 extrapolate, towardsSampleIsPositive);
+                 extrapolate);
       } else if (spaceMode == MagellanGUIAcquisitionSettings.VOLUME_BETWEEN_SURFACES_Z_STACK) {
          return settings.bottomSurface_.isPositionCompletelyBelowSurface(
                  positionCorners, settings.bottomSurface_, zPos - settings.distanceBelowBottomSurface_,
-                 false, towardsSampleIsPositive);
+                 false);
       } else if (spaceMode == MagellanGUIAcquisitionSettings.CUBOID_Z_STACK) {
          return zPos > settings.zEnd_;
       } else {
