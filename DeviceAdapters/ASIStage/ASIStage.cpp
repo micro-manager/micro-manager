05b96aedb028b33a97a56aa220e4e29e68175d72
jondaniels <jondaniels@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2020-10-02 04:38:03 +0000

    ASIStage: effort to correct presumptive error in TIRF calculations (untested)
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@17372 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- DeviceAdapters/ASIStage/ASIStage.cpp
+++ DeviceAdapters/ASIStage/ASIStage.cpp
@@ -6215,7 +6215,7 @@ ASI_TIRF::ASI_TIRF() :
     axis_("F"),//normaly the TIRF axis is the F axis.
     answerTimeoutMs_(1000),
     scaleFactor_(1),
-    unitFactor_(174.532925199433)  // 10000 controller units per mm times pi/180deg
+    unitFactor_(10000)
 {
     InitializeDefaultErrorMessages();
 
@@ -6337,13 +6337,13 @@ double ASI_TIRF::GetAngle()
     }
     else if (answer.length() > 0)
     {
-        double angle;
+        double position;
         char head[64];
         char iBuf[256];
         strcpy(iBuf, answer.c_str());
-        sscanf(iBuf, "%s %lf\r\n", head, &angle);
+        sscanf(iBuf, "%s %lf\r\n", head, &position);
 
-        return angle/(scaleFactor_*unitFactor_);
+        return asin(position/(scaleFactor_*unitFactor_))*180/3.141592653589793;
     }
 
     return 0.0;
@@ -6355,7 +6355,7 @@ int ASI_TIRF::SetAngle(double angle)
     ClearPort();
 
     ostringstream command;
-    command << fixed << "M " << axis_ << "=" << scaleFactor_*unitFactor_*angle;
+    command << fixed << "M " << axis_ << "=" << scaleFactor_*unitFactor_*sin(angle/180*3.141592653589793);
 
     string answer;
     // query the device
