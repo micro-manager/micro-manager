98fdb63816ed59277949ee1560b901aab3c5332d
Nico Stuurman <nico.stuurman@ucsf.edu>
2020-11-09 21:44:33 -0800

    Projector: Added an API to use this code from pycromanager

---
+++ libraries/Projector/src/main/java/org/micromanager/projector/ProjectorAPI.java
@@ -0,0 +1,75 @@
+package org.micromanager.projector;
+
+import ij.gui.Roi;
+import ij.process.FloatPolygon;
+import org.micromanager.Studio;
+import org.micromanager.projector.internal.ProjectorControlForm;
+
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Point2D;
+import java.util.List;
+
+/**
+ * Simple API for Projector control
+ * Created to provide access to Projector function from pycromanager
+ * In java, use ProjectorActions instead, which gives static accessor functions
+ * pycromanager can not use static functions (yet), hence this class...
+ */
+public class ProjectorAPI {
+
+   ProjectorControlForm projectorControlForm_;
+   Studio studio_;
+
+   public ProjectorAPI(Studio studio) {
+      studio_ = studio;
+      projectorControlForm_ = ProjectorControlForm.getSingleton();
+   }
+
+   public  ProjectionDevice getProjectionDevice() {
+      return ProjectorActions.getProjectionDevice(studio_);
+   }
+
+   public  void displayCenterSpot(ProjectionDevice dev) {
+      ProjectorActions.displayCenterSpot(dev);
+   }
+
+   public void displaySpot(ProjectionDevice dev, double x, double y) {
+      ProjectorActions.displaySpot(dev, x, y);
+   }
+
+   public Polygon[] roisAsPolygons(Roi[] rois) {
+      return ProjectorActions.roisAsPolygons(rois);
+   }
+
+   public  void transformAndSetMask(Mapping mapping,
+                                          ProjectionDevice dev, byte[] inputImage, int width, int height,
+                                          Rectangle cameraROI, Integer cameraBinning) {
+      ProjectorActions.transformAndSetMask(mapping, dev, inputImage, width, height, cameraROI, cameraBinning);
+   }
+
+   public List<FloatPolygon> transformRoiPolygons(
+           Polygon[] roiPolygons, Mapping mapping, Rectangle cameraROI, Integer cameraBinning) {
+      return ProjectorActions.transformRoiPolygons(roiPolygons, mapping, cameraROI, cameraBinning);
+   }
+
+   public  List<FloatPolygon> transformROIs(Roi[] rois,
+                                                  Mapping mapping, Rectangle cameraROI, Integer cameraBinning) {
+      return ProjectorActions.transformROIs(rois, mapping, cameraROI, cameraBinning);
+   }
+
+   public Point2D.Double transformPoint(Mapping mapping, Point2D.Double pt,
+                                        Rectangle cameraROI, Integer cameraBinning) {
+      return ProjectorActions.transformPoint(mapping, pt, cameraROI, cameraBinning);
+   }
+
+   public Mapping loadMapping(ProjectionDevice dev) {
+      return ProjectorActions.loadMapping(studio_, dev);
+   }
+
+   public void setExposure(ProjectionDevice dev, double intervalUs) {
+      ProjectorActions.setExposure(dev, intervalUs);
+   }
+
+
+}
