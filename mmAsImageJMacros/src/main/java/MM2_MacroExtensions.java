c6428ef301557b18cec403cac3e878ac8a3b1dbc
Nico Stuurman <nico.stuurman@ucsf.edu>
2020-12-23 13:04:05 -0800

    mmAsImageJMacros: Use Studio API rather than rely on internal behavior

--- mmAsImageJMacros/src/main/java/MM2_MacroExtensions.java
+++ mmAsImageJMacros/src/main/java/MM2_MacroExtensions.java
@@ -1,5 +1,7 @@
 import org.micromanager.data.Image;
-import org.micromanager.internal.MMStudio;
+import org.micromanager.Studio;
+
+
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -8,6 +10,7 @@ import ij.macro.Functions;
 import ij.macro.MacroExtension;
 import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
+import org.micromanager.internal.MMStudio;
 
 public class MM2_MacroExtensions implements PlugIn, MacroExtension {
    @Override
@@ -37,103 +40,125 @@ public class MM2_MacroExtensions implements PlugIn, MacroExtension {
 		return extensions;
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public String handleExtension(String name, Object[] args) {
-		if (name.equals("snap")) {
-			MMStudio studio = MMStudio.getInstance();
-			Image image = studio.live().snap(false).get(0);
-			ImageProcessor ip = studio.data().ij().createProcessor(image);
-			ImagePlus imp = new ImagePlus("Snap", ip);
-			imp.show();
-		} else if (name.equals("setExposure")) {
-			MMStudio studio = MMStudio.getInstance();
-			double exposure = ((Double) args[0]).intValue();
-			studio.setExposure(exposure);
-			studio.refreshGUI();
-		} else if (name.equals("setConfig")) {
-			MMStudio studio = MMStudio.getInstance();
-			String group = ((String) args[0]);
-			String preset = ((String) args[1]);
-			try {
-				studio.core().setConfig(group, preset);
-				studio.refreshGUI();
-			} catch (Exception e) {
-				e.printStackTrace();
+		switch (name) {
+			case "snap": {
+				Studio studio = MMStudio.getInstance();
+				Image image = studio.live().snap(false).get(0);
+				ImageProcessor ip = studio.data().ij().createProcessor(image);
+				ImagePlus imp = new ImagePlus("Snap", ip);
+				imp.show();
+				break;
 			}
-		} else if (name.equals("moveRelativeXYZ")) {
-			MMStudio studio = MMStudio.getInstance();
-			double dx = ((Double) args[0]);
-			double dy = ((Double) args[1]);
-			double dz = ((Double) args[2]);
-			try {
-				studio.core().setRelativePosition(dz);
-				studio.core().setRelativeXYPosition(dx, dy);
-				studio.refreshGUI();
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "setExposure": {
+				MMStudio studio = MMStudio.getInstance();
+				double exposure = ((Double) args[0]).intValue();
+				studio.app().setExposure(exposure);
+				studio.app().refreshGUI();
+				break;
 			}
-		} else if (name.equals("moveAbsoluteXYZ")) {
-			MMStudio studio = MMStudio.getInstance();
-			double dx = ((Double) args[0]);
-			double dy = ((Double) args[1]);
-			double dz = ((Double) args[2]);
-			try {
-				studio.core().setPosition(dz);
-				studio.core().setXYPosition(dx, dy);
-				studio.refreshGUI();
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "setConfig": {
+				MMStudio studio = MMStudio.getInstance();
+				String group = ((String) args[0]);
+				String preset = ((String) args[1]);
+				try {
+					studio.core().setConfig(group, preset);
+					studio.app().refreshGUI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} else if (name.equals("getStageXYZ")) {
-			MMStudio studio = MMStudio.getInstance();
-			try {
-				((Double[]) args[0])[0] = studio.core().getXPosition();
-				((Double[]) args[1])[0] = studio.core().getYPosition();
-				((Double[]) args[2])[0] = studio.core().getPosition();
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "moveRelativeXYZ": {
+				MMStudio studio = MMStudio.getInstance();
+				double dx = ((Double) args[0]);
+				double dy = ((Double) args[1]);
+				double dz = ((Double) args[2]);
+				try {
+					studio.core().setRelativePosition(dz);
+					studio.core().setRelativeXYPosition(dx, dy);
+					studio.app().refreshGUI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} else if (name.equals("getConfigGroups")) {
-			MMStudio studio = MMStudio.getInstance();
-			try {
-				((String[]) args[0])[0] = String.join(";", studio.core().getAvailableConfigGroups().toArray());
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "moveAbsoluteXYZ": {
+				MMStudio studio = MMStudio.getInstance();
+				double dx = ((Double) args[0]);
+				double dy = ((Double) args[1]);
+				double dz = ((Double) args[2]);
+				try {
+					studio.core().setPosition(dz);
+					studio.core().setXYPosition(dx, dy);
+					studio.app().refreshGUI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} else if (name.equals("getGroupPresets")) {
-			MMStudio studio = MMStudio.getInstance();
-			String group = ((String) args[0]);
-			try {
-				((String[]) args[1])[0] = String.join(";", studio.core().getAvailableConfigs(group));
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "getStageXYZ": {
+				MMStudio studio = MMStudio.getInstance();
+				try {
+					((Double[]) args[0])[0] = studio.core().getXPosition();
+					((Double[]) args[1])[0] = studio.core().getYPosition();
+					((Double[]) args[2])[0] = studio.core().getPosition();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} else if (name.equals("getDevices")) {
-			MMStudio studio = MMStudio.getInstance();
-			try {
-				((String[]) args[0])[0] = String.join(";", studio.core().getLoadedDevices().toArray());
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "getConfigGroups": {
+				MMStudio studio = MMStudio.getInstance();
+				try {
+					((String[]) args[0])[0] = String.join(";", studio.core().getAvailableConfigGroups().toArray());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} else if (name.equals("getDeviceProperties")) {
-			MMStudio studio = MMStudio.getInstance();
-			String dev = ((String) args[0]);
-			try {
-				((String[]) args[1])[0] = String.join(";", studio.core().getDevicePropertyNames(dev));
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "getGroupPresets": {
+				MMStudio studio = MMStudio.getInstance();
+				String group = ((String) args[0]);
+				try {
+					((String[]) args[1])[0] = String.join(";", studio.core().getAvailableConfigs(group));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} else if (name.equals("setDeviceProperty")) {
-			MMStudio studio = MMStudio.getInstance();
-			String dev = ((String) args[0]);
-			String prop = ((String) args[1]);
-			String value = ((String) args[2]);
-			try {
-				studio.core().setProperty(dev, prop, value);
-				studio.refreshGUI();
-			} catch (Exception e) {
-				e.printStackTrace();
+			case "getDevices": {
+				MMStudio studio = MMStudio.getInstance();
+				try {
+					((String[]) args[0])[0] = String.join(";", studio.core().getLoadedDevices().toArray());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+			case "getDeviceProperties": {
+				MMStudio studio = MMStudio.getInstance();
+				String dev = ((String) args[0]);
+				try {
+					((String[]) args[1])[0] = String.join(";", studio.core().getDevicePropertyNames(dev));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+			case "setDeviceProperty": {
+				MMStudio studio = MMStudio.getInstance();
+				String dev = ((String) args[0]);
+				String prop = ((String) args[1]);
+				String value = ((String) args[2]);
+				try {
+					studio.core().setProperty(dev, prop, value);
+					studio.app().refreshGUI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
 			}
 		}
 		return null;
