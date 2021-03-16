33604ae587ae91924b086bc2ecb85773480bbcc7
Nick Anthony <nickmanthony@hotmail.com>
2021-03-16 02:33:15 -0500

    Hardcoding path into .am file.

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 echo "Bootstrapping autoconf/automake build system for Micro-Manager..." 1>&2
 
 # Subdirectory must be present, even if empty, to prevent automake errors.
-mkdir -p SecretDeviceAdapters
+mkdir -p MM_CPP/SecretDeviceAdapters
 
 autoreconf --force --install --verbose
 
