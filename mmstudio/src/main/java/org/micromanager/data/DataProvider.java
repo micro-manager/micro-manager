76397722a019d1d8defdfde0e407ed0c8de31f47
Nico Stuurman <nico.stuurman@ucsf.edu>
2021-04-03 10:09:23 -0700

    Studio: DataProvider adds getImagesIgnoringAxes and deprecates getImagesMatching

--- mmstudio/src/main/java/org/micromanager/data/DataProvider.java
+++ mmstudio/src/main/java/org/micromanager/data/DataProvider.java
@@ -31,7 +31,7 @@ public interface DataProvider extends Closeable {
     * Retrieve any image from this DataProvider.
     * 
     * @return a random image from the collection kept by the DataProvider
-    * @throws IOException 
+    * @throws IOException when error occurs loading the data from storage
     */
    Image getAnyImage() throws IOException;
 
@@ -54,19 +54,34 @@ public interface DataProvider extends Closeable {
 
    /**
     * Returns the image at the given postion 
-    * @param coords
-    * @return 
-    * @throws IOException 
+    * @param coords Coords specifying the multi-dimensional index to the image
+    * @return desired Image
+    * @throws IOException when error occurs loading the data from storage
     */
    Image getImage(Coords coords) throws IOException;
 
    /**
-    * Returns a list of images that have coordinated matching the given one
-    * @param coords
-    * @return
-    * @throws IOException 
+    * Returns a list of images that have coords matching the given one
+    * @param coords specification of multi-dimensional index that needs to be
+    *               present in the Coords of the images that will be returned
+    * @return Matching Images
+    * @throws IOException when error occurs loading the data from storage
+    * @deprecated - instead use getImagesIgnoringAxes
     */
+   @Deprecated
    List<Image> getImagesMatching(Coords coords) throws IOException;
+
+
+   /**
+    * Returns a list of image in the DataProvider's collection that have
+    * identical coords after removing the given axes.
+    * @param coords Coords to look for in the provider's collection
+    * @param ignoreTheseAxes Axes that will be removed from copy of Coords in the
+    *                        collection before checking for identity
+    * @return images in the dataProvider's collection with the desired Coords
+    * @throws IOException when error occurs loading the data from storage
+    */
+   List<Image> getImagesIgnoringAxes(Coords coords, String... ignoreTheseAxes) throws IOException;
 
    /**
     * A dataProvider is frozen when no more images can be added
@@ -77,14 +92,14 @@ public interface DataProvider extends Closeable {
 
    /**
     * Coords with highest possible index along each axis
-    * @return 
+    * @return Coords with highest possible index along each axis
     */
    Coords getMaxIndices();
 
    /**
-    * Provides total number of images that can be access through this DataProvider
+    * Provides total number of images that can be accessed through this DataProvider
     * TODO: does this include blank images/empty Coords?
-    * @return 
+    * @return total number of images that can be accessed through this DataProvider
     */
    int getNumImages();
 
@@ -101,7 +116,7 @@ public interface DataProvider extends Closeable {
    /**
     * A dataProvider has a name (not guaranteed to be unique)
     * 
-    * @return 
+    * @return name of this dataProvider
     */
    String getName();
    
