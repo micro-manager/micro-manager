2bef62203236715aba1a3c2675e4504d5aab17c0
Nico Stuurman <nico.stuurman@ucsf.edu>
2021-04-18 19:31:20 -0700

    Studio: Boilerplate Events documentation and MMEvent extensions.

--- mmstudio/src/main/java/org/micromanager/events/internal/DefaultSystemConfigurationLoadedEvent.java
+++ mmstudio/src/main/java/org/micromanager/events/internal/DefaultSystemConfigurationLoadedEvent.java
@@ -2,6 +2,12 @@ package org.micromanager.events.internal;
 
 import org.micromanager.events.SystemConfigurationLoadedEvent;
 
+/**
+ * This interface signals when a configuration file is loaded.
+ *
+ * This event posts on the Studio event bus,
+ * so subscribe using {@link org.micromanager.events.EventManager}.
+ */
 public class DefaultSystemConfigurationLoadedEvent  implements
         SystemConfigurationLoadedEvent {
 }
