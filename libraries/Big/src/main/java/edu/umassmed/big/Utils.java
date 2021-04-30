7183cc6a96034a007973fa0d957f982ccd2bbca2
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2021-04-30 14:03:55 -0500

    Format Big

--- libraries/Big/src/main/java/edu/umassmed/big/Utils.java
+++ libraries/Big/src/main/java/edu/umassmed/big/Utils.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  * Karl Bellve
  * Biomedical Imaging Group
  * University of Massachusetts Medical School
@@ -15,30 +15,35 @@ import java.util.Calendar;
 
 public class Utils {
 
-	public static String generateDate ()
-	{
-		String Date;
-		
-		Calendar today = Calendar.getInstance();
-		if (today.get(Calendar.MONTH) < 10)
-		{
-			if (today.get(Calendar.DAY_OF_MONTH) < 10) Date = "" + today.get(Calendar.YEAR) + "0" + (today.get(Calendar.MONTH) + 1) + "0" + today.get(Calendar.DAY_OF_MONTH);
-			else Date = "" + today.get(Calendar.YEAR) + "0" + (today.get(Calendar.MONTH) + 1) + today.get(Calendar.DAY_OF_MONTH);
-		}
-		else 
-			if (today.get(Calendar.DAY_OF_MONTH) < 10) Date = "" + today.get(Calendar.YEAR) + (today.get(Calendar.MONTH) + 1) + "0" + today.get(Calendar.DAY_OF_MONTH);
-			else Date = "" + today.get(Calendar.YEAR) +  (today.get(Calendar.MONTH) + 1) + today.get(Calendar.DAY_OF_MONTH);
-	
-		return Date;
-	}
+   public static String generateDate() {
+      String Date;
 
-	// get only a piece of the array, use only for 2D, since I think 3D sizes run into memory issues
-	public static short[] getarray(short[] array, int offset, int length) 
-	{
-		short[] result;
-	    result = new short[length];
-	    System.arraycopy(array, offset, result, 0, length);
-	    return result;
-		   
-	}
+      Calendar today = Calendar.getInstance();
+      if (today.get(Calendar.MONTH) < 10) {
+         if (today.get(Calendar.DAY_OF_MONTH) < 10) {
+            Date = "" + today.get(Calendar.YEAR) + "0" + (today.get(Calendar.MONTH) + 1) + "0"
+                  + today.get(Calendar.DAY_OF_MONTH);
+         } else {
+            Date = "" + today.get(Calendar.YEAR) + "0" + (today.get(Calendar.MONTH) + 1) + today
+                  .get(Calendar.DAY_OF_MONTH);
+         }
+      } else if (today.get(Calendar.DAY_OF_MONTH) < 10) {
+         Date = "" + today.get(Calendar.YEAR) + (today.get(Calendar.MONTH) + 1) + "0" + today
+               .get(Calendar.DAY_OF_MONTH);
+      } else {
+         Date = "" + today.get(Calendar.YEAR) + (today.get(Calendar.MONTH) + 1) + today
+               .get(Calendar.DAY_OF_MONTH);
+      }
+
+      return Date;
+   }
+
+   // get only a piece of the array, use only for 2D, since I think 3D sizes run into memory issues
+   public static short[] getarray(short[] array, int offset, int length) {
+      short[] result;
+      result = new short[length];
+      System.arraycopy(array, offset, result, 0, length);
+      return result;
+
+   }
 }
