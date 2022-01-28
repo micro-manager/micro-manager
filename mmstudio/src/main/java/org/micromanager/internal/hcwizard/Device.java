micro-manager/micro-manager

commit 62817483cbdb4951c30b3016b6b0b301c428eed6

Studio: Cleanup of HCWizard code.

mmstudio/src/main/java/org/micromanager/internal/hcwizard/Device.java
@@ -36,41 +36,41 @@ import org.micromanager.internal.utils.ReportingUtils;
 
 /**
  * Data structure describing a general MM device.
- * Part of the MicroscopeModel. 
+ * Part of the MicroscopeModel.
  *
- */public final class Device {
-      // This class behaves simultaneously as any device type, so it has the
-      // information for all device types. Not sure why subclasses were not
-      // used....
-      private String name_;
-      private String adapterName_;
-      private String library_;
-      private PropertyItem properties_[];
-      private ArrayList<PropertyItem> setupProperties_;
-      private String description_;
-      private DeviceType type_;
-      private Hashtable<Integer, Label> setupLabels_;
-      private double delayMs_;
-      private boolean usesDelay_;
-      private int focusDirection_ = 0;
-      private int numPos_ = 0;
-      private String parentHub_;
-      private String childDevices_[];
-      private boolean initialized_;
+ */
+public final class Device {
+   // This class behaves simultaneously as any device type, so it has the
+   // information for all device types. Not sure why subclasses were not
+   // used....
+   private final String adapterName_;
+   private final String library_;
+   private final ArrayList<PropertyItem> setupProperties_;
+   private final String description_;
+   private final Hashtable<Integer, Label> setupLabels_;
+   private String name_;
+   private PropertyItem[] properties_;
+   private DeviceType type_;
+   private double delayMs_;
+   private boolean usesDelay_;
+   private int focusDirection_ = 0;
+   private int numPos_ = 0;
+   private String parentHub_;
+   private String[] childDevices_;
+   private boolean initialized_;
 
    public Device(String name, String lib, String adapterName, String descr, 
-           boolean discoverable, String master,Vector<String> slaves ) {
+           boolean discoverable, String main, Vector<String> followers) {
       name_ = name;
       library_ = lib;
       adapterName_ = adapterName;
       description_ = descr;
       type_ = DeviceType.AnyType;
-      setupLabels_ = new Hashtable<Integer, Label>();
+      setupLabels_ = new Hashtable<>();
       properties_ = new PropertyItem[0];
-      setupProperties_ = new ArrayList<PropertyItem>();
+      setupProperties_ = new ArrayList<>();
       usesDelay_ = false;
       delayMs_ = 0.0;
-      parentHub_ = new String();
       childDevices_ = new String[0];
       initialized_ = false;
    }
@@ -81,12 +81,11 @@ import org.micromanager.internal.utils.ReportingUtils;
       adapterName_ = adapterName;
       description_ = descr;
       type_ = DeviceType.AnyType;
-      setupLabels_ = new Hashtable<Integer, Label>();
+      setupLabels_ = new Hashtable<>();
       properties_ = new PropertyItem[0];
-      setupProperties_ = new ArrayList<PropertyItem>();
+      setupProperties_ = new ArrayList<>();
       usesDelay_ = false;
       delayMs_ = 0.0;
-      parentHub_ = new String();
       childDevices_ = new String[0];
    }
 
@@ -103,7 +102,7 @@ import org.micromanager.internal.utils.ReportingUtils;
    }
    
    public String getTypeAsString() {
-      String devType = new String("Unknown");
+      String devType = "Unknown";
       
       if (type_ == DeviceType.CameraDevice) {
          devType = "Camera";
@@ -132,9 +131,9 @@ import org.micromanager.internal.utils.ReportingUtils;
       } else if (type_ == DeviceType.XYStageDevice) {
          devType = "XY Stage";    
       } else if (type_ == DeviceType.StateDevice) {
-          devType = "Discrete State Device";    
+         devType = "Discrete State Device";
       } else if (type_ == DeviceType.MagnifierDevice) {
-          devType = "Magnifier";    
+         devType = "Magnifier";
       } else if (type_ == DeviceType.GalvoDevice) {
          devType = "Galvo";
       } else {
@@ -146,7 +145,8 @@ import org.micromanager.internal.utils.ReportingUtils;
 
    /**
     * Obtain all properties and their current values.
-    * @param core
+    *
+    * @param core The Micro-Manager core.
     * @throws Exception
     */
    public void loadDataFromHardware(CMMCore core) throws Exception {
@@ -159,7 +159,7 @@ import org.micromanager.internal.utils.ReportingUtils;
       type_ = core.getDeviceType(name_);
       usesDelay_ = core.usesDeviceDelay(name_);
       
-      for (int j=0; j<propNames.size(); j++){
+      for (int j = 0; j < propNames.size(); j++) {
          properties_[j] = new PropertyItem();
          properties_[j].name = propNames.get(j);
          properties_[j].value = core.getProperty(name_, propNames.get(j));
@@ -167,17 +167,18 @@ import org.micromanager.internal.utils.ReportingUtils;
          properties_[j].preInit = core.isPropertyPreInit(name_, propNames.get(j));
          properties_[j].type = core.getPropertyType(name_, propNames.get(j));
          StrVector values = core.getAllowedPropertyValues(name_, propNames.get(j));
-         properties_[j].allowed = new String[(int)values.size()];
-         for (int k=0; k<values.size(); k++){
+         properties_[j].allowed = new String[(int) values.size()];
+         for (int k = 0; k < values.size(); k++) {
             properties_[j].allowed[k] = values.get(k);
          }
-        properties_[j].sort();
+         properties_[j].sort();
       }
       
       if (type_ == DeviceType.StateDevice) {
          numPos_ = core.getNumberOfStates(name_);
-      } else
+      } else {
          numPos_ = 0;
+      }
    }
    
    public static Device[] getLibraryContents(String libName, CMMCore core) throws Exception {
@@ -185,8 +186,8 @@ import org.micromanager.internal.utils.ReportingUtils;
       StrVector devDescrs = core.getAvailableDeviceDescriptions(libName);
       LongVector devTypes = core.getAvailableDeviceTypes(libName);
       
-      Device[] devList = new Device[(int)adapterNames.size()];
-      for (int i=0; i<adapterNames.size(); i++) {
+      Device[] devList = new Device[(int) adapterNames.size()];
+      for (int i = 0; i < adapterNames.size(); i++) {
 
          // not all adapters fill this yet
          devList[i] = new Device("Undefined", libName, adapterNames.get(i), devDescrs.get(i));
@@ -209,10 +210,11 @@ import org.micromanager.internal.utils.ReportingUtils;
    }
    
    public String[] getPreInitProperties() {
-      Vector<String> piProps = new Vector<String>();
+      Vector<String> piProps = new Vector<>();
       for (PropertyItem p : properties_) {
-         if (p.preInit)
+         if (p.preInit) {
             piProps.add(p.name);
+         }
       }
       return piProps.toArray(new String[piProps.size()]);
    }
@@ -234,6 +236,7 @@ import org.micromanager.internal.utils.ReportingUtils;
    public String getAdapterName() {
       return adapterName_;
    }
+
    public String getDescription() {
       return description_;
    }
@@ -243,7 +246,7 @@ import org.micromanager.internal.utils.ReportingUtils;
    }
    
    public void addSetupLabel(Label lab) {
-      setupLabels_.put(new Integer(lab.state_), lab);
+      setupLabels_.put(lab.state_, lab);
    }
 
    public void getFocusDirectionFromHardware(CMMCore core) throws Exception {
@@ -256,7 +259,7 @@ import org.micromanager.internal.utils.ReportingUtils;
       // we can only add the state labels after initialization of the device!!
       if (type_ == DeviceType.StateDevice)  {
          StrVector stateLabels = core.getStateLabels(name_);
-         numPos_ = (int)stateLabels.size();
+         numPos_ = (int) stateLabels.size();
          setupLabels_.clear();
          for (int state = 0; state < numPos_; state++) {
             setSetupLabel(state, stateLabels.get(state));
@@ -279,15 +282,17 @@ import org.micromanager.internal.utils.ReportingUtils;
    public String getPropertyValue(String propName) throws MMConfigFileException {
 
       PropertyItem p = findProperty(propName);
-      if (p == null)
+      if (p == null) {
          throw new MMConfigFileException("Property " + propName + " is not defined");
+      }
       return p.value;
    }
    
    public void setPropertyValue(String name, String value) throws MMConfigFileException {
       PropertyItem p = findProperty(name);
-      if (p == null)
+      if (p == null) {
          throw new MMConfigFileException("Property " + name + " is not defined");
+      }
       p.value = value;
    }
    
@@ -300,17 +305,18 @@ import org.micromanager.internal.utils.ReportingUtils;
    }
    
    public String getSetupPropertyValue(String propName) throws MMConfigFileException {
-
       PropertyItem p = findSetupProperty(propName);
-      if (p == null)
+      if (p == null) {
          throw new MMConfigFileException("Property " + propName + " is not defined");
+      }
       return p.value;
    }
    
    public void setSetupPropertyValue(String name, String value) throws MMConfigFileException {
       PropertyItem p = findSetupProperty(name);
-      if (p == null)
+      if (p == null) {
          throw new MMConfigFileException("Property " + name + " is not defined");
+      }
       p.value = value;
    }
 
@@ -341,42 +347,45 @@ import org.micromanager.internal.utils.ReportingUtils;
    }
    
    public Label getSetupLabelByState(int j) {
-      return setupLabels_.get(new Integer(j));
+      return setupLabels_.get(j);
    }
    
    public void setSetupLabel(int pos, String label) {
-      Label l = setupLabels_.get(new Integer(pos));
+      Label l = setupLabels_.get(pos);
       if (l == null) {
          // label does not exist so we must create one
-         setupLabels_.put(new Integer(pos), new Label(label, pos));
-      } else
+         setupLabels_.put(pos, new Label(label, pos));
+      } else {
          l.label_ = label;
+      }
    }
    
    public boolean isCore() {
       return name_.contentEquals(new StringBuffer().append(MMCoreJ.getG_Keyword_CoreDevice()));
    }
+
    public void setName(String newName) {
       name_ = newName;
    }
    
    public PropertyItem findProperty(String name) {
-      for (int i=0; i<properties_.length; i++) {
-         PropertyItem p = properties_[i];
-         if (p.name.contentEquals(new StringBuffer().append(name)))
+      for (PropertyItem p : properties_) {
+         if (p.name.contentEquals(new StringBuffer().append(name))) {
             return p;
+         }
       }
       return null;
    }
    
    public PropertyItem findSetupProperty(String name) {
-      for (int i=0; i<setupProperties_.size(); i++) {
-         PropertyItem p = setupProperties_.get(i);
-         if (p.name.contentEquals(new StringBuffer().append(name)))
+      for (PropertyItem p : setupProperties_) {
+         if (p.name.contentEquals(new StringBuffer().append(name))) {
             return p;
+         }
       }
       return null;
    }
+
    public double getDelay() {
       return delayMs_;
    }
@@ -392,11 +401,9 @@ import org.micromanager.internal.utils.ReportingUtils;
    public void setFocusDirection(int direction) {
       if (direction > 0) {
          focusDirection_ = +1;
-      }
-      else if (direction < 0) {
+      } else if (direction < 0) {
          focusDirection_ = -1;
-      }
-      else {
+      } else {
          focusDirection_ = 0;
       }
    }
@@ -427,13 +434,14 @@ import org.micromanager.internal.utils.ReportingUtils;
    
    public void updateSetupProperties() {
       setupProperties_.clear();
-      for (int i=0; i<properties_.length; i++) {
-         setupProperties_.add(new PropertyItem(properties_[i].name, properties_[i].value, properties_[i].preInit));
+      for (PropertyItem propertyItem : properties_) {
+         setupProperties_
+               .add(new PropertyItem(propertyItem.name, propertyItem.value, propertyItem.preInit));
       }
    }
       
    public String getPort() {
-      for (int i=0; i<getNumberOfProperties(); i++) {
+      for (int i = 0; i < getNumberOfProperties(); i++) {
          PropertyItem p = getProperty(i);
          if (p != null && p.name.compareTo(MMCoreJ.getG_Keyword_Port()) == 0) {
             return p.value;
@@ -443,12 +451,8 @@ import org.micromanager.internal.utils.ReportingUtils;
    }
 
    public Label[] getAllSetupLabels() {
-//      Vector<Label> labels = new Vector<Label>();
-//      for (Integer state : setupLabels_.keySet()) {
-//         labels.add(setupLabels_.get(state));
-//      }
-      Label lblArray[] = new Label[setupLabels_.size()];
+      Label[] lblArray = new Label[setupLabels_.size()];
       return setupLabels_.values().toArray(lblArray);
    }
    
- }
+}

mmstudio/src/main/java/org/micromanager/internal/hcwizard/ComPropTableModel.java
@@ -11,7 +11,7 @@ import org.micromanager.internal.utils.ReportingUtils;
  */
 class ComPropTableModel extends AbstractTableModel implements MMPropertyTableModel {
    private static final long serialVersionUID = 1L;
-   public final String[] COLUMN_NAMES = new String[] {
+   public static final String[] COLUMN_NAMES = new String[] {
          "Device",
          "Property",
          "Value"
@@ -19,7 +19,7 @@ class ComPropTableModel extends AbstractTableModel implements MMPropertyTableMod
    
    MicroscopeModel model_;
    Device dev_;
-   PropertyItem props_[];
+   PropertyItem[] props_;
    
    public ComPropTableModel(MicroscopeModel model, Device portDev) {
       updateValues(model, portDev);
@@ -34,27 +34,28 @@ class ComPropTableModel extends AbstractTableModel implements MMPropertyTableMod
          ReportingUtils.logMessage("Property Baudrate is not defined");
       }
       
-      if (!dev.isSerialPort())
+      if (!dev.isSerialPort()) {
          return;
+      }
       
       model_.dumpComPortsSetupProps(); // ?
       
-      ArrayList<PropertyItem> props = new ArrayList<PropertyItem>();
-      ArrayList<String> dn = new ArrayList<String>();
-      for (int i=0; i<dev_.getNumberOfProperties(); i++) {
+      ArrayList<PropertyItem> props = new ArrayList<>();
+      ArrayList<String> dn = new ArrayList<>();
+      for (int i = 0; i < dev_.getNumberOfProperties(); i++) {
          PropertyItem p = dev_.getProperty(i);
          if (!p.readOnly) {
             props.add(p);
             dn.add(dev_.getName());
             PropertyItem setupProp = dev_.findSetupProperty(p.name);
-            if (setupProp != null)
+            if (setupProp != null) {
                p.value = setupProp.value;
-          
+            }
          }
       }
       
       props_ = new PropertyItem[props.size()];
-      for (int i=0; i<props.size(); i++) {
+      for (int i = 0; i < props.size(); i++) {
          props_[i] = props.get(i);
       }
    }
@@ -62,27 +63,30 @@ class ComPropTableModel extends AbstractTableModel implements MMPropertyTableMod
    public int getRowCount() {
       return props_.length;
    }
+
    public int getColumnCount() {
       return COLUMN_NAMES.length;
    }
+
    @Override
    public String getColumnName(int columnIndex) {
       return COLUMN_NAMES[columnIndex];
    }
+
    public Object getValueAt(int rowIndex, int columnIndex) {
-      
-      if (columnIndex == 0)
+      if (columnIndex == 0) {
          return dev_.getName();
-      else if (columnIndex == 1)
+      } else if (columnIndex == 1) {
          return props_[rowIndex].name;
-      else
+      } else {
          return props_[rowIndex].value;
+      }
    }
    
    public void setValueAt(Object value, int row, int col) {
       if (col == 2) {
          try {
-            props_[row].value = (String)value;
+            props_[row].value = (String) value;
             dev_.setPropertyValue(props_[row].name, props_[row].value);
             fireTableCellUpdated(row, col);
             System.out.println("setVal " + dev_.getPropertyValue("BaudRate"));
@@ -93,10 +97,7 @@ class ComPropTableModel extends AbstractTableModel implements MMPropertyTableMod
    }
    
    public boolean isCellEditable(int nRow, int nCol) {
-      if(nCol == 2 && !props_[nRow].readOnly)
-         return true;
-      else
-         return false;
+      return nCol == 2 && !props_[nRow].readOnly;
    }
    
    public void refresh() {
@@ -112,8 +113,9 @@ class ComPropTableModel extends AbstractTableModel implements MMPropertyTableMod
    }
    
    public PropertyItem getProperty(Setting s) {
-      if (dev_.getName().compareTo(s.deviceName_) == 0)
+      if (dev_.getName().compareTo(s.deviceName_) == 0) {
          return dev_.findSetupProperty(s.propertyName_);
+      }
       return null;
    }
    

mmstudio/src/main/java/org/micromanager/internal/hcwizard/ConfigGroup.java
@@ -33,8 +33,8 @@ public final class ConfigGroup {
    Hashtable<String, ConfigPreset> configs_;
    
    public ConfigGroup(String name) {
-      name_ = new String(name);
-      configs_ = new Hashtable<String, ConfigPreset>();
+      name_ = name;
+      configs_ = new Hashtable<>();
    }
    
    public void addConfigPreset(ConfigPreset p) {
@@ -56,15 +56,16 @@ public final class ConfigGroup {
    }
    
    public ConfigPreset[] getConfigPresets() {
-      Object objs[] = configs_.values().toArray();
+      Object[] objs = configs_.values().toArray();
       ConfigPreset[] cps = new ConfigPreset[objs.length];
-      for (int i=0; i<objs.length; i++)
-         cps[i] = (ConfigPreset)objs[i];
+      for (int i = 0; i < objs.length; i++) {
+         cps[i] = (ConfigPreset) objs[i];
+      }
       return cps;
    }
    
    public String toString() {
-      return new String("Group: " + name_);
+      return "Group: " + name_;
    }
 
    public void removePreset(String name) {

mmstudio/src/main/java/org/micromanager/internal/hcwizard/ConfigPreset.java
@@ -41,12 +41,12 @@ public final class ConfigPreset {
    
    public ConfigPreset() {
       name_ = "Undefined";
-      settings_ = new ArrayList<Setting>();
+      settings_ = new ArrayList<>();
    }
    
    public ConfigPreset(String name) {
       name_ = name;
-      settings_ = new ArrayList<Setting>();
+      settings_ = new ArrayList<>();
    }
    
    public String getName() {
@@ -54,9 +54,10 @@ public final class ConfigPreset {
    }
    
    public boolean addSetting(Setting s) {
-      for (int i=0; i<settings_.size(); i++) {
-         if (getSetting(i).isEqualTo(s))
+      for (int i = 0; i < settings_.size(); i++) {
+         if (getSetting(i).isEqualTo(s)) {
             return false;
+         }
          if (getSetting(i).matches(s)) {
             // replace existing
             settings_.set(i, s);
@@ -69,7 +70,7 @@ public final class ConfigPreset {
    }
    
    public boolean matchSetting(Setting s) {
-      for (int i=0; i<settings_.size(); i++) {
+      for (int i = 0; i < settings_.size(); i++) {
          if (getSetting(i).matches(s)) {
             return true;
          }
@@ -78,7 +79,7 @@ public final class ConfigPreset {
    }
    
    public boolean removeSetting(Setting s) {
-      for (int i=0; i<settings_.size(); i++) {
+      for (int i = 0; i < settings_.size(); i++) {
          if (getSetting(i).isEqualTo(s)) {
             settings_.remove(i);
             return true;

mmstudio/src/main/java/org/micromanager/internal/hcwizard/ConfigWizard.java
@@ -19,6 +19,7 @@
 //            CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 //            INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
 //
+
 package org.micromanager.internal.hcwizard;
 
 import java.awt.Color;
@@ -44,8 +45,14 @@ import java.util.List;
 import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.*;
-
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 import mmcorej.CMMCore;
 import mmcorej.StrVector;
 import net.miginfocom.swing.MigLayout;
@@ -66,7 +73,7 @@ public final class ConfigWizard extends JDialog {
    private JPanel pagePanel_;
    private JButton backButton_;
    private JButton nextButton_;
-   private PagePanel pages_[];
+   private PagePanel[] pages_;
    private int curPage_ = 0;
    private MicroscopeModel microModel_;
    private final CMMCore core_;
@@ -78,7 +85,8 @@ public final class ConfigWizard extends JDialog {
 
 
    /**
-    * Create the application
+    * Create the application.
+    *
     * @param studio Current Studio instance
     * @param defFile
     */
@@ -93,7 +101,7 @@ public final class ConfigWizard extends JDialog {
    }
 
    /**
-    * Initialize the contents of the frame
+    * Initialize the contents of the frame.
     */
    private void initialize() {
       org.micromanager.internal.utils.HotKeys.active_ = false;
@@ -101,7 +109,7 @@ public final class ConfigWizard extends JDialog {
       addWindowListener(new WindowAdapter() {
          @Override
          public void windowClosing(WindowEvent arg0) {
-           onCloseWindow();
+            onCloseWindow();
          }
       });
       getContentPane().setLayout(new MigLayout());
@@ -121,9 +129,7 @@ public final class ConfigWizard extends JDialog {
       add(pagePanel_, "width 700!, height 600!, span, wrap");
 
       backButton_ = new JButton();
-      backButton_.addActionListener((ActionEvent arg0) -> {
-         setPage(curPage_ - 1);
-      });
+      backButton_.addActionListener((ActionEvent arg0) -> setPage(curPage_ - 1));
       backButton_.setText("< Back");
       add(backButton_, "span, split, alignx right");
 
@@ -142,8 +148,8 @@ public final class ConfigWizard extends JDialog {
 
       // Create microscope model used by pages.
       microModel_ = new MicroscopeModel();
-      microModel_.setSendConfiguration(studio_.profile().
-              getSettings(ConfigWizard.class).getBoolean(CFG_OKAY_TO_SEND, true));
+      microModel_.setSendConfiguration(studio_.profile()
+            .getSettings(ConfigWizard.class).getBoolean(CFG_OKAY_TO_SEND, true));
       microModel_.loadAvailableDeviceList(core_);
       microModel_.setFileName(defaultPath_);
 
@@ -161,7 +167,8 @@ public final class ConfigWizard extends JDialog {
          try {
             pages_[i].setModel(microModel_, studio_);
             pages_[i].loadSettings();
-            pages_[i].setTitle("Step " + (i + 1) + " of " + pages_.length + ": " + pages_[i].getTitle());
+            pages_[i].setTitle("Step " + (i + 1) + " of "
+                  + pages_.length + ": " + pages_[i].getTitle());
             pages_[i].setParentDialog(this);
          } catch (Exception e) {
             ReportingUtils.logError(e);
@@ -178,14 +185,12 @@ public final class ConfigWizard extends JDialog {
       // Only invoke from off the EDT, so that pages may do heavy work without
       // hanging the UI.
       if (SwingUtilities.isEventDispatchThread()) {
-         new Thread(() -> {
-            setPage(i);
-         }).start();
+         new Thread(() -> setPage(i)).start();
          return;
       }
       // try to exit the current page
       if (i > 0) {
-         if (!pages_[curPage_].exitPage(curPage_ < i ? true : false)) {
+         if (!pages_[curPage_].exitPage(curPage_ < i)) {
             return;
          }
       }
@@ -193,7 +198,7 @@ public final class ConfigWizard extends JDialog {
       int newPage = Math.max(0, Math.min(pages_.length - 1, i));
 
       // try to enter the new page
-      if (!pages_[newPage].enterPage(curPage_ > newPage ? true : false)) {
+      if (!pages_[newPage].enterPage(curPage_ > newPage)) {
          return;
       }
 
@@ -206,11 +211,7 @@ public final class ConfigWizard extends JDialog {
       getContentPane().repaint();
       pages_[curPage_].refresh();
 
-      if (curPage_ == 0) {
-         backButton_.setEnabled(false);
-      } else {
-         backButton_.setEnabled(true);
-      }
+      backButton_.setEnabled(curPage_ != 0);
 
       if (curPage_ == pages_.length - 1) {
          nextButton_.setText("Finish");
@@ -247,10 +248,10 @@ public final class ConfigWizard extends JDialog {
                   String physicalAddress = "00-00-00-00-00-00";
 
                   StrVector ss = core_.getMACAddresses();
-                  if (0 < ss.size()){
+                  if (0 < ss.size()) {
                      String pa2 = ss.get(0);
-                     if(null != pa2){
-                        if( 0 <  pa2.length()){
+                     if (null != pa2) {
+                        if (0 <  pa2.length()) {
                            physicalAddress = pa2;
                         }
                      }
@@ -259,7 +260,8 @@ public final class ConfigWizard extends JDialog {
                   prependedLine += "Host: " + InetAddress.getLocalHost().getHostName() + " ";
                } catch (UnknownHostException e) {
                }
-               prependedLine += "User: " + core_.getUserId() + " configuration file: " + conff.getName() + "\n";
+               prependedLine += "User: " + core_.getUserId() + " configuration file: "
+                     + conff.getName() + "\n";
             } catch (Throwable t) {
             }
 
@@ -277,14 +279,14 @@ public final class ConfigWizard extends JDialog {
             while (-1 != (c = reader.read())) {
                writer.write(c);
             }
-            try{
+            try {
                reader.close();
-            } catch(IOException e) {
+            } catch (IOException e) {
                ReportingUtils.logError(e);
             }
             try {
                writer.close();
-            } catch(IOException e) {
+            } catch (IOException e) {
                ReportingUtils.logError(e);
             }
             try {
@@ -294,29 +296,21 @@ public final class ConfigWizard extends JDialog {
                List<File> flist = new ArrayList<>();
                flist.add(fileToSend);
                // for each of a colleciton of files to send...
-               for (Object o0 : flist) {
-                  File f0 = (File) o0;
+               for (File o0 : flist) {
+                  File f0 = o0;
                   try {
                      httpu.upload(url, f0);
-                  } catch (java.net.UnknownHostException e) {
-                     returnValue = e.toString();
-
-                  } catch (IOException e) {
-                     returnValue = e.toString();
-                  } catch (SecurityException e) {
-                     returnValue = e.toString();
                   } catch (Exception e) {
                      returnValue = e.toString();
                   }
                }
             } catch (MalformedURLException e) {
                returnValue = e.toString();
-            }
-
-            finally {
+            } finally {
                // now delete the temporary file
-               if(!fileToSend.delete()) {
-                  ReportingUtils.logError("Couldn't delete temporary file " +qualifiedConfigFileName );
+               if (!fileToSend.delete()) {
+                  ReportingUtils.logError(
+                        "Couldn't delete temporary file " + qualifiedConfigFileName);
                }
             }
          }
@@ -346,6 +340,8 @@ public final class ConfigWizard extends JDialog {
                break;
             case 2: // Cancel
                return;
+            default:
+               return;
          }
       }
       if (microModel_.getSendConfiguration()) {
@@ -363,10 +359,11 @@ public final class ConfigWizard extends JDialog {
                statusMessage_ = UploadCurrentConfigFile();
             }
 
-            public String Status() {
+            public String status() {
                return statusMessage_;
             }
          }
+
          Uploader u = new Uploader();
          u.start();
          try {
@@ -374,14 +371,14 @@ public final class ConfigWizard extends JDialog {
          } catch (InterruptedException ex) {
             Logger.getLogger(ConfigWizard.class.getName()).log(Level.SEVERE, null, ex);
          }
-         if (0 < u.Status().length()) {
-            ReportingUtils.logError("Error uploading configuration file: " + u.Status());
+         if (0 < u.status().length()) {
+            ReportingUtils.logError("Error uploading configuration file: " + u.status());
             //ReportingUtils.showMessage("Error uploading configuration file:\n" + u.Status());
          }
       }
 
       studio_.profile().getSettings(ConfigWizard.class).putBoolean(
-           CFG_OKAY_TO_SEND, microModel_.getSendConfiguration());
+            CFG_OKAY_TO_SEND, microModel_.getSendConfiguration());
 
       org.micromanager.internal.utils.HotKeys.active_ = true;
       dispose();
@@ -407,7 +404,7 @@ public final class ConfigWizard extends JDialog {
    }
 
    /**
-    * Read string out of stream
+    * Read string out of stream.
     */
    private static String readStream(InputStream is) throws IOException {
       StringBuffer bf = new StringBuffer();

commit 259047fa6670d735ab715d6d0f70ee6e2e4c1d82

Fix NullPointerException upon adding device
Bug was introduced in 62817483cbdb4951c30b3016b6b0b301c428eed6.

mmstudio/src/main/java/org/micromanager/internal/hcwizard/Device.java
@@ -55,7 +55,7 @@ public final class Device {
    private boolean usesDelay_;
    private int focusDirection_ = 0;
    private int numPos_ = 0;
-   private String parentHub_;
+   private String parentHub_ = "";
    private String[] childDevices_;
    private boolean initialized_;
 