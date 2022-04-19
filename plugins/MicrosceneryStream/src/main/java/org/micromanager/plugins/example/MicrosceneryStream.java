b4772abc6fa336e4d0733ed3a4ecab0dfd6b11ee
Jan Tiemann <j.tiemann@hzdr.de>
2022-04-19 17:45:40 +0200

    move mm microscenery plugin POC to own plugin folder

---
+++ plugins/MicrosceneryStream/src/main/java/org/micromanager/plugins/example/MicrosceneryStream.java
@@ -0,0 +1,103 @@
+/**
+ * A very simple Micro-Manager plugin, intended to be used as an example for
+ * developers wishing to create their own, actually useful plugins. This one
+ * demonstrates performing various common tasks, but does not do anything
+ * really useful.
+ *
+ * Copy this code to a location of your choice, change the name of the project
+ * (and the classes), build the jar file and copy it to the mmplugins folder
+ * in your Micro-Manager directory.
+ *
+ * Once you have it loaded and running, you can attach the NetBean debugger
+ * and use all of NetBean's functionality to debug your code.  If you make a
+ * generally useful plugin, please do not hesitate to send a copy to
+ * info@micro-managaer.org for inclusion in the Micro-Manager source code
+ * repository.
+ *
+ * Nico Stuurman, 2012
+ * copyright University of California
+ *
+ * LICENSE:      This file is distributed under the BSD license.
+ *               License text is included with the source distribution.
+ *
+ *               This file is distributed in the hope that it will be useful,
+ *               but WITHOUT ANY WARRANTY; without even the implied warranty
+ *               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+ */
+
+
+package org.micromanager.plugins.example;
+
+import org.micromanager.MenuPlugin;
+import org.micromanager.Studio;
+
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.SciJavaPlugin;
+
+@Plugin(type = MenuPlugin.class)
+public class MicrosceneryStream implements SciJavaPlugin, MenuPlugin {
+   private Studio studio_;
+   private MicrosceneryStreamFrame frame_;
+
+   /**
+    * This method receives the Studio object, which is the gateway to the
+    * Micro-Manager API. You should retain a reference to this object for the
+    * lifetime of your plugin. This method should not do anything except for
+    * store that reference, as Micro-Manager is still busy starting up at the
+    * time that this is called.
+    */
+   @Override
+   public void setContext(Studio studio) {
+      studio_ = studio;
+   }
+
+   /**
+    * This method is called when your plugin is selected from the Plugins menu.
+    * Typically at this time you should show a GUI (graphical user interface)
+    * for your plugin.
+    */
+   @Override
+   public void onPluginSelected() {
+      if (frame_ == null) {
+         // We have never before shown our GUI, so now we need to create it.
+         frame_ = new MicrosceneryStreamFrame(studio_);
+      }
+      frame_.setVisible(true);
+   }
+
+   /**
+    * This string is the sub-menu that the plugin will be displayed in, in the
+    * Plugins menu.
+    */
+   @Override
+   public String getSubMenu() {
+      return "Developer Tools";
+   }
+
+   /**
+    * The name of the plugin in the Plugins menu.
+    */
+   @Override
+   public String getName() {
+      return "Example plugin";
+   }
+
+   @Override
+   public String getHelpText() {
+      return "Help text that is displayed in certain contexts to tell the user what the plugin does.";
+   }
+
+   @Override
+   public String getVersion() {
+      return "2.0";
+   }
+
+   @Override
+   public String getCopyright() {
+      return "University of California, 2012-2015";
+   }
+}
---
+++ plugins/MicrosceneryStream/src/main/java/org/micromanager/plugins/example/MicrosceneryStreamFrame.java
@@ -0,0 +1,167 @@
+/**
+ * ExampleFrame.java
+ *
+ * This module shows an example of creating a GUI (Graphical User Interface).
+ * There are many ways to do this in Java; this particular example uses the
+ * MigLayout layout manager, which has extensive documentation online.
+ *
+ *
+ * Nico Stuurman, copyright UCSF, 2012, 2015
+ *
+ * LICENSE: This file is distributed under the BSD license. License text is
+ * included with the source distribution.
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+ */
+package org.micromanager.plugins.example;
+
+import com.google.common.eventbus.Subscribe;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.util.List;
+
+import javax.swing.*;
+
+
+import microscenery.MMConnection;
+import microscenery.MMVolumeSender;
+import net.miginfocom.swing.MigLayout;
+
+import org.micromanager.data.Image;
+import org.micromanager.events.ExposureChangedEvent;
+import org.micromanager.Studio;
+import org.micromanager.internal.utils.WindowPositioning;
+
+// Imports for MMStudio internal packages
+// Plugins should not access internal packages, to ensure modularity and
+// maintainability. However, this plugin code is older than the current
+// MMStudio API, so it still uses internal classes and interfaces. New code
+// should not imitate this practice.
+
+
+public class MicrosceneryStreamFrame extends JFrame {
+
+   private Studio studio_;
+   private JTextField userText_;
+   private final JLabel imageInfoLabel_;
+   private final JLabel exposureTimeLabel_;
+   private MMVolumeSender sender;
+
+
+
+   public MicrosceneryStreamFrame(Studio studio) {
+      super("Example Plugin GUI");
+      studio_ = studio;
+
+      super.setLayout(new MigLayout("fill, insets 2, gap 2, flowx"));
+
+      JLabel title = new JLabel("I'm an example plugin!");
+      title.setFont(new Font("Arial", Font.BOLD, 14));
+      super.add(title, "span, alignx center, wrap");
+
+      // Create a text field for the user to customize their alerts.
+      super.add(new JLabel("Alert text: "));
+      userText_ = new JTextField(30);
+      userText_.setText("Something happened!");
+      super.add(userText_);
+
+      JButton sendButton = new JButton("Start Sending");
+      sendButton.addActionListener(e -> {
+         sender = new MMVolumeSender(studio_.core());
+         MMConnection con = sender.getMmConnection();
+         userText_.setText(con.getWidth() +"x" + con.getHeight() +"x" + con.getSlices() );
+      });
+      super.add(sendButton, "wrap");
+
+      JButton stopButton = new JButton("Stop Sending");
+      stopButton.addActionListener(e -> sender.stop());
+      super.add(stopButton, "wrap");
+
+      // Snap an image, show the image in the Snap/Live view, and show some
+      // stats on the image in our frame.
+      imageInfoLabel_ = new JLabel();
+      super.add(imageInfoLabel_, "growx, split, span");
+      JButton snapButton = new JButton("Snap Image");
+      snapButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            // Multiple images are returned only if there are multiple
+            // cameras. We only care about the first image.
+            List<Image> images = studio_.live().snap(true);
+            Image firstImage = images.get(0);
+            showImageInfo(firstImage);
+         }
+      });
+      super.add(snapButton, "wrap");
+
+      exposureTimeLabel_ = new JLabel("");
+      super.add(exposureTimeLabel_, "split, span, growx");
+
+      // Run an acquisition using the current MDA parameters.
+      JButton acquireButton = new JButton("Run Acquisition");
+      acquireButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            // All GUI event handlers are invoked on the EDT (Event Dispatch
+            // Thread). Acquisitions are not allowed to be started from the
+            // EDT. Therefore we must make a new thread to run this.
+            Thread acqThread = new Thread(new Runnable() {
+               @Override
+               public void run() {
+                  studio_.acquisitions().runAcquisition();
+               }
+            });
+            acqThread.start();
+         }
+      });
+      super.add(acquireButton, "wrap");
+
+      super.setIconImage(Toolkit.getDefaultToolkit().getImage(
+              getClass().getResource("/org/micromanager/icons/microscope.gif")));
+      super.setLocation(100, 100);
+      WindowPositioning.setUpLocationMemory(this, this.getClass(), null);
+
+      super.pack();
+
+      // Registering this class for events means that its event handlers
+      // (that is, methods with the @Subscribe annotation) will be invoked when
+      // an event occurs. You need to call the right registerForEvents() method
+      // to get events; this one is for the application-wide event bus, but
+      // there's also Datastore.registerForEvents() for events specific to one
+      // Datastore, and DisplayWindow.registerForEvents() for events specific
+      // to one image display window.
+      studio_.events().registerForEvents(this);
+   }
+
+   /**
+    * To be invoked, this method must be public and take a single parameter
+    * which is the type of the event we care about.
+    * @param event
+    */
+   @Subscribe
+   public void onExposureChanged(ExposureChangedEvent event) {
+      exposureTimeLabel_.setText(String.format("Camera %s exposure time set to %.2fms",
+               event.getCameraName(), event.getNewExposureTime()));
+   }
+
+   /**
+    * Display some information on the data in the provided image.
+    */
+   private void showImageInfo(Image image) {
+      // See DisplayManager for information on these parameters.
+      //HistogramData data = studio_.displays().calculateHistogram(
+      //   image, 0, 16, 16, 0, true);
+      imageInfoLabel_.setText(String.format(
+            "Image size: %dx%d", // min: %d, max: %d, mean: %d, std: %.2f",
+            image.getWidth(), image.getHeight() ) ); //, data.getMinVal(),
+            //data.getMaxVal(), data.getMean(), data.getStdDev()));
+   }
+}
