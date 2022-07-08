2b6edd0734d4a77305ad2947c548742fe1c3087e
Mark A. Tsuchida <matsuchida@wisc.edu>
2022-07-08 13:03:53 -0500

    Checkstyle [OneTopLevelClass]

--- mmstudio/src/main/java/org/micromanager/internal/positionlist/utils/ZGeneratorShepard.java
+++ mmstudio/src/main/java/org/micromanager/internal/positionlist/utils/ZGeneratorShepard.java
@@ -100,55 +100,55 @@ class ZGeneratorShepard implements ZGenerator {
    public String getDescription() {
       return ZGenerator.Type.SHEPINTERPOLATE.toString();
    }
-}
 
-class ShepardInterpolator {
-   private double[] x_;
-   private double[] y_;
-   private double[] z_;
-   public double exponent_;
+   private static class ShepardInterpolator {
+      private double[] x_;
+      private double[] y_;
+      private double[] z_;
+      public double exponent_;
 
-   /*
-    * @param xin x position list
-    * @param yin y position list
-    * @param zin z position list
-    * @param exp radial weighting exponent
-    */
-   public ShepardInterpolator(double[] xin, double[] yin, double[] zin, double exp) {
-      this.exponent_ = exp;
-      if (xin.length != yin.length || xin.length != zin.length) {
-         throw new IllegalArgumentException();
-      }
-      this.x_ = xin;
-      this.y_ = yin;
-      this.z_ = zin;
-   }
-
-   public double interpolate(double xi, double yi) {
-      double weight;
-      double numerator;
-      double denominator;
-      double epsilon = 0.001;
-      double d;
-      int i;
-      numerator = 0;
-      denominator = 0;
-      for (i = 0; i < x_.length; i++) {
-         //calculate weight
-         d = distance(x_[i], xi, y_[i], yi);
-         if (d < epsilon) {
-            //if we're on top of a point, return it's z coordinate,
-            //otherwise d = 0, weight = infinity, and we return a NaN
-            return z_[i];
+      /*
+       * @param xin x position list
+       * @param yin y position list
+       * @param zin z position list
+       * @param exp radial weighting exponent
+       */
+      public ShepardInterpolator(double[] xin, double[] yin, double[] zin, double exp) {
+         this.exponent_ = exp;
+         if (xin.length != yin.length || xin.length != zin.length) {
+            throw new IllegalArgumentException();
          }
-         weight = Math.pow(d, -exponent_);
-         numerator += z_[i] * weight;
-         denominator += weight;
+         this.x_ = xin;
+         this.y_ = yin;
+         this.z_ = zin;
       }
-      return numerator / denominator;
-   }
 
-   private double distance(double x1, double x2, double y1, double y2) {
-      return Math.pow((Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)), 0.5);
+      public double interpolate(double xi, double yi) {
+         double weight;
+         double numerator;
+         double denominator;
+         double epsilon = 0.001;
+         double d;
+         int i;
+         numerator = 0;
+         denominator = 0;
+         for (i = 0; i < x_.length; i++) {
+            //calculate weight
+            d = distance(x_[i], xi, y_[i], yi);
+            if (d < epsilon) {
+               //if we're on top of a point, return it's z coordinate,
+               //otherwise d = 0, weight = infinity, and we return a NaN
+               return z_[i];
+            }
+            weight = Math.pow(d, -exponent_);
+            numerator += z_[i] * weight;
+            denominator += weight;
+         }
+         return numerator / denominator;
+      }
+
+      private double distance(double x1, double x2, double y1, double y2) {
+         return Math.pow((Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)), 0.5);
+      }
    }
 }
