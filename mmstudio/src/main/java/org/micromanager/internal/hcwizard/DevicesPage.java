87d59c670ab6c3ecfc9342f802197af948fb19ec
Nico Stuurman <nico.stuurman@ucsf.edu>
2022-08-18 21:19:29 -0700

    Studio, HCW: Fixes bad web links to unavailable devices.

--- mmstudio/src/main/java/org/micromanager/internal/hcwizard/DevicesPage.java
+++ mmstudio/src/main/java/org/micromanager/internal/hcwizard/DevicesPage.java
@@ -68,7 +68,8 @@ import org.micromanager.internal.utils.ReportingUtils;
 public final class DevicesPage extends PagePanel implements
       ListSelectionListener, MouseListener, TreeSelectionListener {
    private static final long serialVersionUID = 1L;
-   public static final String WEBSITE_ROOT = "https://micro-manager.org/wiki/";
+   public static final String WEBSITE_ROOT = "https://micro-manager.org/";
+   private static final String UNAVAILABLE = " (unavailable)";
 
    private final JTable deviceTable_;
    private final JButton editButton;
@@ -813,7 +814,7 @@ public final class DevicesPage extends PagePanel implements
       }
       String[] badLibs = model.getBadLibraries();
       for (String lib : badLibs) {
-         DeviceTreeNode nd = new DeviceTreeNode(lib + " (unavailable)", true);
+         DeviceTreeNode nd = new DeviceTreeNode(lib + UNAVAILABLE, true);
          root.add(nd);
       }
 
@@ -830,8 +831,12 @@ public final class DevicesPage extends PagePanel implements
    }
 
    private void displayDocumentation() {
+      String libName = libraryDocumentationName_;
+      if (libName.endsWith(UNAVAILABLE)) {
+         libName = libName.replace(UNAVAILABLE, "");
+      }
       try {
-         ij.plugin.BrowserLauncher.openURL(WEBSITE_ROOT + libraryDocumentationName_);
+         ij.plugin.BrowserLauncher.openURL(WEBSITE_ROOT + libName);
       } catch (IOException e1) {
          ReportingUtils.showError(e1);
       }
