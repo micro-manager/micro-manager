6ebf043fd2ba2a1644c633c4fbf50bf55eedf3f9
Nico Stuurman <nstuurman@altoslabs.com>
2022-09-19 14:51:19 -0700

    ClearVolume plugin linting.

--- plugins/MMClearVolumePlugin/src/main/java/edu/ucsf/valelab/mmclearvolumeplugin/events/CanvasDrawCompleteEvent.java
+++ plugins/MMClearVolumePlugin/src/main/java/edu/ucsf/valelab/mmclearvolumeplugin/events/CanvasDrawCompleteEvent.java
@@ -3,9 +3,9 @@ package edu.ucsf.valelab.mmclearvolumeplugin.events;
 
 /**
  * Class whose sole function is to indicate that the screen update has taken place
- * 
+ *
  * @author nico
  */
 public class CanvasDrawCompleteEvent {
-    public CanvasDrawCompleteEvent() {}
+   public CanvasDrawCompleteEvent() {}
 }
