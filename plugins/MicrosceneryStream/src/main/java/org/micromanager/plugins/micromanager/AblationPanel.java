ff0417d56bba88b1db4b575dbfe918484bc92ce2
Jan T <moreApi@users.noreply.github.com>
2023-02-21 18:59:48 +0100

    Ablation: laser offset

--- plugins/MicrosceneryStream/src/main/java/org/micromanager/plugins/micromanager/AblationPanel.java
+++ plugins/MicrosceneryStream/src/main/java/org/micromanager/plugins/micromanager/AblationPanel.java
@@ -25,6 +25,7 @@ import java.util.stream.Collectors;
 public class AblationPanel extends JPanel {
     private final MMConnection mmCon;
     private final Studio studio;
+    private final Settings msSettings;
 
     private List<Vector3f> plannedCut = null;
 
@@ -36,11 +37,13 @@ public class AblationPanel extends JPanel {
         this.mmCon = mmCon;
         this.studio = studio;
 
-        Settings msSettings = Util.getMicroscenerySettings();
+        msSettings = Util.getMicroscenerySettings();
 
         Ablation.initAblationSettings();
         msSettings.set("Ablation.flipX", false);
         msSettings.set("Ablation.flipY", false);
+        msSettings.set("Ablation.laserOffsetXpx", 0);
+        msSettings.set("Ablation.laserOffsetYpx", 0);
 
         this.setLayout(new MigLayout());
 
@@ -65,81 +68,12 @@ public class AblationPanel extends JPanel {
         this.add(new JLabel("mtpp std(ms):"));
         this.add(stdTimeLabel,"wrap");
 
+        JButton calculateLaserOffsetButton = new JButton("Calculate laser offset");
+        calculateLaserOffsetButton.addActionListener(e -> calculateLaserOffset());
+        this.add(calculateLaserOffsetButton, "wrap");
+
         JButton planButton = new JButton("Plan");
-        planButton.addActionListener(e ->{
-            @SuppressWarnings("deprecation")
-            ImagePlus img = this.studio.getSnapLiveManager().getDisplay().getImagePlus();
-            //IJ.getImage()
-            // do calibration like https://imagej.nih.gov/ij/developer/source/ij/plugin/Coordinates.java.html ?
-
-            if (img == null) {
-                this.studio.alerts().postAlert("Ablation not possible", null, "Image required.");
-                return;
-            }
-
-            Roi roi = img.getRoi();
-            if (roi == null) {
-                this.studio.alerts().postAlert("Ablation not possible", null, "Selection required.");
-                return;
-            }
-
-            Polygon polygon = roi.getPolygon();
-            int[] xa = polygon.xpoints;
-            int[] ya = polygon.ypoints;
-            double pixelSize = this.studio.core().getPixelSizeUm();
-
-            Vector3f precision = Objects.requireNonNull(Util.getVector3(msSettings, "Ablation.precision")).div((float) pixelSize);
-
-            // points to sample in image space
-            List<Vector3f> samplePointsIS = new ArrayList<>();
-            Vector3f prev = null;
-            for(int i = 0; i < polygon.npoints; i++){
-                double x = xa[i];
-                double y = ya[i];
-
-                Vector3f cur = new Vector3f((float)x, (float) y,0);
-                if(prev != null){
-                    List<Vector3f> sampled = Ablation.sampleLine(prev, cur, precision);
-                    samplePointsIS.addAll(sampled);
-                }
-                samplePointsIS.add(cur);
-                prev = cur;
-            }
-            if (samplePointsIS.size() > 1){
-                samplePointsIS.addAll(Ablation.sampleLine(prev, samplePointsIS.get(0),precision));
-            }
-
-            int[] newX = samplePointsIS.stream().mapToInt(v -> (int) v.x).toArray();
-            int[] newY = samplePointsIS.stream().mapToInt(v -> (int) v.y).toArray();
-
-
-            PointRoi newPoly = new PointRoi(newX, newY, newX.length);
-            img.setOverlay(newPoly, Color.YELLOW, 3,null);
-
-            // -- transform to stage space --
-            int imgMidX = img.getWidth() / 2;
-            int imgMidY = img.getHeight() / 2;
-            Vector3f stagePos = this.mmCon.getStagePosition();
-
-            plannedCut = samplePointsIS.stream().peek(vec -> {
-                // assuming the laser points to the middle of the image and center of stage
-                // move path "center from middle of image to origin
-                vec.x -= imgMidX;
-                vec.y -= imgMidY;
-
-                if (msSettings.get("Ablation.flipX", false)){
-                    vec.x = vec.x *-1;
-                }
-                if (msSettings.get("Ablation.flipY", false)){
-                    vec.y = vec.y *-1;
-                }
-                // transform to stage space
-                // z is 0 therefore mul is ok
-                vec.mul((float) pixelSize);
-                // move origin relative to current stage position (should be the same as position of the image)
-                vec.add(stagePos);
-            }).collect(Collectors.toList());
-        });
+        planButton.addActionListener(e -> planAblation());
         this.add(planButton, "");
 
         JButton executeBut = new JButton("execute");
@@ -154,6 +88,126 @@ public class AblationPanel extends JPanel {
             );
         });
         this.add(executeBut, "wrap");
+    }
+
+    @SuppressWarnings("deprecation")
+    private void calculateLaserOffset(){
+        ImagePlus img;
+        try {
+            img = this.studio.getSnapLiveManager().getDisplay().getImagePlus();
+        } catch (NullPointerException e){
+            this.studio.alerts().postAlert("Could not grab image", null, "Image required for operation.");
+            return;
+        }
+        //IJ.getImage()
+        // do calibration like https://imagej.nih.gov/ij/developer/source/ij/plugin/Coordinates.java.html ?
+
+        if (img == null) {
+            this.studio.alerts().postAlert("Calculating laser offset not possible", null, "Image required.");
+            return;
+        }
+
+        Roi roi = img.getRoi();
+        if (!(roi instanceof PointRoi)) {
+            this.studio.alerts().postAlert("Calculating laser offset  not possible", null, "Point selection required.");
+            return;
+        }
+        PointRoi pRoi = (PointRoi) roi;
+        if (pRoi.getNCoordinates() != 1){
+            this.studio.alerts().postAlert("Calculating laser offset  not possible", null, "Single point selection required.");
+            return;
+        }
+        int imgMidX = img.getWidth() / 2;
+        int imgMidY = img.getHeight() / 2;
+
+        Point point = pRoi.getContainedPoints()[0];
+
+        msSettings.set("Ablation.laserOffsetXpx", point.x - imgMidX);
+        msSettings.set("Ablation.laserOffsetYpx", point.y - imgMidY);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void planAblation() {
+        ImagePlus img;
+        try {
+            img = this.studio.getSnapLiveManager().getDisplay().getImagePlus();
+        } catch (NullPointerException e){
+            this.studio.alerts().postAlert("Could not grab image", null, "Image required for operation.");
+            return;
+        }
+        //IJ.getImage()
+        // do calibration like https://imagej.nih.gov/ij/developer/source/ij/plugin/Coordinates.java.html ?
+
+        if (img == null) {
+            this.studio.alerts().postAlert("Ablation not possible", null, "Image required.");
+            return;
+        }
+
+        Roi roi = img.getRoi();
+        if (roi == null) {
+            this.studio.alerts().postAlert("Ablation not possible", null, "Selection required.");
+            return;
+        }
+
+        Polygon polygon = roi.getPolygon();
+        int[] xa = polygon.xpoints;
+        int[] ya = polygon.ypoints;
+        double pixelSize = this.studio.core().getPixelSizeUm();
+
+        Vector3f precision = Objects.requireNonNull(Util.getVector3(msSettings, "Ablation.precision")).div((float) pixelSize);
+
+        // points to sample in image space
+        List<Vector3f> samplePointsIS = new ArrayList<>();
+        Vector3f prev = null;
+        for(int i = 0; i < polygon.npoints; i++){
+            double x = xa[i];
+            double y = ya[i];
+
+            Vector3f cur = new Vector3f((float)x, (float) y,0);
+            if(prev != null){
+                List<Vector3f> sampled = Ablation.sampleLine(prev, cur, precision);
+                samplePointsIS.addAll(sampled);
+            }
+            samplePointsIS.add(cur);
+            prev = cur;
+        }
+        if (samplePointsIS.size() > 1){
+            samplePointsIS.addAll(Ablation.sampleLine(prev, samplePointsIS.get(0),precision));
+        }
+
+        int[] newX = samplePointsIS.stream().mapToInt(v -> (int) v.x).toArray();
+        int[] newY = samplePointsIS.stream().mapToInt(v -> (int) v.y).toArray();
+
+
+        PointRoi newPoly = new PointRoi(newX, newY, newX.length);
+        img.setOverlay(newPoly, Color.YELLOW, 3,null);
+
+        // -- transform to stage space --
+        int imgMidX = img.getWidth() / 2;
+        int imgMidY = img.getHeight() / 2;
+        Vector3f stagePos = this.mmCon.getStagePosition();
+
+        plannedCut = samplePointsIS.stream().peek(vec -> {
+            // assuming the laser points to the middle of the image and center of stage
+            // move path "center from middle of image to origin
+            vec.x -= imgMidX;
+            vec.y -= imgMidY;
+
+            vec.x += msSettings.get("Ablation.laserOffsetXpx", 0);
+            vec.y += msSettings.get("Ablation.laserOffsetYpx", 0);
+
+            if (msSettings.get("Ablation.flipX", false)){
+                vec.x = vec.x *-1;
+            }
+            if (msSettings.get("Ablation.flipY", false)){
+                vec.y = vec.y *-1;
+            }
+            // transform to stage space
+            // z is 0 therefore mul is ok
+            vec.mul((float) pixelSize);
+            // move origin relative to current stage position (should be the same as position of the image)
+            vec.add(stagePos);
+        }).collect(Collectors.toList());
     }
 
     public void updateTimings(AblationResults results){
