57697688a0a785971383b6ac96f81d131f49c404
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2023-04-12 15:24:32 -0700

    read non-standard axis names created with AcqEngJ

--- mmstudio/src/main/java/org/micromanager/acquisition/internal/acqengjcompat/AcqEngJMDADataSink.java
+++ mmstudio/src/main/java/org/micromanager/acquisition/internal/acqengjcompat/AcqEngJMDADataSink.java
@@ -1,18 +1,24 @@
 package org.micromanager.acquisition.internal.acqengjcompat;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import mmcorej.TaggedImage;
 import mmcorej.org.json.JSONObject;
 import org.micromanager.acqj.api.AcqEngJDataSink;
 import org.micromanager.acqj.internal.Engine;
+import org.micromanager.acqj.main.AcqEngMetadata;
 import org.micromanager.acqj.main.Acquisition;
 import org.micromanager.acquisition.internal.AcquisitionEngine;
 import org.micromanager.acquisition.internal.DefaultAcquisitionEndedEvent;
 import org.micromanager.acquisition.internal.TaggedImageQueue;
+import org.micromanager.data.Coords;
 import org.micromanager.data.Datastore;
 import org.micromanager.data.Pipeline;
 import org.micromanager.data.PipelineErrorException;
@@ -88,6 +94,24 @@ public final class AcqEngJMDADataSink implements AcqEngJDataSink {
       try {
          AcqEngJAdapter.addMMImageMetadata(tagged.tags);
          DefaultImage image = new DefaultImage(tagged);
+
+         // Add any non-standard (ptzc) coords
+         List<String> nonStandardAxisNames = AcqEngMetadata.getAxes(tagged.tags).keySet()
+               .stream().filter(new Predicate<String>() {
+                  @Override
+                  public boolean test(String s) {
+                     String[] standardAxes = new String[] {AcqEngMetadata.TIME_AXIS,
+                           AcqEngMetadata.Z_AXIS, AcqEngMetadata.CHANNEL_AXIS, "position"};
+                     return !Arrays.asList(standardAxes).contains(s);
+                  }
+               }).collect(Collectors.toList());
+
+         Coords.CoordsBuilder cb = image.getCoords().copyBuilder();
+         for (String axisName : nonStandardAxisNames) {
+            cb.index(axisName, (Integer) AcqEngMetadata.getAxes(tagged.tags).get(axisName));
+         }
+         image = (DefaultImage) image.copyAtCoords(cb.build());
+
          try {
             pipeline_.insertImage(image);
          } catch (Exception e) {
