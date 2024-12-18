de5e536a0328f502943f1686990192e400952778
Brandon <brandon@asiimaging.com>
2024-12-18 13:04:39 -0800

    ASIPLogic: add file

---
+++ plugins/ASIPLogic/src/main/java/com/asiimaging/plogic/ui/asigui/ProgressBar.java
@@ -0,0 +1,25 @@
+package com.asiimaging.plogic.ui.asigui;
+
+import java.awt.Dimension;
+
+import javax.swing.JProgressBar;
+
+public class ProgressBar extends JProgressBar {
+
+   public ProgressBar() {
+      super();
+   }
+
+   public void setRange(final int min, final int max) {
+      setMinimum(min);
+      setMaximum(max);
+   }
+
+   public void setAbsoluteSize(final int width, final int height) {
+      final Dimension size = new Dimension(width, height);
+      setPreferredSize(size);
+      setMinimumSize(size);
+      setMaximumSize(size);
+   }
+
+}
