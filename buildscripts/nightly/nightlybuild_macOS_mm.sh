bafe0706101b67baa014779feba6b3153aa3dba5
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-06-18 13:01:09 -0500

    macOS: Bundle the JRE

--- buildscripts/nightly/nightlybuild_macOS_mm.sh
+++ buildscripts/nightly/nightlybuild_macOS_mm.sh
@@ -209,6 +209,15 @@ find $MM_STAGEDIR -name .svn -prune -exec rm -rf {} +
 
 
 ##
+## Bundle JRE
+##
+
+# The 'jre' directory is preferred over system-installed JREs by the launcher.
+rm -rf $MM_STAGEDIR/jre
+cp -R $(cjdk --arch $MM_ARCH -j temurin-jre:11 java-home) $MM_STAGEDIR/jre
+
+
+##
 ## Temporarily unpack JARs for processing; remove other archs
 ##
 
