200b2ffa4a4f6e1bbf433977605c4d908945c793
Nico Stuurman <nico.stuurman@ucsf.edu>
2025-07-23 17:25:46 -0700

    Studio-Checkstyling.

--- mmstudio/src/main/java/org/micromanager/internal/hcwizard/PropertyTableModel.java
+++ mmstudio/src/main/java/org/micromanager/internal/hcwizard/PropertyTableModel.java
@@ -75,7 +75,8 @@ class PropertyTableModel extends AbstractTableModel implements MMPropertyTableMo
       for (Device device : devices_) {
          for (int j = 0; j < device.getNumberOfProperties(); j++) {
             PropertyItem p = device.getProperty(j);
-            // null check can likely be removed as I believe I found the source, but hey, it should not hurt
+            // null check can likely be removed as I believe I found the source,
+            // but hey, it should not hurt
             if (p != null && !p.readOnly && p.preInit && !device.isSerialPort()) {
                props.add(p);
                dn.add(device.getName());
