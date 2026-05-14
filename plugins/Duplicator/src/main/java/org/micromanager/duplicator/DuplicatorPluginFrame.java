560c3883e20dcd853d52d9874dd6ce6bc02a36ba
Nico Stuurman <nico.stuurman@ucsf.edu>
2026-05-14 16:18:56 -0700

    Duplicator: Fix bug not allowing creations of duplicates with one positions in certain axes.

--- plugins/Duplicator/src/main/java/org/micromanager/duplicator/DuplicatorPluginFrame.java
+++ plugins/Duplicator/src/main/java/org/micromanager/duplicator/DuplicatorPluginFrame.java
@@ -292,7 +292,7 @@ public class DuplicatorPluginFrame extends JDialog {
             }
             for (String axis : minSpinners.keySet()) {
                if ((Integer) minSpinners.get(axis).getValue()
-                        >= (Integer) maxSpinners.get(axis).getValue()) {
+                        > (Integer) maxSpinners.get(axis).getValue()) {
                   studio_.logs().showError(
                         "For axis \"" + axis + "\", max must be more than min.", ourFrame);
                   return;
