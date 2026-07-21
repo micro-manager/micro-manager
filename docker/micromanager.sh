d551ef029abe326c6750eb8a0ffa5b6d2e6bbcb7
Nico Stuurman <nico.stuurman@ucsf.edu>
2026-07-21 16:43:36 -0700

    Docker: re-apply inadvertently remove add-opens flags

--- docker/micromanager.sh
+++ docker/micromanager.sh
@@ -27,5 +27,8 @@ umask 0002 && java -Xmx1024M \
    -Dorg.micromanager.corelog.dir=/tmp \
    -Dplugins.dir="$IMAGEJ_DIR" \
    $MM_UI_SCALE_OPT \
+   --add-opens=java.desktop/java.awt=ALL-UNNAMED \
+   --add-opens=java.desktop/java.awt.color=ALL-UNNAMED \
+   --add-opens=java.desktop/sun.awt=ALL-UNNAMED \
    -jar "$IMAGEJ_DIR/ij.jar" \
    -eval 'run("Micro-Manager Studio");' \
